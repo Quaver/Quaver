213c8a1626d49e8244f0ff7a32b30433d4dbf180
Swan <contact@swan.moe>
2017-08-31 13:01:29 -0400

    Add: Test Scripts Directory
    Former-commit-id: 19c11e23d2a6de721ca451ed8840f2b258a43758

---
+++ Assets/Scripts/Tests/BeatmapParsingTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BeatmapParsingTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Beatmap beatmap = BeatmapParser.Parse(@"C:\Users\swan\Desktop\Stuff\Git\Quaver\Assets\Songs\w1sh\w1sh.sm");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
