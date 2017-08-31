a2a8e4e9b5bbfc0ea5bcdef84b1789a62bacafd6
Swan <contact@swan.moe>
2017-08-31 11:53:34 -0400

    Add: Beatmap parsing structures

---
+++ Assets/Scripts/Beatmaps/NoteData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This structure contains all the bars for a song at a single difficulty. 
+public struct NoteData
+{
+    public List<List<NoteData>> bars;
+}
+
