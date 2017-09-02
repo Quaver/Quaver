d5d1eb4dff53fa3cfe0d8782deb49852513cc134
staravia <denysenesouma@gmail.com>
2017-09-01 21:48:21 -0700

    Added: InstantiateNoteTest and Parse Test
    Former-commit-id: 827e2be436260bcbd0ac09cad47924e1473063f4

---
+++ Assets/Scripts/Qua.Scripts/Gameplay/InstantiateNoteTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateNoteTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ParseQua parsedNotes = new ParseQua();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
