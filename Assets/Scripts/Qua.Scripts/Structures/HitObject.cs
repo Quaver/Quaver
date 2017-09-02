a9c96a40b3d06e231b9a936bbb5e380078a09b80
Swan <contact@swan.moe>
2017-09-02 11:22:46 -0400

    Add: Qua Beatmap Structures
    Former-commit-id: de2443e6e51b90b3f4805d876d2091547541819e

---
+++ Assets/Scripts/Qua.Scripts/Structures/HitObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Qua.Scripts {
+
+	public struct HitObject
+	{
+		public int StartTime;
+		public int KeyLane;
+		public int EndTime;
+	}
+
+}
