de2443e6e51b90b3f4805d876d2091547541819e
Swan <contact@swan.moe>
2017-09-02 11:22:46 -0400

    Add: Qua Beatmap Structures

---
+++ Assets/Scripts/Qua.Scripts/Structures/TimingPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Qua.Scripts {
+
+	public struct TimingPoint 
+	{
+		public int StartTime;
+		public float BPM;
+	}
+
+}
