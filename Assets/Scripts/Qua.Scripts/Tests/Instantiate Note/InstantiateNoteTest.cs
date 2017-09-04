e110226622962daa9af2e08b8f9405a9eed63940
staravia <denysenesouma@gmail.com>
2017-09-03 20:52:57 -0700

    Change: Instantiate Note Test
    Former-commit-id: 996b1e805ddd5a94b3628b5de6d4dba9accc1dca

--- Assets/Scripts/Qua.Scripts/Tests/Instantiate Note/InstantiateNoteTest.cs
+++ Assets/Scripts/Qua.Scripts/Tests/Instantiate Note/InstantiateNoteTest.cs
@@ -5,32 +5,42 @@ using Qua.Scripts;
 
 public class InstantiateNoteTest : MonoBehaviour {
     private QuaFile qFile;
+    public Sprite[] Arrows;
+    public Sprite sliderMiddle;
+    public Sprite sliderEnd;
+    public GameObject hitObjectTest;
+    private int noteSize = 128;
+    private int columnSize = 140;
+
+
     void Start () {
-        qFile = QuaParser.Parse("E:\\GitHub\\Quaver\\TestFiles\\Qua\\format_example.qua");
+        qFile = QuaParser.Parse("E:\\GitHub\\Quaver\\TestFiles\\Qua\\planet_shaper.qua");
         if (qFile.Artist != null)
         {
             int i = 0;
             foreach (HitObject ho in qFile.HitObjects)
             {
-                print(ho.KeyLane + " | " + ho.StartTime);
-                i++;
+                if (i < 200)
+                {
+                    //print(ho.KeyLane + " | " + ho.StartTime);
+                    GameObject hoo = Instantiate(hitObjectTest, transform.Find("HitContainer"));
+                    if (ho.KeyLane == 1 || ho.KeyLane == 2 || ho.KeyLane == 3 || ho.KeyLane == 4)
+                    {
+                        hoo.transform.Find("HitImage").GetComponent<SpriteRenderer>().sprite = Arrows[ho.KeyLane-1];
+                    }
+                    hoo.transform.localScale = Vector3.one * (100f / (float)noteSize) * (columnSize/100f);
+                    hoo.transform.position = new Vector3(ho.KeyLane*((float)columnSize/100f)-((float)columnSize/100f *2), ho.StartTime/1000f*8f, 0);
+                    //Instantiate(hito)
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
             }
-            print(i+" | count: "+qFile.HitObjects.Count);
+            //print(i+" | count: "+qFile.HitObjects.Count);
         }
     }
 	
-	void Update()
-    {
-        /*if (qFile.Artist != null)
-        {
-            int i = 0;
-            foreach (HitObject ho in qFile.HitObjects)
-            {
-                print(ho.KeyLane + " | " + ho.StartTime);
-                i++;
-            }
-            print(i);
-        }*/ 
-    }
 
 }
