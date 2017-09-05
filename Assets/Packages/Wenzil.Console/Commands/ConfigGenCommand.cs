73918ad7527d76f3961b6821115e149aa3f3d846
Swan <contact@swan.moe>
2017-09-05 17:48:04 -0400

    Add: Command to generate a default config file (genconfig)

---
+++ Assets/Packages/Wenzil.Console/Commands/ConfigGenCommand.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using Config.Scripts;
+
+namespace Wenzil.Console.Commands
+{
+    /// <summary>
+    /// ParseBeatmap Command. Takes in a file path and parses a beatmap.
+    /// </summary>
+    public static class ConfigGenCommand
+    {
+        public static readonly string name = "GENCONFIG";
+        public static readonly string description = "Creates a default Quaver configuration file on your machine.";
+        public static readonly string usage = "GENCONFIG";
+
+        public static string Execute(params string[] args)
+        {
+			bool hasGenerated = ConfigGenerator.Generate();
+
+			if (hasGenerated)
+			{
+				return "Config file was successfully generated at: " + ConfigDefault.GameDirectory + "/quaver.cfg";
+			}
+
+			return "File could not be generated.";
+            
+        }
+    }
+}
