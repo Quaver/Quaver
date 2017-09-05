05e9971ea933fad8f392c5ee6138ef055de99fc3
Swan <contact@swan.moe>
2017-09-05 17:16:22 -0400

    Add: Default Configuration Values
    Former-commit-id: d42c20281219fd3938a3dbbb73792e32c376fde2 [formerly 5d59b7b6f28b4c95a3541f437bcd82d61548ff93]
    Former-commit-id: e4963708d5877540734c8d64bf06f8294296347f

---
+++ Assets/Packages/Quaver.Config.Scripts/Classes/ConfigDefault.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Config.Scripts {
+
+	public static class ConfigDefault
+	{
+		public static bool IsValid = false;
+		public static string GameDirectory = Application.dataPath;
+		public static string SongDirectory = GameDirectory + "/songs/";
+		public static string SkinsDirectory = GameDirectory + "/skins/";
+
+		public static byte VolumeGlobal = 100;
+		public static byte VolumeEffect = 50;
+		public static byte VolumeMusic = 75;
+
+		public static byte BackgroundDim = 0;
+
+		public static int WindowHeight = Screen.height;
+		public static int WindowWidth = Screen.width;
+		public static bool WindowFullScreen = false;
+		public static bool WindowLetterboxed = false;
+		
+		public static short CustomFrameLimit = 240;
+		public static bool FPSCounter = true;
+		public static bool FrameTimeDisplay = false;
+
+		public static string Language = "en";
+		public static string QuaverVersion = "db0.0.1";
+		public static string QuaverBuildHash = "Not Implemented";
+
+		public static byte ScrollSpeed = 24;
+		public static bool ScrollSpeedBPMScale = false;
+		public static bool DownScroll = true;
+
+		public static byte GlobalOffset = 0;
+		public static bool LeaderboardVisible = false;
+		public static string Skin = "";
+
+		public static string KeyLaneMania1 = "d";
+		public static string KeyLaneMania2 = "f";
+		public static string KeyLaneMania3 = "j";
+		public static string KeyLaneMania4 = "k";
+
+		public static string KeyScreenshot = "f12";
+		public static string KeyQuickRetry = "`";
+		public static string KeyIncreaseScrollSpeed = "f4";
+		public static string KeyDecreaseScrollSpeed = "f5";
+		public static string KeyPause = "escape";
+		public static string KeyVolumeUp = "up";
+		public static string KeyVolumeDown = "down";
+
+	}
+}
--- Assets/Packages/Quaver.Config.Scripts/Classes/ConfigParser.cs
+++ Assets/Packages/Quaver.Config.Scripts/Classes/ConfigParser.cs
@@ -23,6 +23,7 @@ namespace Config.Scripts {
 			Cfg cfg = new Cfg();
 			cfg.IsValid = true;
 
+
 			foreach(string line in File.ReadAllLines(filePath))
 			{
 				// Parse the config values, also we need to have defaults just in case some dont exist. and update the file.
@@ -33,262 +34,275 @@ namespace Config.Scripts {
 
 					switch(key)
 					{
+						case "GameDirectory":
+							if (value == "")
+							{
+								cfg.GameDirectory = ConfigDefault.GameDirectory;
+								continue;
+							}
+							cfg.GameDirectory = value;
+							break;						
 						case "SongDirectory":
 							if (value == "")
 							{
-								cfg.SongDirectory = "Non Existent";
+								cfg.SongDirectory = ConfigDefault.SongDirectory;
+								continue;
 							}
 							cfg.SongDirectory = value;
 							break;
 						case "SkinsDirectory":
 							if (value == "")
 							{
-								cfg.SkinsDirectory = "Non Existent";
+								cfg.SkinsDirectory = ConfigDefault.SkinsDirectory;
+								continue;
 							}
 							cfg.SkinsDirectory = value;
 							break;
 						case "VolumeGlobal":
 							if (value == "")
 							{
-								cfg.VolumeGlobal = 100;
+								cfg.VolumeGlobal = ConfigDefault.VolumeGlobal;
+								continue;
 							}
 							cfg.VolumeGlobal = byte.Parse(value);
 							break;	
 						case "VolumeEffect":
 							if (value == "")
 							{
-								cfg.VolumeEffect = 100;
+								cfg.VolumeEffect = ConfigDefault.VolumeEffect;
+								continue;
 							}
 							cfg.VolumeEffect = byte.Parse(value);
 							break;
 						case "VolumeMusic":
 							if (value == "")
 							{
-								cfg.VolumeMusic = 100;
+								cfg.VolumeMusic = ConfigDefault.VolumeMusic;
+								continue;
 							}
 							cfg.VolumeMusic = byte.Parse(value);
 							break;							
 						case "BackgroundDim":
 							if (value == "")
 							{
-								cfg.BackgroundDim = 0;
+								cfg.BackgroundDim = ConfigDefault.BackgroundDim;
+								continue;
 							}
 							cfg.BackgroundDim = byte.Parse(value);
 							break;	
-						case "MonitorDisplay":
-							if (value == "")
-							{
-								cfg.MonitorDisplay = 1;
-							}
-							cfg.MonitorDisplay = byte.Parse(value);
-							break;
-						case "MonitorRefreshRate":
-							if (value == "")
-							{
-								cfg.MonitorRefreshRate = 60;
-							}
-							cfg.MonitorRefreshRate = byte.Parse(value);
-							break;
 						case "WindowHeight":
 							if (value == "")
 							{
-								cfg.WindowHeight = 900;
+								cfg.WindowHeight = ConfigDefault.WindowHeight;
+								continue;
 							}
 							cfg.WindowHeight = short.Parse(value);
 							break;
 						case "WindowWidth":
 							if (value == "")
 							{
-								cfg.WindowWidth = 1600;
+								cfg.WindowWidth = ConfigDefault.WindowWidth;
+								continue;
 							}
 							cfg.WindowWidth = short.Parse(value);
 							break;
 						case "WindowFullScreen":
 							if (value == "")
 							{
-								cfg.WindowFullScreen = true;
+								cfg.WindowFullScreen = ConfigDefault.WindowFullScreen;
+								continue;
 							}
 							cfg.WindowFullScreen = bool.Parse(value);
 							break;
 						case "WindowLetterboxed":
 							if (value == "")
 							{
-								cfg.WindowLetterboxed = false;
+								cfg.WindowLetterboxed = ConfigDefault.WindowLetterboxed;
+								continue;
 							}
 							cfg.WindowLetterboxed = bool.Parse(value);
-							break;
-						case "FullScreenHeight":
-							if (value == "")
-							{
-								cfg.FullScreenHeight = 1080;
-							}
-							cfg.FullScreenHeight = short.Parse(value);
-							break;
-						case "FullScreenWidth":
-							if (value == "")
-							{
-								cfg.FullScreenWidth = 1920;
-							}
-							cfg.FullScreenWidth = short.Parse(value);
 							break;
 						case "CustomFrameLimit":
 							if (value == "")
 							{
-								cfg.CustomFrameLimit = 240;
+								cfg.CustomFrameLimit = ConfigDefault.CustomFrameLimit;
+								continue;
 							}
 							cfg.CustomFrameLimit = short.Parse(value);
 							break;
 						case "FPSCounter":
 							if (value == "")
 							{
-								cfg.FPSCounter = false;
+								cfg.FPSCounter = ConfigDefault.FPSCounter;
+								continue;
 							}
 							cfg.FPSCounter = bool.Parse(value);
 							break;
 						case "FrameTimeDisplay":
 							if (value == "")
 							{
-								cfg.FrameTimeDisplay = false;
+								cfg.FrameTimeDisplay = ConfigDefault.FrameTimeDisplay;
+								continue;
 							}
 							cfg.FrameTimeDisplay = bool.Parse(value);
 							break;
 						case "Language":
 							if (value == "")
 							{
-								cfg.Language = "en";
+								cfg.Language = ConfigDefault.Language;
+								continue;
 							}
 							cfg.Language = value;
 							break;
 						case "QuaverVersion":
 							if (value == "")
 							{
-								cfg.QuaverVersion = "0.1";
+								cfg.QuaverVersion = ConfigDefault.QuaverVersion;
+								continue;
 							}
 							cfg.QuaverVersion = value;
 							break;
 						case "QuaverBuildHash":
 							if (value == "")
 							{
-								cfg.QuaverBuildHash = "d92d0u8f32dh7c98c9d382h89";
+								cfg.QuaverBuildHash = ConfigDefault.QuaverBuildHash;
+								continue;
 							}
 							cfg.QuaverBuildHash = value;
 							break;
 						case "ScrollSpeed":
 							if (value == "")
 							{
-								cfg.ScrollSpeed = 34;
+								cfg.ScrollSpeed = ConfigDefault.ScrollSpeed;
+								continue;
 							}
 							cfg.ScrollSpeed = byte.Parse(value);
 							break;		
 						case "ScrollSpeedBPMScale":
 							if (value == "")
 							{
-								cfg.ScrollSpeedBPMScale = false;
+								cfg.ScrollSpeedBPMScale = ConfigDefault.ScrollSpeedBPMScale;
+								continue;
 							}
 							cfg.ScrollSpeedBPMScale = bool.Parse(value);
 							break;
 						case "DownScroll":
 							if (value == "")
 							{
-								cfg.DownScroll = false;
+								cfg.DownScroll = ConfigDefault.DownScroll;
+								continue;
 							}
 							cfg.DownScroll = bool.Parse(value);
 							break;
 						case "GlobalOffset":
 							if (value == "")
 							{
-								cfg.GlobalOffset = 0;
+								cfg.GlobalOffset = ConfigDefault.GlobalOffset;
+								continue;
 							}
 							cfg.GlobalOffset = byte.Parse(value);
 							break;
 						case "LeaderboardVisible":
 							if (value == "")
 							{
-								cfg.LeaderboardVisible = false;
+								cfg.LeaderboardVisible = ConfigDefault.LeaderboardVisible;
+								continue;
 							}
 							cfg.LeaderboardVisible = bool.Parse(value);
 							break;	
 						case "Skin":
 							if (value == "")
 							{
-								cfg.Skin = "";
+								cfg.Skin = ConfigDefault.Skin;
+								continue;
 							}
 							cfg.Skin = value;
 							break;						
 						case "KeyManiaLane1":
 							if (value == "")
 							{
-								cfg.KeyLaneMania1 = "d";
+								cfg.KeyLaneMania1 = ConfigDefault.KeyLaneMania1;
+								continue;
 							}
 							cfg.KeyLaneMania1 = value;
 							break;
 						case "KeyManiaLane2":
 							if (value == "")
 							{
-								cfg.KeyLaneMania2 = "f";
+								cfg.KeyLaneMania2 = ConfigDefault.KeyLaneMania2;
+								continue;
 							}
 							cfg.KeyLaneMania2 = value;
 							break;
 						case "KeyManiaLane3":
 							if (value == "")
 							{
-								cfg.KeyLaneMania3 = "k";
+								cfg.KeyLaneMania3 = ConfigDefault.KeyLaneMania3;
+								continue;
 							}
 							cfg.KeyLaneMania3 = value;
 							break;
 						case "KeyManiaLane4":
 							if (value == "")
 							{
-								cfg.KeyLaneMania4 = "k";
+								cfg.KeyLaneMania4 = ConfigDefault.KeyLaneMania4;
+								continue;
 							}
 							cfg.KeyLaneMania4 = value;
 							break;	
 						case "KeyScreenshot":
 							if (value == "")
 							{
-								cfg.KeyScreenshot = "f12";
+								cfg.KeyScreenshot = ConfigDefault.KeyScreenshot;
+								continue;
 							}
 							cfg.KeyScreenshot = value;
 							break;	
 						case "KeyQuickRetry":
 							if (value == "")
 							{
-								cfg.KeyQuickRetry = "`";
+								cfg.KeyQuickRetry = ConfigDefault.KeyQuickRetry;
+								continue;
 							}
 							cfg.KeyQuickRetry = value;
 							break;
 						case "KeyIncreaseScrollSpeed":
 							if (value == "")
 							{
-								cfg.KeyIncreaseScrollSpeed = "f4";
+								cfg.KeyIncreaseScrollSpeed = ConfigDefault.KeyIncreaseScrollSpeed;
+								continue;
 							}
 							cfg.KeyIncreaseScrollSpeed = value;
 							break;	
 						case "KeyDecreaseScrollSpeed":
 							if (value == "")
 							{
-								cfg.KeyDecreaseScrollSpeed = "f3";
+								cfg.KeyDecreaseScrollSpeed = ConfigDefault.KeyDecreaseScrollSpeed;
+								continue;
 							}
 							cfg.KeyDecreaseScrollSpeed = value;
 							break;
 						case "KeyPause":
 							if (value == "")
 							{
-								cfg.KeyPause = "escape";
+								cfg.KeyPause = ConfigDefault.KeyPause;
+								continue;
 							}
 							cfg.KeyPause = value;
 							break;		
 						case "KeyVolumeUp":
 							if (value == "")
 							{
-								cfg.KeyVolumeUp = "up";
+								cfg.KeyVolumeUp = ConfigDefault.KeyVolumeUp;
+								continue;
 							}
 							cfg.KeyVolumeUp = value;
 							break;
 						case "KeyVolumeDown":
 							if (value == "")
 							{
-								cfg.KeyVolumeDown = "down";
+								cfg.KeyVolumeDown = ConfigDefault.KeyVolumeDown;
+								continue;
 							}
 							cfg.KeyVolumeDown = value;
 							break;																																																							
