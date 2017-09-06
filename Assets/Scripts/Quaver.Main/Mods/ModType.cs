8f7224e45e0ee712bd852728b7047ec206537762
Swan <contact@swan.moe>
2017-09-06 17:50:30 -0400

    Add: Game Modifier Setup

---
+++ Assets/Scripts/Quaver.Main/Mods/ModType.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Main.Mods
+{
+	public enum ModType
+	{
+		DifficultyDecrease,
+		DifficultyIncrease,
+		Special
+	}
+}
