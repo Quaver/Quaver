a0fb550c8f09cc19a5dd9e6d0006031d07bb5e6f
Swan <contact@swan.moe>
2017-09-06 11:13:46 -0400

    Change: Directory & Namespace Organization

---
+++ Assets/Scripts/Quaver.Graphics/NoteBurst.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Graphics 
+{
+    public class NoteBurst : MonoBehaviour
+    {
+        public float burstLength;
+        public float burstSize;
+        public float startSize;
+        private float timeElapsed = 0;
+        // Update is called once per frame
+        void Update()
+        {
+            timeElapsed += Time.deltaTime;
+            transform.localScale = (Vector3.one  + Vector3.one * startSize * Mathf.Sqrt(timeElapsed / burstLength)) * burstSize;
+            transform.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f- Mathf.Pow(timeElapsed / burstLength,0.2f));
+            if (timeElapsed >= burstLength)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
