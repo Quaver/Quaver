0813dc441d2c86acafdcd2c93926398f96b3c7cf
Swan <contact@swan.moe>
2017-09-06 17:50:30 -0400

    Add: Game Modifier Setup
    Former-commit-id: 4542c468b956ae4530b48a64954ae00094d7d8ce [formerly 8f7224e45e0ee712bd852728b7047ec206537762]
    Former-commit-id: 35241706aa820230b6e58394858b3e57dbe5997a

---
+++ Assets/Scripts/Quaver.Main/Mods/ModSuddenDeath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Main.Mods
+{
+	public class ModSuddenDeath : Mod
+	{
+		public ModSuddenDeath()
+		{
+			Name = "Sudden Death";
+			Type = ModType.Special;
+			ModIdentifier = ModIdentifier.NoFail;
+			Description = "Miss once... and you die.";
+			ScoreMultiplier = 0.0f;
+			IncompatibleMods =  new ModIdentifier[] { ModIdentifier.NoFail };
+			Ranked = true;
+			FailureAllowed = true;
+		}
+	}	
+}
