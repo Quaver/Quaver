ef2c6782880ab3b08eeafcbc12cecf50624aa13f
staravia <denysenesouma@gmail.com>
2017-09-06 20:13:32 -0700

    Add: Multi Scroll Direction Mod + Gravity Mod
    Former-commit-id: aa101f200150b7aced54bd0c53abdc9390e09500 [formerly 1d52eb423fb8e6a58b5fa866343f5c5e63cfb5b6]
    Former-commit-id: 46c3398fa524dbe9a479ccb4c247fd608dacb3f2

--- Assets/Scripts/Quaver.Graphics/NoteBurst.cs
+++ Assets/Scripts/Quaver.Graphics/NoteBurst.cs
@@ -9,16 +9,22 @@ namespace Quaver.Graphics
         public float burstLength;
         public float burstSize;
         public float startSize;
+        public bool started = true;
         private float timeElapsed = 0;
         // Update is called once per frame
         void Update()
         {
-            timeElapsed += Time.deltaTime;
-            transform.localScale = (Vector3.one  + Vector3.one * startSize * Mathf.Sqrt(timeElapsed / burstLength)) * burstSize;
-            transform.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f- Mathf.Pow(timeElapsed / burstLength,0.2f));
-            if (timeElapsed >= burstLength)
+            if (started)
             {
-                Destroy(this.gameObject);
+                timeElapsed += Time.deltaTime;
+                transform.localScale = (Vector3.one + Vector3.one * startSize * Mathf.Sqrt(timeElapsed / burstLength)) * burstSize;
+                transform.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f - Mathf.Pow(timeElapsed / burstLength, 0.2f));
+                if (timeElapsed >= burstLength)
+                {
+                    started = false;
+                    transform.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+                    timeElapsed = 0;
+                }
             }
         }
     }
