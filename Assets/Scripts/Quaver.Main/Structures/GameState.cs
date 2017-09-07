7cea3a8fb9ef1ba001dedbfd7503ac1dd723169f
staravia <denysenesouma@gmail.com>
2017-09-06 20:13:32 -0700

    Add: Multi Scroll Direction Mod + Gravity Mod
    Former-commit-id: aa101f200150b7aced54bd0c53abdc9390e09500 [formerly 8b80fd16ff214630b678b988f987d7a5a524f1df]
    Former-commit-id: 46c3398fa524dbe9a479ccb4c247fd608dacb3f2

--- Assets/Scripts/Quaver.Main/Structures/GameState.cs
+++ Assets/Scripts/Quaver.Main/Structures/GameState.cs
@@ -7,15 +7,17 @@ namespace Quaver.Main
     public class GameState : MonoBehaviour
     {
         public bool isActive = false;
+        public bool loaded = false;
         public GameObject StateObject;
         //State Start
-        public void StateStart()
+        public bool StateStart()
         {
             if (StateObject == null)
             {
                 isActive = true;
                 StateObject = Instantiate(this.gameObject);
             }
+            return true;
         }
         //State End
         public void StateEnd()
