e9e290ce6d4b0d7722589feffe66bdd7830d4dd2
Swan <contact@swan.moe>
2017-09-07 18:18:54 -0400

    Add: Notification System Setup
    Former-commit-id: f39ffe55aac8b4ca85b0f071e4a25183581981e0 [formerly 8be8b8f4b9ed6c33a63e115d66fadeab51450ede]
    Former-commit-id: 5cd6958e29064680afb475f345b782280fbe9bbc

---
+++ Assets/Scripts/Quaver.Notifications/Notification.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Notifications
+{
+    /// <summary>
+    /// This class handles everything to do with notifications
+	/// It will display a notification and handle its click event.
+	///</summary>
+	public class Notification
+	{
+		/// <summary>
+		///	The name of the notification.
+		/// </summary>
+		public string Name;
+
+		/// <summary>
+		/// The type of notification defined in the NotificationType enum
+		/// </summary>
+		public NotificationType Type;
+		
+		/// <summary>
+		/// The action that will take place when a notification is clicked.
+		/// </summary>
+		public NotificationAction Action;
+
+		/// <summary>
+		/// The specific color of the notification for when it will be displayed to
+		/// the user.
+		/// </summary>
+		public Color Color;
+		
+		/// <summary>
+		/// The content or text of the notification.
+		/// </summary>
+		public string Content;
+
+		/// <summary>
+		/// The URL of the notification, if necessary.
+		/// </summary>
+		public string URL;
+		
+
+		// Display the notification with the given details above.
+		public void DisplayNotification()
+		{
+			
+		}
+
+		// Implement later, this'll be the method that defines what to do when a notification is clicked.
+		public void ClickHandler(NotificationAction notificationAction, string notificationContent, string URL)
+		{
+			switch (notificationAction)
+			{
+				case NotificationAction.Disappear:
+					// Make notification disappear
+					break;
+				case NotificationAction.OpenChat:
+					// Open Chat and make notification disappear
+					break;
+				case NotificationAction.OpenLink:
+					// Open a URL
+					break;
+				case NotificationAction.OpenLog:
+					// Open the logs folder
+					break;
+				case NotificationAction.OpenReplays:
+					// Open the replays directory
+					break;
+				case NotificationAction.OpenScreenshot:
+					// Open the screenshots directory
+					break;
+				default: 
+					// Just make it disapepar.
+					break;
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/Quaver.Notifications/NotificationAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Notifications
+{
+	public enum NotificationAction
+	{
+		Disappear,
+		OpenChat,
+		OpenLink,
+		OpenScreenshot,
+		OpenLog,
+		OpenReplays
+	}
+}
