0de634adf9ef83148407009f03e89c8053895bb5
Swan <contact@swan.moe>
2017-09-07 18:18:54 -0400

    Add: Notification System Setup

---
+++ Assets/Scripts/Quaver.Notifications/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Notifications
+{
+	public enum NotificationType
+	{
+		ClientError,
+		ClientWarning,
+		ServerAlert,
+		ServerMessage
+	}
+}
