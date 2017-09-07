db59d7d0a922778927f2bfd75757815ccb4bb8e3
Swan <contact@swan.moe>
2017-09-07 19:18:20 -0400

    Add: Send Client Notification When Screenshot Taken
    Former-commit-id: d5df4d9faa81b5f71c59006d626dc90b5b27ca11 [formerly 11d4a6208329310d683fb0b917ff1475a64fd5df]
    Former-commit-id: 46632c86e1a945dba92d1f008d9f10cb8962c0e3

--- Assets/Scripts/Quaver.Main/ScreenshotService/ScreenshotService.cs
+++ Assets/Scripts/Quaver.Main/ScreenshotService/ScreenshotService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using Quaver.Config;
+using Quaver.Notifications;
 
 namespace Quaver.Main.Screenshots
 {
@@ -14,7 +15,13 @@ namespace Quaver.Main.Screenshots
 			{
 				string screenshotPath = config.ScreenshotsDirectory + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
 				Application.CaptureScreenshot(screenshotPath);
-				Debug.Log("Screenshot successfully saved at: " + screenshotPath);
+
+				string notifName = "Test";
+				string notifContent = "Screenshot successfully saved at: " + screenshotPath;
+
+				// Send new notification 
+				Notification notif = new Notification(notifName, NotificationType.Screenshot, NotificationAction.OpenScreenshot, 
+														new Color(0.2F, 0.3F, 0.4F), notifContent);
 			}
 		}
 	}
