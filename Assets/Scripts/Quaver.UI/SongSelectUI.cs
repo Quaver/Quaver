58f8f62bdeddf1c0f0b3fb1df92a1e43ebf291fc
staravia <denysenesouma@gmail.com>
2017-09-09 11:58:30 -0700

    Update: SongSelectObject UI
    Former-commit-id: 2140b84e28c8af3d77f41f9f3a07b57c980aa08e [formerly 0c37da0ebb7aca711af167ea73bf790a334b6f2a]
    Former-commit-id: 225ce4c365f4c244c24a077fd801c288f2aab8fe

---
+++ Assets/Scripts/Quaver.UI/SongSelectUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quaver.Main;
+using UnityEngine.UI;
+
+namespace Quaver.Main
+{
+    public class SongSelectUI : MonoBehaviour
+    {
+        public GameObject SongSelect;
+        public GameObject SelectionSet;
+        private SongSelectObject[] SongList;
+
+        private int ObjectYSize = 0;
+        private int Selection = 0;
+        private float SelectYPos = 0;
+        private int SelectionDiffCount;
+
+        //ANIMATION
+        private float posTween = 0;
+
+        public void Start()
+        {
+            SongList = new SongSelectObject[10];
+            for (int i = 0; i < 10; i++)
+            {
+                SongList[i] = new SongSelectObject(SongSelect, SelectionSet.transform, ObjectYSize, i, this.GetComponent<SongSelectUI>());
+                int curPos = i;
+                //DONT FORGET TO REMOVE THE EVENT LISTENER AFTER DESTROYING OBJECTS
+                SongList[i].SelectObject.GetComponent<Button>().onClick.AddListener(() => { print("A"); Clicked(curPos); });
+                ObjectYSize += SongList[i].sizeY+5;
+            }
+
+        }
+
+        public void Clicked(int pos)
+        {
+            SelectYPos = SongList[pos].posY;
+        }
+
+
+        private void Update()
+        {
+            posTween += (SelectYPos - posTween) / 15f;
+            SelectionSet.transform.localPosition = new Vector2(-550, -posTween + 540);
+            SelectYPos += Mathf.Min(Input.GetAxis("Mouse ScrollWheel")*1500f,2000f);
+            SelectYPos = Mathf.Min(Mathf.Max(0, SelectYPos),ObjectYSize);
+        }
+    }
+}
