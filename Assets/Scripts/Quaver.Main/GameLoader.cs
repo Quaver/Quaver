10790d6e63e48d265856215572a3f95157064925
Swan <contact@swan.moe>
2017-09-10 19:07:10 -0400

    Add: Game Loader Script
    Former-commit-id: e131f6d1792700d76748b1ec3ec0dc1ee5b2f265 [formerly c00de92724cf429bde3d625f570d471e98d745e7]
    Former-commit-id: dd51f14573ffebb1d7164890614f7123a6b0550e

---
+++ Assets/Scripts/Quaver.Main/GameLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quaver.Cache;
+using Quaver.Config;
+
+public class GameLoader : MonoBehaviour 
+{
+	public Cfg UserConfig;
+	public List<CachedBeatmap> LoadedBeatmaps = new List<CachedBeatmap>();
+
+	void Awake()
+	{
+		// Create the beatmap database if it doesn't already exist.
+		BeatmapCacheIndex.CreateDatabase();
+
+		// Load the User Config - If it doesn't already exist, it will generate one
+		// and all the necessary files.
+		UserConfig = ConfigLoader.Load();
+
+		// Load Beatmaps into the list of LoadedBeatmaps
+		LoadedBeatmaps = BeatmapCacheIndex.LoadBeatmaps();
+		Debug.LogWarning(LoadedBeatmaps.Count);
+
+	}
+}
