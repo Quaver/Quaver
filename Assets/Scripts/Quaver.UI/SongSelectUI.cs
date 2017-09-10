e745afee85b794dfb8640dbb9e73bc87f07f2282
staravia <denysenesouma@gmail.com>
2017-09-10 02:48:59 -0700

    Update: Song Select
    Former-commit-id: c853fd7a9cde264d7edee359804e78c6cac66e31 [formerly eacf4536b59844eff379a8e427db2c48550f02e7]
    Former-commit-id: 3f6c4c65e620678c91cd41460364a97ff8ca4880

--- Assets/Scripts/Quaver.UI/SongSelectUI.cs
+++ Assets/Scripts/Quaver.UI/SongSelectUI.cs
@@ -8,45 +8,87 @@ namespace Quaver.Main
 {
     public class SongSelectUI : MonoBehaviour
     {
+        //Reference Values
         public GameObject SongSelect;
         public GameObject SelectionSet;
+        public GameObject DiffSelect;
         private SongSelectObject[] SongList;
+        private SongSelectObject[] DifficultyList;
 
         private int ObjectYSize = 0;
         private int Selection = 0;
+        private int offsetFromSelection = 0;
         private float SelectYPos = 0;
         private int SelectionDiffCount;
 
-        //ANIMATION
+        private int totalBeatmaps;
+
+        //Animatioon
         private float posTween = 0;
+        private float offsetTween = 0;
+
+        //TEST
+        private int testSelectionSize = 20;
 
         public void Start()
         {
-            SongList = new SongSelectObject[10];
-            for (int i = 0; i < 10; i++)
+            SongList = new SongSelectObject[testSelectionSize];
+            DifficultyList = new SongSelectObject[0];
+            for (int i = 0; i < testSelectionSize; i++)
             {
-                SongList[i] = new SongSelectObject(SongSelect, SelectionSet.transform, ObjectYSize, i, this.GetComponent<SongSelectUI>());
+                SongList[i] = new SongSelectObject(false, SongSelect, SelectionSet.transform, ObjectYSize, i, this.GetComponent<SongSelectUI>());
                 int curPos = i;
                 //DONT FORGET TO REMOVE THE EVENT LISTENER AFTER DESTROYING OBJECTS
-                SongList[i].SelectObject.GetComponent<Button>().onClick.AddListener(() => { Clicked(curPos); });
+                SongList[i].SelectObject.GetComponent<Button>().onClick.AddListener(() => { Clicked(curPos, false); });
                 ObjectYSize += SongList[i].sizeY+5;
             }
             SelectYPos = ObjectYSize;
+            totalBeatmaps = SongList.Length;
 
         }
 
-        public void Clicked(int pos)
+        public void Clicked(int pos, bool subSelection)
         {
             SelectYPos = SongList[pos].posY;
+            print("1");
+            if (!subSelection)
+            {
+                print("2");
+                Selection = pos;
+                for (int i = 0; i < DifficultyList.Length; i++)
+                {
+                    Destroy(DifficultyList[i].SelectObject);
+                }
+                DifficultyList = new SongSelectObject[SongList[pos].diffCount];
+                int newSongPos = 0;
+                for (int i = 0; i < SongList[pos].diffCount; i++)
+                {
+                    DifficultyList[i] = new SongSelectObject(true, DiffSelect, SelectionSet.transform, SongList[pos].posY - 75 - newSongPos, i, this.GetComponent<SongSelectUI>());
+                    newSongPos += DifficultyList[i].sizeY+ 5;
+                }
+                offsetFromSelection = newSongPos;
+                offsetTween = 0;
+            }
         }
 
 
         private void Update()
         {
-            posTween += (SelectYPos - posTween) / 15f;
+            SelectYPos += Mathf.Min(Input.GetAxis("Mouse ScrollWheel") * 1500f, 2000f);
+            SelectYPos = Mathf.Min(Mathf.Max(440 - offsetFromSelection, SelectYPos), ObjectYSize - 520); //540 - 100 (ui Size)
+            posTween += (SelectYPos - posTween) * Mathf.Min(Time.deltaTime*5f,1);
             SelectionSet.transform.localPosition = new Vector2(-550, -posTween + 540);
-            SelectYPos += Mathf.Min(Input.GetAxis("Mouse ScrollWheel")*1500f,2000f);
-            SelectYPos = Mathf.Min(Mathf.Max(0, SelectYPos),ObjectYSize);
+            if (offsetTween != offsetFromSelection)
+            {
+                offsetTween += (offsetFromSelection - offsetTween) * Mathf.Min(Time.deltaTime * 10f, 1);
+                for (int i = 0; i <totalBeatmaps; i++)
+                {
+                    float tweener = SongList[i].ParentTransform.localPosition.y;
+                    if (i<Selection) tweener += (SongList[i].posY - offsetTween - tweener) * Mathf.Min(Time.deltaTime * 15f, 1); 
+                    else  tweener +=  (SongList[i].posY - tweener) * Mathf.Min(Time.deltaTime * 15f, 1);
+                    SongList[i].ParentTransform.localPosition = new Vector2(5, tweener);
+                }
+            }
         }
     }
 }
