0ccc9f6a56ccbc74b2d1305c95fceb4777037ea7
Swan <contact@swan.moe>
2017-09-11 18:58:37 -0400

    Add: Audio Player for tracks on the file system

---
+++ Assets/Scripts/Quaver.Audio/AudioPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Quaver.Config;
+using Quaver.Cache;
+using Quaver.Qua;
+using UnityEngine;
+
+namespace Quaver.Audio
+{
+	public class AudioPlayer
+	{
+		// This is purely responsible for loading a beatmap's audio file.
+		// It will take in a cached map, parse it, and load it's audio from the file system.
+		public static void LoadSong(CachedBeatmap map, AudioSource gameAudio)
+		{
+			// Parse the cached beatmap, and find the audio file from it
+			QuaFile beatmap = QuaParser.Parse(map.Path);
+			string audioPath = map.Directory + "/" + beatmap.AudioFile;
+
+			if (!File.Exists(audioPath))
+			{
+				Debug.LogError("[AUDIO PLAYER] Error: File cannot be played because it was not found!");
+				return;
+			}
+
+            string url = "file:///" + audioPath;
+
+            WWW audioLoader = new WWW(url);
+
+            while (!audioLoader.isDone)
+            {
+                Debug.Log("[AUDIO PLAYER] Loading Audio Track: " + url);
+            }
+
+            if (audioLoader.isDone)
+            {
+                gameAudio.clip = audioLoader.GetAudioClip(false, false, AudioType.OGGVORBIS);
+
+                if (!gameAudio.isPlaying && gameAudio.clip.isReadyToPlay)
+                {
+                    Debug.Log("[AUDIO PLAYER] Audio Track Loaded! Starting Song.");
+                    gameAudio.Play();
+                }
+            }
+			
+		}
+	}
+}
