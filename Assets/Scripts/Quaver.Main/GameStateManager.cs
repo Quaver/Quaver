8f4f2d41948c7e662f8926a5164b6a6a27181e52
Swan <contact@swan.moe>
2017-09-11 18:31:54 -0400

    Add: Select a random beatmap upon game start
    Former-commit-id: 99edbf80a0138eaa57c26e555f6ecaefb1d787e6 [formerly 3ea6f7d7a06fc3e096b5c9a33322326e89409e6c]
    Former-commit-id: 2df734ae2c759fbdc07c8f40c1cf324c1d2ac321

--- Assets/Scripts/Quaver.Main/GameStateManager.cs
+++ Assets/Scripts/Quaver.Main/GameStateManager.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Quaver.Config;
 using Quaver.Screenshot;
+using Quaver.SongSelect;
+using Quaver.Cache;
 using UnityEngine.UI;
 using UnityStandardAssets.ImageEffects;
 
@@ -12,10 +14,19 @@ namespace Quaver.Main
 
     public class GameStateManager : MonoBehaviour
     {
-        // Config File
+        // IMPORTANT! These are the parsed config & all the loaded beatmaps!
+        // These need to be loaded first before any other state begins!!!!!!
+        // VERY IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         public Cfg GameConfig;
+        public List<CachedBeatmap> LoadedBeatmaps = new List<CachedBeatmap>();
+        List<MapDirectory> MapDirectories = new List<MapDirectory>();
 
-        // Reference Varialbes
+        // IMPORTANT! This will hold the currently selected map in our MapDirectories list.
+        // It will be used to boot up the main menu music & auto-select the map when the user
+        // goes to the song selection screen.
+        CachedBeatmap currentMap;
+        
+        // Reference Variables
         public GameState[] States;
         public ParticleSystem DustRenderer;
         public GameObject bgImage;
@@ -25,10 +36,14 @@ namespace Quaver.Main
         public GameObject loadingScreenTest;
         private int testState = 0;
 
-        private void Awake()
+        void Awake()
         {
-            // The first thing we want to do is load our config before anything else.
-            GameConfig = ConfigLoader.Load();
+            // TODO: We'll want to initialize a state here which will be our loading screen
+            // before the game begin. Only the will we continue forward to the main menu
+            // when everything has loaded.
+            // States[0].StateStart(this);
+            LoadConfiguration();
+
         }
 
         private void Start()
@@ -39,8 +54,6 @@ namespace Quaver.Main
             //Do game start stuff here
             //Starts play mode (TEST)
             States[0].StateStart(this);
-
-
 
             /*
             // This is just a test for the songs folder, we'll choose a random beatmap later instead of just choosing a 
@@ -88,6 +101,54 @@ namespace Quaver.Main
 
         }
         
+        // This is the first thing that will be called upon Game Start. It is responsible for
+        // creating and syncing our beatmap database, loading our configuration file,
+        // and sorting the loaded beatmaps into their respective directories, so they can 
+        // be used during song select.
+        void LoadConfiguration()
+        {
+            // Create and/or sync the beatmap database.
+            BeatmapCacheIndex.CreateDatabase();
+
+            // The first thing we want to do is load our config before anything else.
+            GameConfig = ConfigLoader.Load();
+
+            // Here, we load all the beatmaps from the database, these are all the playable maps.
+            LoadedBeatmaps = BeatmapCacheIndex.LoadBeatmaps(GameConfig);
+
+            // Now, we want to sort all of the beatmaps.
+            MapDirectories = OrderMapsBy.Directory(LoadedBeatmaps);
+
+            // Select a random map from the map directories, which will be our first selected map.
+            // Generate a random number from the list of loaded beatmaps, then we will
+            // go through them and find the correct map.
+            if (MapDirectories.Count != 0)
+            {
+                int randomMapNumber = Random.Range(0, LoadedBeatmaps.Count);
+                int mapsLookedThrough = 0;
+                foreach (MapDirectory dir in MapDirectories)
+                {
+                    foreach (CachedBeatmap map in dir.Beatmaps)
+                    {
+                        // If the maps looked through is equivalent to the random map number we generated
+                        // then we consider that the selected map.
+                        if (mapsLookedThrough == randomMapNumber)
+                        {
+                            currentMap = map;
+                        }
+
+                        mapsLookedThrough++;
+                    }
+                }
+            }
+
+            // This is all that we need to load, so we can continue on with the main menu state.
+            Debug.Log("[CONFIG LOADER] There were: " + LoadedBeatmaps.Count + " beatmaps in " + MapDirectories.Count + " directories loaded."); 
+            Debug.Log("[CONFIG LOADER] The random map selected on start up is: " + currentMap.Path);
+        }
+
+        // This is a method used for testing to switch our state to the next one. This will eventually
+        // be removed, once we have proper buttons to switch states.
         public void SwitchState()
         {
             //Test button click
