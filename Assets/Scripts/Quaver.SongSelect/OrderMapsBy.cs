85cac8a2951f13fe92f85b9f3cc21e653d96e814
Swan <contact@swan.moe>
2017-09-11 17:50:24 -0400

    Add: Sorting beatmaps by directory :eggplant: :sweat_drops:
    Former-commit-id: 45f833583225dbfd036344a9ebcd3a705e18a11c [formerly a3bc8769d1028f04240cda4933e373ecd6b61d11]
    Former-commit-id: de66be4c80414739bd28d949da9f1a79ac3a1cf2

---
+++ Assets/Scripts/Quaver.SongSelect/OrderMapsBy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Quaver.Cache;
+
+namespace Quaver.SongSelect
+{
+	public class OrderMapsBy
+	{
+		// Responsible for sorting maps by their directory and returning a list of directories.
+		// This essentially will be useful for song select, when the user is grouping by directory.
+		// This will be the default option.
+		public static List<MapDirectory> Directory(List<CachedBeatmap> loadedBeatmaps)
+		{
+			// Sort loaded beatmaps into directory
+			loadedBeatmaps = loadedBeatmaps.OrderBy(x => x.Directory).ToList();
+
+			List<MapDirectory> sortedMaps = new List<MapDirectory>();
+
+			// For every map, we want to check every other map and create a list with that 
+			foreach (CachedBeatmap loadedMap in loadedBeatmaps)
+			{
+				bool dirExists = false;
+
+				foreach (MapDirectory mapSet in sortedMaps)
+				{
+					if (mapSet.Directory == loadedMap.Directory)
+					{
+						dirExists = true;
+						break;
+					}
+				}
+
+				if (dirExists)
+					continue;
+
+				// Go through all the other maps in the loaded maps, and find ones with the same directory.
+				MapDirectory directory = new MapDirectory();
+				directory.Beatmaps = new List<CachedBeatmap>();
+
+				foreach(CachedBeatmap otherBeatmap in loadedBeatmaps)
+				{
+					if (loadedMap.Directory == otherBeatmap.Directory)
+					{
+						directory.Directory = loadedMap.Directory;
+						directory.Beatmaps.Add(otherBeatmap);
+					}
+				}
+
+				sortedMaps.Add(directory);
+			}
+
+			return sortedMaps;
+		} 
+	}
+}
