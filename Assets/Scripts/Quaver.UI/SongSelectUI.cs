25ab37ebbd01382b402e8279603ad03dccf74a96
staravia <denysenesouma@gmail.com>
2017-09-11 04:05:14 -0700

    Update: Song Select UI

--- Assets/Scripts/Quaver.UI/SongSelectUI.cs
+++ Assets/Scripts/Quaver.UI/SongSelectUI.cs
@@ -32,7 +32,7 @@ namespace Quaver.UI
         private float offsetTween = 0;
 
         //TEST
-        private int testSelectionSize = 800; //use this to stress test
+        private int testSelectionSize = 20; //use this to stress test
 
         public void Start()
         {
@@ -40,7 +40,7 @@ namespace Quaver.UI
             DifficultyList = new SongSelectObject[0];
             for (int i = 0; i < testSelectionSize; i++)
             {
-                SongList[i] = new SongSelectObject(false, SongSelect, SelectionSet.transform, ObjectYSize, i, this.GetComponent<SongSelectUI>());
+                SongList[i] = new SongSelectObject(0, SongSelect, SelectionSet.transform, ObjectYSize, i, this.GetComponent<SongSelectUI>());
                 int curPos = i;
                 //DONT FORGET TO REMOVE THE EVENT LISTENER AFTER DESTROYING OBJECTS
                 SongList[i].SelectObject.GetComponent<Button>().onClick.AddListener(() => { Clicked(curPos, false); });
@@ -63,15 +63,16 @@ namespace Quaver.UI
                     Destroy(DifficultyList[i].SelectObject);
                 }
                 DifficultyList = new SongSelectObject[SongList[pos].diffCount];
-                int newSongPos = 0;
+                int newSongPos = -5;
                 for (int i = 0; i < SongList[pos].diffCount; i++)
                 {
-                    DifficultyList[i] = new SongSelectObject(true, DiffSelect, SelectionSet.transform, SongList[pos].posY - 75 - newSongPos, i, this.GetComponent<SongSelectUI>());
+                    DifficultyList[i] = new SongSelectObject(i+1, DiffSelect, SelectionSet.transform, SongList[pos].posY - 75 - newSongPos, i, this.GetComponent<SongSelectUI>());
                     DifficultyList[i].TitleText.text = "Random Difficulty " + (i+1);
                     int curPos = i;
                     DifficultyList[i].SelectObject.GetComponent<Button>().onClick.AddListener(() => { Clicked(curPos, true); });
                     newSongPos += DifficultyList[i].sizeY+ 5;
                 }
+                newSongPos += 5;
                 offsetFromSelection = newSongPos;
                 offsetTween = 0;
                 SelectYPos = SongList[pos].posY - (int)(offsetFromSelection/2f);
@@ -92,13 +93,14 @@ namespace Quaver.UI
             if (Input.GetMouseButtonDown(1)) mouseRightDown = true;
             else if (Input.GetMouseButtonUp(1)) mouseRightDown = false;
             if (mouseRightDown) SelectYPos = (int)((Input.mousePosition.y/Screen.height) * (float)(ObjectYSize));
-            SelectYPos = Mathf.Min(Mathf.Max(440 - offsetFromSelection, SelectYPos), ObjectYSize - 520); //540 - 100 (ui Size)
+            SelectYPos = Mathf.Min(Mathf.Max(480 - offsetFromSelection, SelectYPos), ObjectYSize - 520); //540 - 100 (ui Size)
+
             //Set Selection Position
             posTween += (SelectYPos - posTween) * Mathf.Min(Time.deltaTime * 5f, 1);
-            SelectionSet.transform.localPosition = new Vector2(-550, -posTween + 540);
+            SelectionSet.transform.localPosition = new Vector2(-430, -posTween + 540);
 
             //Set offsetPos (when song is selected)
-            if (offsetTween != offsetFromSelection)
+            if (offsetTween < offsetFromSelection)
             {
                 offsetTween += (offsetFromSelection - offsetTween) * Mathf.Min(Time.deltaTime * 10f, 1);
                 for (int i = 0; i <totalBeatmaps; i++)
@@ -107,6 +109,19 @@ namespace Quaver.UI
                     if (i< SongSelection) tweener += (SongList[i].posY - offsetTween - tweener) * Mathf.Min(Time.deltaTime * 15f, 1); 
                     else  tweener +=  (SongList[i].posY - tweener) * Mathf.Min(Time.deltaTime * 15f, 1);
                     SongList[i].ParentTransform.localPosition = new Vector2(5, tweener);
+                }
+            }
+
+            //Animate Difficulty Selection UI Position
+           if (offsetTween >= offsetFromSelection - 20 && DifficultyList.Length >0)
+            {
+                if (DifficultyList[DifficultyList.Length - 1].SelectObject.transform.localPosition.x > 4.99f)
+                {
+                    for (int i = 0; i < DifficultyList.Length; i++)
+                    {
+                        float tweener = DifficultyList[i].SelectObject.transform.localPosition.x + (5-DifficultyList[i].SelectObject.transform.localPosition.x) * Mathf.Min(Time.deltaTime * 15f, 1);
+                        DifficultyList[i].SelectObject.transform.localPosition = new Vector2(tweener , DifficultyList[i].posY);
+                    }
                 }
             }
         }
