fc45d5aa8e39f50f17e36d38b51821db941ca788
Swan <contact@swan.moe>
2017-09-11 17:50:24 -0400

    Add: Sorting beatmaps by directory :eggplant: :sweat_drops:
    Former-commit-id: 45f833583225dbfd036344a9ebcd3a705e18a11c [formerly c752f6a618f34a3cec7ba46751043514de6275e1]
    Former-commit-id: de66be4c80414739bd28d949da9f1a79ac3a1cf2

---
+++ Assets/Scripts/Quaver.SongSelect/MapDirectory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quaver.Cache;
+
+namespace Quaver.SongSelect
+{
+	public struct MapDirectory
+	{
+		public string Directory;
+		public List<CachedBeatmap> Beatmaps;
+	}
+}
