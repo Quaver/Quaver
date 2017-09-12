957bf70ff039cf9047ba8efcfab503b76bf78f76
Swan <contact@swan.moe>
2017-09-12 15:47:36 -0400

    Add: BackgroundLoader class (test this)
    Former-commit-id: df48d0b368a9d48aab09c56f93d4b587752ad17a [formerly 5bf951079ef3712c32590a3a75772d26f67d9a92]
    Former-commit-id: 1a11c2d6fcd9dfbcc9c5ddc54e73d9c97b809bf7

---
+++ Assets/Scripts/Quaver.UI/BackgroundLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+using Quaver.Cache;
+
+namespace Quaver.UI
+{
+	public class BackgroundLoader
+	{
+		// Responsible for loading a beatmap's background as a sprite.
+		public static Sprite Load(CachedBeatmap map)
+		{
+			try
+			{
+				string bgPath = map.BackgroundPath.Replace("\"", "");
+				Sprite sprite = new Sprite();
+
+				if (File.Exists(bgPath));
+				{
+					byte[] data = File.ReadAllBytes(bgPath);
+					Texture2D texture = new Texture2D(64, 64, TextureFormat.ARGB32, false);
+					texture.LoadImage(data);
+					sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(texture.width/2, texture.height/2));
+				}
+
+				return sprite;
+
+			} catch (Exception e)
+			{
+				Debug.LogException(e);
+			}
+
+			return new Sprite();
+		}
+	}
+}
