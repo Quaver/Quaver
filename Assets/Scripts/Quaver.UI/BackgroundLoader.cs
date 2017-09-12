e3510bdc09744f1e52eeef498c7ff1113176de26
staravia <denysenesouma@gmail.com>
2017-09-12 15:22:10 -0700

    Update: BG Update to beatmap

--- Assets/Scripts/Quaver.UI/BackgroundLoader.cs
+++ Assets/Scripts/Quaver.UI/BackgroundLoader.cs
@@ -10,29 +10,27 @@ namespace Quaver.UI
 	public class BackgroundLoader
 	{
 		// Responsible for loading a beatmap's background as a sprite.
-		public static Sprite Load(CachedBeatmap map)
+		public static void Load(CachedBeatmap map, GameObject bg)
 		{
 			try
 			{
-				string bgPath = map.BackgroundPath.Replace("\"", "");
+                string bgPath = map.BackgroundPath.Replace("\"", "");
 				Sprite sprite = new Sprite();
 
 				if (File.Exists(bgPath))
 				{
 					byte[] data = File.ReadAllBytes(bgPath);
-					Texture2D texture = new Texture2D(64, 64, TextureFormat.ARGB32, false);
+					Texture2D texture = new Texture2D(64, 64, TextureFormat.ARGB32, true);
 					texture.LoadImage(data);
-					sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(texture.width/2, texture.height/2));
+					sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f,0.5f));
 				}
+                bg.GetComponent<SpriteRenderer>().sprite = sprite;
+                bg.transform.localScale = Vector3.one * (20f * (100f / (float)sprite.rect.size.y));
 
-				return sprite;
-
-			} catch (Exception e)
+            } catch (Exception e)
 			{
 				Debug.LogException(e);
 			}
-
-			return new Sprite();
 		}
 	}
 }
