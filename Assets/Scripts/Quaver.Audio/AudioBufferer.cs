7252a66825f81c9841c03d9049e4ea4933bf0a6a
staravia <denysenesouma@gmail.com>
2017-09-12 14:17:37 -0700

    Update: Audio Buffering
    Former-commit-id: 588bcddf432bf41827b82275f293b62af4ba4229 [formerly e169b827eae3c0d01b3557bf205ccfac433565e4]
    Former-commit-id: a9fd089184567836767d12ea7acf0bc717bae0d2

---
+++ Assets/Scripts/Quaver.Audio/AudioBufferer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quaver.Cache;
+
+namespace Quaver.Audio
+{
+
+
+    public class AudioBufferer : MonoBehaviour
+    {
+
+        private WWW audioLoader;
+        private GameObject obj;
+        private AudioSource bufferAudio;
+        private CachedBeatmap bufferMap;
+        private bool bufferPreview;
+
+        public AudioBufferer(CachedBeatmap map, AudioSource gameAudio, bool usePreviewTime = false)
+        {
+
+            bufferAudio = gameAudio;
+            bufferMap = map;
+            bufferPreview = usePreviewTime;
+
+            string url = "file:///" + map.AudioPath;
+            audioLoader = new WWW(url);
+            obj = new GameObject();
+            obj.AddComponent<AudioBufferer>().StartCoroutine(BufferAudio());
+        }
+
+        private IEnumerator BufferAudio()
+        {
+
+            bufferAudio.Stop();
+
+            //Wait until audio is loaded
+            while (!audioLoader.isDone) yield return null;
+            yield return new WaitForSeconds(0.01f);
+
+            //Set and play audio
+            bufferAudio.clip = audioLoader.GetAudioClip(false,true, AudioType.OGGVORBIS);
+            if (bufferPreview) bufferAudio.time = bufferMap.AudioPreviewTime / 1000f;
+            bufferAudio.Play();
+
+            //Removes game object
+            Debug.Log("[AUDIO BUFFER] DONE!");
+            Destroy(obj);
+        }
+    }
+}
