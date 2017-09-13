b8d37961cf7144548ecd91848dcddc32163881c5
Swan <contact@swan.moe>
2017-09-13 19:17:41 -0400

    Add: Skin struct
    Former-commit-id: c4814a9667557c8b6506b29c69bb9f63100a1307 [formerly d7b0e0ee64a10fab4dba19d06e209543d66754c2]
    Former-commit-id: eb06c4ba3bd0cd3c13507e430eac16334305f6a5

---
+++ Assets/Scripts/Quaver.Skin/Skin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Skin
+{
+	public struct Skin
+	{
+		public string Name;
+		public string Author;
+		public string Version;
+	}
+}
