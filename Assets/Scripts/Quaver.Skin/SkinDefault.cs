6b6ce1d4cd4d5279d0aa2d506b4fa248f39fd2ad
Swan <contact@swan.moe>
2017-09-13 19:17:14 -0400

    Add: Class for skin defaults
    Former-commit-id: 29f61ba136b173180c910e7da69fa4379d50a2a1 [formerly 309ae295fe3a0b2f69d3679e4c2c3dbabf19f0ce]
    Former-commit-id: 5a7bebe3e05e85ef95e39504c3319e6af2c290e4

---
+++ Assets/Scripts/Quaver.Skin/SkinDefault.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Skin
+{
+	public static class SkinDefault
+	{
+		public static string Name = "Default";
+		public static string Author = "Swan";
+		public static string Version = "1.0.0";
+	}
+}
