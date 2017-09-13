014dd4fa71e84f3817929868ad6fa212f561d1b9
staravia <denysenesouma@gmail.com>
2017-09-12 19:29:11 -0700

    Update: SongSelectUI
    Former-commit-id: 8c7a0b8153fc084ff2c6c6b1b26d5657c73f230b [formerly 8124f215e136a67ee0c7ebe82f53372631929798]
    Former-commit-id: d8ece314642ba9f08927f8d2c69ee871f4451974

--- Assets/Scripts/Quaver.Main/SongSelectScreen.cs
+++ Assets/Scripts/Quaver.Main/SongSelectScreen.cs
@@ -29,7 +29,7 @@ namespace Quaver.Main
         private int totalBeatmaps;
 
         //Diff/Song Selection
-        private int SongSelection = 0;
+        private int SongSelection = -1;
 
         //Beatmap display Variables
         private bool mouseRightDown = false;
@@ -63,6 +63,7 @@ namespace Quaver.Main
                 SongList[i].SelectObject.GetComponent<Button>().onClick.AddListener(() => { Clicked(curPos, false); });
                 SongList[i].TitleText.text = SortedMapSets[i].Beatmaps[0].Title;
                 SongList[i].SubText.text = SortedMapSets[i].Beatmaps[0].Artist; //+ " | " + SortedBeatmaps[i].;
+                BackgroundLoader.LoadTexture(SortedMapSets[i].Beatmaps[0], SongList[i].bgImage);
                 //SongList[i].bgImage.texture = SortedBeatmaps[i].
                 ObjectYSize += SongList[i].sizeY + 5;
             }
@@ -85,7 +86,7 @@ namespace Quaver.Main
             SelectionSet.transform.localPosition = new Vector2(-430, -posTween + 540);
 
             //Set offsetPos (when song is selected)
-            if (offsetTween < offsetFromSelection)
+            if (offsetTween != offsetFromSelection)
             {
                 offsetTween += (offsetFromSelection - offsetTween) * Mathf.Min(Time.deltaTime * 10f, 1);
                 for (int i = 0; i < totalBeatmaps; i++)
@@ -98,9 +99,9 @@ namespace Quaver.Main
             }
 
             //Animate Difficulty Selection UI Position
-            if (offsetTween >= offsetFromSelection - 20 && DifficultyList.Length > 0)
+            if (DifficultyList.Length > 0)
             {
-                if (DifficultyList[DifficultyList.Length - 1].SelectObject.transform.localPosition.x > 4.99f)
+                if (offsetTween >= offsetFromSelection - 20 && DifficultyList[DifficultyList.Length - 1].SelectObject.transform.localPosition.x > 4.99f)
                 {
                     for (int i = 0; i < DifficultyList.Length; i++)
                     {
@@ -116,37 +117,61 @@ namespace Quaver.Main
         {
             if (!subSelection)
             {
-                SongSelection = pos;
-                AudioPlayer.LoadSong(SortedMapSets[pos].Beatmaps[0], Manager.SongAudioSource,true);
-                BackgroundLoader.Load(SortedMapSets[pos].Beatmaps[0], Manager.bgImage);
                 for (int i = 0; i < DifficultyList.Length; i++)
                 {
                     DifficultyList[i].SelectObject.GetComponent<Button>().onClick.RemoveAllListeners();
                     Destroy(DifficultyList[i].SelectObject);
                     Destroy(DifficultyList[i]);
                 }
-                DifficultyList = new SongSelectObject[SortedMapSets[pos].Beatmaps.Count];
-                int newSongPos = -5;
-                for (int i = 0; i < SortedMapSets[pos].Beatmaps.Count; i++)
+                if (pos != SongSelection)
                 {
-                    DifficultyList[i] = new SongSelectObject(i + 1, DiffSelect, SelectionSet.transform, SongList[pos].posY - 75 - newSongPos, SortedMapSets[pos], SortedMapSets[pos].Beatmaps[i]);
-                    DifficultyList[i].TitleText.text = SortedMapSets[pos].Beatmaps[i].Difficulty;
-                    DifficultyList[i].SubText.text = "★" + string.Format("{0:f2}", SortedMapSets[pos].Beatmaps[i].Stars / 100f);
-                    int curPos = i;
-                    DifficultyList[i].SelectObject.GetComponent<Button>().onClick.AddListener(() => { Clicked(curPos, true); });
-                    newSongPos += DifficultyList[i].sizeY + 5;
+
+                    //Creates new diff list
+                    DifficultyList = new SongSelectObject[SortedMapSets[pos].Beatmaps.Count];
+                    int newSongPos = -5;
+                    for (int i = 0; i < SortedMapSets[pos].Beatmaps.Count; i++)
+                    {
+                        DifficultyList[i] = new SongSelectObject(i + 1, DiffSelect, SelectionSet.transform, SongList[pos].posY - 75 - newSongPos, SortedMapSets[pos], SortedMapSets[pos].Beatmaps[i]);
+                        DifficultyList[i].TitleText.text = SortedMapSets[pos].Beatmaps[i].Difficulty;
+                        DifficultyList[i].SubText.text = "★" + string.Format("{0:f2}", SortedMapSets[pos].Beatmaps[i].Stars / 100f);
+                        BackgroundLoader.LoadTexture(DifficultyList[i].Beatmap, DifficultyList[i].bgImage);
+                        int curPos = i;
+                        DifficultyList[i].SelectObject.GetComponent<Button>().onClick.AddListener(() => { Clicked(curPos, true); });
+                        newSongPos += DifficultyList[i].sizeY + 5;
+                    }
+
+                    //Sets Pos + Loads Audio/BG
+                    SongSelection = pos;
+                    if (Manager.currentMap != null && DifficultyList[0].Beatmap.AudioPath != Manager.currentMap.AudioPath)
+                    {
+                        AudioPlayer.LoadSong(SortedMapSets[pos].Beatmaps[0], Manager.SongAudioSource, true);
+                        BackgroundLoader.LoadSprite(SortedMapSets[pos].Beatmaps[0], Manager.bgImage);
+                    }
+
+                    //Change Diff UI Values
+                    newSongPos += 5;
+                    offsetFromSelection = newSongPos;
+                    offsetTween = 0;
+                    SelectYPos = SongList[pos].posY - (int)(offsetFromSelection / 2f);
+                    Manager.currentMap = DifficultyList[0].Beatmap;
                 }
-                newSongPos += 5;
-                offsetFromSelection = newSongPos;
-                offsetTween = 0;
-                SelectYPos = SongList[pos].posY - (int)(offsetFromSelection / 2f);
+                else
+                {
+                    offsetFromSelection = 0;
+                    SongSelection = -1;
+                    SelectYPos = SongList[pos].posY;
+                    DifficultyList = new SongSelectObject[0];
+                }
             }
             else
             {
                 SelectYPos = DifficultyList[pos].posY;
+                if (DifficultyList[pos].Beatmap.AudioPath != Manager.currentMap.AudioPath)
+                {
+                    BackgroundLoader.LoadSprite(Manager.currentMap, Manager.bgImage);
+                    AudioPlayer.LoadSong(DifficultyList[pos].Beatmap, Manager.SongAudioSource, true);
+                }
                 Manager.currentMap = DifficultyList[pos].Beatmap;
-                BackgroundLoader.Load(Manager.currentMap, Manager.bgImage);
-                AudioPlayer.LoadSong(DifficultyList[pos].Beatmap, Manager.SongAudioSource,true);
             }
         }
 
--- Assets/Scripts/Quaver.UI/BackgroundLoader.cs
+++ Assets/Scripts/Quaver.UI/BackgroundLoader.cs
@@ -4,13 +4,14 @@ using System.IO;
 using System;
 using UnityEngine;
 using Quaver.Cache;
+using UnityEngine.UI;
 
 namespace Quaver.UI
 {
 	public class BackgroundLoader
 	{
 		// Responsible for loading a beatmap's background as a sprite.
-		public static void Load(CachedBeatmap map, GameObject bg)
+		public static void LoadSprite(CachedBeatmap map, GameObject bg, bool xsnap = false, bool ysnap = false)
 		{
 			try
 			{
@@ -25,12 +26,34 @@ namespace Quaver.UI
 					sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f,0.5f));
 				}
                 bg.GetComponent<SpriteRenderer>().sprite = sprite;
-                bg.transform.localScale = Vector3.one * (20f * (100f / (float)sprite.rect.size.y));
+                //bg.transform.localScale = Vector3.one * (20f * (100f / (float)sprite.rect.size.y));
+                bg.transform.localScale = Vector3.one * ((float)Screen.width / (float)Screen.height) * 20f * (100f / (float)sprite.rect.size.x);
 
             } catch (Exception e)
 			{
 				Debug.LogException(e);
 			}
 		}
-	}
+
+        // Responsible for loading a beatmap's background as a texture.
+        public static void LoadTexture(CachedBeatmap map, RawImage bg)
+        {
+            try
+            {
+                string bgPath = map.BackgroundPath.Replace("\"", "");
+
+                if (File.Exists(bgPath))
+                {
+                    byte[] data = File.ReadAllBytes(bgPath);
+                    Texture2D texture = new Texture2D(64, 64, TextureFormat.ARGB32, true);
+                    texture.LoadImage(data);
+                    bg.texture = texture;
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+    }
 }
