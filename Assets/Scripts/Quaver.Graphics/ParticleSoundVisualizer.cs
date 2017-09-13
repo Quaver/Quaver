854491a51449eb0febd94ef86d126026a1d49dd3
staravia <denysenesouma@gmail.com>
2017-09-13 14:50:52 -0700

    Add: Particle Sound Visualizer Class
    Former-commit-id: 4673f533b900b270a074a1658c68d336bc5e1530 [formerly fbbe87ac0d7bff450e279f56826dadd578a10be4]
    Former-commit-id: 52108a3b7429bdb3ad4feb9944ca8f0a936521cb

---
+++ Assets/Scripts/Quaver.Graphics/ParticleSoundVisualizer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSoundVisualizer : MonoBehaviour {
+
+    //Assign these manually
+    public bool isActive = true;
+    public ParticleSystem particle;
+    private ParticleSystem.MainModule main;
+
+    //Reference Variables
+    private float[] spectrum = new float[256];
+    private float bassScale = 0;
+    private float avgScale;
+
+    void Start()
+    {
+        if (particle != null)
+        {
+            main = particle.main;
+        }
+    }
+
+	void Update () {
+		if (isActive && particle != null)
+        {
+            AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+            bassScale = Mathf.Pow(spectrum[0] * 0.8f + spectrum[1] * 0.2f,2f);
+            for (int i = 0; i < 8; i++) avgScale += spectrum[i];
+            avgScale = Mathf.Pow(avgScale/8f,0.25f);
+
+            main.simulationSpeed = bassScale*0.7f + avgScale*0.3f + 0.05f;
+        }
+	}
+}
