9cf7208d48df8cbfcbc719f4b3bad1a5faabed9e
Swan <contact@swan.moe>
2017-09-12 23:24:49 -0400

    Add: Placeholder State enum

---
+++ Assets/Scripts/Quaver.Main/State.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Main
+{
+	public enum State
+	{
+		MainMenu,
+		SongSelect,
+		Paused,
+		Options,
+		Playing,
+	}
+}
