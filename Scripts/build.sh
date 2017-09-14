36f07d08efa4c89a61f243785b921e492f7b39e9
Swan <contact@swan.moe>
2017-09-14 17:47:10 -0400

    '8
    Former-commit-id: f32cf19d89b841f325ff06545901ea23790fc6a3 [formerly bf6a0e2b01a3469621c9a098278caab3f0ce1d4b]
    Former-commit-id: 55e9f5a6bf487820642a565e0636e46cb81a476e

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,39 +1,42 @@
 #! /bin/sh
 
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
-
-# Change this the name of your project. This will be the name of the final executables as well.
-project="ci-build"
+project="<YOUR PROJECT NAME HERE>"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
   -quit
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
   -quit
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
   -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
+
+
+echo 'Attempting to zip builds'
+zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
