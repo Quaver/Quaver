53039e7d38eeb73b51fef4d464099e98f4eb1ed0
Swan <contact@swan.moe>
2017-09-14 16:37:59 -0400

    Add: TravisCI Configuration
    Former-commit-id: e6cd4cfc2c1a1d22dcaeb4554b28d2d2eb19d518 [formerly f787206532ff5cddb9540926dfb6c159f8fd6d5e]
    Former-commit-id: 00528c40571ef13d21304411991beb52cbda95cd

---
+++ install.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+BASE_URL=http://netstorage.unity3d.com/unity
+HASH=88d00a7498cd
+VERSION=5.5.1f1
+
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
