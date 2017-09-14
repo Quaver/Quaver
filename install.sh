0c8385730fda7c0b908d8e6c26d2d42a3a2e7c30
Swan <contact@swan.moe>
2017-09-14 16:54:46 -0400

    Fix: TravisCI Configuration Scripts
    Former-commit-id: 9d9027e3fa00be88cfec44345c56cd3476d9d330 [formerly fd35db0843c2c3300f135e0261f8921a8fdcebcb]
    Former-commit-id: 8f04ca8d05474d78fa78b0c4ddd14121664e36fe

--- install.sh
+++ install.sh
@@ -1,29 +1,12 @@
+
 #! /bin/sh
 
-BASE_URL=http://netstorage.unity3d.com/unity
-HASH=88d00a7498cd
-VERSION=5.5.1f1
+# Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
-download() {
-  file=$1
-  url="$BASE_URL/$HASH/$package"
+# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
+# installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
+echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
 
-  echo "Downloading from $url: "
-  curl -o `basename "$package"` "$url"
-}
-
-install() {
-  package=$1
-  download "$package"
-
-  echo "Installing "`basename "$package"`
-  sudo installer -dumplog -package `basename "$package"` -target /
-}
-
-# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
-# of available packages, where PLATFORM is `osx` or `win`
-
-install "MacEditorInstaller/Unity-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
+echo 'Installing Unity.pkg'
+sudo installer -dumplog -package Unity.pkg -target /
