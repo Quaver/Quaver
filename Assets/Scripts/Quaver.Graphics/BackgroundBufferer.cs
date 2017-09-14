07dbb10b2e319d4bc3d46772a627e12188fc2714
staravia <denysenesouma@gmail.com>
2017-09-13 18:46:35 -0700

    Update: Background Dim + Background Buffer
    Former-commit-id: 55b63444ad6c1922caa5fd775c87249c14cecbd5 [formerly 929934eba3830b50e98036f6959843f3695809a8]
    Former-commit-id: 527359541f0e7dbef3d06cef3e39da97ebe7f028

--- Assets/Scripts/Quaver.Graphics/BackgroundBufferer.cs
+++ Assets/Scripts/Quaver.Graphics/BackgroundBufferer.cs
@@ -1,22 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
 using UnityEngine;
+using Quaver.Cache;
+using UnityEngine.UI;
 
 namespace Quaver.Graphics
 {
     public class BackgroundBufferer : MonoBehaviour
     {
 
-        // Use this for initialization
-        void Start()
+        private bool spriteMode;
+        private string bufferPath;
+        private RawImage bufferTexture;
+        private GameObject bufferSprite;
+
+        //Initialization
+        public void init(bool spriteBuffer, string bgPath, RawImage bgTexture, GameObject bgSprite = null)
         {
+            spriteMode = spriteBuffer;
+            bufferPath = bgPath;
+            bufferTexture = bgTexture;
+            bufferSprite = bgSprite;
+            StartCoroutine(BufferImage());
 
         }
 
-        // Update is called once per frame
-        void Update()
+        //Buffer Image async
+        private IEnumerator BufferImage()
         {
+            
+            if (spriteMode && bufferSprite != null)
+            {
+                //Create buffer image
+                Sprite sprite = new Sprite();
+                Texture2D texture = new Texture2D(64, 64, TextureFormat.ARGB32, true);
+                WWW data = new WWW(bufferPath);
 
+                //Wait before loading image onto unity as texture
+                yield return data;
+                yield return new WaitForSeconds(0.5f);
+
+                //Apply image to texture
+                data.LoadImageIntoTexture(texture);
+
+                //Apply texture to bg + resize bg if bg isnt null
+                if (bufferSprite != null)
+                {
+                    sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+                    bufferSprite.GetComponent<SpriteRenderer>().sprite = sprite;
+                    bufferSprite.GetComponent<BackgroundDimAnimator>().dim = bufferSprite.GetComponent<BackgroundDimAnimator>().Manager.SelectScreenDim;
+
+                    if (sprite.rect.size.y / sprite.rect.size.x >= Screen.width / Screen.height)
+                        bufferSprite.transform.localScale = Vector3.one * (20f * (100f / (float)sprite.rect.size.y));
+                    else
+                        bufferSprite.transform.localScale = Vector3.one * ((float)Screen.width / (float)Screen.height) * 20f * (100f / (float)sprite.rect.size.x);
+                }
+            }
+            else if (!spriteMode && bufferTexture != null)
+            {
+                //Create buffer image
+                Texture2D texture = new Texture2D(64, 64, TextureFormat.ARGB32, true);
+                WWW data = new WWW(bufferPath);
+
+                //Wait before loading image onto unity as texture
+                yield return data;
+                yield return new WaitForSeconds(0.5f);
+
+                //Apply image texture
+                data.LoadImageIntoTexture(texture);
+
+                //Apply texture to ui
+                if (bufferTexture != null)
+                    bufferTexture.texture = texture;
+            }
+
+            //Removes game object
+            Debug.Log("[Background Buffer] DONE!");
+            Destroy(this.gameObject);
         }
     }
 }
