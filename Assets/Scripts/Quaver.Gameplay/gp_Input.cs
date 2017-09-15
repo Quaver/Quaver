eefb8a9750291c45598cb32f65a8a3f1aaa8f169
staravia <denysenesouma@gmail.com>
2017-09-14 22:36:47 -0700

    Update: Gameplay Organization

---
+++ Assets/Scripts/Quaver.Gameplay/gp_Input.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Quaver.Gameplay
+{
+    public partial class NoteRendering
+    {
+        private void input_CheckInput()
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                if (!_keyDown[k])
+                {
+                    if (Input.GetKeyDown(_config_KeyBindings[k]))
+                    {
+                        skin_NoteDown(k);
+                        input_JudgeNote(k + 1, _curSongTime);
+                    }
+                }
+                else
+                {
+                    if (Input.GetKeyUp(_config_KeyBindings[k]))
+                    {
+                        skin_NoteUp(k);
+                        input_JudgeLN(k + 1, _curSongTime);
+                    }
+                }
+            }
+        }
+
+        // Check if LN is released on time or early
+        private void input_JudgeLN(int kkey, float timePos)
+        {
+            int curNote = -1; //Cannot create null struct :(
+            float closestTime = 1000f;
+            for (int i = 0; i < _lnQueue.Count; i++)
+            {
+                if (_lnQueue[i].KeyLane == kkey)
+                {
+                    closestTime = timePos - _lnQueue[i].EndTime;
+                    curNote = i;
+                }
+            }
+            if (curNote >= 0 && curNote < _lnQueue.Count)
+            {
+                if (closestTime < -_judgeTimes[5])
+                {
+                    //Darkens early/mis-released LNs. Use skin images instead later
+
+                    NoteObject newNote = new NoteObject();
+
+                    newNote.StartTime = (int)_curSongTime;
+                    newNote.EndTime = _lnQueue[curNote].EndTime;
+                    newNote.KeyLane = _lnQueue[curNote].KeyLane;
+
+                    newNote.HitSet = _lnQueue[curNote].HitSet;
+                    newNote.HitSprite = _lnQueue[curNote].HitSprite;
+                    newNote.SliderEndSprite = _lnQueue[curNote].SliderEndSprite;
+                    newNote.SliderMiddleSprite = _lnQueue[curNote].SliderMiddleSprite;
+                    newNote.SliderEndObject = _lnQueue[curNote].SliderEndObject;
+                    newNote.SliderMiddleObject = _lnQueue[curNote].SliderMiddleObject;
+
+                    newNote.HitSprite.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+                    newNote.SliderMiddleSprite.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+                    newNote.SliderEndSprite.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+
+                    _offLNQueue.Add(newNote);
+                    _lnQueue.RemoveAt(curNote);
+                    print("[Note Render] EARLY LN RELEASE");
+                }
+                else if (closestTime > -_judgeTimes[5] && closestTime < _judgeTimes[5])
+                {
+                    np_RemoveNote(_lnQueue[curNote].HitSet); ;
+                    _lnQueue.RemoveAt(curNote);
+                    print("[Note Render] PERFECT LN RELEASE");
+                }
+            }
+        }
+        
+        //Check if note is hit on time/late/early
+        private void input_JudgeNote(int kkey, float timePos)
+        {
+            if (_hitQueue[kkey - 1].Count > 0 && _hitQueue[kkey - 1][0].StartTime - _curSongTime < _judgeTimes[4])
+            {
+                float closestTime = _hitQueue[kkey - 1][0].StartTime - _curSongTime;
+                float absTime = Mathf.Abs(closestTime);
+                if (absTime < _judgeTimes[4])
+                {
+                    if (absTime < _judgeTimes[0])
+                    {
+                        print("[Note Render] MARV");
+                    }
+                    else if (absTime < _judgeTimes[1])
+                    {
+                        print("[Note Render] PERF");
+                    }
+                    else if (absTime < _judgeTimes[2])
+                    {
+                        print("[Note Render] GREAT");
+                    }
+                    else if (absTime < _judgeTimes[3])
+                    {
+                        print("[Note Render] GOOD");
+                    }
+                    else
+                    {
+                        print("[Note Render] BAD");
+                    }
+                    //Check if LN
+                    if (_hitQueue[kkey - 1][0].EndTime > 0)
+                    {
+                        _lnQueue.Add(_hitQueue[kkey - 1][0]);
+                        _hitQueue[kkey - 1].RemoveAt(0);
+                    }
+                    else
+                    {
+                        np_RemoveNote(_hitQueue[kkey - 1][0].HitSet);
+                        _hitQueue[kkey - 1].RemoveAt(0);
+                    }
+
+                    skin_NoteBurst(kkey);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Quaver.Gameplay/gp_Skinning.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c)  Swan. All rights reserved.  
+// See the Copyright notice in the root of the project.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quaver.Graphics;
+
+namespace Quaver.Gameplay
+{
+    public partial class NoteRendering
+    {
+        /*SKIN.INI VALUES*/
+        private const int skin_bgMaskBufferSize = 12; //Adds a buffer space to the bg mask (+8 pixels wide)
+        private const int skin_noteBufferSpacing = 0; //Spaces notes 0 pixels a part
+        private const int skin_timingBarPixelSize = 2;
+        private const float skin_hitLightingScale = 4.0f; //Sets the scale of the hit lighting (relative to units)
+        private const int skin_columnSize = 250;
+        private const int skin_receptorYOffset = 50; //Sets the receptor's y position offset, relative to the edge of the screen.
+                                                     //private float[] skin_receptorRotations = new float[4] { -90f, 0f, 180f, 90f }; //Rotation of arrows if arrow skin is used
+        private float[] _skin_receptorRotations = new float[4] { 0, 0, 0, 0 }; //Rotation of arrows if arrow skin is used
+
+        //Other skinning variables
+        private float _receptorYPos;
+        private float[] _receptorXPos, _receptorXOffset, _receptorSize;
+
+        //Skinning GameObject References (Temp)
+        public Sprite[] receptorSprite;
+        public GameObject NoteHitParticle;
+        public GameObject circleParticleSystem;
+        public GameObject timingBar;
+
+        //Initialize Skin component
+        private void skin_init()
+        {
+            //Declare Receptor Variables
+            int i = 0;
+            _receptorXPos = new float[4];
+            _receptorXOffset = new float[4];
+            _receptorSize = new float[4];
+
+            //Set Receptor Variables
+            if (_config_upScroll) _scrollNegativeFactor = -1f;
+            _receptorYPos = _scrollNegativeFactor * (skin_columnSize / 256f + (float)skin_receptorYOffset / 100f - 10f);
+            _receptors = new GameObject[4];
+            _hitLighting = new GameObject[4];
+
+            //Create Gameobjects for each receptor
+            for (i = 0; i < 4; i++)
+            {
+                //Create receptor + xpos
+                _receptors[i] = receptorBar.transform.Find("R" + (i + 1)).gameObject;
+                _receptorXPos[i] = (i - 1.5f) * ((skin_columnSize + (float)skin_noteBufferSpacing) / config_PixelUnitSize);
+
+                //Set receptor pos
+                if (i >= 2 && mod_split) _receptors[i].transform.localPosition = new Vector3(_receptorXPos[i], -_receptorYPos, 1);
+                else _receptors[i].transform.localPosition = new Vector3(_receptorXPos[i], _receptorYPos, 1);
+
+                //Scale + rotate receptors
+                _receptors[i].transform.localScale = Vector3.one * (skin_columnSize / (float)_receptors[i].transform.GetComponent<SpriteRenderer>().sprite.rect.size.x);
+                _receptors[i].transform.transform.eulerAngles = new Vector3(0, 0, _skin_receptorRotations[i]);
+
+                //Create hitlighting for receptors
+                _hitLighting[i] = lightingBar.transform.Find("L" + (i + 1)).gameObject;
+                _hitLighting[i].transform.localScale = new Vector3(_receptors[i].transform.localScale.x,_scrollNegativeFactor * skin_hitLightingScale, 1f);
+                _hitLighting[i].transform.localPosition = _receptors[i].transform.localPosition + new Vector3(0, _hitLighting[i].transform.localScale.y * 2f, 1f);
+            }
+
+            //Set bg Mask Scale
+            bgMask.transform.localScale = new Vector3(
+                ((float)(skin_columnSize + skin_bgMaskBufferSize + skin_noteBufferSpacing) / config_PixelUnitSize) * 4f * (config_PixelUnitSize / (float)bgMask.transform.GetComponent<SpriteRenderer>().sprite.rect.size.x),
+                20f * (config_PixelUnitSize / (float)bgMask.transform.GetComponent<SpriteRenderer>().sprite.rect.size.y) , 1f);
+        }
+
+        private void skin_NoteDown(int kkey)
+        {
+            _keyDown[kkey] = true;
+            _receptors[kkey].transform.localScale = Vector3.one * (skin_columnSize / config_PixelUnitSize) * 1.1f;
+            _receptors[kkey].transform.GetComponent<SpriteRenderer>().sprite = receptorSprite[1];
+            _hitLighting[kkey].SetActive(true); //temp
+        }
+
+        private void skin_NoteUp(int kkey)
+        {
+            _keyDown[kkey] = false;
+            _receptors[kkey].transform.GetComponent<SpriteRenderer>().sprite = receptorSprite[0];
+            _hitLighting[kkey].SetActive(false); //temp
+        }
+
+        private void skin_NoteBurst(int kkey)
+        {
+            //Create particles
+            GameObject cp = Instantiate(circleParticleSystem, particleContainer.transform);
+            cp.transform.localPosition = _receptors[kkey - 1].transform.localPosition + new Vector3(0, 0, 4f);
+
+            GameObject hb = Instantiate(NoteHitParticle, particleContainer.transform);
+            hb.transform.localPosition = _receptors[kkey - 1].transform.localPosition + new Vector3(0, 0, -2f);
+            hb.transform.eulerAngles = _receptors[kkey - 1].transform.eulerAngles;
+            hb.transform.localScale = _receptors[kkey - 1].transform.localScale;
+            hb.GetComponent<NoteBurst>().startSize = hb.transform.localScale.x - 0.05f;
+            hb.GetComponent<NoteBurst>().burstSize = 0.1f;
+            hb.GetComponent<NoteBurst>().burstLength = 0.35f;
+        }
+
+    }
+}
