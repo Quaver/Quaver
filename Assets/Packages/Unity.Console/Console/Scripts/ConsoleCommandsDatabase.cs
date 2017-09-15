1715422dd6a7e60efc3a17fea9ddb832c87d5c5c
Swan <contact@swan.moe>
2017-09-14 21:13:54 -0400

    Add: Copyright Notice to all scripts

--- Assets/Packages/Unity.Console/Console/Scripts/ConsoleCommandsDatabase.cs
+++ Assets/Packages/Unity.Console/Console/Scripts/ConsoleCommandsDatabase.cs
@@ -1,6 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+﻿// Copyright (c)  Swan. All rights reserved.  
+// See the Copyright notice in the root of the project.
 
 using System;
 using System.Collections.Generic;
