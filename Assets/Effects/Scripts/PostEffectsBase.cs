61784a67cf60d1f3470f91501b904371c1558ffb
staravia <denysenesouma@gmail.com>
2017-09-15 20:13:51 -0700

    Update: PostEffectBase.cs
    Former-commit-id: eb0799473600b8b7d23137316262970d5eb17870 [formerly ec886487f0ac432f63dfd7ea3275b399460114cc]
    Former-commit-id: 4e8b58a1a018228a45c23b99f5288d9bae906c2e

--- Assets/Effects/Scripts/PostEffectsBase.cs
+++ Assets/Effects/Scripts/PostEffectsBase.cs
@@ -1,4 +1,3 @@
-
 using System;
 using UnityEngine;
 
@@ -76,13 +75,11 @@ namespace UnityStandardAssets.ImageEffects
             return CheckSupport(false);
         }
 
-
         public virtual bool CheckResources()
         {
             Debug.LogWarning("CheckResources () for " + ToString() + " should be overwritten.");
             return isSupported;
         }
-
 
         protected void Start()
         {
@@ -127,7 +124,6 @@ namespace UnityStandardAssets.ImageEffects
             return true;
         }
 
-
         public bool Dx11Support()
         {
             return supportDX11;
@@ -148,12 +144,8 @@ namespace UnityStandardAssets.ImageEffects
                 NotSupported();
                 return false;
             }
-            else
-            {
-                return false;
-            }
+            else return false;
         }
-
 
         protected void NotSupported()
         {
@@ -162,14 +154,8 @@ namespace UnityStandardAssets.ImageEffects
             return;
         }
 
-
         protected void DrawBorder(RenderTexture dest, Material material)
         {
-            float x1;
-            float x2;
-            float y1;
-            float y2;
-
             RenderTexture.active = dest;
             bool invertY = true; // source.texelSize.y < 0.0ff;
             // Set up the simple Matrix
@@ -180,60 +166,30 @@ namespace UnityStandardAssets.ImageEffects
             {
                 material.SetPass(i);
 
-                float y1_; float y2_;
-                if (invertY)
-                {
-                    y1_ = 1.0f; y2_ = 0.0f;
-                }
-                else
-                {
-                    y1_ = 0.0f; y2_ = 1.0f;
-                }
+                float y1_;
+                if (invertY) y1_ = 1f;
+                else y1_ = 0f;
 
                 // left
-                x1 = 0.0f;
-                x2 = 0.0f + 1.0f / (dest.width * 1.0f);
-                y1 = 0.0f;
-                y2 = 1.0f;
+
+                float[] x1 = new float[4] { 0, 1f - (1f / (dest.width * 1f)), 0, 0 };
+                float[] x2 = new float[4] { 1.0f / (float)dest.width, 1f, 1f, 1f };
+                float[] y1 = new float[4] { 0, 0, 0, 1f - 1.0f / (float)dest.height };
+                float[] y2 = new float[4] { 1f, 1f, 1f / (float)dest.height, 1f };
+
                 GL.Begin(GL.QUADS);
 
-                GL.TexCoord2(0.0f, y1_); GL.Vertex3(x1, y1, 0.1f);
-                GL.TexCoord2(1.0f, y1_); GL.Vertex3(x2, y1, 0.1f);
-                GL.TexCoord2(1.0f, y2_); GL.Vertex3(x2, y2, 0.1f);
-                GL.TexCoord2(0.0f, y2_); GL.Vertex3(x1, y2, 0.1f);
-
-                // right
-                x1 = 1.0f - 1.0f / (dest.width * 1.0f);
-                x2 = 1.0f;
-                y1 = 0.0f;
-                y2 = 1.0f;
-
-                GL.TexCoord2(0.0f, y1_); GL.Vertex3(x1, y1, 0.1f);
-                GL.TexCoord2(1.0f, y1_); GL.Vertex3(x2, y1, 0.1f);
-                GL.TexCoord2(1.0f, y2_); GL.Vertex3(x2, y2, 0.1f);
-                GL.TexCoord2(0.0f, y2_); GL.Vertex3(x1, y2, 0.1f);
-
-                // top
-                x1 = 0.0f;
-                x2 = 1.0f;
-                y1 = 0.0f;
-                y2 = 0.0f + 1.0f / (dest.height * 1.0f);
-
-                GL.TexCoord2(0.0f, y1_); GL.Vertex3(x1, y1, 0.1f);
-                GL.TexCoord2(1.0f, y1_); GL.Vertex3(x2, y1, 0.1f);
-                GL.TexCoord2(1.0f, y2_); GL.Vertex3(x2, y2, 0.1f);
-                GL.TexCoord2(0.0f, y2_); GL.Vertex3(x1, y2, 0.1f);
-
-                // bottom
-                x1 = 0.0f;
-                x2 = 1.0f;
-                y1 = 1.0f - 1.0f / (dest.height * 1.0f);
-                y2 = 1.0f;
-
-                GL.TexCoord2(0.0f, y1_); GL.Vertex3(x1, y1, 0.1f);
-                GL.TexCoord2(1.0f, y1_); GL.Vertex3(x2, y1, 0.1f);
-                GL.TexCoord2(1.0f, y2_); GL.Vertex3(x2, y2, 0.1f);
-                GL.TexCoord2(0.0f, y2_); GL.Vertex3(x1, y2, 0.1f);
+                for (int j = 0; j < 4; i++)
+                {
+                    GL.TexCoord2(0.0f, y1_);
+                    GL.Vertex3(x1[j], y1[j], 0.1f);
+                    GL.TexCoord2(1.0f, y1_);
+                    GL.Vertex3(x2[j], y1[j], 0.1f);
+                    GL.TexCoord2(1.0f, 1f - y1_);
+                    GL.Vertex3(x2[j], y2[j], 0.1f);
+                    GL.TexCoord2(0.0f, 1f - y1_);
+                    GL.Vertex3(x1[j], y2[j], 0.1f);
+                }
 
                 GL.End();
             }
