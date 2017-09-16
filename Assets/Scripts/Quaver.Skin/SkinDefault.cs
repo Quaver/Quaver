6fe699785aaf73525027f90a6fd395e204dffa0b
Swan <contact@swan.moe>
2017-09-15 21:05:57 -0400

    Add: The rest of the values to the SkinDefault clas
    Former-commit-id: 01d9c60d0dfc12affc00a4884e62bea3d0182d9a [formerly a9af9352988605f87ed3b7f5c7172a5a07cbd95e]
    Former-commit-id: 1a44d83d3a25be700e0c64f8e947a0d97ded02e1

--- Assets/Scripts/Quaver.Skin/SkinDefault.cs
+++ Assets/Scripts/Quaver.Skin/SkinDefault.cs
@@ -10,7 +10,35 @@ namespace Quaver.Skin
     public static class SkinDefault
     {
         public static string Name = "Default";
-        public static string Author = "Swan";
+        public static string Author = "Staravia";
         public static string Version = "1.0.0";
+
+         // [Menu]
+        public static bool CustomBackground = false;
+
+        // [Cursor]
+        public static bool CursorRotate = true;
+        public static bool CursorTrailRotate = false;
+        public static bool CursorExpand = false;
+        public static bool CursorCentre = true;
+
+        // [Gameplay]
+        public static int BgMaskBufferSize = 12;
+        public static int NoteBufferSpacing = 0;
+        public static int TimingBarPixelSize = 2;
+        public static float HitLightingScale = 4.0f;
+        public static int ColumnSize = 250;
+        public static int ReceptorYOffset = 50;
+
+        // [Colours]
+        public static Color ColourLight1;
+        public static Color ColourLight2;
+        public static Color ColourLight3;
+        public static Color ColourLight4;
+        public static Color Colour1;
+        public static Color Colour2;
+        public static Color Colour3;
+        public static Color Colour4;
+        public static Color ColourHold;       
     }
 }
