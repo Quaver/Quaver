97fcc94c4ba1d1d15ae6300c0df3cb436e2def26
staravia <denysenesouma@gmail.com>
2017-09-15 20:30:23 -0700

    Update: FPSCounter.cs again
    Former-commit-id: ddeb8ce66c6f54bfb7d43542c8415faa91b57ee1 [formerly 2cdf9cb4d996a8f989507bee9eb39d0bc1ba1294]
    Former-commit-id: 1bc164765e5cd5aaaf6875e107490b7df11ce0ca

--- Assets/Scripts/Quaver.UI/FPSCounter.cs
+++ Assets/Scripts/Quaver.UI/FPSCounter.cs
@@ -22,8 +22,7 @@ namespace Quaver.UI
             _latencyTextTween += ((500f * Time.deltaTime) - _latencyTextTween) / 100f;
             _fpsTextWeen += ((1f / Time.deltaTime) - _fpsTextWeen) / 100f;
             FpsText.text = (Mathf.Round(_fpsTextWeen * 10f) / 10f) + " fps";
-            LatencyText.text = "\u00B1" + Mathf.Round(_latencyTextTween * 100f) / 100f + " ms";
+            LatencyText.text = "\u00B1" + (Mathf.Round(_latencyTextTween * 100f) / 100f) + " ms";
         }
     }
 }
-
