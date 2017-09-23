db7c11bc4d2fd3618ef6295cd2ce1785b7a36026
Swan <contact@swan.moe>
2017-09-23 19:22:35 -0400

    Add: ThreadWrapper utility class
    Former-commit-id: 56fcb1fc4d0c7e4bccb5a0ef57efd9a0ec6ad18b [formerly 94b75b1170325f6c250df5f1a1712b51b8e3db0a]
    Former-commit-id: 87d8b9e614b41922183c58c0522ee16d340f5c8c

---
+++ Assets/Scripts/Quaver.Utils/ThreadWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using Quaver.Qua;
+
+namespace Quaver.Utils
+{
+    public class ThreadWrapper
+    {
+        public static QuaFile ParseQuaInNewThread(string filePath)
+        {
+            QuaFile parsedMap = new QuaFile();
+
+            Thread newThread = new Thread(() => { parsedMap = QuaParser.Parse(filePath); });
+            newThread.Start();
+            newThread.Join();
+
+            return parsedMap;
+        }
+    }
+}
