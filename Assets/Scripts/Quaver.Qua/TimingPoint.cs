7cad728838ca3c6fc1bc7ded297e94f89961c695
Swan <contact@swan.moe>
2017-09-24 20:31:23 -0400

    Add: XML Documentation to TimingPoint
    Former-commit-id: ebe8daeacbfb01e0f280529e88ec67a56c411cf5 [formerly 080322ff20b312cb89a80a97a4af5048f1155006]
    Former-commit-id: 0d8da95fe27bb32d0e53b429941885ab62af0c2e

--- Assets/Scripts/Quaver.Qua/TimingPoint.cs
+++ Assets/Scripts/Quaver.Qua/TimingPoint.cs
@@ -7,7 +7,14 @@ namespace Quaver.Qua
 {
     public struct TimingPoint
     {
+        /// <summary>
+        /// The offset in the song where the Timing Point begins.
+        /// </summary>
         public float StartTime;
+
+        /// <summary>
+        /// The BPM of the particular timing point
+        /// </summary>
         public float BPM;
     }
 }
