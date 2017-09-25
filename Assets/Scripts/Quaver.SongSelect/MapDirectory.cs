307100e8158e5ec847285b23274ace2ae60e5fc4
Swan <contact@swan.moe>
2017-09-24 20:46:32 -0400

    Add: XML Documentation to MapDirectory
    Former-commit-id: 81c2a01bb4bf2dd64d641e3e8cac8597f2a7b74c [formerly 00f2b5ac1a54edd2290f278cfcbb7adf394955ed]
    Former-commit-id: 8bb0f4e1a36acfb55490fb0ed1061e83ff70eb42

--- Assets/Scripts/Quaver.SongSelect/MapDirectory.cs
+++ Assets/Scripts/Quaver.SongSelect/MapDirectory.cs
@@ -7,7 +7,14 @@ namespace Quaver.SongSelect
 {
     public struct MapDirectory
     {
+        /// <summary>
+        /// The path of the directory that holds the beatmaps.
+        /// </summary>
         public string Directory;
+
+        /// <summary>
+        /// The list of cached beatmaps inside of this directory.
+        /// </summary>
         public List<CachedBeatmap> Beatmaps;
     }
 }
