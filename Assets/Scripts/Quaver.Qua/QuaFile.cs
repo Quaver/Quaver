1a7b1e60145e9e75ec380581ba9df3b3591a1be1
Swan <contact@swan.moe>
2017-09-24 20:28:51 -0400

    Add: XML Documentation to QuaFile
    Former-commit-id: 539e2e34cb546dbe91fc9f1bdc503be151af6fd1 [formerly 8b30b557a58ca9303d08d7adfe1f9eb22f9cb760]
    Former-commit-id: 0eb9ae64f8dbca76f0a3fa0c99ed081be50d347b

--- Assets/Scripts/Quaver.Qua/QuaFile.cs
+++ Assets/Scripts/Quaver.Qua/QuaFile.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,37 +6,115 @@ namespace Quaver.Qua
 {
     public class QuaFile
     {
-        // # General
+        // <--- #General --->
+        /// <summary>
+        /// Keeps track of whether or not the data in the QuaFile object is valid.
+        /// </summary>
         public bool IsValidQua;
+
+        /// <summary>
+        ///  The name of the audio file (.ogg)
+        /// </summary>
         public string AudioFile;
+
+        /// <summary>
+        /// The amount of time before the audio starts.
+        /// </summary>
         public int AudioLeadIn;
+
+        /// <summary>
+        /// The offset of the song which will be used to play a preview during song select
+        /// </summary>
         public int SongPreviewTime;
+
+        /// <summary>
+        /// The file name of the song background.
+        /// </summary>
         public string BackgroundFile;
-
-        // # Metadata
+        // <--- #General End --->
+        
+        // <--- #Metadata --->
+        /// <summary>
+        /// The title of the song.
+        /// </summary>
         public string Title;
+
+        /// <summary>
+        /// The unicode title of the song.
+        /// </summary>
         public string TitleUnicode;
+
+        /// <summary>
+        /// The artist of the song.
+        /// </summary>
         public string Artist;
+
+        /// <summary>
+        /// The unicode artist of the song.
+        /// </summary>
         public string ArtistUnicode;
+
+        /// <summary>
+        /// The source of the song (Album, Mixtape, etc.)
+        /// </summary>
         public string Source;
+
+        /// <summary>
+        /// Specific tags for the song (Used when searching)
+        /// </summary>
         public string Tags;
+
+        /// <summary>
+        /// The creator of the beatmap
+        /// </summary>
         public string Creator;
+
+        /// <summary>
+        /// The name of the difficulty for the beatmap
+        /// </summary>
         public string DifficultyName;
+
+        /// <summary>
+        /// The map's unique identifier if it was uploaded.
+        /// </summary>
         public int MapID;
+
+        /// <summary>
+        /// The mapset's unique identifier if it was uploaded.
+        /// </summary>
         public int MapSetID;
+
+        /// <summary>
+        /// A user defined description of the map.
+        /// </summary>
         public string Description;
-
-        // Difficulty
+        // <--- #Metadata End -->
+        
+        // <--- #Difficulty -->
+        /// <summary>
+        /// The amount of hitpoints drain on the map.
+        /// </summary>
         public float HPDrain;
+
+        /// <summary>
+        /// The difficulty of accuracy for the map.
+        /// </summary>
         public float AccuracyStrain;
-
-        // Timing
+        // <--- #Difficulty End -->
+        
+        /// <summary>
+        /// All of the map's timing sections.
+        /// </summary>
         public List<TimingPoint> TimingPoints;
-
-        // SV
+       
+        /// <summary>
+        /// The points in the map where the SV changes.
+        /// </summary>
         public List<SliderVelocity> SliderVelocities;
 
-        // HitObjects
+        /// <summary>
+        /// The physical hit objects in the map.
+        /// </summary>
         public List<HitObject> HitObjects;
     
         // Constructor
