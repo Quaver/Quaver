2e95a0854b611251933b92fdc984c1738aab52eb
Swan <contact@swan.moe>
2017-09-24 20:29:52 -0400

    Add: XML Documentation to HitObject
    Former-commit-id: 4b92893111f634c06f4a56fb9789e5b4853d7b53 [formerly 4ee9da000d4a14a2e5baa428f593d98d0f9fdb06]
    Former-commit-id: 4507fac37194e97011d400c24d27712b13ea3aaf

--- Assets/Scripts/Quaver.Qua/HitObject.cs
+++ Assets/Scripts/Quaver.Qua/HitObject.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,8 +6,19 @@ namespace Quaver.Qua
 {
     public struct HitObject
     {
+        /// <summary>
+        /// The offset of in the song where the hitobject is supposed to be hit
+        /// </summary>
         public int StartTime;
+
+        /// <summary>
+        /// The lane in which the key is in.
+        /// </summary>
         public int KeyLane;
+
+        /// <summary>
+        /// The end time of the key (if > 0, it's an LN)
+        /// </summary>
         public int EndTime;
     }
 }
