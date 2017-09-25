a530742d8ff427729684fb352d72714372eaca4d
Swan <contact@swan.moe>
2017-09-24 20:52:13 -0400

    Add: XML Documentation to OrderMapsBy
    Former-commit-id: 06dfe79963dd9d0607b84692801f79cb619dc9f8 [formerly 8472a0feaa15b04ce41369f53cb853c1340cbab1]
    Former-commit-id: 1f3db6c84fd80844f0dc40086fbd8053c131e461

--- Assets/Scripts/Quaver.SongSelect/OrderMapsBy.cs
+++ Assets/Scripts/Quaver.SongSelect/OrderMapsBy.cs
@@ -8,9 +8,13 @@ namespace Quaver.SongSelect
 {
     public class OrderMapsBy
     {
-        // Responsible for sorting maps by their directory and returning a list of directories.
-        // This essentially will be useful for song select, when the user is grouping by directory.
-        // This will be the default option.
+        /// <summary>
+        /// Responsible for sorting maps by their directory and returning a list of directories.
+        /// This essentially will be useful for song select, when the user is grouping by directory.
+        /// This will be the default option.
+        /// </summary>
+        /// <param name="loadedBeatmaps">The list of beatmaps that were loaded</param>
+        /// <returns>A sorted list of MapDirectory</returns>
         public static List<MapDirectory> Directory(List<CachedBeatmap> loadedBeatmaps)
         {
             // Sort loaded beatmaps into directory
@@ -54,21 +58,33 @@ namespace Quaver.SongSelect
             return sortedMaps;
         }
 
-        // This will take an existing MapDirectory and return a new one with the maps ordered by Artist
-        // However, this will still order the maps in alphabetical order as well!
+        /// <summary>
+        /// This will take an existing MapDirectory and return a new one with the maps ordered by Artist
+        /// However, this will still order the maps in alphabetical order as well!
+        /// </summary>
+        /// <param name="beatmapSets">The list of MapDirectory we want to order by Artist.</param>
+        /// <returns>The ordered List of MapDirectory</returns>
         public static List<MapDirectory> Artist(List<MapDirectory> beatmapSets)
         {
             return beatmapSets.OrderByDescending(set => set.Beatmaps[0].Artist).ThenByDescending(set => set.Beatmaps[0].Title).ToList();
         }
 
-        // This will take an existing MapDirectory and return a new one with the maps ordered by Title
+        /// <summary>
+        /// This will take an existing MapDirectory and return a new one with the maps ordered by Title
+        /// </summary>
+        /// <param name="beatmapSets">The list of MapDirectory you want to order</param>
+        /// <returns>The sorted List of MapDirectory</returns>
         public static List<MapDirectory> Title(List<MapDirectory> beatmapSets)
         {
             return beatmapSets.OrderByDescending(set => set.Beatmaps[0].Title).ToList();
         }
 
-        // This will take an existing MapDirectory and return a new one with the maps ordered by Creator
-        // but also orders the maps in alphabetical order by title as well!
+        /// <summary>
+        /// This will take an existing MapDirectory and return a new one with the maps ordered by Creator
+        /// but also orders the maps in alphabetical order by title as well!
+        /// </summary>
+        /// <param name="beatmapSets">The list of MapDirectory you want to order.</param>
+        /// <returns>The sorted list of MapDirectory</returns>
         public static List<MapDirectory> Creator(List<MapDirectory> beatmapSets)
         {
             return OrderMapsBy.Title(beatmapSets.OrderByDescending(set => set.Beatmaps[0].Creator).ToList());
