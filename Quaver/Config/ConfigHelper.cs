e264542f177ddc5801f1b13aea22b6e090c4ab74
Swan <contact@swan.moe>
2017-10-02 17:08:16 -0400

    Initial Commit
    Former-commit-id: f46562094f1d0179905bb0545d53b58733e74ef2 [formerly 6d0deb2a48d99232062332e597f96137e242c1b3]
    Former-commit-id: 08c602be4bb61524e22f6f02e3ef4b486ee4ae85

---
+++ Quaver/Config/ConfigHelper.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Config
+{
+    /// <summary>
+    /// Helper class for any methods that are used for reading config files.
+    /// </summary>
+    public class ConfigHelper
+    {
+
+        /// <summary>
+        ///     Reads a string and checks if it's a valid directory.
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        internal static string ReadDirectory(string defaultDir, string newDir)
+        {
+            // If the config specified directory already exists, then we'll return 
+            // the new directory's value, otherwise we'll make sure a default directory 
+            // is created and return the original default value.
+            if (Directory.Exists(newDir))
+                return newDir;
+
+            Directory.CreateDirectory(defaultDir);
+            return defaultDir;
+        }
+
+        /// <summary>
+        ///     Purely responsible for reading percentage data. We don't want these values
+        ///     to be greater than 100.
+        /// </summary>
+        /// <param name="defaultVal"></param>
+        /// <param name="newVal"></param>
+        /// <returns></returns>
+        internal static byte ReadPercentage(byte defaultVal, string newVal)
+        {
+            // Try to parse the byte value, if the value is greater than 100 (%),
+            // return the default percentage.
+            try
+            {
+                var newPercentage = byte.Parse(newVal);
+                return newPercentage > 100 ? defaultVal : newPercentage;
+            }
+            catch (Exception e)
+            {
+                return defaultVal;
+            }
+        }
+
+        /// <summary>
+        ///     Responsible for reading any Int32
+        /// </summary>
+        /// <param name="defaultVal"></param>
+        /// <param name="newVal"></param>
+        /// <returns></returns>
+        internal static int ReadInt32(int defaultVal, string newVal)
+        {
+            try
+            {
+                return int.Parse(newVal);
+            }
+            catch (Exception e)
+            {
+                return defaultVal;
+            }
+        }
+
+        /// <summary>
+        ///     Responsible for reading boolean values from the config file.
+        /// </summary>
+        /// <param name="defaultVal"></param>
+        /// <param name="newVal"></param>
+        /// <returns></returns>
+        internal static bool ReadBool(bool defaultVal, string newVal)
+        {
+            try
+            {
+                return bool.Parse(newVal);
+            }
+            catch (Exception e)
+            {
+                return defaultVal;
+            }
+        }
+
+        /// <summary>
+        ///     Reads string values from the config file
+        ///     TODO: Change for Language!, Do validation here.
+        /// </summary>
+        /// <param name="defaultVal"></param>
+        /// <param name="newVal"></param>
+        /// <returns></returns>
+        internal static string ReadString(string defaultVal, string newVal)
+        {
+            return string.IsNullOrWhiteSpace(newVal) ? defaultVal : newVal;
+        }
+
+        /// <summary>
+        ///     Reads Int16 values from the config file.
+        /// </summary>
+        /// <param name="defaultVal"></param>
+        /// <param name="newVal"></param>
+        /// <returns></returns>
+        internal static short ReadInt16(short defaultVal, string newVal)
+        {
+            try
+            {
+                return short.Parse(newVal);
+            }
+            catch (Exception e)
+            {
+                return defaultVal;
+            }
+        }
+
+        /// <summary>
+        ///     Reads the skin value from the config file.
+        /// </summary>
+        /// <param name="defaultSkin"></param>
+        /// <param name="newVal"></param>
+        /// <returns></returns>
+        internal static string ReadSkin(string defaultSkin, string newVal)
+        {
+            return Directory.Exists(Configuration.SkinDirectory + "/" + newVal) ? newVal : defaultSkin;
+        }
+
+        /// <summary>
+        ///     Reads an XNA Key value from a string.
+        /// </summary>
+        /// <param name="defaultKey"></param>
+        /// <param name="newVal"></param>
+        /// <returns></returns>
+        internal static Keys ReadKeys(Keys defaultKey, string newVal)
+        {
+            return Enum.TryParse(newVal, out Keys newKey) ? newKey : defaultKey;
+        }
+    }
+}
---
+++ Quaver/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Quaver.Config;
+using System.Reflection;
+using Microsoft.Xna.Framework.Input;
+
+namespace Quaver
+{
+    /// <summary>
+    ///     The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Configuration.InitializeConfig();
+            // Start game
+            using (var game = new Game1())
+            {
+                game.Run();
+
+            }
+        }
+    }
+}
