78c99e856ba4ee7f33e1aaa0e2beb88e274d5610
Swan <contact@swan.moe>
2017-10-02 19:06:49 -0400

    Add: Preprocessor directive for testing Qua Parsing
    Former-commit-id: afc568627c230821fd2c2b1e7fa0674567a8fc3d [formerly 44270be8accaaca4a8490c55dda995e0e652d89a]
    Former-commit-id: 1edd2494ef5f6c4291a49fd9fe13ae2e3e2ae9f5

--- Quaver/Program.cs
+++ Quaver/Program.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿#define DEBUG
+
+using System;
+using Quaver.Quas;
 using Quaver.Config;
-using System.Reflection;
-using Microsoft.Xna.Framework.Input;
 
 namespace Quaver
 {
@@ -19,11 +18,35 @@ namespace Quaver
         private static void Main()
         {
             Configuration.InitializeConfig();
+
+#if DEBUG
+            // Parsing a new Qua for testing purposes. We've specified a preprocessor directive here, 
+            // so this'll only run in debug mode.
+            var filePath = @"C:\Users\swan\Desktop\Stuff\Git\Quaver2.0\Quaver\Test\Qua\backbeat.qua";
+            var qua = new Qua(filePath, false);
+            Console.WriteLine($"Displaying data for parsed .qua file: {filePath}\n\n" +
+                              $"Artist: {qua.Artist}\n" +
+                              $"Title: {qua.Title}\n" +
+                              $"Source: {qua.Source}\n" +
+                              $"Tags: {qua.Tags}\n" +
+                              $"Creator: {qua.Creator}\n" +
+                              $"DifficultyName {qua.DifficultyName}\n" +
+                              $"MapId: {qua.MapId}\n" +
+                              $"MapSetId: {qua.MapSetId}\n" +
+                              $"AudioFile: {qua.AudioFile}\n" +
+                              $"AudioLeadIn: {qua.AudioLeadIn}\n" +
+                              $"SongPreviewTime: {qua.SongPreviewTime}\n" +
+                              $"BackgroundFile: {qua.BackgroundFile}\n" +
+                              $"HpDrain: {qua.HpDrain}\n" +
+                              $"AccuracyStreain: {qua.AccuracyStrain}\n" +
+                              $"Timing Points: {qua.TimingPoints.Count}\n" +
+                              $"Slider Velocities: {qua.SliderVelocities.Count}\n" +
+                              $"HitObjects: {qua.HitObjects.Count}");
+#endif
             // Start game
             using (var game = new Game1())
             {
                 game.Run();
-
             }
         }
     }
