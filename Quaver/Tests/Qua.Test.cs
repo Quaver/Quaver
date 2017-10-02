abcfd4a100ba5c8b22e4780cfdf1b7d891b30494
Swan <contact@swan.moe>
2017-10-02 19:22:24 -0400

    Move: Parser test code to it's own class
    Former-commit-id: b97f8b06aa7649f8bf027a888315bcf79fed578c [formerly 294559dfd24daaf157f5106a43deab4b1c50b707]
    Former-commit-id: 686b6f662bebe7e5fd17c1906abf575155a8e17b

---
+++ Quaver/Tests/Qua.Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.QuaFile;
+
+namespace Quaver.Tests
+{
+    public static class QuaTest
+    {
+        [Conditional("DEBUG")]
+        public static void ParseQuaTest()
+        {
+            // Parsing a new Qua for testing purposes. We've specified a preprocessor directive here, 
+            // so this'll only run in debug mode.
+            var filePath = @"C:\Users\swan\Desktop\Stuff\Git\Quaver2.0\Quaver\Test\Qua\backbeat.qua";
+            var qua = new Qua(filePath, false);
+            Console.WriteLine($"Displaying data for parsed .qua file: {filePath}\n\n" +
+                              $"Artist: {qua.Artist}\n" +
+                              $"Title: {qua.Title}\n" +
+                              $"Source: {qua.Source}\n" +
+                              $"Tags: {qua.Tags}\n" +
+                              $"Creator: {qua.Creator}\n" +
+                              $"DifficultyName {qua.DifficultyName}\n" +
+                              $"MapId: {qua.MapId}\n" +
+                              $"MapSetId: {qua.MapSetId}\n" +
+                              $"AudioFile: {qua.AudioFile}\n" +
+                              $"AudioLeadIn: {qua.AudioLeadIn}\n" +
+                              $"SongPreviewTime: {qua.SongPreviewTime}\n" +
+                              $"BackgroundFile: {qua.BackgroundFile}\n" +
+                              $"HpDrain: {qua.HpDrain}\n" +
+                              $"AccuracyStreain: {qua.AccuracyStrain}\n" +
+                              $"Timing Points: {qua.TimingPoints.Count}\n" +
+                              $"Slider Velocities: {qua.SliderVelocities.Count}\n" +
+                              $"HitObjects: {qua.HitObjects.Count}");
+        }
+    }
+}
