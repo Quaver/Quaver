3690b6166a0c04cb337201e6e93ed711f914e43f
Swan <contact@swan.moe>
2017-10-02 19:06:17 -0400

    Add: Qua class w/ parser
    Former-commit-id: 54112af179875cf3078a0cbd4696fd74089c52d6 [formerly c69f9524d3ab0b0c9141e02cdbe806580617b773]
    Former-commit-id: 6af2081fed0c71b31bdd77db2c2beec1ba5b79f1

---
+++ Quaver/Quas/HitObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Quas
+{
+    internal struct HitObject
+    {
+        /// <summary>
+        /// The offset of in the song where the hitobject is supposed to be hit
+        /// </summary>
+        internal int StartTime { get; set; }
+
+        /// <summary>
+        /// The lane in which the key is in.
+        /// </summary>
+        internal int KeyLane { get; set; }
+
+        /// <summary>
+        /// The end time of the key (if > 0, it's an LN)
+        /// </summary>
+        internal int EndTime { get; set; }
+    }
+}
---
+++ Quaver/Quas/TimingPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Quas
+{
+    internal struct TimingPoint
+    {
+        /// <summary>
+        /// The offset in the song where the Timing Point begins.
+        /// </summary>
+        internal float StartTime { get; set; }
+
+        /// <summary>
+        /// The BPM of the particular timing point
+        /// </summary>
+        internal float Bpm { get; set; }
+    }
+}
