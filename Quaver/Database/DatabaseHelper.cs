5c9d3f2a320cc7fd050f717b1e389c21845ef468
Swan <contact@swan.moe>
2017-10-04 17:59:20 -0400

    Fix: Only run bm syncer if there are maps to delete
    Former-commit-id: 82e58b94e4ef6f4cfc02c3f7e3a5a3cd09fb5a29 [formerly 810b191f35779ce4b815e54397c19d9ad363dfb6]
    Former-commit-id: 852430a91a7f789c41f551f777afcd6367c24e14

--- Quaver/Database/DatabaseHelper.cs
+++ Quaver/Database/DatabaseHelper.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Quaver.Config;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Quaver.Beatmaps;
+using Quaver.Config;
 using Quaver.QuaFile;
 using SQLite;
 
@@ -15,42 +14,44 @@ namespace Quaver.Database
     internal static class DatabaseHelper
     {
         /// <summary>
-        /// Holds the path of the SQLite database
+        ///     Holds the path of the SQLite database
         /// </summary>
         internal static readonly string databasePath = Configuration.GameDirectory + "/quaver.db";
 
         /// <summary>
-        /// Initializes the beatmap database & create a table named Beatmaps with the Beatmap class properties.
+        ///     Initializes the beatmap database & create a table named Beatmaps with the Beatmap class properties.
         /// </summary>
         internal static async Task InitializeBeatmapDatabaseAsync()
         {
             var conn = new SQLiteAsyncConnection(databasePath);
             await conn.CreateTableAsync<Beatmap>();
             await SyncBeatmapDatabaseAsync();
+            Console.WriteLine("[DATABASE HELPER] Beatmap synchronization completed!");
         }
 
         /// <summary>
-        /// Syncs the beatmap database. Makes sure that only playable beatmaps are in the database, and deletes
-        /// all corrupt ones.
+        ///     Syncs the beatmap database. Makes sure that only playable beatmaps are in the database, and deletes
+        ///     all corrupt ones.
         /// </summary>
         internal static async Task SyncBeatmapDatabaseAsync()
         {
             // Find all .qua files in the songs folder.
-            string[] files = Directory.GetFiles(Configuration.SongDirectory, "*.qua", SearchOption.AllDirectories);
+            var files = Directory.GetFiles(Configuration.SongDirectory, "*.qua", SearchOption.AllDirectories);
             Console.WriteLine($"[DATABASE HELPER] Found {files.Length} .qua files in the /Songs/ Directory!");
 
             // Find all of the beatmaps currently in the database, and check if the amount matches the # of found files.
             var conn = new SQLiteAsyncConnection(databasePath);
             var query = conn.Table<Beatmap>();
 
-            await query.ToListAsync().ContinueWith(async (t) =>
+            await query.ToListAsync().ContinueWith(async t =>
             {
                 var foundMaps = t.Result;
                 Console.WriteLine($"[DATABASE HELPER] Found {foundMaps.Count} beatmaps in the database!");
 
                 if (foundMaps.Count != files.Length)
                 {
-                    Console.WriteLine($"[DATABASE HELPER] Incorrect number of .qua files vs maps detected. {files.Length} vs {foundMaps.Count}!");
+                    Console.WriteLine(
+                        $"[DATABASE HELPER] Incorrect number of .qua files vs maps detected. {files.Length} vs {foundMaps.Count}!");
 
                     // This'll store the list of beatmaps that we'll be adding to the database.
                     var beatmapsToCache = new List<Beatmap>();
@@ -60,7 +61,7 @@ namespace Quaver.Database
                     {
                         // First run a check if the map exists in the database already
                         // To avoid potential path issues, we change all backslashes to forward slashes.
-                        var inDb = foundMaps.Any((map) => map.Path.Replace("\\", "/") == quaFile.Replace("\\", "/"));
+                        var inDb = foundMaps.Any(map => map.Path.Replace("\\", "/") == quaFile.Replace("\\", "/"));
                         if (inDb) continue;
 
                         // Try to parse the beatmap, convert it to a beatmap object and add it to the database
@@ -69,7 +70,8 @@ namespace Quaver.Database
                         // If the file couldn't be parsed correctly, we want to display an error and delete the file.
                         if (!parsedQua.IsValidQua)
                         {
-                            Console.WriteLine($"[DATABASE HELPER] Error: .Qua could not successfully be parsed: {quaFile}!");
+                            Console.WriteLine(
+                                $"[DATABASE HELPER] Error: .Qua could not successfully be parsed: {quaFile}!");
                             File.Delete(quaFile);
                             continue;
                         }
@@ -101,7 +103,7 @@ namespace Quaver.Database
 
             var watch = Stopwatch.StartNew();
 
-            await conn.InsertAllAsync(beatmaps).ContinueWith((t) =>
+            await conn.InsertAllAsync(beatmaps).ContinueWith(t =>
                 Console.WriteLine($"[DATABASE HELPER] Successfully added {beatmaps.Count} beatmaps to the database."));
 
             watch.Stop();
@@ -117,20 +119,21 @@ namespace Quaver.Database
             var conn = new SQLiteAsyncConnection(databasePath);
             var query = conn.Table<Beatmap>();
 
-            await query.ToListAsync().ContinueWith((t) =>
+            await query.ToListAsync().ContinueWith(t =>
             {
                 var foundMaps = t.Result;
 
-                List<Beatmap> mapsToRemove = new List<Beatmap>();
+                var mapsToRemove = new List<Beatmap>();
 
                 foreach (var map in foundMaps)
                 {
-                    var matchedQua = files.Any((file) => file.Replace("\\", "/") == map.Path);
+                    var matchedQua = files.Any(file => file.Replace("\\", "/") == map.Path);
                     if (!matchedQua) mapsToRemove.Add(map);
                 }
 
                 // Now that we have all of the maps that don't have .qua files, let's remove them from the database.
-                RemoveBeatmapsFromDatabase(mapsToRemove);
+                if (mapsToRemove.Count >0)
+                    RemoveBeatmapsFromDatabase(mapsToRemove);
             });
         }
 
@@ -146,7 +149,6 @@ namespace Quaver.Database
             var conn = new SQLiteConnection(databasePath);
 
             foreach (var map in beatmaps)
-            {
                 try
                 {
                     conn.Execute($"DELETE from beatmap WHERE path=\"{map.Path}\"");
@@ -155,10 +157,6 @@ namespace Quaver.Database
                 {
                     Console.WriteLine(e.ToString());
                 }
-                
-            }
-            
-            Console.WriteLine($"[DATABASE HELPER] Sync Complete!");
         }
     }
 }
