5b9985959b7bf61226763c349da103e13d45ea4c
Swan <contact@swan.moe>
2017-10-04 13:25:33 -0400

    Add: Sample Database helper class
    Former-commit-id: 2dc69ba7668142d270bf4c2f8f3503e1613d70a8 [formerly e7d56ab292898e6aaa49e95b50a6fa94a5bc87ea]
    Former-commit-id: 79e8623d2fd68004268b93d131542bbd8032de81

---
+++ Quaver/Database/DatabaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.Config;
+using System.IO;
+using Quaver.Beatmaps;
+using SQLite;
+
+namespace Quaver.Database
+{
+    internal static class DatabaseHelper
+    {
+        /// <summary>
+        /// Holds the path of the SQLite database
+        /// </summary>
+        internal static readonly string databasePath = Path.Combine(Configuration.GameDirectory, "quaver.db");
+
+        /// <summary>
+        /// Initialize the beatmap database & create a table named Beatmaps with the Beatmap class properties.
+        /// </summary>
+        internal static async Task InitializeBeatmapDatabaseAsync()
+        {
+            var conn = new SQLiteAsyncConnection(databasePath);
+            await conn.CreateTableAsync<Beatmap>();
+            Console.WriteLine("[DATABASE HELPER] Successfully initialized database & created table: \"Beatmaps\"");
+        }
+    }
+}
