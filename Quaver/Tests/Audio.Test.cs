c3c872fe8c563976b8c8eebabc16c5ae15938f89
Swan <contact@swan.moe>
2017-10-05 14:44:34 -0400

    Add: Audio Testing
    Former-commit-id: 528b0722f149395295c69dc1807b701d94833581 [formerly 8e6c469fc9178731cec9a3b484b083ad39750e4e]
    Former-commit-id: ef4ddfd405c2d30a4ac90214d7aad34f1e6f833a

---
+++ Quaver/Tests/Audio.Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ManagedBass;
+using Quaver.Audio;
+
+namespace Quaver.Tests
+{
+    internal static class AudioTest
+    {
+        internal static void PlaySongPreview(bool run)
+        {
+            if (!run)
+                return;
+
+            const string filePath =
+                @"C:\Users\swan\AppData\Local\osu!\Songs\516109 Aitsuki Nakuru - Monochrome Butterfly\audio.mp3";
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("[DEBUG] Audio File not found, so test cannot be run! Check your path!");
+                return;
+            }
+
+            var stream = AudioImplementation.LoadAudioStream(filePath);
+            var positionInMilliseconds = 52012;
+            AudioImplementation.PlayAudioStream(stream, positionInMilliseconds);
+            Console.WriteLine("[DEBUG] Waiting 2 seconds to pause audio.");
+            Thread.Sleep(2000);
+            AudioImplementation.PauseAudioStream(stream);
+            Console.WriteLine("[DEBUG] Waiting 5 seconds to resume audio");
+            Thread.Sleep(5000);
+            AudioImplementation.ResumeAudioStream(stream);
+        }
+    }
+}
