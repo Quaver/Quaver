77efe66250963d4e4242f9423f8e4c79de8ffbe1
Swan <contact@swan.moe>
2017-10-05 13:37:18 -0400

    Add: Audio Loader method
    Former-commit-id: 308a19fb7c798626d9fdd10ca676718434957056 [formerly 54b022c84945904f5b28e93011fa86d32f7ec496]
    Former-commit-id: 919eebaf6f3d1a853ee6e3093bc8586f96234263

---
+++ Quaver/Audio/AudioEngine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagedBass;
+
+namespace Quaver.Audio
+{
+    internal class AudioImplementation
+    {
+        internal static void Play(string filePath)
+        {
+            if (Bass.Init())
+            {
+                var stream = Bass.CreateStream(filePath);
+
+                if (stream != 0)
+                    Bass.ChannelPlay(stream);
+                else Console.WriteLine("[AUDIO ENGINE] Error: {0}", Bass.LastError);
+            }
+        }
+    }
+}
