8a2c43f7cb3e2ec8d4c5241dd27748550d609986
Swan <contact@swan.moe>
2017-10-06 18:12:48 -0400

    Add: CustomColors Struct
    Former-commit-id: 78055dc961a54b70d0b6120b000a0111a16c5d2b [formerly ae9fa3f74d94e89c73a4ef5c24d4939f8b66ea5b]
    Former-commit-id: 0d153403911760257d3c329ef80d2a169a04931f

---
+++ Quaver/src/Graphics/CustomColors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Quaver.Graphics
+{
+    public struct CustomColors
+    {
+        public static readonly Color Swan = new Color(219, 136, 194, 1); // # db88c2. LOL.
+        public static readonly Color Green = new Color(59, 233, 106, 1);
+        public static readonly Color LightBlue = new Color(76, 146, 211, 1);
+    }
+}
