49d4b2973f8d648430f150f187397e1818fd6c80
Swan <contact@swan.moe>
2017-10-05 20:01:12 -0400

    Fix: Properly change the volume of the stream
    Former-commit-id: b2d6b5db4d74ca9a9b88be65122c42c6e875a69d [formerly c5b83baa5a26a3639e4d2dfb29b4d6b3b3f60aaa]
    Former-commit-id: ebffa972845c31cb7f99d21738f56efc3635a295

--- Quaver/Tests/Audio.Test.cs
+++ Quaver/Tests/Audio.Test.cs
@@ -20,7 +20,7 @@ namespace Quaver.Tests
                 return;
 
             const string filePath =
-                @"C:\Users\swan\Desktop\New folder\1. Camellia - WHAT THE CAT!\audio.ogg";
+                @"C:\Users\swan\Desktop\New folder\587394 Silent Siren - Soukai Rock\Soukai ROCK 1.4x.ogg";
 
             if (!File.Exists(filePath))
             {
