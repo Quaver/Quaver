c2d2e08ef6aa9e5925828fc3276f4fbd1e94efd1
Swan <contact@swan.moe>
2017-10-06 12:49:08 -0400

    .
    Former-commit-id: 0cffbe614d3a9ed93dd1f08d9fe19ab1ab157d46 [formerly 43333ff3e53914b7244e1b1fdfb2b6520837d428]
    Former-commit-id: 84cbffb0c843e67f5395ea72ba4735082eea9b85

--- Quaver/src/GameState/GameStateBase.cs
+++ Quaver/src/GameState/GameStateBase.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
