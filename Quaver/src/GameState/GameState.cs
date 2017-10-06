dfe17e42382b16e489f4a7b0355089eac9109f9f
staravia <denysenesouma@gmail.com>
2017-10-05 22:22:47 -0700

    Add: GameState
    Former-commit-id: 1150c1ca2d48f0086eaeca9b7fe94af20db59425 [formerly 54ee64d1e9f47eae80bc8364e2b3830381ac40a3]
    Former-commit-id: 79bdf00e55e76d0c1159294cb6b2cb7adbde1180

---
+++ Quaver/src/GameState/GameState.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Quaver.src.GameState
+{
+    public enum State
+    {
+        MainMenu,
+        SongSelect,
+        SongLoading,
+        PlayScreen,
+        PlayPause,
+        GameOver,
+        ScoreScreen,
+        LoadingScreen
+    }
+    public interface IGameState
+    {
+
+        // Initialize the game settings here      
+        void Initialize();
+
+        // Load all content here
+        void LoadContent(ContentManager content);
+
+        // Unload any content here
+        void UnloadContent();
+
+        // Updates the game
+        void Update(GameTime gameTime);
+
+        // Draws the game
+        void Draw(SpriteBatch spriteBatch);
+
+    }
+
+    public abstract class GameStateBase : IGameState
+    {
+        protected GraphicsDevice _graphicsDevice;
+
+        //The State of the GameState class. Will be declared when Instantiated.
+        private State _currentState;
+        public State CurrentState
+        {
+            get { return _currentState; }
+            set { _currentState = value; }
+        }
+
+        public GameStateBase(GraphicsDevice graphicsDevice)
+        {
+            _graphicsDevice = graphicsDevice;
+        }
+
+        // Initialize the game settings here  
+        public abstract void Initialize();
+
+        // Load all content here
+        public abstract void LoadContent(ContentManager content);
+
+        // Unload any content here
+        public abstract void UnloadContent();
+
+        // Updates the game
+        public abstract void Update(GameTime gameTime);
+
+        // Draws the game
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
---
+++ Quaver/src/Gameplay/State_Gameplay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.src.GameState;
+
+namespace Quaver.src.Gameplay
+{
+    partial class State_Gameplay : GameStateBase
+    {
+        public State_Gameplay(GraphicsDevice graphicsDevice) :base(graphicsDevice)
+        {
+            //Important to set the current state of this GameState
+            CurrentState = State.PlayScreen;
+        }
+        public override void Initialize()
+        {
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+        }
+
+        public override void UnloadContent()
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+        }
+        
+    }
+}
