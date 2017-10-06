c197e55ba6d6cee1261da798c622de264c6714e4
Swan <contact@swan.moe>
2017-10-06 12:08:58 -0400

    Change: Cleanup Sprite Namespace Code

---
+++ Quaver/src/Graphics/ISprite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Graphics
+{
+    /// <summary>
+    ///     TODO: Add Summary Here
+    /// </summary>
+    internal interface ISprite
+    {
+        void Instantiate();
+        void Draw();
+        void Destroy();
+    }
+}
