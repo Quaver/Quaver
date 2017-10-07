4edaa7ef8f3a7e16817cb69f02dd1f38ed7a8d4c
Swan <contact@swan.moe>
2017-10-07 14:14:49 -0400

    Change: Move Beatmap Utility methods to separate class
    Former-commit-id: 4b3820b659df00d767070831840f66bfde34aa17 [formerly 5726f35142f71962a8dbfa850f2a5580dcea2a8d]
    Former-commit-id: 4dce0e3f123245f3891555328ba2022d47d131ff

--- Quaver/src/Beatmaps/Beatmap.cs
+++ Quaver/src/Beatmaps/Beatmap.cs
@@ -130,7 +130,7 @@ namespace Quaver.Beatmaps
         {
             return new Beatmap
             {
-                Md5Checksum = GetMd5Checksum(path),
+                Md5Checksum = BeatmapUtils.GetMd5Checksum(path),
                 IsValidBeatmap = true,
                 Path = path,
                 Artist = qua.Artist,
@@ -147,57 +147,6 @@ namespace Quaver.Beatmaps
                 Tags = qua.Tags,
                 SongLength = Qua.FindSongLength(qua),
             };
-        }
-
-        /// <summary>
-        ///     Gets the Md5 Checksum of a file, more specifically a .qua file.
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        public static string GetMd5Checksum(string path)
-        {
-            using (var md5 = MD5.Create())
-            {
-                using (var stream = File.OpenRead(path))
-                {
-                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty).ToLower();
-                }
-            }
-        }
-
-        /// <summary>
-        ///     Responsible for taking a list of beatmaps, and grouping each directory into a dictionary.
-        /// </summary>
-        /// <param name="beatmaps"></param>
-        /// <returns></returns>
-        public static Dictionary<string, List<Beatmap>> GroupBeatmapsByDirectory(List<Beatmap> beatmaps)
-        {
-            return (from beatmap in beatmaps
-                group beatmap by System.IO.Path.GetDirectoryName(beatmap.Path)
-                into g
-                select g).ToDictionary(x => x.Key, x => x.ToList());
-        }
-
-        /// <summary>
-        ///     Orders the beatmaps by title
-        /// </summary>
-        /// <param name="beatmaps"></param>
-        /// <returns></returns>
-        public static Dictionary<string, List<Beatmap>> OrderBeatmapsByTitle(Dictionary<string, List<Beatmap>> beatmaps)
-        {
-            return beatmaps.OrderBy(x => x.Value[0].Title).ToDictionary(pair => pair.Key, pair => pair.Value);
-        }
-
-        /// <summary>
-        ///     Orders the beatmaps by artist, and then by title.
-        /// </summary>
-        /// <param name="beatmaps"></param>
-        /// <returns></returns>
-        public static Dictionary<string, List<Beatmap>> OrderBeatmapsByArtist(Dictionary<string, List<Beatmap>> beatmaps)
-        {
-            var dict = beatmaps.OrderBy(x => x.Value[0].Artist).ThenBy(x => x.Value[0].Title)
-                .ToDictionary(pair => pair.Key, pair => pair.Value);
-            return dict;
         }
     }
 }
