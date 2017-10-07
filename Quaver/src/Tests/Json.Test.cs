a2a9856c0ee02fd69e24da4012b8acdf64a90a4c
Swan <contact@swan.moe>
2017-10-07 19:26:54 -0400

    Update submodules & add tests

---
+++ Quaver/src/Tests/Json.Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Quaver.Tests
+{
+    public class English
+    {
+        public string score { get; set; }
+        public string hpDrain { get; set; }
+        public string play { get; set; }
+    }
+
+    internal static class JsonTest
+    {
+        internal static void SerializeJsonTest()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream("Quaver.Resources.Quaver_Localization.en.english.json"))
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    var serializer = new JsonSerializer();
+                    English meme = serializer.Deserialize<English>(reader);
+                    Console.WriteLine(meme.play);
+                }
+            }
+        }
+    }
+}
