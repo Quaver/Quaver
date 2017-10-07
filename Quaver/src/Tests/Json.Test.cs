8dae1815b2ab2e33de3dab60a353a9d30c4da182
Swan <contact@swan.moe>
2017-10-07 19:51:13 -0400

    Update tests
    Former-commit-id: d08fd4e6ef76a49c76dc44998a7b5c8610b036c5 [formerly 489734c7c971f73107f47dccaa6e68fe54065fdf]
    Former-commit-id: 1c39d170f48c4102dc916897fda1696095c5a831

--- Quaver/src/Tests/Json.Test.cs
+++ Quaver/src/Tests/Json.Test.cs
@@ -6,31 +6,17 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Quaver.Localization;
+using Quaver.Utility;
 
 namespace Quaver.Tests
 {
-    private class English
-    {
-        public string score { get; set; }
-        public string hpDrain { get; set; }
-        public string play { get; set; }
-    }
-
     internal static class JsonTest
     {
-        internal static void DserializeJsonTest()
+        internal static void DeserializeJsonTest()
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            using (Stream stream = assembly.GetManifestResourceStream("Quaver.Resources.Quaver_Localization.en.english.json"))
-            using (StreamReader sr = new StreamReader(stream))
-            {
-                using (JsonReader reader = new JsonTextReader(sr))
-                {
-                    var serializer = new JsonSerializer();
-                    English meme = serializer.Deserialize<English>(reader);
-                    Console.WriteLine(meme.play);
-                }
-            }
+            var english = JsonResourceReader.Read("Quaver.Resources.Quaver_Localization.en.english.json");
+            Console.WriteLine(english.play);
         }
     }
 }
