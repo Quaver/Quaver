389ed4d155d1a84d4ca19c73185369a435278a28
Swan <contact@swan.moe>
2017-10-07 19:51:02 -0400

    Add: Class for reading JSON from resources

---
+++ Quaver/src/Utility/JsonResourceReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Quaver.Localization;
+
+namespace Quaver.Utility
+{
+    internal static class JsonResourceReader
+    {
+        /// <summary>
+        ///     Reads a JSON resource and serializes it into a class.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="resource"></param>
+        internal static English Read(string resource)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (var stream = assembly.GetManifestResourceStream(resource))
+            using (var sr = new StreamReader(stream))
+            {
+                using (var reader = new JsonTextReader(sr))
+                {
+                    var serializer = new JsonSerializer();
+                    return serializer.Deserialize<English>(reader);
+                }
+            }
+        }
+    }
+}
