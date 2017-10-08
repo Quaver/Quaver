86c9a6cd4f033ef93d49d29534c964717327a706
Swan <contact@swan.moe>
2017-10-07 20:15:56 -0400

    Change: Generalize language class to support all of them
    Former-commit-id: 619c8c69b66d91d462b06932ac99300b42ca2e16 [formerly 60ea33d7435851635de6a559a4d3172e0573786d]
    Former-commit-id: 1210e334d502beb0a66e5f95cb85280fd711dabb

---
+++ Quaver/src/Localization/Language.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Localization
+{
+    internal class Language
+    {
+        public string Score { get; set; }
+        public string HpDrain { get; set; }
+        public string Play { get; set; }
+        public string Settings { get; set; }
+        public string Exit { get; set; }
+    }
+}
