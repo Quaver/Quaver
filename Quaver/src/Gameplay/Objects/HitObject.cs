aef6dbe2758a6801b2a94d2f98c802309f0bd27f
staravia <denysenesouma@gmail.com>
2017-10-11 08:18:27 -0700

    Add: HitObjects

---
+++ Quaver/src/Gameplay/Objects/HitObject.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using Quaver.Graphics;
+
+namespace Quaver.src.Gameplay.Objects
+{
+    public class HitObject
+    {
+        //Sprite Objects
+        internal Sprite _HitBodySprite;
+        internal Sprite _HoldBodySprite;
+        internal Sprite _HoldEndSprite;
+
+        public HitObject()
+        {
+
+        }
+
+        /// <summary>
+        /// When the note is supposed to be pressed.
+        /// </summary>
+        public float StartTime
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// When the note is supposed to be held until. (If the HitObject is a long note)
+        /// </summary>
+        public float EndTime
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Determines if the HitObject is a long note or a regular note.
+        /// </summary>
+        public bool isLongNote
+        {
+            get; set;
+        }
+        
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _HitBodySprite.Draw(spriteBatch);
+
+            if (isLongNote)
+            {
+                _HoldBodySprite.Draw(spriteBatch);
+                _HitBodySprite.Draw(spriteBatch);
+            }
+        }
+    }
+}
