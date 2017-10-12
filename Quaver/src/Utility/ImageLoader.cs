7afe6733f509f20e332a521f6e14b123eb574260
Swan <contact@swan.moe>
2017-10-12 12:51:23 -0400

    Add: ImageLoader utility class

---
+++ Quaver/src/Utility/ImageLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.Main;
+
+namespace Quaver.Utility
+{
+    internal class ImageLoader
+    {
+        /// <summary>
+        ///     Loads an image into a Texture2D
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        internal static Texture2D Load(string path)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Open))
+            {
+                return Texture2D.FromStream(GameBase.GraphicsDevice, fileStream);
+            }
+        }
+    }
+}
