f3799eb08f650fe6f93aea4bf51c2460529d4be0
staravia <denysenesouma@gmail.com>
2017-10-13 11:00:07 -0700

    Add: FPS Counter

---
+++ Quaver/src/Tests/FPSCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using Quaver.Main;
+
+namespace Quaver.Tests
+{
+    internal static class FPSCounter
+    {
+        private static double _fpsCurrent = 0;
+        private static double _fpsCount = 0;
+        private static int _interval = 0;
+        private static SpriteFont _FpsFont = GameBase.Content.Load<SpriteFont>("Fonts/testFont");
+
+        public static void Count(double dt)
+        {
+            _fpsCount += dt;
+            _interval++;
+            //After 20 frames, it will update the current FPS
+            if (_interval >= 20)
+            {
+                _fpsCurrent = 1/(_fpsCount/20);
+                _fpsCount = 0;
+                _interval = 0;
+            }
+        }
+
+        public static double Get()
+        {
+            return _fpsCurrent;
+        }
+
+        public static void Draw()
+        {
+            GameBase.SpriteBatch.DrawString(_FpsFont, Math.Floor(_fpsCurrent).ToString() + " FPS", new Vector2(0, 0), Color.LightGreen);
+        }
+
+    }
+}
