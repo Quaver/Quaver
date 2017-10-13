2d5ea37e6f2e6b167a5a8e77f5e581f04429bd2f
staravia <denysenesouma@gmail.com>
2017-10-13 11:20:57 -0700

    Add: Boundary class
    Former-commit-id: c561c203d71154a093dcbe0b741418ff7fa68cd1 [formerly bd007e283bac2978695c1866df98f95a4406207b]
    Former-commit-id: 6ae3bf2ea8aaf6edc3b11ad3445ea3e4fc1ba86c

---
+++ Quaver/src/Graphics/Boundary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using Quaver.Utility;
+using Quaver.Main;
+
+namespace Quaver.Graphics
+{
+    /// <summary>
+    ///     This is used for sprite/UI layout
+    /// </summary>
+    internal class Boundary : Drawable
+    {
+        public override void Draw()
+        {
+
+            //Draw children
+            for(int i = 0; i < Children.Count; i++)
+            {
+                Children[i].Draw();
+            }
+        }
+
+        public override void Destroy()
+        {            
+        }
+
+        public override void Instantiate()
+        {            
+        }
+    }
+}
