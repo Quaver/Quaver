d6e5b3f894fc010161486d8def7f29001d1db6df
staravia <denysenesouma@gmail.com>
2017-10-16 02:28:51 -0700

    Add: Gameplay/Receptors
    Former-commit-id: 0ec30a0fe6e43f70cf6210a8fcf5fe3a553217de [formerly ba239487d15f8282df57c82406b68d69a6d6dbfe]
    Former-commit-id: 75a060e75d6748679d4797b230cd4eaaa5f385cb

---
+++ Quaver/src/Gameplay/Gameplay-Input.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Quaver.GameState;
+using Quaver.Utility;
+using Quaver.Graphics;
+using Quaver.Main;
+
+namespace Quaver.Gameplay
+{
+    internal partial class StatePlayScreen : GameStateBase
+    {
+        //Config Variables
+        private Keys[] _PlayKey = new Keys[4] { Keys.A, Keys.S, Keys.K, Keys.L };
+
+        //Input Variables
+        private KeyboardState _KeyboardState;
+        private bool[] _KeyDown = new bool[4];
+
+        internal void InitializeInput()
+        {
+            //Create Reference Variables
+        }
+
+        internal void CheckInput()
+        {
+            _KeyboardState = Keyboard.GetState();
+            
+            for (int i=0; i< 4; i++)
+            {
+                if (_KeyboardState.IsKeyDown(_PlayKey[i]))
+                {
+                    if (!_KeyDown[i])
+                    {
+                        _KeyDown[i] = true;
+
+                        //ADD KEY PRESS LOGIC HERE
+                        UpdateReceptor(i,true);
+                    }
+                }
+                if (_KeyboardState.IsKeyUp(_PlayKey[i]))
+                {
+                    if (_KeyDown[i])
+                    {
+                        _KeyDown[i] = false;
+
+                        //ADD KEY RELEASE LOGIC HERE
+                        UpdateReceptor(i,false);
+                    }
+                }
+            }
+
+        }
+    }
+}
