bc9ae9826023d79da3c7e56d80e1339c9ec8855e
staravia <denysenesouma@gmail.com>
2017-10-17 16:00:45 -0700

    Update: Gameplay State
    Former-commit-id: c4c0b42ed14b93bf0e5e628ade9245255d759c83 [formerly 08ad0894dc04e5abb69ad8bae53e451bf5b12195]
    Former-commit-id: 8c7408f19ba7296973f79b08fa4582f16d0ba0ac

---
+++ Quaver/src/Gameplay/Gameplay-Mods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Gameplay
+{
+    internal partial class StatePlayScreen
+    {
+        /*GAME MODS*/
+        private const bool _mod_noSV = false;
+        private const bool _mod_pull = false;
+        private const bool _mod_split = false;
+        private const bool _mod_spin = false;
+        private const bool _mod_shuffle = false;
+    }
+}
---
+++ Quaver/src/Gameplay/Objects/TimingObject.cs
@@ -0,0 +1,9 @@
+﻿namespace Quaver.Gameplay
+{
+    internal class TimingObject
+    {
+        public float TargetTime { get; set; }
+        public float BPM { get; set; }
+        public float SvMultiplier { get; set; }
+    }
+}
