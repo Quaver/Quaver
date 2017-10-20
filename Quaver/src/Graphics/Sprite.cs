a5f95cc481f8b7a4bc1493b374d021842afe57d1
Swan <contact@swan.moe>
2017-10-20 18:43:23 -0400

    Change: Clean up Sprite class

--- Quaver/src/Graphics/Sprite.cs
+++ Quaver/src/Graphics/Sprite.cs
@@ -21,47 +21,38 @@ namespace Quaver.Graphics
         /// <summary>
         /// Image Texture of the sprite.
         /// </summary>
-        public Texture2D Image
-        {
-            get;
-            set;
-        }
+        public Texture2D Image { get; set; }
 
         /// <summary>
         /// Angle of the sprite with it's origin in the centre. (TEMPORARILY NOT USED YET)
         /// </summary>
-        public float Rotation
-        {
-            get;
-            set;
-        }
+        public float Rotation { get; set; }
 
-        //Constructor
+        // Ctor
         public Sprite()
         {
             Tint = Color.White;
+
             //Todo: Set fallback image
             //Image = FALLBACK IMAGE;
         }
 
+        /// <summary>
+        ///     Draws the sprite to the screen.
+        /// </summary>
         public override void Draw()
         {
             //Draw itself
             GameBase.SpriteBatch.Draw(Image, GlobalRect, Tint);
 
             //Draw children
-            for(int i = 0; i < Children.Count; i++)
-            {
-                Children[i].Draw();
-            }
+            Children.ForEach(x => x.Draw());
         }
 
-        public override void Destroy()
-        {            
-        }
+        // TODO: Implement Destroy
+        public override void Destroy() { }
 
-        public override void Instantiate()
-        {            
-        }
+        // TODO: Implement Instantiate
+        public override void Instantiate() { }
     }
 }
