e2cd71d8289c440c493dd74d426da60a9fadd66e
Swan <contact@swan.moe>
2017-10-20 20:21:01 -0400

    Change: Partially clean up StatePlayScreen
    Former-commit-id: 43bec79a7dc1b821635c7e414701ed67bcffdd9e [formerly 618e3d0c11a6e3bde017e8b4593006f1f2ab8932]
    Former-commit-id: 30f7919bf2e33a0a0647419c30481166826f49bb

---
+++ Quaver/src/Gameplay/TimingObject.cs
@@ -0,0 +1,20 @@
+﻿namespace Quaver.Gameplay
+{
+    internal class TimingObject
+    {
+        /// <summary>
+        ///     TODO: Add Summary...
+        /// </summary>
+        public float TargetTime { get; set; }
+
+        /// <summary>
+        ///     TODO: Add Summary...
+        /// </summary>
+        public float BPM { get; set; }
+
+        /// <summary>
+        ///     TODO: Add Summary...
+        /// </summary>
+        public float SvMultiplier { get; set; }
+    }
+}
