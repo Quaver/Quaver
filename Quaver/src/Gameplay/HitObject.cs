f58903801a591578f75eff335ede25630ec7bb76
Swan <contact@swan.moe>
2017-10-21 20:29:55 -0400

    Change: Clean up HitObject class code
    Former-commit-id: ce67facec96ceac2606adfe590d4c5a0ad0ba41f [formerly da877118e005006215bdf8f611b4c9200c172d9c]
    Former-commit-id: e0401e84f601f593984f97532b7960914be28a7a

--- Quaver/src/Gameplay/HitObject.cs
+++ Quaver/src/Gameplay/HitObject.cs
@@ -15,188 +15,141 @@ namespace Quaver.Gameplay
 {
     public class HitObject
     {
-        //Parent Container
-        private Drawable _ParentContainer;
-
-        //Sprite Objects
-        private Sprite _HitBodySprite;
-        private Sprite _HoldBodySprite;
-        private Sprite _HoldEndSprite;
-
-        //Position Variables
-        private Vector2 _Position;
-        private float _HitObjectSize;
-
-        public HitObject()
-        {
-            //TODO: add load images/stuff loogic later.
-        }
-
-        public void Initialize()
-        {
-            _HoldBodySprite = new Sprite()
-            {
-                Image = GameBase.LoadedSkin.NoteHoldBody,
-                Alignment = Alignment.TopLeft,
-                Position = _Position,
-                Size = Vector2.One * HitObjectSize,
-                Parent = _ParentContainer
-            };
-            _HoldEndSprite = new Sprite()
-            {
-                Image = GameBase.LoadedSkin.NoteHoldEnd,
-                Alignment = Alignment.TopLeft,
-                Position = _Position,
-                Size = Vector2.One * HitObjectSize,
-                Parent = _ParentContainer
-            };
-
-            _HitBodySprite = new Sprite()
-            {
-                Image = GameBase.LoadedSkin.NoteHitObject1,
-                Alignment = Alignment.TopLeft,
-                Position = _Position,
-                Size = Vector2.One * HitObjectSize,
-                Parent = _ParentContainer
-            };
-            UpdateObject();
-        }
-
         /// <summary>
         /// The parent of the HitObjects
         /// </summary>
-        internal Drawable ParentContainer
-        {
-            get
-            {
-                return _ParentContainer;
-            }
-            set
-            {
-                _ParentContainer = value;
-            }
-        }
+        internal Drawable ParentContainer { get; set; }
 
         /// <summary>
         /// The lane which the HitObject belongs to.
         /// </summary>
-        public int KeyLane
-        {
-            get; set;
-        }
+        public int KeyLane { get; set; }
 
         /// <summary>
         /// When the note is supposed to be pressed.
         /// </summary>
-        public float StartTime
-        {
-            get; set;
-        }
+        public float StartTime { get; set; }
 
         /// <summary>
         /// When the note is supposed to be held until. (If the HitObject is a long note)
         /// </summary>
-        public float EndTime
-        {
-            get; set;
-        }
+        public float EndTime { get; set; }
 
         /// <summary>
         /// The _svPart's index that the object StartTime lies right after.
         /// </summary>
-        public int SvPosition
-        {
-            get; set;
-        }
+        public int SvPosition { get; set; }
 
         /// <summary>
         /// The Object's Y-Offset From the receptor.
         /// </summary>
-        public ulong OffsetFromReceptor
-        {
-            get; set;
-        }
+        public ulong OffsetFromReceptor { get; set; }
 
         /// <summary>
         /// Determines if the HitObject is a long note or a regular note.
         /// </summary>
-        public bool isLongNote
-        {
-            get; set;
-        } = true;
+        public bool IsLongNote { get; set; } = true;
 
         /// <summary>
         /// The position of the HitObject Sprites
         /// </summary>
-        public Vector2 HitObjectPosition
+        public Vector2 HitObjectPosition { get; set; }
+
+        /// <summary>
+        ///     The size of the hit object
+        /// </summary>
+        public float HitObjectSize { get; set; }
+
+        /// <summary>
+        ///     TODO: Add Summary
+        /// </summary>
+        private Sprite HitBodySprite { get; set; }
+
+        /// <summary>
+        ///     TODO: Add Summary
+        /// </summary>
+        private Sprite HoldBodySprite { get; set; }
+
+        /// <summary>
+        ///     TODO: Add Summary
+        /// </summary>
+        private Sprite HoldEndSprite { get; set; }
+
+        /// <summary>
+        ///     TODO: Initializes the HitObject sprite
+        /// </summary>
+        public void Initialize()
         {
-            get
+            HoldBodySprite = new Sprite()
             {
-                return _Position;
-            }
-            set
+                Image = GameBase.LoadedSkin.NoteHoldBody,
+                Alignment = Alignment.TopLeft,
+                Position = HitObjectPosition,
+                Size = Vector2.One * HitObjectSize,
+                Parent = ParentContainer
+            };
+
+            HoldEndSprite = new Sprite()
             {
-                _Position = value;
-            }
+                Image = GameBase.LoadedSkin.NoteHoldEnd,
+                Alignment = Alignment.TopLeft,
+                Position = HitObjectPosition,
+                Size = Vector2.One * HitObjectSize,
+                Parent = ParentContainer
+            };
+
+            HitBodySprite = new Sprite()
+            {
+                Image = GameBase.LoadedSkin.NoteHitObject1,
+                Alignment = Alignment.TopLeft,
+                Position = HitObjectPosition,
+                Size = Vector2.One * HitObjectSize,
+                Parent = ParentContainer
+            };
+
+            UpdateObject();
         }
 
         /// <summary>
-        /// The Y Position of the HitObject Sprites
+        ///     TODO: Add Summary
         /// </summary>
-        public float HitObjectY
-        {
-            get
-            {
-                return _Position.Y;
-            }
-            set
-            {
-                _Position.Y = value;
-            }
-        }
-
-        public float HitObjectSize
-        {
-            get
-            {
-                return _HitObjectSize;
-            }
-            set
-            {
-                _HitObjectSize = value;
-            }
-        }
-
         public void UpdateObject()
         {
-            if (isLongNote)
+            if (IsLongNote)
             {
-                _HoldBodySprite.PositionY = _Position.Y + HitObjectSize / 2f;
-                _HoldEndSprite.PositionY = _Position.Y + HitObjectSize;
+                HoldBodySprite.PositionY = HitObjectPosition.Y + HitObjectSize / 2f;
+                HoldEndSprite.PositionY = HitObjectPosition.Y + HitObjectSize;
 
-                _HoldBodySprite.UpdateRect();
-                _HoldEndSprite.UpdateRect();
+                HoldBodySprite.UpdateRect();
+                HoldEndSprite.UpdateRect();
             }
-            _HitBodySprite.PositionY = _Position.Y;
-            _HitBodySprite.UpdateRect();
+
+            HitBodySprite.PositionY = HitObjectPosition.Y;
+            HitBodySprite.UpdateRect();
         }
         
+        /// <summary>
+        ///     TODO: Add Summary
+        /// </summary>
         public void Draw()
         {
-            if (isLongNote)
+            if (IsLongNote)
             {
-                _HoldBodySprite.Draw();
-                _HoldEndSprite.Draw();
+                HoldBodySprite.Draw();
+                HoldEndSprite.Draw();
             }
 
-            _HitBodySprite.Draw();
+            HitBodySprite.Draw();
         }
 
+        /// <summary>
+        ///     TODO: Add Summary
+        /// </summary>
         public void Destroy()
         {
-            _HoldBodySprite.Destroy();
-            _HoldEndSprite.Destroy();
-            _HitBodySprite.Destroy();
+            HoldBodySprite.Destroy();
+            HoldEndSprite.Destroy();
+            HitBodySprite.Destroy();
         }
     }
 }
