68c34dbf8989d371fb8ca9b592f625b7ef31fe6d
staravia <denysenesouma@gmail.com>
2017-10-21 21:31:54 -0700

    Update: SpriteManager

--- Quaver/src/Graphics/SpriteManager.cs
+++ Quaver/src/Graphics/SpriteManager.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Quaver.Graphics;
 
-namespace Quaver.src.Graphics
+namespace Quaver.Graphics
 {
     /// <summary>
     /// This class manages the drawing of every drawable object.
@@ -17,45 +17,69 @@ namespace Quaver.src.Graphics
         /// <summary>
         /// A list of every Drawable object that will be drawn
         /// </summary>
-        private static List<Drawable> _drawList = new List<Drawable>();
+        internal static List<Drawable> _drawList = new List<Drawable>();
 
         /// <summary>
         /// A list of every Drawable asset that is active (in memory)
         /// </summary>
-        private static List<Drawable> _drawPool = new List<Drawable>();
+        internal static List<Drawable> _spritePool = new List<Drawable>();
+
+        /// <summary>
+        /// Add a new Drawable object to the Sprite Pool.
+        /// </summary>
+        /// <param name="current"></param>
+        internal static void AddToSpritePool(Drawable current)
+        {
+            _spritePool.Add(current);
+        }
+
+        /// <summary>
+        /// Remove the Drawable object from the Sprite Pool.
+        /// </summary>
+        /// <param name="current"></param>
+        internal static void RemoveFromSpritePool(Drawable current)
+        {
+            RemoveFromDrawList(current);
+            var cIndex = _drawList.FindIndex(r => r == current);
+            if (cIndex >= 0) _spritePool.RemoveAt(cIndex);
+        }
 
         /// <summary>
         /// This method will add a drawable to the Draw List so it can be drawn.
         /// </summary>
         /// <param name="current"></param>
-        private static void AddToDrawList(Drawable current)
+        internal static void AddToDrawList(Drawable current)
         {
-            
+            _drawList.Add(current);
         }
 
         /// <summary>
-        /// This method will remove a Drawable from the Draw List
+        /// This method will remove a Drawable from the Draw List.
         /// </summary>
         /// <param name="current"></param>
-        private static void RemoveFromDrawList(Drawable current)
+        internal static void RemoveFromDrawList(Drawable current)
         {
-
+            var cIndex = _drawList.FindIndex(r => r == current);
+            if (cIndex >= 0) _drawList.RemoveAt(cIndex);
         }
 
         /// <summary>
         /// This will remove every single Drawable object in the Draw List.
         /// </summary>
-        private static void ClearDrawList()
+        internal static void ClearDrawList()
         {
-            
+            _drawList.RemoveRange(0,_drawList.Count-1);
         }
 
         /// <summary>
-        /// This method will draw every object in the Draw List
+        /// This method will draw every object in the Draw List.
         /// </summary>
-        private static void Draw()
+        internal static void Draw()
         {
-            
+            foreach (var current in _drawList)
+            {
+                current.Draw();
+            }
         }
 
     }
