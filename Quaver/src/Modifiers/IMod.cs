f46be6a41140c809dad40bd83e325ef08a306938
Swan <contact@swan.moe>
2017-10-23 15:23:41 -0400

    Add: IMod interface
    Former-commit-id: 452b02e3233f687c481ae1c17693be307e73f8a0 [formerly 68481e9784f198828ca5dcfe538f8c0795323d34]
    Former-commit-id: 00d8b479efa82b8251379953d9181b249e1a09f2

---
+++ Quaver/src/Modifiers/IMod.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Modifiers
+{
+    internal interface IMod
+    {
+        /// <summary>
+        ///     The name of the Mod
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        ///     The identifier of the the mod.
+        /// </summary>
+        ModIdentifier ModIdentifier { get; set; }
+
+        /// <summary>
+        ///     The type of mod as defined in the enum
+        /// </summary>
+        ModType Type { get; set; }
+
+        /// <summary>
+        ///     The description of the Mod
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        ///     Is the mod ranked?
+        /// </summary>
+        bool Ranked { get; set; }
+
+        /// <summary>
+        ///     The addition to the score multiplier this mod will have
+        /// </summary>
+        float ScoreMultiplierAddition { get; set; }
+
+        /// <summary>
+        ///     The identifier of mods that are incompatible with this one.
+        /// </summary>
+        ModIdentifier[] IncompatibleMods { get; set; }
+
+        /// <summary>
+        ///     All the mod logic should go here.
+        /// </summary>
+        void InitializeMod();
+    }
+}
