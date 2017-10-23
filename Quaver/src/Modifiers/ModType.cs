da9345a63734f282c4b751de6a2a3a31345cf3a8
Swan <contact@swan.moe>
2017-10-23 15:24:05 -0400

    Add: ModType enum
    Former-commit-id: b5f50b42141a8804dc1c216e2f8cca962e332161 [formerly cf402128650369a0884d882710ce289d669291bf]
    Former-commit-id: 808a5c493ddb92fd0028fa67c3b4347cbc10a85c

---
+++ Quaver/src/Modifiers/ModType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Modifiers
+{
+    internal enum ModType
+    {
+        DifficultyIncrease,
+        DifficultyDecrease,
+        Special
+    }
+}
