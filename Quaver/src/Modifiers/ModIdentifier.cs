a4696324f0b3fd278e3e0b0b00363f2201cf763e
Swan <contact@swan.moe>
2017-10-23 15:23:52 -0400

    Add: ModIdentifier enum
    Former-commit-id: 0547b4c1270c0d4bc3b2ee58e76632a646784599 [formerly d65945450034159c9a0c9e968922e7809cbcf9c0]
    Former-commit-id: 3f4259cee831d7f9c5767a308a028780c3b0e20f

---
+++ Quaver/src/Modifiers/ModIdentifier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Modifiers
+{
+    internal enum ModIdentifier
+    {
+        NoSliderVelocity = 1 << 0, // No Slider Velocity
+        Speed15X = 1 << 1, // 1.5x Speed
+    }
+}
