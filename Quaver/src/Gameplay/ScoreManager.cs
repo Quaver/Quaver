1522c060537263b87d9242336dd54fa897d991f6
staravia <denysenesouma@gmail.com>
2017-10-24 20:50:07 -0700

    Add: ScoreManager
    Former-commit-id: ff5e5772b031b847af33b2bce12beef7c94380d2 [formerly 02eb38d1fdb76650b8f6e7aeef29105f93ebab26]
    Former-commit-id: 3c53f5ddfe1abf564c50e5eeda0e001703eda4f4

---
+++ Quaver/src/Gameplay/ScoreManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Gameplay
+{
+    /// <summary>
+    /// THIS CLASS IS IMPORTANT. This is where all the scoring will be calculated.
+    /// This class will be updated in the future in such a way that it is near impossible to be manipulated with.
+    /// </summary>
+    internal class ScoreManager
+    {
+
+    }
+}
