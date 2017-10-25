3990c8a533bf9bac0ea37b18b09e16a6ef5967e1
staravia <denysenesouma@gmail.com>
2017-10-24 21:36:49 -0700

    Update: NoteManager
    Former-commit-id: 572228fb090487c7c60ec472dbd632a0beb4c87e [formerly e17d1b4e4f9f4b22edb3aa8d50a2dbaacc72d4a7]
    Former-commit-id: f0aa23e6b4fd7d07b280687c0e271a07cb8bacbc

--- Quaver/src/Gameplay/NoteManager.cs
+++ Quaver/src/Gameplay/NoteManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Quaver.Logging;
 
 namespace Quaver.src.Gameplay
 {
@@ -11,5 +13,22 @@ namespace Quaver.src.Gameplay
     /// </summary>
     class NoteManager
     {
+        /// <summary>
+        /// This method gets called when a key gets pressed.
+        /// </summary>
+        /// <param name="keyLane"></param>
+        internal static void Input(int keyLane, bool keyDown)
+        {
+            if (keyDown)
+            {
+                //Do key press stuff
+                LogTracker.QuickLog("KeyPress: " + keyLane, Color.Blue);
+            }
+            else
+            {
+                //Do key release stuff
+                LogTracker.QuickLog("KeyRelease: " + keyLane, Color.DarkBlue);
+            }
+        }
     }
 }
