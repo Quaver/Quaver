a7c5aebc511bcf2380a42e288dadd5d02184dfaf
staravia <denysenesouma@gmail.com>
2017-10-27 22:06:58 -0700

    Add: ScoreManager stuff

--- Quaver/src/Gameplay/ScoreManager.cs
+++ Quaver/src/Gameplay/ScoreManager.cs
@@ -12,6 +12,16 @@ namespace Quaver.Gameplay
     /// </summary>
     internal class ScoreManager
     {
+        //Hit Judge tracking
+        public static int ScoreMarvelous { get; set; }
+        public static int ScorePerfect { get; set; }
+        public static int ScoreGreat { get; set; }
+        public static int ScoreGood { get; set; }
+        public static int ScoreBad { get; set; }
+        public static int ScoreMiss { get; set; }
+
+        //Hit Window
+        internal static float[] HitWindow { get; } = new float[5] { 16, 43, 76, 106, 130 };
 
     }
 }
