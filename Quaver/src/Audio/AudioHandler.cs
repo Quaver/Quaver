5f1c0abd700b0fffc417faa4b4bd63d423feb089
Swan <contact@swan.moe>
2017-11-01 16:09:17 -0400

    Add: AudioHandler (Global Audio Engine)
    Former-commit-id: 28c1ab4cb626d1ba4397dbb34c5e0adf38fc777d [formerly b411e37701f59ee1a1b6ad3cfb372498c556b72f]
    Former-commit-id: eb9d186fc2a15e5974c8449de233374d9eacac98

---
+++ Quaver/src/Audio/AudioHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagedBass;
+using Configuration = Quaver.Config.Configuration;
+
+namespace Quaver.Audio
+{
+    internal static class AudioHandler
+    {
+        /// <summary>
+        ///     Contains all of the audio streams which will be continuously freed throughout the game's lifecycle.
+        /// </summary>
+        internal static List<AudioStream> AllStreams { get; set; } = new List<AudioStream>();
+
+        /// <summary>
+        ///     Changes the master volume of all streams
+        /// </summary>
+        internal static void ChangeMasterVolume()
+        {
+            Bass.GlobalStreamVolume = Configuration.VolumeGlobal * 100;
+        }
+
+        /// <summary>
+        ///     Frees all available streams, ran continuously throughout the game.
+        ///     See: QuaverGame.elapsedEventHandler
+        /// </summary>
+        internal static void FreeAvailableStreams()
+        {
+            var newStreamList = new List<AudioStream>();
+            Console.WriteLine(AllStreams.Count);
+
+            foreach (var audioStream in AllStreams)
+            {
+                if (Bass.ChannelIsActive(audioStream.Stream) == PlaybackState.Stopped)
+                    Bass.StreamFree(audioStream.Stream);
+                else
+                    newStreamList.Add(audioStream);
+            }
+
+            // Replace the list of streams with the new list of active ones.
+            AllStreams = newStreamList;
+        }
+    }
+}
