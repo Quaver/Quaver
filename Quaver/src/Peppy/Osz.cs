eaced274654b89bad5177df16811ea62709eeb5a
Swan <contact@swan.moe>
2017-11-04 16:44:52 -0400

    Add: Base for importing Peppy Maps
    Former-commit-id: 814405f1c8a9c528db000d885ad0eda2b9f04d5f [formerly 002669883bd1bce83469b65d5b934c3e45826474]
    Former-commit-id: cf4e62cf6eec3904c2bfaeeb6e74a46fdf67d2dd

---
+++ Quaver/src/Peppy/Osz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quaver.Peppy
+{
+    internal class Osz
+    {
+        /// <summary>
+        ///     Upon clicking the import button, we'll want to display an open file dialog.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void OnImportButtonClick(object sender, EventArgs e)
+        {
+            // Create the openFileDialog object.
+            var openFileDialog = new OpenFileDialog()
+            {
+                InitialDirectory = "c:\\",
+                Filter = "Peppy Beatmap Set (*.osz)|*.osz",
+                FilterIndex = 0,
+                RestoreDirectory = true
+            };
+
+            // If the dialog couldn't be shown, that's an issue, so we'll return for now.
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            // Grab the path of the specifed file.
+            var fileName = openFileDialog.InitialDirectory + openFileDialog.FileName;
+            Console.WriteLine(fileName);
+
+            // Extract the .osu & relevant audio files, and attempt to convert them.
+            // Once fully converted, create a new directory in the songs folder and 
+            // tell GameBase that the import queue is ready. Depending on the current state,
+            // we may import them automatically.
+        }
+    }
+}
