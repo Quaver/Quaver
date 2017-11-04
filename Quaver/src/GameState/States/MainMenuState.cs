c613bf674a9295c48f931aebe4bb2c0c524ab4b5
staravia <denysenesouma@gmail.com>
2017-11-03 18:31:54 -0700

    add: MainMenuState
    Former-commit-id: 09ae287f021fe8d9e7cb930a5c256458322a7666 [formerly dcf608cfcc43cb9526a5fe0433ced315196e5c41]
    Former-commit-id: b1329297e731148ec0138c710879cf086b7b21a9

---
+++ Quaver/src/GameState/States/MainMenuState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Quaver.GameState
+{
+    internal class MainMenuState : IGameState
+    {
+        public State CurrentState { get; set; } = State.MainMenu;
+
+        public void Initialize() { }
+
+        public void LoadContent() { }
+
+        public void UnloadContent() { }
+
+        public void Update(GameTime gameTime) { }
+
+        public void Draw() { }
+    }
+}
