fbd29a91bdba7e46c22382f42a79d2c4cc6ef78d
Swan <contact@swan.moe>
2017-11-03 20:47:01 -0400

    Change: Remove KeyboardState & MouseState from IInputManager

--- Quaver/src/Input/IInputManager.cs
+++ Quaver/src/Input/IInputManager.cs
@@ -14,15 +14,5 @@ namespace Quaver.Input
         ///     The current state for the specifc input manager
         /// </summary>
         State CurrentState { get; set; }
-
-        /// <summary>
-        ///     The current keyboard state.
-        /// </summary>
-        KeyboardState KeyboardState { get; set; }
-
-        /// <summary>
-        ///     The current Mouse State
-        /// </summary>
-        MouseState MouseState { get; set; }
     }
 }
