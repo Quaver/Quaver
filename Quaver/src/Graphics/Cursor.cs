4eb3765dacc1ef77bc71b27f4023df1d777be5dd
staravia <denysenesouma@gmail.com>
2017-11-06 00:04:10 -0800

    Update: Mouse Click Animation

--- Quaver/src/Graphics/Cursor.cs
+++ Quaver/src/Graphics/Cursor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using Quaver.Graphics;
 using Quaver.Main;
 using Quaver.Utility;
@@ -15,19 +16,57 @@ namespace Quaver.Graphics
     /// </summary>
     class Cursor : Sprite
     {
+        //Mouse Click
+        private bool MouseDown { get; set; }
+
+        //Cursor Size
+        private float CursorSize { get; set; } = 40;
+
+        //Click Size
+        private float ClickCurrentSize { get; set; }
+        private float ClickTargetSize { get; set; }
+
         public Cursor()
         {
-            Size = new Vector2(40, 40);
+            Size = new Vector2(CursorSize, CursorSize);
             Image = GameBase.LoadedSkin.NoteHitObject1;
             Alignment = Alignment.TopLeft;
             Position = Vector2.Zero;
         }
 
+        private void MouseClicked()
+        {
+            //Do stuff to cursor when mouse is clicked
+        }
+
         public override void Update(double dt)
         {
+            //Mouse Click
+            if (GameBase.MouseState.LeftButton == ButtonState.Pressed)
+            {
+                if (!MouseDown)
+                {
+                    MouseDown = true;
+                    MouseClicked();
+                    ClickTargetSize = CursorSize / 3;
+                    ClickCurrentSize = ClickTargetSize;
+                }
+            }
+            else
+            {
+                MouseDown = false;
+                ClickTargetSize = 0;
+            }
+
+            //Resize Cursor
+            ClickCurrentSize = Util.Tween(ClickTargetSize, ClickCurrentSize, Math.Min(dt / 40, 1));
+            SizeX = CursorSize + ClickCurrentSize;
+            SizeY = SizeX;
+
+            //Move Cursor
             Position = Util.PointToVector2(GameBase.MouseState.Position);
-            PositionX -= 20;
-            PositionY -= 20;
+            PositionX -= (CursorSize + ClickCurrentSize) /2;
+            PositionY -= (CursorSize + ClickCurrentSize) /2;
 
             UpdateRect();
             base.Update(dt);
