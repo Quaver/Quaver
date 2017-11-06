5508394745f63e27a1cd8145d2bd9fd8533e5579
Swan <contact@swan.moe>
2017-11-06 12:55:39 -0500

    Add: ICommand interface
    Former-commit-id: 7b552f1625ee0ffc3eca7387649165857c65e8ec [formerly 136bf95156a67fabc93e45e58825c7c9dcbcbe55]
    Former-commit-id: 5334024b1f62edc496eac116f7b4e86d0ed47649

---
+++ Quaver/src/Commands/ICommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Commands
+{
+    public interface ICommand
+    {
+        /// <summary>
+        ///     The name of the command
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        ///     How many arguments the command takes
+        /// </summary>
+        int Args { get; set; }
+
+        /// <summary>
+        ///     Description of the command
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        ///     Command usage
+        /// </summary>
+        string Usage { get; set; }
+
+        /// <summary>
+        ///     Executes the command.
+        /// </summary>
+        void Execute();
+    }
+}
