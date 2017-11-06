db81b63613f73689fbb800bd262e1b2548ba4985
Swan <contact@swan.moe>
2017-11-06 12:56:20 -0500

    Add: Command to list beatmap sets
    Former-commit-id: ea2a609070bdabca638d7729186528148ce1a7bf [formerly 2ab91082c803e7a0d7ea60f2921ebcf1be408021]
    Former-commit-id: 291521f1a444b05ac36433531436e818b8131cc2

---
+++ Quaver/src/Commands/ListMapsetsCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.Main;
+
+namespace Quaver.Commands
+{
+    internal class ListMapsetsCommand : ICommand
+    {
+        public string Name { get; set; } = "LISTMAPSETS";
+
+        public int Args { get; set; } = 0;
+
+        public string Description { get; set; } = "Lists all of the loaded beatmap sets";
+
+        public string Usage { get; set; } = "> listmapsets";
+
+        public void Execute()
+        {
+            var commandString = new StringBuilder();
+
+            //  It's a dictonary where the keys are strings, so please.
+            var i = 0;
+            foreach (var mapset in GameBase.Beatmaps)
+            {
+                commandString.AppendLine($"[{i}] {new DirectoryInfo(mapset.Key).Name}  - {mapset.Value.Count} maps");
+                i++;
+            }
+            
+            Console.WriteLine(commandString);
+        }
+    }
+}
