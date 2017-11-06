8464bec08698fb729f7aa18344374ecd0e8ceec4
Swan <contact@swan.moe>
2017-11-06 12:56:00 -0500

    Add: Command Handler to execute correct command
    Former-commit-id: 0ba3d00eacd6dd2a30f4a2d240cb80ebcdbd0ea5 [formerly cfeee58f5eb238496f8a2ca5ab7826e7ba72f44c]
    Former-commit-id: 4bfff207b2f9dd0e2b686d590407da8864c5cea3

---
+++ Quaver/src/Commands/CommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Commands
+{
+    internal class CommandHandler
+    {
+        /// <summary>
+        ///     Stores all of the game's commands.
+        /// </summary>
+        private static ICommand[] Commands { get; set; } =
+        {
+            new ListMapsetsCommand()       
+        };
+
+        /// <summary>
+        ///     Executes a given command if it exists
+        /// </summary>
+        internal static void Execute(string input)
+        {
+            var args = input.Split(' ');
+            var command = args[0];
+
+            // Check if the command exists, and if the args are correct
+            foreach (var cmd in Commands)
+            {
+                if (!command.ToLower().Equals(cmd.Name.ToLower()))
+                    continue;
+
+                cmd.Execute();
+                return;
+            }
+                    
+            Console.WriteLine("Command not found.");
+        }
+    }
+}
