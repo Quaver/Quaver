66e6c967bbb0eba61114134036aec963bccb24e4
Swan <contact@swan.moe>
2017-11-06 13:06:51 -0500

    Add: Help Command
    Former-commit-id: 327180d9ee02837b1d250b1bb33b2567a2418b7e [formerly b1f83e60c6932771de628f2c44641b9a9d67830e]
    Former-commit-id: 4feeffdf67321e802b0fd61a457958e5950db8ff

---
+++ Quaver/src/Commands/HelpCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Commands
+{
+    internal class HelpCommand : ICommand
+    {
+        public string Name { get; set; } = "HELP";
+
+        public int Args { get; set; } = 0;
+
+        public string Description { get; set; } = "Lists out all current commands";
+
+        public string Usage { get; set; } = "> help";
+
+        public void Execute()
+        {
+            var commandString = new StringBuilder();
+            commandString.AppendLine();
+            commandString.AppendLine("The following are a list of commands you can execute:");
+            commandString.AppendLine();
+
+            foreach (var cmd in CommandHandler.Commands)
+                commandString.AppendLine($"{cmd.Name.ToUpper()} - {cmd.Description}");
+            
+            Console.WriteLine(commandString);
+        }
+    }
+}
