57272881f98c9b5e943c0a53efb5f4bcbee9cfec
Swan <contact@swan.moe>
2017-11-07 10:20:41 -0500

    Add: Command to clear console
    Former-commit-id: 5bddfb64f611a745313ffb9bb0944ea11aa9a4aa [formerly 203c1e289aad6c620190a7e1a674c15b77c2b8b2]
    Former-commit-id: dad4433616db2908b86b50162f385b46f266d17e

---
+++ Quaver/src/Commands/Command/ClearCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Commands
+{
+    internal class ClearCommand : ICommand
+    {
+        public string Name { get; set; } = "CLEAR";
+
+        public int Args { get; set; } = 0;
+
+        public string Description { get; set; } = "Clears the console of any logs.";
+
+        public string Usage { get; set; } = "> clear";
+
+        public void Execute()
+        {
+            Console.Clear();
+        }
+    }
+}
