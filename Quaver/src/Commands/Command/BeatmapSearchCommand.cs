6a0f80a9b62b4da3f03e899b858c7741b12dc8ae
Swan <contact@swan.moe>
2017-11-07 10:54:39 -0500

    Add: Beatmap search command :thumbsup:
    Former-commit-id: 7f02442d0c8a4858f2b6e7f2202ecc55c70b579b [formerly ef6d0ab2e2bcc0b624e6af81a5a6355b52d7781e]
    Former-commit-id: 054db6f1f113179491741c6833aa925bba714e2d

---
+++ Quaver/src/Commands/Command/BeatmapSearchCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.Beatmaps;
+using Quaver.Main;
+
+namespace Quaver.Commands
+{
+    internal class BeatmapSearchCommand : ICommand
+    {
+        public string Name { get; set; } = "SEARCH";
+
+        public int Args { get; set; } = 0;
+
+        public string Description { get; set; } = "Find a list of beatmaps that match a given search term";
+
+        public string Usage { get; set; } = "> search <query>";
+
+        public void Execute(string[] args)
+        {
+            var query = string.Join(" ", args.Skip(1).ToArray());
+
+            var foundMapsets = BeatmapUtils.SearchBeatmaps(GameBase.Beatmaps, query);
+
+            var cmdString = new StringBuilder();
+            cmdString.AppendLine();
+            cmdString.AppendLine($"Found {foundMapsets.Count} mapsets for search: {query}");
+            cmdString.AppendLine();
+
+            var i = 0;
+            foreach (var mapset in foundMapsets)
+            {
+                foreach (var map in mapset.Value)
+                {
+                    cmdString.AppendLine($"[{i}] {map.Artist} - {map.Title} [{map.DifficultyName}]");
+                    i++;
+                }
+            }
+
+            Console.WriteLine(cmdString);;
+        }
+    }
+}
