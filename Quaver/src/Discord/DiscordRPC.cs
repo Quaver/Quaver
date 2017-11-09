cc13d9db3780d1edebbc85285c3d0bf0fbd41424
Swan <contact@swan.moe>
2017-11-09 17:25:03 -0500

    Add: Set up Discord RPC

---
+++ Quaver/src/Discord/DiscordRPC.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Discord
+{
+    public class DiscordRPC
+    {
+        [DllImport("discord-rpc", EntryPoint = "Discord_Initialize", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Initialize(string applicationId, ref EventHandlers handlers, bool autoRegister, string optionalSteamId);
+
+        [DllImport("discord-rpc", EntryPoint = "Discord_Shutdown", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Shutdown();
+
+        [DllImport("discord-rpc", EntryPoint = "Discord_RunCallbacks", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void RunCallbacks();
+
+        [DllImport("discord-rpc", EntryPoint = "Discord_UpdatePresence", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void UpdatePresence(ref RichPresence presence);
+
+        [DllImport("discord-rpc", EntryPoint = "Discord_Respond", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Respond(string userId, Reply reply);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void ReadyCallback();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void DisconnectedCallback(int errorCode, string message);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void ErrorCallback(int errorCode, string message);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void JoinCallback(string secret);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void SpectateCallback(string secret);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void RequestCallback(JoinRequest request);
+
+        public struct RichPresence
+        {
+            public string state; /* max 128 bytes */
+            public string details; /* max 128 bytes */
+            public long startTimestamp;
+            public long endTimestamp;
+            public string largeImageKey; /* max 32 bytes */
+            public string largeImageText; /* max 128 bytes */
+            public string smallImageKey; /* max 32 bytes */
+            public string smallImageText; /* max 128 bytes */
+            public string partyId; /* max 128 bytes */
+            public int partySize;
+            public int partyMax;
+            public string matchSecret; /* max 128 bytes */
+            public string joinSecret; /* max 128 bytes */
+            public string spectateSecret; /* max 128 bytes */
+            public bool instance;
+        }
+
+        public struct EventHandlers
+        {
+            public ReadyCallback readyCallback;
+            public DisconnectedCallback disconnectedCallback;
+            public ErrorCallback errorCallback;
+            public JoinCallback joinCallback;
+            public SpectateCallback spectateCallback;
+            public RequestCallback requestCallback;
+        }
+
+        [Serializable]
+        public struct JoinRequest
+        {
+            public string userId;
+            public string username;
+            public string avatar;
+        }
+
+        public enum Reply
+        {
+            No = 0,
+            Yes = 1,
+            Ignore = 2
+        }
+    }
+}
