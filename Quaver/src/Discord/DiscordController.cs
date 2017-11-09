a21bbd6efb3bc27fb114a56cb86138f6fcaa41af
Swan <contact@swan.moe>
2017-11-09 17:25:03 -0500

    Add: Set up Discord RPC
    Former-commit-id: b381ad2032c5371a397e292482ea9f202cb40299 [formerly 8ce3ecd5697f9bc0ba05b467a4277f264815955b]
    Former-commit-id: d31d9e91ede083a3f877a0b3575dd9b2291aa60d

---
+++ Quaver/src/Discord/DiscordController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Discord
+{
+    internal class DiscordController
+    {
+        public DiscordRPC.RichPresence presence;
+        DiscordRPC.EventHandlers handlers;
+        public string applicationId = "376180410490552320";
+        public string optionalSteamId;
+
+        /// <summary>
+        ///     Initializes Discord RPC
+        /// </summary>
+        public void Initialize()
+        {
+            handlers = new DiscordRPC.EventHandlers();
+            handlers.readyCallback = ReadyCallback;
+            handlers.disconnectedCallback += DisconnectedCallback;
+            handlers.errorCallback += ErrorCallback;
+            DiscordRPC.Initialize(applicationId, ref handlers, true, optionalSteamId);
+        }
+
+        public void ReadyCallback()
+        {
+            Console.WriteLine("Discord RPC is ready!");
+        }
+
+        public void DisconnectedCallback(int errorCode, string message)
+        {
+            Console.WriteLine($"Error: {errorCode} - {message}");
+        }
+
+        public void ErrorCallback(int errorCode, string message)
+        {
+            Console.WriteLine($"Error: {errorCode} - {message}");
+        }
+    }
+}
