e72bd1f1d6bcc26df1a1495f8ecffc1ed2d035ed
Swan <contact@swan.moe>
2017-11-11 14:07:56 -0500

    Refactor: Clean up MainMenu & SongLoading State
    Former-commit-id: 50b804fe2a673a4fdfb671cc94cbc707b3954e4d [formerly 6e57d36c49271a33955e686bd634043a490f23ee]
    Former-commit-id: 9a7f69ba9862bc3d520a9c3e296c4a3858f856ef

--- Quaver/src/GameState/States/SongLoadingState.cs
+++ Quaver/src/GameState/States/SongLoadingState.cs
@@ -14,7 +14,14 @@ namespace Quaver.GameState.States
 {
     internal class SongLoadingState : IGameState
     {
+        /// <summary>
+        ///     Current State
+        /// </summary>
         public State CurrentState { get; set; } = State.LoadingScreen;
+
+        /// <summary>
+        ///     Update Ready
+        /// </summary>
         public bool UpdateReady { get; set; }
 
         /// <summary>
@@ -22,6 +29,38 @@ namespace Quaver.GameState.States
         ///     If we've successfully loaded it, move onto the play state.
         /// </summary>
         public void Initialize()
+        {
+            LoadBeatmap();
+        }
+
+        /// <summary>
+        ///     Load Content
+        /// </summary>
+        public void LoadContent() { }
+
+        /// <summary>
+        ///     Unload Content
+        /// </summary>
+        public void UnloadContent() { }
+
+        /// <summary>
+        ///     Update
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime) {}
+
+        /// <summary>
+        ///     Draw
+        /// </summary>
+        public void Draw()
+        {
+            GameBase.GraphicsDevice.Clear(Color.Red);
+        }
+
+        /// <summary>
+        ///     Responsible for loading the beatmap and switching the state.
+        /// </summary>
+        private void LoadBeatmap()
         {
             try
             {
@@ -60,60 +99,6 @@ namespace Quaver.GameState.States
             {
                 Console.WriteLine(e);
             }
-        }
-
-        public void LoadContent()
-        {
-            /*try
-            {
-                Console.WriteLine("[SONG LOADING STATE] Attempting to load beatmap...");
-
-                // Throw an exception if there is no selected beatmap.
-                if (GameBase.SelectedBeatmap == null)
-                    throw new Exception("No selected beatmap, we should not be on this screen!!!");
-
-                // Try and parse the .qua and check if it is valid.
-                var quaPath = $"{Config.Configuration.SongDirectory}/{GameBase.SelectedBeatmap.Directory}/{GameBase.SelectedBeatmap.Path}";
-
-                var qua = new Qua(quaPath);
-
-                if (!qua.IsValidQua)
-                    throw new Exception("[SONG LOADING STATE] The .qua file could NOT be loaded!");
-
-                // Set the beatmap's Qua. 
-                // We parse it and set it each time the player is going to play to kmake sure they are
-                // actually playing the correct map.
-                GameBase.SelectedBeatmap.Qua = qua;
-                Console.WriteLine("[SONG LOADING STATE] Qua successfully loaded.");
-
-                // Attempt to load the audio.
-                GameBase.SelectedBeatmap.LoadAudio();
-
-                if (GameBase.SelectedBeatmap.Song.GetAudioLength() < 1)
-                    throw new Exception("[SONG LOADING STATE] Audio file could not be loaded.");
-
-                Console.WriteLine("[SONG LOADING STATE] Audio file successfully loaded.");
-
-                // Get Beatmap MD5
-                var md5 = BeatmapUtils.GetMd5Checksum(quaPath);
-                Console.WriteLine($"[SONG LOADING STATE] Successfully loaded beatmap: {md5}");
-
-                // Load Play State
-                GameStateManager.Instance.ChangeState(new PlayScreenState(md5));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }*/
-        }
-
-        public void UnloadContent() { }
-
-        public void Update(GameTime gameTime) {}
-
-        public void Draw()
-        {
-            GameBase.GraphicsDevice.Clear(Color.Red);
         }
     }
 }
