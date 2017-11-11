3066d797b903e0e4155c4ecdb0c9c5a70370d5f4
staravia <denysenesouma@gmail.com>
2017-11-10 20:31:58 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Quaver/Quaver.csproj
    
    
    Former-commit-id: d7f5e6d09d573a839927683fb833fefb90d9d8ab [formerly 1cc422a793b3ca0c999f11374a174e3c923772c0]
    Former-commit-id: 6f77a06a3f2ae31d75118819399d504bfb5b1024

---
+++ Quaver/src/Graphics/UI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.Main;
+
+namespace Quaver.Graphics
+{
+    internal class UI
+    {
+        /// <summary>
+        ///     diff-select-mask 
+        /// </summary>
+        internal Texture2D DiffSelectMask { get; set; }
+
+        /// <summary>
+        ///     set-select-mask 
+        /// </summary>
+        internal Texture2D SetSelectMask { get; set; }
+
+        /// <summary>
+        ///     blank-box
+        /// </summary>
+        internal Texture2D BlankBox { get; set; }
+
+        /// <summary>
+        ///     hollow-box
+        /// </summary>
+        internal Texture2D HollowBox { get; set; }
+
+        /// <summary>
+        ///     Loads all the ui elements into content
+        /// </summary>
+        public void LoadElementsAsContent()
+        {
+            DiffSelectMask = GameBase.Content.Load<Texture2D>("diff-select-mask");
+            SetSelectMask = GameBase.Content.Load<Texture2D>("set-select-mask");
+            BlankBox = GameBase.Content.Load<Texture2D>("blank-box");
+            HollowBox = GameBase.Content.Load<Texture2D>("hollow-box");
+        }
+    }
+}
