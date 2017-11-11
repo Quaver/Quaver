93952c0612c602f4c22349fbef71ff28fe40e70b
staravia <denysenesouma@gmail.com>
2017-11-11 08:34:33 -0800

    Update: syntax

--- Quaver/src/GameState/States/SongLoadingState.cs
+++ Quaver/src/GameState/States/SongLoadingState.cs
@@ -23,10 +23,6 @@ namespace Quaver.GameState.States
         /// </summary>
         public void Initialize()
         {
-        }
-
-        public void LoadContent()
-        {
             try
             {
                 Console.WriteLine("[SONG LOADING STATE] Attempting to load beatmap...");
@@ -68,6 +64,51 @@ namespace Quaver.GameState.States
             {
                 Console.WriteLine(e);
             }
+        }
+
+        public void LoadContent()
+        {
+            /*try
+            {
+                Console.WriteLine("[SONG LOADING STATE] Attempting to load beatmap...");
+
+                // Throw an exception if there is no selected beatmap.
+                if (GameBase.SelectedBeatmap == null)
+                    throw new Exception("No selected beatmap, we should not be on this screen!!!");
+
+                // Try and parse the .qua and check if it is valid.
+                var quaPath = $"{Config.Configuration.SongDirectory}/{GameBase.SelectedBeatmap.Directory}/{GameBase.SelectedBeatmap.Path}";
+
+                var qua = new Qua(quaPath);
+
+                if (!qua.IsValidQua)
+                    throw new Exception("[SONG LOADING STATE] The .qua file could NOT be loaded!");
+
+                // Set the beatmap's Qua. 
+                // We parse it and set it each time the player is going to play to kmake sure they are
+                // actually playing the correct map.
+                GameBase.SelectedBeatmap.Qua = qua;
+                Console.WriteLine("[SONG LOADING STATE] Qua successfully loaded.");
+
+                // Attempt to load the audio.
+                GameBase.SelectedBeatmap.LoadAudio();
+
+                if (GameBase.SelectedBeatmap.Song.GetAudioLength() < 1)
+                    throw new Exception("[SONG LOADING STATE] Audio file could not be loaded.");
+
+                Console.WriteLine("[SONG LOADING STATE] Audio file successfully loaded.");
+
+                // Get Beatmap MD5
+                var md5 = BeatmapUtils.GetMd5Checksum(quaPath);
+                Console.WriteLine($"[SONG LOADING STATE] Successfully loaded beatmap: {md5}");
+
+                // Load Play State
+                GameStateManager.Instance.ChangeState(new PlayScreenState(md5));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }*/
         }
 
         public void UnloadContent() { }
