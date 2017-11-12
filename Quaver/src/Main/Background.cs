0b49737f39d3343848f26ef9efc14ad8eb4b758a
staravia <denysenesouma@gmail.com>
2017-11-11 20:34:26 -0800

    Create: Background Manager
    Former-commit-id: 42dd949a7dfd4413bbb1e6fe9b107b873c900b1a [formerly 3d85cbfbd74fd2d7a39dc017133642e40ad0c0ff]
    Former-commit-id: 6a096abfe7b642aab3562181973e74fcac3104ff

---
+++ Quaver/src/Main/Background.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.Graphics;
+
+namespace Quaver.Main
+{
+    class BackgroundManager
+    {
+        public static Sprite Background;
+
+        public static void Initialize()
+        {
+            Background = new Sprite()
+            {
+                SizeX = GameBase.Window.Size.X,
+                SizeY = GameBase.Window.Size.Y,
+                Image = GameBase.LoadedSkin.NoteHitObject1
+            };
+        }
+
+        public static void UnloadContent()
+        {
+            
+        }
+
+        public static void Update(double dt)
+        {
+            
+        }
+
+        public static void Change(Texture2D newBG)
+        {
+            Background.Image = newBG;
+        }
+
+        public static void Draw()
+        {
+            Background.Draw();
+        }
+
+    }
+}
