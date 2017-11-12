a55586e1ba79fabf45d65c12401d81f112b6e1d7
staravia <denysenesouma@gmail.com>
2017-11-12 11:27:56 -0800

    Update: Font
    Former-commit-id: f8c423edda8a7e577d5e6804d26e40bb2be84444 [formerly fbf29ce4266460382ea0e53f92e5b05ffe42cc9a]
    Former-commit-id: d70e5985fb64422900f671fe4b29bdcdb6870c0e

--- Quaver/src/Graphics/Sprite/TextBoxSprite.cs
+++ Quaver/src/Graphics/Sprite/TextBoxSprite.cs
@@ -77,7 +77,7 @@ namespace Quaver.Graphics
         /// <summary>
         ///     The font of this object
         /// </summary>
-        public SpriteFont Font { get; } = GameBase.Content.Load<SpriteFont>("testFont");
+        public SpriteFont Font { get; } = GameBase.Content.Load<SpriteFont>("RalewayMedium");
 
         /// <summary>
         ///     The color of this object
