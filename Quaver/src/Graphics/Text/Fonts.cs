4757ff07a4c551b9f973731b22c3638d37402942
staravia <denysenesouma@gmail.com>
2017-11-12 11:48:35 -0800

    Add: Quaver.Graphics.Fonts

---
+++ Quaver/src/Graphics/Text/Fonts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.Main;
+
+namespace Quaver.Graphics
+{
+    internal class Fonts
+    {
+        public SpriteFont Medium12 { get; } = GameBase.Content.Load<SpriteFont>("Medium12");
+        public SpriteFont Medium16 { get; } = GameBase.Content.Load<SpriteFont>("Medium16");
+        public SpriteFont Medium24 { get; } = GameBase.Content.Load<SpriteFont>("Medium24");
+        public SpriteFont Medium48 { get; } = GameBase.Content.Load<SpriteFont>("Medium48");
+        public SpriteFont Bold12 { get; } = GameBase.Content.Load<SpriteFont>("Bold12");
+        public SpriteFont Test { get; } = GameBase.Content.Load<SpriteFont>("testFont");
+    }
+}
