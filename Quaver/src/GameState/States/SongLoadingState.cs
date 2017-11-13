8b6762e7cc3252fec2e813878165b4248d23308c
staravia <denysenesouma@gmail.com>
2017-11-13 12:28:17 -0800

    Update: Logging in SongLoading
    Former-commit-id: 4d20be2452c7b122e430595e288ab4bbc6871e26 [formerly 7b0cf1d27b9e0bef4c71cf0128bd41a0a1371f49]
    Former-commit-id: 7b3d2433ca8e46e870d5347636588c9bbb94bde0

--- Quaver/src/GameState/States/SongLoadingState.cs
+++ Quaver/src/GameState/States/SongLoadingState.cs
@@ -85,6 +85,7 @@ namespace Quaver.GameState.States
                 if (GameBase.SelectedBeatmap.Song.GetAudioLength() < 1)
                     throw new Exception("[SONG LOADING STATE] Audio file could not be loaded.");
 
+                LogManager.ConsoleLog("[SONG LOADING STATE]: Done Loading Beatmap", ConsoleColor.DarkBlue);
                 //Initialize Gameplay
                 //InitializeGameplay(qua);
 
@@ -123,6 +124,7 @@ namespace Quaver.GameState.States
                 var quaPath = $"{Config.Configuration.SongDirectory}/{GameBase.SelectedBeatmap.Directory}/{GameBase.SelectedBeatmap.Path}";
                 var md5 = BeatmapUtils.GetMd5Checksum(quaPath);
                 GameBase.GameStateManager.ChangeState(new PlayScreenState(md5));
+                LogManager.ConsoleLog("[SONG LOADING STATE]: Done Changing States", ConsoleColor.DarkBlue);
             }
             catch (Exception ex)
             {
