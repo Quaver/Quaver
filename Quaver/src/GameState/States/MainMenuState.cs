4cf75cddf443052787a91d9b0292fb8010494575
Swan <contact@swan.moe>
2017-11-14 19:11:54 -0500

    Add: Change Window title to current map in play state

--- Quaver/src/GameState/States/MainMenuState.cs
+++ Quaver/src/GameState/States/MainMenuState.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms.VisualStyles;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -49,6 +49,8 @@ namespace Quaver.GameState.States
 
         public void Initialize()
         {
+            GameBase.GameWindow.Title = "Quaver";
+
             // Initialize the main menu's audio player.
             MenuAudioPlayer.Initialize();
 
