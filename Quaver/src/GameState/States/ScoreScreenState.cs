94d1a0ca7a3466575599f87bc94d5c65749ac48d
staravia <denysenesouma@gmail.com>
2017-11-16 21:36:49 -0800

    Add: ScoreScreen State

---
+++ Quaver/src/GameState/States/ScoreScreenState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Quaver.GameState.States
+{
+    class ScoreScreenState : IGameState
+    {
+        public State CurrentState { get; set; } = State.ScoreScreen;
+        public bool UpdateReady { get; set; }
+
+        public void Initialize() { }
+
+        public void LoadContent() { }
+
+        public void UnloadContent() { }
+
+        public void Update(GameTime gameTime) { }
+
+        public void Draw() { }
+    }
+}
