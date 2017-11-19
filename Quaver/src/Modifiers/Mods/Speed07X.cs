eac34978b667e7e3edbf92b3137c1fd6c99d671c
Swan <contact@swan.moe>
2017-11-19 18:41:49 -0500

    Add: Individual mod for each speed
    Former-commit-id: 5a8d1d5423f74c0f4f7f453afaca6ba6c14694ca [formerly 7be25fb7d77befdde3d13ec320673bab0d6f1d5e]
    Former-commit-id: af70d2d6f0a7e9a26de73e467a117e1752000740

---
+++ Quaver/src/Modifiers/Mods/Speed07X.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Quaver.Logging;
+
+namespace Quaver.Modifiers.Mods
+{
+    internal class Speed07X : IMod
+    {
+        /// <summary>
+        ///     The name of the mod.
+        /// </summary>
+        public string Name { get; set; } = "0.7x";
+
+        /// <summary>
+        ///     The identifier of the mod.
+        /// </summary>
+        public ModIdentifier ModIdentifier { get; set; } = ModIdentifier.Speed07X;
+
+        /// <summary>
+        ///     The type of mod as defined in the enum
+        /// </summary>
+        public ModType Type { get; set; } = ModType.Speed;
+
+        /// <summary>
+        ///     The description of the Mod
+        /// </summary>
+        public string Description { get; set; } = "Sets the song's speed to 0.7x";
+
+        /// <summary>
+        ///     Is the mod ranked?
+        /// </summary>
+        public bool Ranked { get; set; } = false;
+
+        /// <summary>
+        ///     The addition to the score multiplier this mod will have
+        /// </summary>
+        public float ScoreMultiplierAddition { get; set; } = 0;
+
+        /// <summary>
+        ///     The identifier of mods that are incompatible with this one.
+        /// </summary>
+        public ModIdentifier[] IncompatibleMods { get; set; } =
+        {
+            ModIdentifier.Speed05X, ModIdentifier.Speed06X, ModIdentifier.Speed08X, ModIdentifier.Speed09X,
+            ModIdentifier.Speed11X, ModIdentifier.Speed12X, ModIdentifier.Speed13X, ModIdentifier.Speed14X, ModIdentifier.Speed15X,
+            ModIdentifier.Speed16X, ModIdentifier.Speed17X, ModIdentifier.Speed18X, ModIdentifier.Speed19X, ModIdentifier.Speed20X
+        };
+
+        /// <summary>
+        ///     All the mod logic should go here, setting unique variables. NEVER call this directly. Always use
+        ///     ModManager.AddMod();
+        /// </summary>
+        public void InitializeMod()
+        {
+            GameBase.GameClock = 0.7f;
+            Logger.Log($"Speed is now set to {GameBase.GameClock}x", Color.Cyan);
+
+            // Change the song's speed
+            if (GameBase.SelectedBeatmap.Song != null)
+                GameBase.SelectedBeatmap.Song.ChangeSongSpeed();
+        }
+    }
+}
---
+++ Quaver/src/Modifiers/Mods/Speed15X.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Quaver.Logging;
+
+namespace Quaver.Modifiers.Mods
+{
+    internal class Speed15X : IMod
+    {
+        /// <summary>
+        ///     The name of the mod.
+        /// </summary>
+        public string Name { get; set; } = "1.5x";
+
+        /// <summary>
+        ///     The identifier of the mod.
+        /// </summary>
+        public ModIdentifier ModIdentifier { get; set; } = ModIdentifier.Speed15X;
+
+        /// <summary>
+        ///     The type of mod as defined in the enum
+        /// </summary>
+        public ModType Type { get; set; } = ModType.Speed;
+
+        /// <summary>
+        ///     The description of the Mod
+        /// </summary>
+        public string Description { get; set; } = "Sets the song's speed to 1.5x";
+
+        /// <summary>
+        ///     Is the mod ranked?
+        /// </summary>
+        public bool Ranked { get; set; } = false;
+
+        /// <summary>
+        ///     The addition to the score multiplier this mod will have
+        /// </summary>
+        public float ScoreMultiplierAddition { get; set; } = 0;
+
+        /// <summary>
+        ///     The identifier of mods that are incompatible with this one.
+        /// </summary>
+        public ModIdentifier[] IncompatibleMods { get; set; } =
+        {
+            ModIdentifier.Speed05X, ModIdentifier.Speed06X, ModIdentifier.Speed07X, ModIdentifier.Speed08X, ModIdentifier.Speed09X,
+            ModIdentifier.Speed11X, ModIdentifier.Speed12X, ModIdentifier.Speed13X, ModIdentifier.Speed14X, 
+            ModIdentifier.Speed16X, ModIdentifier.Speed17X, ModIdentifier.Speed18X, ModIdentifier.Speed19X, ModIdentifier.Speed20X
+        };
+
+        /// <summary>
+        ///     All the mod logic should go here, setting unique variables. NEVER call this directly. Always use
+        ///     ModManager.AddMod();
+        /// </summary>
+        public void InitializeMod()
+        {
+            GameBase.GameClock = 1.5f;
+            Logger.Log($"Speed is now set to {GameBase.GameClock}x", Color.Cyan);
+
+            // Change the song's speed
+            if (GameBase.SelectedBeatmap.Song != null)
+                GameBase.SelectedBeatmap.Song.ChangeSongSpeed();
+        }
+    }
+}
