f3034725a04e31c90e56e9f9b2d0590bdb82444b
Swan <contact@swan.moe>
2017-11-20 15:30:55 -0500

    Add: Initial schema for replay frames
    Former-commit-id: 7076063f8c56336eba6844efa5670c8e163dae9d [formerly 26e82afb040d1ff0702d244c61c6a72c07e5b97d]
    Former-commit-id: 5bd146129cdaccc73f8c293e6150f0467485ef6a

---
+++ Quaver/src/Replays/ReplayFrame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Replays
+{
+    internal class ReplayFrame
+    {
+        /// <summary>
+        ///     The difference in song time of the current and last frames taken in milliseconds
+        /// </summary>
+        internal int TimeSinceLastFrame { get; set; }
+
+        /// <summary>
+        ///     The state of the keys during this frame.
+        /// </summary>
+        internal KeyPressState KeyPressState { get; set; }
+    }
+}
