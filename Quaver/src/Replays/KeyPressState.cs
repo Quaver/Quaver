14187b11678c70507ecc28d766c06e475c5bc56a
Swan <contact@swan.moe>
2017-11-20 15:30:21 -0500

    Add: Bit flags for Key Press State
    Former-commit-id: 19bd8bcb79250fcb5bef9bca2cc909c384bc9258 [formerly 7e31dbb3da5bd182b2b02b09833d7122363346d7]
    Former-commit-id: 248039dfcd1f2d62a6b3184670fa193d96b29480

---
+++ Quaver/src/Replays/KeyPressState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Replays
+{
+    /// <summary>
+    ///     This is the key press state that will be used in replay frames.
+    ///     In a replay frame, it will essentially be the bitwise combination of
+    ///     the keys pressed so that it can be easily read.
+    /// </summary>
+    [Flags]
+    internal enum KeyPressState
+    {
+        K1 = 1 << 0,
+        K2 = 1 << 1, 
+        K3 = 1 << 2, 
+        K4 = 1 << 3,
+        K5 = 1 << 4,
+        K6 = 1 << 5,
+        K7 = 1 << 6
+    }
+}
