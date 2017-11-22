667087177f72bddfb4d1d52ee75c9fbd21d83d6a
staravia <denysenesouma@gmail.com>
2017-11-21 22:26:10 -0800

    Update: Judge difficulty scales with gameclock
    Former-commit-id: 719adec638ae244385337bee3abd5863f422c7b6 [formerly 7889ce619a66b90f09f62dd8e4d89a541be6b34c]
    Former-commit-id: 25072fda80d4b7583336ade7fff43267c7347df5

--- Quaver/src/Gameplay/ScoreManager.cs
+++ Quaver/src/Gameplay/ScoreManager.cs
@@ -127,9 +127,9 @@ namespace Quaver.Gameplay
             JudgeDifficulty = od;
 
             //Create Difficulty Curve
-            var curve = (float)Math.Pow(od+1, -0.42);
-            HitWindowPress = new float[5] { 18, 80 * curve, 132 * curve, 170 * curve, 250 * curve };
-            HitWindowRelease = new float[4] { 30, HitWindowPress[1]*1.25f, HitWindowPress[2] * 1.25f, HitWindowPress[3] * 1.25f };
+            var curve = (float)Math.Pow(od+1, -0.42) * GameBase.GameClock;
+            HitWindowPress = new float[5] { 18 * GameBase.GameClock, 80 * curve, 132 * curve, 170 * curve, 250 * curve };
+            HitWindowRelease = new float[4] { 30 * GameBase.GameClock, HitWindowPress[1]*1.25f, HitWindowPress[2] * 1.25f, HitWindowPress[3] * 1.25f };
         }
 
         /// <summary>
