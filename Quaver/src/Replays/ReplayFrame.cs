671be7175a1f8516504e14728d1466b2812c5f19
Swan <contact@swan.moe>
2017-11-23 16:48:08 -0500

    Add: GameTime & SongTime to ReplayFrame
    Former-commit-id: 8330ac7764c4e82d10b8194a3be88bc5de44e0ee [formerly 8ba87b8a2411d5ff82e7d7ea74d1c8307b6b8fe3]
    Former-commit-id: 42a5924e995546462da7aaecf068357b8ddd28e3

--- Quaver/src/Replays/ReplayFrame.cs
+++ Quaver/src/Replays/ReplayFrame.cs
@@ -9,6 +9,16 @@ namespace Quaver.Replays
     internal class ReplayFrame
     {
         /// <summary>
+        ///     Holds the game time the frame was captured at
+        /// </summary>
+        internal long GameTime { get; set; }
+
+        /// <summary>
+        ///     The time in the song this replay occurred at.
+        /// </summary>
+        internal int SongTime { get; set; }
+
+        /// <summary>
         ///     The difference in song time of the current and last frames taken in milliseconds
         /// </summary>
         internal int TimeSinceLastFrame { get; set; }
