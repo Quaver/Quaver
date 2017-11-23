2df489663555eed2c3605a17b2953f927d3d15c1
Swan <contact@swan.moe>
2017-11-23 16:48:24 -0500

    Add: Completed replay frame capture system
    Former-commit-id: 0147c1a09c315da2026033597bed9a4a9184ee88 [formerly bf13705687f03c3943d1e893156277654304bf4f]
    Former-commit-id: b026a551854e5c989071efa2e5a59f236dd0d9e7

---
+++ Quaver/src/Replays/ReplayHelper.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using Quaver.Audio;
+using Quaver.Config;
+using Quaver.Logging;
+using Quaver.Modifiers;
+using Quaver.QuaFile;
+
+namespace Quaver.Replays
+{
+    /// <summary>
+    ///     Dedicated class for helping with replays
+    /// </summary>
+    internal class ReplayHelper
+    {
+        /// <summary>
+        ///     The interval at which to capture replays.
+        //      In this case, we're going to be capturing replays at a frame rate of 60 fps.
+        //      So we'll want to capture frames every 16.67ms or so.
+        /// </summary>
+        internal static float ReplayFpsInterval { get; set; } = 1000 / 60;
+
+        /// <summary>
+        ///     The previous task.
+        /// </summary>
+        internal static Task LastTask { get; set; }
+
+        /// <summary>
+        ///     Stores the path of the test replay file
+        /// </summary>
+        internal static string DebugFilePath { get; } = Configuration.DataDirectory + "/" + "last_replay.txt";
+
+        /// <summary>
+        ///     This adds the correct frames to replays, called every frame during gameplay
+        /// </summary>
+        internal static void AddReplayFrames(List<ReplayFrame> ReplayFrames, Qua qua)
+        {
+            if (LastTask == null || LastTask.IsCompleted)
+                LastTask = Task.Run(() =>
+                {
+                    // Get the elapsed time
+                    var elapsed = GameBase.GameTime.ElapsedMilliseconds;
+
+                    // Create the base of the new frame
+                    var frame = new ReplayFrame
+                    {
+                        GameTime = elapsed,
+                        SongTime = (int)SongManager.Position,
+                    };
+
+                    // Add the first frame to the list of replay frames.
+                    if (ReplayFrames.Count == 0)
+                    {
+                        // Check keyboard state
+                        CheckKeyboardstate(qua, frame);
+
+                        AppendToDebugFile(frame);
+
+                        ReplayFrames.Add(frame);
+                        return;
+                    }
+
+                    // Add the song time since the last frame
+                    frame.TimeSinceLastFrame = (int)(elapsed - ReplayFrames.Last().GameTime);
+                    
+                    // Grab the key press state of the last frame
+                    var lastKeyPressState = ReplayFrames.Last().KeyPressState;
+
+                    // Get the current keyboard state
+                    CheckKeyboardstate(qua, frame);
+
+                    // If the press state of the current frame isn't the same as the last
+                    // then this frame must be very important.
+                    if (lastKeyPressState != frame.KeyPressState)
+                    {
+                        AppendToDebugFile(frame);
+                        ReplayFrames.Add(frame);
+                        return;
+                    }
+
+                    // Start capturing frames at 60fps
+                    if (ReplayFrames.Count != 0 && elapsed - ReplayFrames.Last().GameTime >= ReplayFpsInterval)
+                    {
+                        AppendToDebugFile(frame);
+                        ReplayFrames.Add(frame);
+                    }
+                });
+        }
+
+        /// <summary>
+        ///     Checks the keyboard state and adds it to the frame
+        /// </summary>
+        private static void CheckKeyboardstate(Qua qua, ReplayFrame frame)
+        {
+            switch (qua.KeyCount)
+            {
+                case 4:
+                    // Key 1
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania1))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K1;
+
+                    // Key 2
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania2))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K2;
+
+                    // Key 3
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania3))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K3;
+
+                    // Key 4
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania4))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K4;
+                    break;
+                case 7:
+                    // Key 1
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania7k1))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K1;
+                    // Key 2
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania7k2))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K2;
+                    // Key 3
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania7k3))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K3;
+                    // Key 4
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania7k4))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K4;
+                    // Key 5
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania7k5))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K5;
+                    // Key 6
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania7k6))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K6;
+                    // Key 7
+                    if (GameBase.KeyboardState.IsKeyDown(Configuration.KeyMania7k7))
+                        frame.KeyPressState = frame.KeyPressState | KeyPressState.K7;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        ///     Appends a frame to a test file if debug mode is on.
+        /// </summary>
+        /// <param name="frame"></param>
+        private static void AppendToDebugFile(ReplayFrame frame)
+        {
+            if (!Configuration.Debug)
+                return;
+
+            var sw = new StreamWriter(DebugFilePath, true)
+            {
+                AutoFlush = true
+            };
+
+            sw.WriteLine($"{frame.SongTime}|{frame.TimeSinceLastFrame}|{frame.KeyPressState.ToString()}");
+            sw.Close();
+        }
+    }
+}
