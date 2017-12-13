fcef8f23553c648c71cde4c1c30b9f826d0a9b43
staravia <denysenesouma@gmail.com>
2017-12-12 22:26:31 -0800

    Update: Note Deviance
    Former-commit-id: 37a30f50cafc5fd0682bbebf2a5fe04c9575af01 [formerly aa9638bda4632468e0c431bc80447716060551d7]
    Former-commit-id: d570083016431e3f392e7335b2710aefbeed0165

--- Quaver/src/Gameplay/NoteDevianceData.cs
+++ Quaver/src/Gameplay/NoteDevianceData.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Quaver.src.Gameplay
+namespace Quaver.Gameplay
 {
     class NoteDevianceData
     {
         /// <summary>
         ///     Records how off the note pressed is from receptor
         /// </summary>
-        internal float Offset { get; set; }
+        internal double Offset { get; set; }
 
         /// <summary>
         ///     Records note's song position
         /// </summary>
-        internal float Position { get; set; }
+        internal double Position { get; set; }
 
         /// <summary>
         ///     What type of color/judgement the note is. (Perf/Great/ect.)
