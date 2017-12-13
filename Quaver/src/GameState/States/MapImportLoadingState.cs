85784bf4afa14b16bd30e0cabec935d9ac51fbcc
Swan <contact@swan.moe>
2017-12-13 12:26:37 -0500

    Refactor: Move AfterImport operations to MapImportLoadingState
    Former-commit-id: 608a4b98ffc1d33c75b2a1c97ef9fe4184bd7928 [formerly aae866445933215d84482843b518f683ce1e9f0a]
    Former-commit-id: ec0938bb072a09294e894fea89df355efb7ba59c

--- Quaver/src/GameState/States/MapImportLoadingState.cs
+++ Quaver/src/GameState/States/MapImportLoadingState.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Quaver.Audio;
 using Quaver.Beatmaps;
 using Quaver.Discord;
-
+using Quaver.Graphics.Sprite;
+using Quaver.Logging;
 using Quaver.QuaFile;
 
 namespace Quaver.GameState.States
@@ -49,6 +51,54 @@ namespace Quaver.GameState.States
         public void Draw()
         {
             GameBase.GraphicsDevice.Clear(Color.Red);
+        }
+
+
+        /// <summary>
+        ///     Handles the selection of new a new beatmap and 
+        ///     removes the loading state after importing
+        /// </summary>
+        /// <returns></returns>
+        internal static async Task AfterImport()
+        {
+            var oldMaps = GameBase.Beatmaps;
+
+            // Import all the maps to the db
+            await GameBase.LoadAndSetBeatmaps();
+
+            // Update the selected beatmap with the new one.
+            // This button should only be on the song select state, so no need to check for states here.
+            var newMapsets = GameBase.Beatmaps.Where(x => !oldMaps.Any(y => y.Directory == x.Directory)).ToList();
+
+            // In the event that the user imports maps when there weren't any maps previously.
+            if (oldMaps.Count == 0)
+            {
+                BeatmapUtils.SelectRandomBeatmap();
+                GameBase.LoadBackground();
+                BackgroundManager.Change(GameBase.CurrentBackground);
+                SongManager.Load();
+                SongManager.Play();
+            }
+            else if (newMapsets.Count > 0)
+            {
+                var map = newMapsets.Last().Beatmaps.Last();
+                Console.WriteLine(map.Artist + " " + map.Title);
+
+                // Switch map and load audio for song and play it.
+                GameBase.ChangeBeatmap(map);
+
+                // Load and change background after import
+                GameBase.LoadBackground();
+                BackgroundManager.Change(GameBase.CurrentBackground);
+
+                SongManager.ReloadSong();
+
+                GameBase.ChangeDiscordPresence(
+                    $"{GameBase.SelectedBeatmap.Artist} - {GameBase.SelectedBeatmap.Title}", "Listening");
+            }
+
+            Logger.Log("Successfully completed the conversion task. Stopping loader.", Color.Cyan);
+            GameBase.GameStateManager.RemoveState();
         }
     }
 }
