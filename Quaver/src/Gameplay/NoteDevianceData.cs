4b43fd0dc9ea8f3a03a16f9fbf92d1ad6ec1e7db
staravia <denysenesouma@gmail.com>
2017-12-12 22:15:30 -0800

    Add: NoteDevianceData
    Former-commit-id: 7282b102267402aa17205dff2b995568c4cfe028 [formerly a148570c268077a613f4399e441f2c184496fa76]
    Former-commit-id: 7dc554f9af0581a00d2ed8e81109e1963dc91af2

---
+++ Quaver/src/Gameplay/NoteDevianceData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.src.Gameplay
+{
+    class NoteDevianceData
+    {
+        /// <summary>
+        ///     Records how off the note pressed is from receptor
+        /// </summary>
+        internal float Offset { get; set; }
+
+        /// <summary>
+        ///     Records note's song position
+        /// </summary>
+        internal float Position { get; set; }
+
+        /// <summary>
+        ///     What type of color/judgement the note is. (Perf/Great/ect.)
+        /// </summary>
+        internal int Type { get; set; }
+    }
+}
