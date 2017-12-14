4b36798956308ab8ca88dadfdff828f8a5da3728
staravia <denysenesouma@gmail.com>
2017-12-14 14:26:23 -0800

    Add: IHelper interface
    Former-commit-id: c940d2c2e4b3ecbbc83bfac7ca305b3da0815d92 [formerly 0941f24c0cd34d3aae3cadcfc3c61bc6450afbad]
    Former-commit-id: 75034afeb4e6465ea14e7f1548a67fd18456027d

---
+++ Quaver/src/GameState/IHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.GameState
+{
+    interface IHelper
+    {
+        void Initialize(IGameState state);
+        void UnloadContent();
+        void Update(double dt);
+        void Draw();
+    }
+}
