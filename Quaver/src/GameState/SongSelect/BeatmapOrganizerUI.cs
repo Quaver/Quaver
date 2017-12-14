8c818854cbee43ce05e49d7feb04cb0c26fda3b4
staravia <denysenesouma@gmail.com>
2017-12-14 14:37:51 -0800

    Update: BeatmapOrganizer (WIP)
    Former-commit-id: d58804b5d72528e5257ab3afb743b9cb26ddb60f [formerly b852c2013125e0996839807e8cb31333a6205dc5]
    Former-commit-id: 5a2e3f53d02205a2a2ff77f85e9e0c2f54f51894

--- Quaver/src/GameState/SongSelect/BeatmapOrganizerUI.cs
+++ Quaver/src/GameState/SongSelect/BeatmapOrganizerUI.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Quaver.Graphics;
+using Quaver.Graphics.Button;
+using Quaver.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,97 @@ using System.Threading.Tasks;
 
 namespace Quaver.GameState.SongSelect
 {
-    class BeatmapOrganizerUI
+    class BeatmapOrganizerUI : IHelper
     {
 
+        /// <summary>
+        ///     Reference to the list of song select buttons
+        /// </summary>
+        private List<Button> SongSelectButtons { get; set; } = new List<Button>();
+        public object TogglePitch { get; private set; }
+
+        public void Draw()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Initialize(IGameState state)
+        {
+            CreateSongSelectButtons();
+        }
+
+        public void UnloadContent()
+        {
+            SongSelectButtons.Clear();
+        }
+
+        public void Update(double dt)
+        {
+            //throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Creates the song select buttons
+        /// </summary>
+        public void CreateSongSelectButtons()
+        {
+            var ButtonPos = 50f;
+            //Create buttons for every beatmap set TODO: Use beatmap set instead of beatmaps
+            foreach (var mapset in GameBase.VisibleBeatmaps)
+            {
+                //Create Song Buttons
+                foreach (var map in mapset.Beatmaps)
+                {
+                    var mapText = map.Artist + " - " + map.Title + " [" + map.DifficultyName + "]";
+
+                    // Create the new button
+                    var newButton = new SongSelectButton(map, 1)
+                    {
+                        Image = GameBase.UI.BlankBox,
+                        Alignment = Alignment.TopLeft,
+                        PositionY = ButtonPos,
+                        PositionX = 5,
+                        //Parent = Boundary
+                    };
+
+                    // Define event handler for the button
+                    newButton.Clicked += (sender, e) => OnSongSelectButtonClick(sender, e, mapText, map);
+
+                    // Add the4 button the current list
+                    SongSelectButtons.Add(newButton);
+
+                    // Change the Y value
+                    ButtonPos += newButton.SizeY + 2;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Changes the map when a song select button is clicked.
+        /// </summary>
+        public void OnSongSelectButtonClick(object sender, EventArgs e, string text, Beatmap map)
+        {
+            Logger.Update("MapSelected", "Map Selected: " + text);
+
+            // Get the background path from the previous map
+            var oldMapBgPath = GameBase.SelectedBeatmap.Directory + "/" + GameBase.SelectedBeatmap.BackgroundPath;
+            var oldMapAudioPath = GameBase.SelectedBeatmap.Directory + "/" + GameBase.SelectedBeatmap.AudioPath;
+
+            GameBase.ChangeBeatmap(map);
+
+            /*
+            // Change Pitch Text
+            TogglePitch.TextSprite.Text = $"Toggle Pitch: {Configuration.Pitched}";
+
+            // Only load the audio again if the new map's audio isn't the same as the old ones.
+            if (oldMapAudioPath != map.Directory + "/" + map.AudioPath)
+                SongManager.ReloadSong(true);
+
+            // Load background asynchronously if the backgrounds actually do differ
+            if (oldMapBgPath != map.Directory + "/" + map.BackgroundPath)
+                Task.Run(() => GameBase.LoadBackground())
+                    .ContinueWith(t => BackgroundManager.Change(GameBase.CurrentBackground));
+                    */
+        }
     }
 }
