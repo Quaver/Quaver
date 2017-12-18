411d7c9e4fddbc0fc346305a9f572d8f9d946523
staravia <denysenesouma@gmail.com>
2017-12-18 14:17:10 -0800

    Add: Implement Gameplay Input

---
+++ Quaver/src/Input/ManiaKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Input
+{
+    internal class ManiaKey : EventArgs
+    {
+        private int Key { get; set; }
+
+        internal ManiaKey(int key)
+        {
+            Key = key;
+        }
+
+        internal int GetKey()
+        {
+            return Key;
+        }
+    }
+}
