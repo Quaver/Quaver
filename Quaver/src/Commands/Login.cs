a553a47fdd0a6f107437044eb0ee6ca6fcaba247
Swan <contact@swan.moe>
2017-12-18 16:52:40 -0500

    Add: Command to Login to server
    Former-commit-id: b9b2e177d6ae8aa47d975f53dea20dc58df24829 [formerly ff92d9458932bce611de70f108f4341adc095b27]
    Former-commit-id: e43f4be44ca8a0cdbae5f2f1eedd322c8fe6618b

---
+++ Quaver/src/Commands/Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.Online.Wobble;
+
+namespace Quaver.Commands
+{
+    internal class Login : ICommand
+    {
+        public string Name { get; set; } = "LOGIN";
+
+        public int Args { get; set; } = 2;
+
+        public string Description { get; set; } = "Command that allows you to log into the Quaver server.";
+
+        public string Usage { get; set; } = "login <username> <password>";
+
+        public void Execute(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid Args Given. Must be 'login <username> <password>'");
+                return;
+            }
+
+            // Connect
+            SocketClient.Connect(args[1], args[2]);
+        }
+    }
+}
