194cba97cf69ee796d8009e5d175a42e189de8ea
staravia <denysenesouma@gmail.com>
2017-12-20 21:11:46 -0800

    Update: Removed placeholder hit window variables
    Former-commit-id: ca0f5849789671c9510ab705271bacc9c2886480 [formerly 2488227c5bb141cd41f031177569a1e281a7adc9]
    Former-commit-id: f563c57e25e66a4bf862690a3ecbe24ba1ff08d7

--- Quaver/src/GameState/Gameplay/GameplayReferences.cs
+++ Quaver/src/GameState/Gameplay/GameplayReferences.cs
@@ -26,8 +26,8 @@ namespace Quaver.GameState.Gameplay
         internal static int GradeIndex { get; set; } = 0; // PlayScreen.GetRelativeGradeIndex()
         internal static float Accuracy { get; set; } = 0;
         internal static float AccScale { get; set; } // PlayScreen.RelativeAccGetScale()
-        internal static float PressWindowLatest { get; set; } = 70; // HitWindowPress[4]
-        internal static float ReleaseWindowLatest { get; set; } = 140; // HitWindowRelease[3]
+        internal static float PressWindowLatest { get; set; } // HitWindowPress[4]
+        internal static float ReleaseWindowLatest { get; set; } // HitWindowRelease[3]
 
         //todo: temp Timing variables
         internal static List<TimingObject> SvQueue { get; set; }
