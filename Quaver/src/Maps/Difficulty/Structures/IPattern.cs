f6497cfdb0e304a87c7ef7887af6f43b5ff39ccb
Swan <contact@swan.moe>
2017-12-27 12:53:21 -0500

    Add: Implement an IPattern interface

---
+++ Quaver/src/Maps/Difficulty/Structures/IPattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Maps.Difficulty.Structures
+{
+    internal interface IPattern
+    {
+        /// <summary>
+        ///     The total time this pattern takes
+        /// </summary>
+        int TotalTime { get; set; }
+
+        /// <summary>
+        ///     The list of hitobjects in this pattern
+        /// </summary>
+        List<HitObjectInfo> HitObjects { get; set; }
+    }
+}
