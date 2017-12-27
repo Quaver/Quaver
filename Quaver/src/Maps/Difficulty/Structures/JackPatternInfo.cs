4a6b67eb5d67acf07b206b7f6fdc07ec2cde9600
Swan <contact@swan.moe>
2017-12-27 14:19:25 -0500

    Add: JackPatternInfo struct

---
+++ Quaver/src/Maps/Difficulty/Structures/JackPatternInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Maps.Difficulty.Structures
+{
+    internal struct JackPatternInfo : IPattern
+    {
+        /// <summary>
+        ///     The total time this pattern takes
+        /// </summary>
+        public int TotalTime { get; set; }
+
+        /// <summary>
+        ///     The lane the pattern takes place in
+        /// </summary>
+        public int Lane { get; set; }
+
+        /// <summary>
+        ///     The list of HitObjects in this pattern
+        /// </summary>
+        public List<HitObjectInfo> HitObjects { get; set; }
+    }
+}
