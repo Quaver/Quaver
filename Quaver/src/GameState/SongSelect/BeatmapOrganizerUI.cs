954194efe704cf5d4bd86b6633fa7cdc892b87cb
staravia <denysenesouma@gmail.com>
2017-12-28 20:16:12 -0800

    Update: Song Select Button on select
    Former-commit-id: 9465ceabd79a4549872c2983f33daa881939e3a3 [formerly f059d94f7459ca94c30654ff4afe0b68ca35ee9b]
    Former-commit-id: 78e02cd60d21e5f498f01ab98dafb2290fe460f3

--- Quaver/src/GameState/SongSelect/BeatmapOrganizerUI.cs
+++ Quaver/src/GameState/SongSelect/BeatmapOrganizerUI.cs
@@ -11,6 +11,7 @@ using Quaver.Database.Beatmaps;
 using Quaver.Graphics.Sprite;
 using Quaver.Audio;
 using Quaver.Database;
+using Quaver.Utility;
 
 namespace Quaver.GameState.SongSelect
 {
@@ -27,6 +28,12 @@ namespace Quaver.GameState.SongSelect
         public object TogglePitch { get; private set; }
 
         private float OrganizerSize { get; set; }
+
+        private float TargetPosition { get; set; }
+
+        private int SelectedMapIndex { get; set; } = 0;
+
+        private float SelectedMapTween { get; set; } = 0;
 
         public void Draw()
         {
@@ -48,6 +55,20 @@ namespace Quaver.GameState.SongSelect
 
         public void Update(double dt)
         {
+            var tween = Math.Min(dt / 70, 1);
+            Boundary.PositionY = Util.Tween(TargetPosition, Boundary.PositionY, tween);
+            SelectedMapTween = Util.Tween(SelectedMapIndex, SelectedMapTween, tween);
+            //GameBase.Window.Y-(GameBase.Window.Z/2f)
+            for (var i=0; i<SongSelectButtons.Count; i++)
+            {
+                var button = SongSelectButtons[i];
+                var selectedOffset = Math.Abs(SelectedMapTween - i)+1;
+
+                button.PositionX = 
+                    (float)Math.Sin(Math.PI * (Boundary.PositionY + button.PositionY)/ GameBase.Window.Z) * 30 
+                    + 50/ selectedOffset
+                    - 30;
+            }
             Boundary.Update(dt);
         }
 
@@ -64,6 +85,7 @@ namespace Quaver.GameState.SongSelect
                 foreach (var map in mapset.Beatmaps)
                 {
                     var mapText = map.Artist + " - " + map.Title + " [" + map.DifficultyName + "]";
+                    var index = SongSelectButtons.Count;
 
                     // Create the new button
                     var newButton = new SongSelectButton(map, GameBase.WindowYRatio)
@@ -71,12 +93,12 @@ namespace Quaver.GameState.SongSelect
                         Image = GameBase.UI.BlankBox,
                         Alignment = Alignment.TopLeft,
                         PositionY = OrganizerSize,
-                        PositionX = 5 + OrganizerSize*0.2f,
+                        PositionX = 5,
                         Parent = Boundary
                     };
 
                     // Define event handler for the button
-                    newButton.Clicked += (sender, e) => OnSongSelectButtonClick(sender, e, mapText, map);
+                    newButton.Clicked += (sender, e) => OnSongSelectButtonClick(sender, e, mapText, map, index);
 
                     // Add the4 button the current list
                     SongSelectButtons.Add(newButton);
@@ -90,10 +112,14 @@ namespace Quaver.GameState.SongSelect
         /// <summary>
         ///     Changes the map when a song select button is clicked.
         /// </summary>
-        private void OnSongSelectButtonClick(object sender, EventArgs e, string text, Beatmap map)
+        private void OnSongSelectButtonClick(object sender, EventArgs e, string text, Beatmap map, int index)
         {
             Logger.Update("MapSelected", "Map Selected: " + text);
-            
+            SongSelectButtons[SelectedMapIndex].Selected = false;
+            SongSelectButtons[index].Selected = true;
+            SelectedMapIndex = index;
+            TargetPosition = GameBase.Window.Y + (GameBase.Window.Z / 2) - ((float)index / SongSelectButtons.Count) * OrganizerSize;
+
             // Get the background path from the previous map
             var oldMapBgPath = GameBase.SelectedBeatmap.Directory + "/" + GameBase.SelectedBeatmap.BackgroundPath;
             var oldMapAudioPath = GameBase.SelectedBeatmap.Directory + "/" + GameBase.SelectedBeatmap.AudioPath;
@@ -118,8 +144,7 @@ namespace Quaver.GameState.SongSelect
 
         public void SetBeatmapOrganizerPosition(float scale)
         {
-            Boundary.PositionY = scale * OrganizerSize;
-            Boundary.PositionX = scale * OrganizerSize * 0.2f;
+            TargetPosition = scale * OrganizerSize;
         }
     }
 }
