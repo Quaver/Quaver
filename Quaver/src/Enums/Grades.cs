37b9efa6a8f9282200a1867568167e54d77e7d70
Swan <contact@swan.moe>
2017-12-29 11:55:45 -0500

    Add: Enum for grades
    Former-commit-id: 5a1d9585970d41fd610af1877553637e28840deb [formerly adabf02f4c9b4b7ee34f7610974909c0f724e958]
    Former-commit-id: 2f4054eaf453eaa7380d63512f00462f63b41dbb

---
+++ Quaver/src/Enums/Grades.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Enums
+{
+    /// <summary>
+    ///     Enum representation of grades
+    /// </summary>
+    internal enum Grades
+    {
+        A, // 90-94%
+        B, // 80-89%
+        C, // 70-79%
+        D, // 60-69%
+        F, // <60%
+        S, // 95%
+        SS, // ?
+        X, // ?
+        XX, // ?
+        XXX // Meme grade. All 0ms hits lol.
+    }
+}
