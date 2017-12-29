71dac544dcc535ce5758988701223a3a41b650f4
staravia <denysenesouma@gmail.com>
2017-12-28 23:53:12 -0800

    Update: BarObject

--- Quaver/src/GameState/Gameplay/BarObject.cs
+++ Quaver/src/GameState/Gameplay/BarObject.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Quaver.Graphics;
+using Quaver.Graphics.Sprite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +11,41 @@ namespace Quaver.GameState.Gameplay
     class BarObject
     {
         /// <summary>
-        ///     Where the bar is in relation with time
-        /// </summary>
-        public float StartTime { get; set; }
-
-        /// <summary>
         ///     The bar's object from the receptor
         /// </summary>
         public ulong OffsetFromReceptor { get; set; }
+
+        /// <summary>
+        ///     The Sprite of the bar
+        /// </summary>
+        public Sprite BarSprite { get; set; }
+
+        /// <summary>
+        ///     The parent of the bar object
+        /// </summary>
+        public Drawable ParentContainer { get; set; }
+
+        /// <summary>
+        ///     The position of the bar
+        /// </summary>
+        public float PositionY { get; set; }
+
+        public void Initialize(Drawable parent, float sizex, float sizey)
+        {
+            //Create bar
+            BarSprite = new Sprite()
+            {
+                Alignment = Alignment.TopLeft,
+                PositionY = PositionY,
+                SizeX = sizex,
+                SizeY = sizey,
+                Parent = ParentContainer
+            };
+        }
+
+        public void Destroy()
+        {
+            BarSprite.Destroy();
+        }
     }
 }
