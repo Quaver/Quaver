caa9bcedb178ecdb402d3eb8486850f233890802
staravia <denysenesouma@gmail.com>
2017-12-28 23:23:04 -0800

    Add: Bar Object
    Former-commit-id: 9a8aafb7231dba272080f78e09c4072c18a080a7 [formerly a84a43c00272f3c09a1d8d61b8d625adade7e244]
    Former-commit-id: c2562e8d4f80fbdb9d4606b12753be82e354a69f

---
+++ Quaver/src/GameState/Gameplay/BarObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.GameState.Gameplay
+{
+    class BarObject
+    {
+        /// <summary>
+        ///     Where the bar is in relation with time
+        /// </summary>
+        public float StartTime { get; set; }
+
+        /// <summary>
+        ///     The bar's object from the receptor
+        /// </summary>
+        public ulong OffsetFromReceptor { get; set; }
+    }
+}
