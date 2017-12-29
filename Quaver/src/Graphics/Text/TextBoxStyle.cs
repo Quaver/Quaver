09360547d0ef664517f37ed800156030db0ce2eb
staravia <denysenesouma@gmail.com>
2017-12-28 16:21:51 -0800

    Add: TextBoxStyles
    Former-commit-id: df2b3bd6b3f2e2f6a300f8acfad260d420ad4a0b [formerly ce02ad9c7596a2fe4547e76db7d6ddd8c32894b2]
    Former-commit-id: 83be6bae56a4bad24e2474e25f9a2038376ceb23

---
+++ Quaver/src/Graphics/Text/TextBoxStyle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Graphics.Text
+{
+    enum TextBoxStyle
+    {
+        OverflowSingleLine,
+        OverflowMultiLine,
+        ScaledSingleLine,
+        WordwrapSingleLine,
+        WordwrapMultiLine
+    }
+}
