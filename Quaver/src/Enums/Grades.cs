c5ff319853ef1b72834adf25d88385c244445dbc
Swan <contact@swan.moe>
2017-12-29 23:31:36 -0500

    Add: Grades.None
    Former-commit-id: f1fbfbab8aa4e283c60bc50492869ae288346a97 [formerly aaa7c939cfc8399729d6958150138743651317a0]
    Former-commit-id: e4eadeff41e9760028bce27ae55f1bc4aaab454b

--- Quaver/src/Enums/Grades.cs
+++ Quaver/src/Enums/Grades.cs
@@ -9,17 +9,18 @@ namespace Quaver.Enums
     /// <summary>
     ///     Enum representation of grades
     /// </summary>
-    internal enum Grades
+    public enum Grades
     {
-        A, // 90-94%
-        B, // 80-89%
-        C, // 70-79%
-        D, // 60-69%
-        F, // <60%
-        S, // 95%
-        SS, // ?
-        X, // ?
-        XX, // ?
-        XXX // Meme grade. All 0ms hits lol.
+        None,
+        A,
+        B,
+        C,
+        D,
+        F,
+        S,
+        SS,
+        X,
+        XX,
+        XXX
     }
 }
