a6abcc3351e6fee451724e470ff494614b150fcc
Swan <contact@swan.moe>
2017-12-30 21:01:18 -0500

    Add: ChordPatternInfo

---
+++ Quaver/src/Maps/Difficulty/Patterns/ChordPatternInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Maps.Difficulty.Patterns
+{
+    internal class ChordPatternInfo : IPattern
+    {
+        /// <summary>
+        ///     The total time the pattern takes
+        /// </summary>
+        public int TotalTime { get; set; }
+
+        /// <summary>
+        ///     The time the pattern starts
+        /// </summary>
+        public int StartingObjectTime { get; set; }
+
+        /// <summary>
+        ///     The list of objects in the pattern
+        /// </summary>
+        public List<HitObjectInfo> HitObjects { get; set; }
+
+        /// <summary>
+        ///     The type of chord this pattern is
+        /// </summary>
+        public ChordType ChordType { get; set; }
+    }
+}
