d80eda6720815c1f4080db690eb0d26519059d21
Swan <contact@swan.moe>
2017-12-31 14:46:56 -0500

    Add: PatternList struct that holds all patterns
    Former-commit-id: eb8824088a259ae3c404d1fd418d15f96463f158 [formerly 09c4b7f36ed08223d63c4aacaa990f70ff5d1c2a]
    Former-commit-id: feb382035a7f0aac81946dcb0068080f9933b8bd

---
+++ Quaver/src/Maps/Difficulty/Patterns/PatternList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Maps.Difficulty.Patterns
+{
+    internal struct PatternList
+    {
+        /// <summary>
+        ///     Jack Patterns
+        /// </summary>
+        internal List<JackPatternInfo> Jacks { get; set; }
+
+        /// <summary>
+        ///     Vibro Patterns
+        /// </summary>
+        internal List<JackPatternInfo> Vibro { get; set; }
+
+        /// <summary>
+        ///     Chord Patterns
+        /// </summary>
+        internal List<ChordPatternInfo> Chords { get; set; }
+
+        /// <summary>
+        ///     Stream patterns
+        /// </summary>
+        internal List<StreamPatternInfo> Streams { get; set; }
+    }
+}
