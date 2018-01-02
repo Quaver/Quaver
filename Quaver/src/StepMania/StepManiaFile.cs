ce8fb2431ae162a922deb2c69f176ec2f6e59a90
Swan <contact@swan.moe>
2018-01-02 13:48:38 -0500

    Add: Complete StepMania (.sm) chart file parsing
    Former-commit-id: 7b53cf2393fe9c00eb33754ed8147e2439dcff7b [formerly 1c9c7a1489d5d224584d7cd86d4645901352fc0e]
    Former-commit-id: 8e19c50a6f6d3f8a478815c5a6e246076ee310d7

--- Quaver/src/StepMania/StepManiaFile.cs
+++ Quaver/src/StepMania/StepManiaFile.cs
@@ -72,7 +72,13 @@ namespace Quaver.StepMania
 
             var sm = new StepManiaFile { Bpms = new List<Bpm>(), Charts = new List<Chart>() };
 
-            foreach (var line in File.ReadAllLines(path))
+            var file = File.ReadAllLines(path);
+
+            var inNotes = false; // Keeps track of if we are currently in the #NOTES section
+            var inNoteData = false;
+            var currentColons = 0; // Keeps track of the c
+
+            foreach (var line in file)
             {
                 if (line.Contains("#"))
                 {
@@ -86,36 +92,28 @@ namespace Quaver.StepMania
                     {
                         case "#TITLE":
                             sm.Title = value;
-                            Console.WriteLine(value);
-                            break;
+                            continue;
                         case "#SUBTITLE":
                             sm.Subtitle = value;
-                            Console.WriteLine(value);
-                            break;
+                            continue;
                         case "#ARTIST":
                             sm.Artist = value;
-                            Console.WriteLine(value);
-                            break;
+                            continue;
                         case "#CREDIT":
                             sm.Credit = value;
-                            Console.WriteLine(value);
-                            break;
+                            continue;
                         case "#MUSIC":
                             sm.Music = value;
-                            Console.WriteLine(value);
-                            break;
+                            continue;
                         case "#BACKGROUND":
                             sm.Background = value;
-                            Console.WriteLine(value);
-                            break;
+                            continue;
                         case "#OFFSET":
                             sm.Offset = float.Parse(value);
-                            Console.WriteLine(sm.Offset);
-                            break;
+                            continue;
                         case "#SAMPLESTART":
                             sm.SampleStart = float.Parse(value);
-                            Console.WriteLine(sm.SampleStart);
-                            break;
+                            continue;
                         case "#BPMS":
                             var bpms = value.Split(',').ToList();
 
@@ -124,9 +122,114 @@ namespace Quaver.StepMania
                                 // An individual bpm is split by "offset=bpm"
                                 var bpmSplit = bpm.Split('=').ToList();
                                 sm.Bpms.Add(new Bpm { StartTime = float.Parse(bpmSplit[0]), BeatsPerMinute = float.Parse(bpmSplit[1])});
-                                Console.WriteLine(bpmSplit[0] + "|" + bpmSplit[1]);
+                            }
+                            continue;
+                        case "#NOTES":
+                            inNotes = true;
+                            sm.Charts.Add(new Chart { Measures = new List<NoteMeasure>() });
+                            currentColons = 0; // Reset colon counter.
+                            inNoteData = false;
+                            continue;
+                    }                    
+                }
+
+                if (inNotes)
+                {
+                    // Skip comments
+                    if (line.Contains("//"))
+                        continue;
+
+                    if (line.Contains(":"))
+                        currentColons++;
+
+                    switch (currentColons)
+                    {
+                        // Type of map
+                        case 1:
+                            // Only parse dance-single maps. Setting inNotes to false will 
+                            // cause the parser to skip this step in the parsing phase until it reaches
+                            // another #NOTES section - Remove the current chart as well
+                            if (!line.ToLower().Contains("dance-single"))
+                            {
+                                sm.Charts.Remove(sm.Charts.Last());
+                                inNotes = false;
+                                continue;
+                            }
+
+                            sm.Charts.Last().ChartType = line.Trim().Trim(':');
+                            break;
+                        // Creator? Don't need to parse this.
+                        case 2:
+                            break;
+                        // Difficulty
+                        case 3:
+                            if (line.ToLower().Contains("beginner") || line.ToLower().Contains("easy") ||
+                                line.ToLower().Contains("medium") || line.ToLower().Contains("hard") ||
+                                line.ToLower().Contains("challenge"))
+                            {
+                                sm.Charts.Last().Difficulty = line.Trim().Trim(':');
                             }
                             break;
+                        // Numerical meter, doesn't need parsing.
+                        case 4:
+                            break;
+                        // Groove Radar Values - doesn't need parsing.
+                        case 5:
+                            // However, after this line, we will be parsing note data, 
+                            inNoteData = true;
+                            currentColons = -1; // Set the current colons back to -1 to avoid skipping.
+                            sm.Charts.Last().Measures.Add(new NoteMeasure { NoteRows = new List<NoteRow>() }); // Add first measure of notes
+                            continue;
+                    }
+
+                    // Parse Note Data
+                    if (inNoteData)
+                    {
+                        // If there are 4 characters in this line, that must mean we're at a row of objects
+                        if (line.Trim().Length == 4)
+                        {
+                            var row = new NoteRow
+                            {
+                                Lane1 = NoteType.None,
+                                Lane2 = NoteType.None,
+                                Lane3 = NoteType.None,
+                                Lane4 = NoteType.None
+                            };
+
+                            // Turn the row of objects into an array so we can try to parse each row.
+                            var rowArray = line.Trim().ToCharArray();
+
+                            for (var i = 0; i < rowArray.Length; i++)
+                            {
+                                switch (i)
+                                {
+                                    // Lane 1
+                                    case 0:
+                                        row.Lane1 = Enum.TryParse(rowArray[i].ToString(), out NoteType typeLane1) ? typeLane1 : NoteType.None;
+                                        break;
+                                    // Lane 2
+                                    case 1:
+                                        row.Lane2 = Enum.TryParse(rowArray[i].ToString(), out NoteType typeLane2) ? typeLane2 : NoteType.None;
+                                        break;
+                                    // Lane 3
+                                    case 2:
+                                        row.Lane3 = Enum.TryParse(rowArray[i].ToString(), out NoteType typeLane3) ? typeLane3 : NoteType.None;
+                                        break;
+                                    // Lane 4
+                                    case 3:
+                                        row.Lane4 = Enum.TryParse(rowArray[i].ToString(), out NoteType typeLane4) ? typeLane4 : NoteType.None;
+                                        break;
+                                }
+                            }
+
+                            // Add the row to the list of measures
+                            sm.Charts.Last().Measures.Last().NoteRows.Add(row);
+                            continue;
+                        }
+
+                        // If the line is a ',', that means it marks the end of a new measure.
+                        if (line.Trim().Contains(","))
+                            sm.Charts.Last().Measures.Add(new NoteMeasure {NoteRows = new List<NoteRow>()});
                     }
                 }
             }
@@ -135,6 +238,9 @@ namespace Quaver.StepMania
         }
     }
 
+    /// <summary>
+    ///     The #BPMS section of the metadata header
+    /// </summary>
     internal struct Bpm
     {
         /// <summary>
@@ -148,7 +254,10 @@ namespace Quaver.StepMania
         internal float BeatsPerMinute { get; set; }
     }
 
-    internal struct Chart
+    /// <summary>
+    ///     The chart date itself, also known as the #NOTES section of the file
+    /// </summary>
+    internal class Chart
     {
         /// <summary>
         ///     The type of chart
@@ -167,11 +276,34 @@ namespace Quaver.StepMania
         internal string Difficulty { get; set; }
 
         /// <summary>
-        ///     The list of ntoe data
+        ///     The list measures containing note data
         /// </summary>
-        internal List<NoteType> Notes { get; set; }
+        internal List<NoteMeasure> Measures { get; set; }
     }
 
+    // An individual measure for the map (as separated by ',' in the .sm file)
+    internal struct NoteMeasure
+    {
+        /// <summary>
+        ///     The list of notes in this measure
+        /// </summary>
+        internal List<NoteRow> NoteRows { get; set; }
+    }
+
+    /// <summary>
+    ///     An individual row of notes
+    /// </summary>
+    internal struct NoteRow
+    {
+        internal NoteType Lane1 { get; set; }
+        internal NoteType Lane2 { get; set; }
+        internal NoteType Lane3 { get; set; }
+        internal NoteType Lane4 { get; set; }
+    }
+
+    /// <summary>
+    ///     The type of note
+    /// </summary>
     internal enum NoteType
     {
         None, // 0
