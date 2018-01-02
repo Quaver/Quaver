eb8a418e11bec25498e8d5b865324bddac79cb59
Swan <contact@swan.moe>
2018-01-01 19:20:33 -0500

    Add: Command to parse StepMania charts
    Former-commit-id: 0b1fcd412dc3f9c41e4cf03450ba1c42f95844b0 [formerly dcc4cb707f91d66ede56a9be5f4ac95057e3536a]
    Former-commit-id: 435dc8ce9788dab445ad814a9a963571e289cfe5

---
+++ Quaver/src/Commands/ParseStepMania.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.StepMania;
+
+namespace Quaver.Commands
+{
+    internal class ParseStepMania : ICommand
+    {
+        public string Name { get; set; } = "SM";
+
+        public int Args { get; set; } = 2;
+
+        public string Description { get; set; } = "Parses a StepMania (.sm) file";
+
+        public string Usage { get; set; } = "sm <path to .sm file>";
+
+        public void Execute(string[] args)
+        {
+            var argsList = new List<string>(args);
+            argsList.RemoveAt(0);
+            var path = string.Join(" ", argsList);
+
+            try
+            {
+                var sm = StepManiaFile.Parse(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
