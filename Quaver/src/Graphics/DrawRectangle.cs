b5db92c3f32c33c24170b6a9962d2255423891a0
staravia <denysenesouma@gmail.com>
2018-01-03 00:05:43 -0800

    Add: DrawRectangle to replace Vector4
    Former-commit-id: bf9e699cdbe7d535afe00b0cdaaaff9e80f69c2f [formerly e1cedd5b5b9890cad9e69696516ffa2aa86d4c8d]
    Former-commit-id: 7c3b094b39af4396ab5ac5f3fb312063fc512bb4

---
+++ Quaver/src/Graphics/DrawRectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Graphics
+{
+    internal class DrawRectangle
+    {
+        /// <summary>
+        ///     Position X
+        /// </summary>
+        internal float X { get; set; }
+
+        /// <summary>
+        ///     Position Y
+        /// </summary>
+        internal float Y { get; set; }
+
+        /// <summary>
+        ///     Width Size
+        /// </summary>
+        internal float Width { get; set; }
+
+        /// <summary>
+        ///     Height Size
+        /// </summary>
+        internal float Height { get; set; }
+
+        internal DrawRectangle(float x, float y, float width, float height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+}
