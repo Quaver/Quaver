0bad7522fe907b0f35733f878dc706a299dd15fb
Swan <contact@swan.moe>
2018-01-04 16:10:51 -0500

    Update login commands w/ Quaver's OnlineEvent instance
    Former-commit-id: 9b5a417e7ae2374cf323bacf05cd79ebc2187b89 [formerly 26c8997f39d569b5cfc650a6b6fb6d91d4f51848]
    Former-commit-id: 9e9660bfa557d127ee8f4bb6da2c9e5c5987b646

--- Quaver/src/Commands/Login.cs
+++ Quaver/src/Commands/Login.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Quaver.Online;
 #if !PUBLIC
 using Quaver.Framework;
 
@@ -28,7 +29,7 @@ namespace Quaver.Commands
             }
 
             // Connect
-            RattleClient.Connect(args[1], args[2]);
+            RattleClient.Connect(args[1], args[2], Rattle.OnlineEvents);
         }
     }
 }
