c56b5c345f6bdb0d63be499f9678774d7db338c1
staravia <denysenesouma@gmail.com>
2018-01-04 00:58:02 -0800

    Add: UDim

---
+++ Quaver/src/Graphics/UDim.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Graphics
+{
+    /// <summary>
+    ///     Universal Dimension Value used for UI and other scalars
+    /// </summary>
+    internal class UDim
+    {
+        /// <summary>
+        ///     Scale of this value
+        /// </summary>
+        internal float Scale { get; set; }
+
+        /// <summary>
+        ///     Offset of this value
+        /// </summary>
+        internal int Offset { get; set; }
+    }
+}
---
+++ Quaver/src/Graphics/UDim2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Graphics
+{
+    /// <summary>
+    ///     2 Dimensional Udim value used for UI
+    /// </summary>
+    internal class UDim2
+    {
+        /// <summary>
+        ///     X UDim value
+        /// </summary>
+        internal UDim X { get; set; }
+
+        /// <summary>
+        ///     Y UDim Value
+        /// </summary>
+        internal UDim Y { get; set; }
+    }
+}
