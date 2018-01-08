71aa88dbb00b9506d43d19f2caba81687c143496
staravia <denysenesouma@gmail.com>
2018-01-08 05:28:37 -0800

    Update: BassDelayOffset scales with GameClock
    Former-commit-id: 348b1add4ed35e593e92a79fb6e1c5e7601e1a0c [formerly d1d05ecc5f7812fb85f6188449c97749fab7f539]
    Former-commit-id: a25bcb5258db4e1188441c3b7d20c6f98982ec6e

--- Quaver/src/GameState/Gameplay/PlayScreen/Timing.cs
+++ Quaver/src/GameState/Gameplay/PlayScreen/Timing.cs
@@ -128,7 +128,7 @@ namespace Quaver.GameState.Gameplay.PlayScreen
         internal double GeCurrentSongTime()
         {
             //Add global offset to actual song time
-            return ActualSongTime + SongManager.BassDelayOffset - (Configuration.GlobalOffset * GameBase.GameClock);
+            return ActualSongTime + (SongManager.BassDelayOffset - Configuration.GlobalOffset) * GameBase.GameClock;
         }
 
         internal ulong[] GetSVCalc(List<TimingObject> svQueue)
