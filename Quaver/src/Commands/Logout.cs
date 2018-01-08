79dff59380060f827fec167d681d21eeda5f385c
Swan <contact@swan.moe>
2018-01-08 16:26:13 -0500

    Add: Command to logout of the server
    Former-commit-id: e6979dc9a31a7819521d2bb23f2829d3a06fc14b [formerly 61d86f3966c6f914ba64c855fe6f72c4e7d8b414]
    Former-commit-id: feb591fe4f58c8cf32763e92aa9c12e57a27c22b

---
+++ Quaver/src/Commands/Logout.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#if !PUBLIC
+using Quaver.Framework;
+
+namespace Quaver.Commands
+{
+    internal class Logout : ICommand
+    {
+        public string Name { get; set; } = "LOGOUT";
+
+        public int Args { get; set; } = 1;
+
+        public string Description { get; set; } = "Logs out of the server";
+
+        public string Usage { get; set; } = "logout";
+
+        public void Execute(string[] args)
+        {
+            RattleClient.Disconnect();
+        }
+    }
+}
+#endif
