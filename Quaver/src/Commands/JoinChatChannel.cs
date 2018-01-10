26a72be9aab311d3a3fc9dc9bab282dc7714602f
Swan <contact@swan.moe>
2018-01-10 13:16:29 -0500

    Add: JoinChatChannel command

---
+++ Quaver/src/Commands/JoinChatChannel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.Logging;
+#if !PUBLIC
+using Quaver.Framework;
+using Quaver.Online;
+
+namespace Quaver.Commands
+{
+    internal class JoinChatChannel : ICommand
+    {
+        public string Name { get; set; } = "JOINCHAT";
+
+        public int Args { get; set; } = 2;
+
+        public string Description { get; set; } = "Joins a chat channel provided";
+
+        public string Usage { get; set; } = "joinchat <#channel>";
+
+        public void Execute(string[] args)
+        {
+            var didJoin = RattleClient.JoinChatChannel(Rattle.ChatChannels, args[1]);
+
+            if (!didJoin)
+            {
+                Logger.Log($"Could not join channel: {args[1]} as you are already in it!", LogColors.GameError);
+                return;
+            }
+
+            Logger.Log($"Attempting to join channel: {args[1]}...", LogColors.GameSuccess);
+        }
+    }
+}
+#endif
