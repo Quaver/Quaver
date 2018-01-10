ab48b5134c4d7a34e65a616852fef6ac6f910cb7
staravia <denysenesouma@gmail.com>
2018-01-10 03:33:10 -0800

    Add: Bakeable Sprite
    Former-commit-id: 84297e6362bb43084ae70494f14275fc09b08210 [formerly 9b289767e398036dae5ed96e10218187583c4062]
    Former-commit-id: 46a3a019113c7bacb76d816b115a7c803e224e46

---
+++ Quaver/src/Graphics/Sprite/BakeableSprite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using Quaver.Utility;
+
+
+namespace Quaver.Graphics.Sprite
+{
+    /// <summary>
+    ///     This is used for optimization for static sprites. 
+    ///     All sprites will be saved onto a texture to save space
+    /// </summary>
+    internal class BakeableSprite : Drawable
+    {
+        /// <summary>
+        ///     All sprites will be baked onto here
+        /// </summary>
+        private RenderTarget2D BakedTexture { get;  set; }
+
+        /// <summary>
+        ///     This will bake all the children of this object onto a texture and destroy them.
+        /// </summary>
+        internal void Bake()
+        {
+            //BakedTexture = new RenderTarget2D(GameBase.GraphicsDevice, )
+        }
+
+        /// <summary>
+        ///     Draw
+        /// </summary>
+        internal override void Draw()
+        {
+
+        }
+    }
+}
