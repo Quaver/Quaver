f1869cfcc31f673cac372cd177b410818160288a
Swan <contact@swan.moe>
2018-01-10 11:49:58 -0500

    Add: ChatChannel class

---
+++ Quaver/src/Online/ChatChannel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.Framework.Events.Packets.Structures;
+
+namespace Quaver.Online
+{
+    internal class ChatChannel
+    {
+        /// <summary>
+        ///     The name of the chat channel
+        /// </summary>
+        internal string ChannelName { get; set; }
+    }
+}
