e517fd0a4e96685e69b9e3b0c03051dfb362ac16
staravia <denysenesouma@gmail.com>
2018-01-10 08:49:53 -0800

    Add: OptionsMenuState
    Former-commit-id: 7ff6cb0bd89aab4dbe827e99caf77a8b0bf9fa0a [formerly 70c53f8532cfa0b36fee965800985651660f7256]
    Former-commit-id: 7c086975fe74806e1febcede3e0df6c71c402db1

---
+++ Quaver/src/GameState/States/OptionsMenuState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.GameState.States
+{
+    class OptionsMenuState : IGameState
+    {
+        public State CurrentState { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool UpdateReady { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnloadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(double dt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
