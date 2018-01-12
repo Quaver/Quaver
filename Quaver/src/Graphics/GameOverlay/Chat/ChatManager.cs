09a0afd1419a0b74828ea64a520e96e48292d341
staravia <denysenesouma@gmail.com>
2018-01-12 15:16:04 -0800

    Update: GameOverlay
    Former-commit-id: efbab467301ea35003beeaf162ac8fbfae64968e [formerly cb66096d781089eca9003702e6624d3c2ffc1118]
    Former-commit-id: bc0e3f1b9c8e3cf78ca82fc98131fceb4661ac8c

--- Quaver/src/Graphics/GameOverlay/Chat/ChatManager.cs
+++ Quaver/src/Graphics/GameOverlay/Chat/ChatManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Quaver.Graphics.Button;
+using Quaver.Graphics.Sprite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,24 +11,49 @@ namespace Quaver.Graphics.GameOverlay.Chat
 {
     class ChatManager : IGameOverlayComponent
     {
-        public void Draw()
-        {
-            throw new NotImplementedException();
-        }
+        private Boundary Boundary { get; set; }
+
+        private Sprite.Sprite Sprite { get; set; }
+
+        private TextInputField ChatInputField { get; set; }
 
         public void Initialize()
         {
-            throw new NotImplementedException();
+            // Create main boundary
+            Boundary = new Boundary();
+
+            // Create background dimmer
+            Sprite = new Graphics.Sprite.Sprite()
+            {
+                Size = new UDim2(0, 0, 1, 1),
+                Alignment = Alignment.MidCenter,
+                Alpha = 0.7f,
+                Tint = Color.Black,
+                Parent = Boundary
+            };
+
+            // Create input box
+            ChatInputField = new TextInputField(new Vector2(400, 30), "Type something")
+            {
+                PosY = -200,
+                Alignment = Alignment.BotLeft,
+                Parent = Boundary
+            };
         }
 
         public void UnloadContent()
         {
-            throw new NotImplementedException();
+            Boundary.Destroy();
         }
 
         public void Update(double dt)
         {
-            throw new NotImplementedException();
+            Boundary.Update(dt);
+        }
+
+        public void Draw()
+        {
+            Boundary.Draw();
         }
     }
 }
