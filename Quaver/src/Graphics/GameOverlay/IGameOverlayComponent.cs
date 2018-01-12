866c76b5d5df69ab1a5253e8f958692ee47538ab
staravia <denysenesouma@gmail.com>
2018-01-12 14:17:59 -0800

    Add: GameOverlay components
    Former-commit-id: 5f42dd2e61466b23cf9243d3f5e7de8d0262761e [formerly cba87ce43dea9ef64f9c95826948980a6bdd8b99]
    Former-commit-id: 33f7976ebf0d1207dcd3113602fa10f25624b254

---
+++ Quaver/src/Graphics/GameOverlay/IGameOverlayComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Graphics.GameOverlay
+{
+    interface IGameOverlayComponent
+    {
+        void Initialize();
+        void UnloadContent();
+        void Update(double dt);
+        void Draw();
+    }
+}
