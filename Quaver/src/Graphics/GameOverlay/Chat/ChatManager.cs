cd9cd1dde4da0dd2495027b6623ecef8b8e14b7c
staravia <denysenesouma@gmail.com>
2018-01-12 14:17:59 -0800

    Add: GameOverlay components

---
+++ Quaver/src/Graphics/GameOverlay/Chat/ChatManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Graphics.GameOverlay.Chat
+{
+    class ChatManager : IGameOverlayComponent
+    {
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnloadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(double dt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
