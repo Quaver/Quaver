98aa87d947c6b5f88f4b013e6ef50596ea0e9780
staravia <denysenesouma@gmail.com>
2018-01-25 04:39:28 -0800

    Update: Added graph to visualize fps
    Former-commit-id: 21ae42a499b249b926ca058a161d5b65838d3e7a [formerly 6474824533aa48a1b29ac7ed39d0bb55b883ed73]
    Former-commit-id: 7246e84de951b71c811dd998b00c24dacc40378d

--- Quaver/src/Utility/FpsCounter.cs
+++ Quaver/src/Utility/FpsCounter.cs
@@ -29,14 +29,24 @@ namespace Quaver.Utility
         private static int Interval { get; set; }
 
         /// <summary>
-        ///     The SpriteFont for the FPS Counter.
-        /// </summary>
-        private static SpriteFont Font { get; } = Fonts.Medium16;
-
-        /// <summary>
-        /// After this many frames, it will update the current FPS
+        ///     After this many frames, it will update the current FPS
         /// </summary>
         private const int FrameCount = 100;
+
+        /// <summary>
+        ///     The average fps for each interval
+        /// </summary>
+        private static double[] AverageFpsIntervals = new double[20];
+
+        /// <summary>
+        ///     Each bar sizes in the graph
+        /// </summary>
+        private static int[] CurrentBarSize = new int[20];
+
+        /// <summary>
+        ///     The current Max FPS
+        /// </summary>
+        private static double CurrentMaxFPS;
 
         /// <summary>
         /// Use this to calculate FPS on every frame.
@@ -51,14 +61,30 @@ namespace Quaver.Utility
             if (Interval < FrameCount)
                 return;
 
+            // Calculate current fps
             FpsCurrent = 1000 / (FpsCount / FrameCount);
+
+            // Shift AverageFpsIntervals back by 1 and calculate max fps
+            CurrentMaxFPS = 10;
+            for (var i = 19; i > 0; i--)
+            {
+                AverageFpsIntervals[i] = AverageFpsIntervals[i - 1];
+                if (AverageFpsIntervals[i] > CurrentMaxFPS) CurrentMaxFPS = AverageFpsIntervals[i];
+            }
+
+            // Calculate current position on list + max fps
+            if (FpsCurrent > CurrentMaxFPS) CurrentMaxFPS = FpsCurrent;
+                CurrentMaxFPS += 10;
+
+            AverageFpsIntervals[0] = FpsCurrent;
+
+            // Calculate Current bar sizes for graph
+            for (var i = 0; i < 20; i++)
+                CurrentBarSize[i] = (int)(40 * AverageFpsIntervals[i] / CurrentMaxFPS);
 
             // Reset both the FPS Count & Intrval
             FpsCount = 0;
             Interval = 0;
-
-            //If it ever stutters, it will log it. (If dt is higher than 1/144th of a second)
-            //if (dt > 6) Logger.Log("LAG/STUTTER. DT DELAY: "+Math.Round(dt,2)+"ms",Color.Red,5);
         }
 
         /// <summary>
@@ -66,7 +92,25 @@ namespace Quaver.Utility
         /// </summary>
         public static void Draw()
         {
-            GameBase.SpriteBatch.DrawString(Font, Math.Floor(FpsCurrent) + " FPS", new Vector2(0, GameBase.WindowRectangle.Height - 24), Color.White);
+            // Draw graph and color according to fps.
+            for (var i=0; i<20; i++)
+            {
+                if (AverageFpsIntervals[i] < 60)
+                    GameBase.SpriteBatch.Draw(GameBase.UI.BlankBox, new Rectangle(i * 15, (int)GameBase.WindowRectangle.Height - CurrentBarSize[i], 15, CurrentBarSize[i]), Color.Red);
+                else if (AverageFpsIntervals[i] < 144)
+                    GameBase.SpriteBatch.Draw(GameBase.UI.BlankBox, new Rectangle(i * 15, (int)GameBase.WindowRectangle.Height - CurrentBarSize[i], 15, CurrentBarSize[i]), Color.DarkOrange);
+                else if (AverageFpsIntervals[i] < 240)
+                    GameBase.SpriteBatch.Draw(GameBase.UI.BlankBox, new Rectangle(i * 15, (int)GameBase.WindowRectangle.Height - CurrentBarSize[i], 15, CurrentBarSize[i]), Color.Gold);
+                else if (AverageFpsIntervals[i] < 500)
+                    GameBase.SpriteBatch.Draw(GameBase.UI.BlankBox, new Rectangle(i * 15, (int)GameBase.WindowRectangle.Height - CurrentBarSize[i], 15, CurrentBarSize[i]), Color.LightGreen);
+                else if (AverageFpsIntervals[i] < 1000)
+                    GameBase.SpriteBatch.Draw(GameBase.UI.BlankBox, new Rectangle(i * 15, (int)GameBase.WindowRectangle.Height - CurrentBarSize[i], 15, CurrentBarSize[i]), Color.DeepSkyBlue);
+                else
+                    GameBase.SpriteBatch.Draw(GameBase.UI.BlankBox, new Rectangle(i * 15, (int)GameBase.WindowRectangle.Height - CurrentBarSize[i], 15, CurrentBarSize[i]), Color.Azure);
+
+                // Draw text
+                GameBase.SpriteBatch.DrawString(Fonts.Medium12, Math.Floor(FpsCurrent) + " FPS", new Vector2(2, GameBase.WindowRectangle.Height - 20), Color.Black);
+            }
         }
     }
 }
