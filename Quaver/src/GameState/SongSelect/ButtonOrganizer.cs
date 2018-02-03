7133a60126168804b3dfd0ad7ab749f957ff20ef
staravia <denysenesouma@gmail.com>
2018-02-03 09:53:19 -0800

    Update: Button Organizer Init

--- Quaver/src/GameState/SongSelect/ButtonOrganizer.cs
+++ Quaver/src/GameState/SongSelect/ButtonOrganizer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Quaver.Graphics.Button;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,21 @@ namespace Quaver.GameState.SongSelect
 {
     class ButtonOrganizer : IHelper
     {
-        public void Draw()
-        {
-            throw new NotImplementedException();
-        }
+        /// <summary>
+        ///     Reference to the list of song select buttons
+        /// </summary>
+        private List<SongSelectButton> SongSelectButtons { get; set; } = new List<SongSelectButton>();
+
+        private List<EventHandler> SongSelectEvents { get; set; } = new List<EventHandler>();
+
+        // Indexing
+        private int SongButtonPoolSize { get; set; }
+        private int BeatmapStartIndex { get; set; }
+        private int SelectedBeatmapIndex { get; set; }
+
+        private double DiffButtonsStart { get; set; }
+        private double DiffButtonsEnd { get; set; }
+
 
         public void Initialize(IGameState state)
         {
@@ -26,6 +38,28 @@ namespace Quaver.GameState.SongSelect
         public void Update(double dt)
         {
             throw new NotImplementedException();
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Generates a button pool depending on your screen size
+        /// </summary>
+        public void GenerateButtonPool()
+        {
+
+        }
+
+        /// <summary>
+        ///     Will shift buttons down by 1 seemlessly
+        /// </summary>
+        /// <param name="amount"></param>
+        public void ShiftButtonPool(int amount)
+        {
+
         }
     }
 }
