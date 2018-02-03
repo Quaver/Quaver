02500bbc6ef4a49274b42d1fa8d8d2a8c6d8003c
staravia <denysenesouma@gmail.com>
2018-02-03 08:34:16 -0800

    Add: ButtonOrganizer
    Former-commit-id: 4bafc22e793b6fd218f87b5e69ad2bcef9242afa [formerly ee6400c59ded5f7a90597a8026383e8bf47a6458]
    Former-commit-id: bae3498dc04c7bc18695cfd0b212689b0d8288d0

---
+++ Quaver/src/GameState/SongSelect/ButtonOrganizer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.GameState.SongSelect
+{
+    class ButtonOrganizer : IHelper
+    {
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(IGameState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnloadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(double dt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
