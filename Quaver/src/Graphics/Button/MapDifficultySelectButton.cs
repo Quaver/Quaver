b2cd75013f72baa9a8ff48074be93271d46cc610
staravia <denysenesouma@gmail.com>
2018-02-12 19:58:06 -0800

    Add: MapDifficultySelectButton
    Former-commit-id: 6f460158e0711b627b513b80057092104cb8bf89 [formerly 44c622aa4ba4003dba1ce49e1eed7fd0cf03567f]
    Former-commit-id: 888227afb225defc6317ff74ecb5abbc544fc579

---
+++ Quaver/src/Graphics/Button/MapDifficultySelectButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.Graphics.Button
+{
+    class MapDifficultySelectButton : Button
+    {
+        internal override void MouseOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void MouseOver()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
