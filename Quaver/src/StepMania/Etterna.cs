f73fa769782c1a04d601fee44949f2bcf056fd72
Swan <contact@swan.moe>
2018-02-18 16:22:21 -0500

    Add method to read EtternaCache files
    Former-commit-id: 43849687866f5835a1ef008b07ce67320c74dd99 [formerly 282dd50263ed6f10a37f63e58af3e91d7d9e8602]
    Former-commit-id: cae934a6083c69c2f38d83ba4c40f6e5a6fc8278

---
+++ Quaver/src/StepMania/Etterna.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quaver.StepMania
+{
+    internal static class Etterna
+    {
+        /// <summary>
+        ///     Read and serializes an Etterna cache file
+        /// </summary>
+        internal static EtternaCache ReadCacheFile(string path)
+        {
+            var ec = new EtternaCache();
+
+            var file = File.ReadAllLines(path);
+
+            foreach (var line in file)
+            {
+                if (!line.Contains("#"))
+                    continue;
+
+                var key = line.Substring(0, line.IndexOf(':')).Trim().ToUpper();
+                var value = line.Split(':').Last().Trim().Replace(";", "");
+
+                switch (key)
+                {
+                    case "#VERSION":
+                        ec.Version = value;
+                        break;
+                    case "#TITLE":
+                        ec.Title = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));
+                        break;
+                    case "#SUBTITLE":
+                        ec.Subtitle = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));
+                        break;
+                    case "#ARTIST":
+                        ec.Artist = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));
+                        break;
+                    case "#CREDIT":
+                        ec.Credit = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));
+                        break;
+                    case "#BANNER":
+                        ec.Banner = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));
+                        break;
+                    case "#BACKGROUND":
+                        ec.Background = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));
+                        break;
+                    case "#CDTITLE":
+                        ec.CdTitle = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));
+                        break;
+                    case "#MUSIC":
+                        ec.Music = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));
+                        break;
+                    case "#SAMPLESTART":
+                        ec.SampleStart = float.Parse(value, CultureInfo.InvariantCulture);
+                        break;
+                    case "#SONGFILENAME":
+                        ec.SongFileName = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));;
+                        break;
+                    case "#STEPFILENAME":
+                        ec.StepFileName = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(value));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(ec.SongFileName);
+            Console.WriteLine(ec.Artist);
+            return ec;
+        }
+    }
+}
