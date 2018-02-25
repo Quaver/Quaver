7f094287ecc9c84ac4441aaf3b892e72687b8098
Swan <contact@swan.moe>
2018-02-24 22:25:14 -0500

    Always start with steam if not in debug mode

--- Quaver/src/Steam/SteamAPIHelper.cs
+++ Quaver/src/Steam/SteamAPIHelper.cs
@@ -62,6 +62,21 @@ namespace Quaver.Steam
         /// </summary>
         internal static void Initialize()
         {
+#if DEBUG
+            // Creates a file with the Steam Application Id, this is required for debugging
+            File.WriteAllText($"{Directory.GetCurrentDirectory()}/steam_appid.txt", ApplicationId.ToString());
+
+            Console.WriteLine("In order to test Steam features in the debug release, you'll need to have a steam_appid.txt file." +
+                            "One has already been created for you, so you can proceed as normal.");
+#endif
+
+            // Make sure the game is started with steam.
+            if (SteamAPI.RestartAppIfNecessary((AppId_t) ApplicationId))
+            {
+                QuaverGame.Quit();
+                return;
+            }
+                
             // Attempt to initialize the Steam API
             IsInitialized = SteamAPI.Init();
 
@@ -69,9 +84,7 @@ namespace Quaver.Steam
             if (!IsInitialized)
             {
                 Logger.LogError("SteamAPI.Init() call has failed! Steam is not initialized!", LogType.Runtime);
-
-                // Display a dialog to users to restart the game with steam
-                PromptToOpenSteam();
+                QuaverGame.Quit();
                 return;
             }
 
@@ -89,6 +102,7 @@ namespace Quaver.Steam
                 $"[STEAM API HELPER] Logged into Steam as: {SteamFriends.GetPersonaName()} <{SteamUser.GetSteamID()}>",
                 LogType.Runtime);
 #endif
+
             // Initialize all Steam callbacks
             InitializeCallbacks();
             
@@ -105,47 +119,6 @@ namespace Quaver.Steam
         }
 
         /// <summary>
-        ///     This function prompts the user to open the game with steam. If they say yes, then the game will restart with steam 
-        ///     accordingly.
-        /// </summary>
-        private static void PromptToOpenSteam()
-        {
-            if (IsInitialized)
-                return;
-
-            // TODO: Add an actual prompt for this.
-            Console.WriteLine("Open the game with steam?");
-            var choice = Console.ReadLine();
-
-            if (choice.ToLower() == "y" || choice.ToLower() == "yes")
-                OpenWithSteam();
-        }
-
-        /// <summary>
-        ///     Opens the game with steam. If in DEBUG mode, it will create a steam_appid.txt file with our application id
-        ///     so that we can actually begin to use steam features in development.
-        /// </summary>
-        private static void OpenWithSteam()
-        {
-            // Makes the game start with steam. 
-            // NOTE: During development, if the steam_appid.txt file is present, then it will return false,
-            // meaning that we don't need to continuously launch with steam during development.
-            if (!SteamAPI.RestartAppIfNecessary((AppId_t) ApplicationId))
-                return;
-
-#if DEBUG
-            // Creates a file with the Steam Application Id, this is required for our game.
-            File.WriteAllText($"{Directory.GetCurrentDirectory()}/steam_appid.txt", ApplicationId.ToString());
-
-            Console.WriteLine(
-                "In order to test Steam features in debug mode, you'll need to have a steam_appid.txt file. " +
-                "One has already been created for you, and the program will now restart");
-#endif
-            // Quit game
-            QuaverGame.Quit();
-        }
-
-        /// <summary>
         ///     Connects to the Flamingo server
         /// </summary>
         private static void ConnectToFlamingo(bool devServer = false)
@@ -154,12 +127,12 @@ namespace Quaver.Steam
             if (devServer)
                 ConnectingToDevServer = true;
 
-            // Make sure steam is actually open and initialized
-            PromptToOpenSteam();
-
             // If we're still not initialized at this point, we shouldn't continue
             if (!IsInitialized)
+            {
+                QuaverGame.Quit();
                 return;
+            }
 
             // Generate an auth session token and wait for a response from Steam
             // After calling this, it should call OnValidateAuthSessionTicketResponse(GetAuthSessionTicketResponse_t pCallback);
@@ -175,17 +148,13 @@ namespace Quaver.Steam
         /// <param name="pCallback"></param>
         private static void OnValidateAuthSessionTicketResponse(GetAuthSessionTicketResponse_t pCallback)
         {
-            Logger.LogImportant($"Result: {pCallback.m_eResult}|{pCallback.m_hAuthTicket.m_HAuthTicket}", LogType.Runtime);
-            Logger.LogImportant($"{AuthSessionTicket.m_HAuthTicket}", LogType.Runtime);
-
             // Make the server login request if we've received confirmation that the auth session ticket
             // was successfully created
             switch (pCallback.m_eResult)
             {
                 // Send the login request to Flamingo.
                 case EResult.k_EResultOK:
-                    Console.WriteLine("Actual Length: " + PcbTicket);
-                    FlamingoClient.Connect(SteamUser.GetSteamID().ToString(), SteamFriends.GetPersonaName(), PTicket, PcbTicket, ConnectingToDevServer);
+                    Flamingo.Connect(SteamUser.GetSteamID().ToString(), SteamFriends.GetPersonaName(), PTicket, PcbTicket, ConnectingToDevServer);
                     break;
                 // All error cases returned from Steam
                 default:
