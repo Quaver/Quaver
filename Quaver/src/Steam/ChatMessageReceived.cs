0e300550896bdef3422d500244a4d8efe157315e
Swan <contact@swan.moe>
2018-02-26 12:33:07 -0500

    Add event handler for ChatMessageReceived

---
+++ Quaver/src/Steam/ChatMessageReceived.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.Logging;
+using Quaver.Net.Handlers;
+
+namespace Quaver.Steam
+{
+    internal class ChatMessageReceived
+    {
+        /// <summary>
+        ///     Called when we receive a chat message from the server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        internal static void OnChatMessageReceived(object sender, ChatMessageReceivedEventArgs e)
+        {
+            // Chat channel message
+            if (e.Message.Channel.StartsWith("#"))
+            {
+                Logger.LogInfo($"[{e.Message.DateTime.Hour}:{e.Message.DateTime.Minute}:{e.Message.DateTime.Second}] Channel: {e.Message.Channel} - {e.Message.Sender}: {e.Message.Text}", LogType.Network);
+                return;
+            }
+            
+            // Private Message
+            Logger.LogInfo($"[{e.Message.DateTime.Hour}:{e.Message.DateTime.Minute}:{e.Message.DateTime.Second}] From: {e.Message.Sender}: {e.Message.Text}", LogType.Network);
+        }
+    }
+}
