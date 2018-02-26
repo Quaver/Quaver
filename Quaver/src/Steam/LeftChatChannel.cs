d98cd0deb1acc6301554ac3987f4487a764285a8
Swan <contact@swan.moe>
2018-02-26 12:33:35 -0500

    Add event handler for LeftChatChannel

---
+++ Quaver/src/Steam/LeftChatChannel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quaver.Logging;
+using Quaver.Net.Handlers;
+
+namespace Quaver.Steam
+{
+    internal static class LeftChatChannel
+    {
+        /// <summary>
+        ///     Called when the server confirms that we've successfully left a chat channel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        internal static void OnChatChannelLeft(object sender, LeftChatChannelEventArgs e)
+        {
+            Logger.LogSuccess($"Successfully left Chat Channel: {e.Channel.Name}", LogType.Network);
+        }
+    }
+}
