c8f9a9da0dfbf63d4b709b2eb10a931c172d216e
Swan <contact@swan.moe>
2018-03-04 14:21:49 -0500

    Log out previous chat messages when joining channels

--- Quaver/src/Online/Events/JoinedChatChannel.cs
+++ Quaver/src/Online/Events/JoinedChatChannel.cs
@@ -18,6 +18,10 @@ namespace Quaver.Online.Events
         internal static void OnChatChannelJoined(object sender, JoinedChatChannelEventArgs e)
         {
             Logger.LogSuccess($"Successfully joined Chat Channel: {e.Channel.Name}", LogType.Network);
+
+            // TODO: Do something with the old messages you get.
+            foreach (var msg in e.PreviousMessages)
+                Logger.LogSuccess($"[{msg.DateTime.Hour}:{msg.DateTime.Minute}:{msg.DateTime.Second}] {msg.Channel} - {msg.Sender}: {msg.Text}", LogType.Network);
         }
     }
 }
