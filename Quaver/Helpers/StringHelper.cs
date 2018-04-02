5bf6e16d1440cb005f70b98fe6af84b5770756be
Swan <contact@swan.moe>
2018-04-02 09:58:17 -0400

    Move utils to `StringHelper` class
    Former-commit-id: eb603c762078a55fd49516d9f5518991d454ecad [formerly de61083243bd57870408a3e9d916bfb14f50b439]
    Former-commit-id: 13c65ba21190a1a42957024411af54de4dfd8eab

---
+++ Quaver/Helpers/StringHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Quaver.Helpers
+{
+    internal class StringHelper
+    {
+        /// <summary>
+        /// Converts score to string (1234567) format
+        /// </summary>
+        /// <param name="score"></param>
+        /// <returns></returns>
+        internal static string ScoreToString(int score)
+        {
+            return score.ToString("0000000");
+        }
+
+        /// <summary>
+        ///     Makes a string safe to be written as a file name.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        internal static string FileNameSafeString(string str)
+        {
+            var invalidPathChars = Path.GetInvalidFileNameChars();
+            return invalidPathChars.Aggregate(str, (current, invalidChar) => current.Replace(invalidChar.ToString(), ""));
+        }
+
+        /// <summary>
+        ///     Returns a color to a string
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        internal static string ColorToString(Color color)
+        {
+            return $"{color.R},{color.G},{color.G},{color.A}";
+        }
+    }
+}
