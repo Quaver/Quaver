fab8b81f9496d2884bc209e7cc5590f70a8dd32d
Swan <contact@swan.moe>
2018-04-02 16:17:00 -0400

    Fix last broken string
    Former-commit-id: 71cd52056a8778ea6f8a85a580b4e93758a901d7 [formerly 684d1fe038d3533399aab24c7e5bf4378d2194f8]
    Former-commit-id: ac3eb293d58b29797f3258a87a2cd7e769b50142

--- Quaver/Discord/DiscordController.cs
+++ Quaver/Discord/DiscordController.cs
@@ -143,7 +143,7 @@ namespace Quaver.Discord
                     sb.Append(" + ");
 
                     if (GameBase.CurrentGameModifiers.Exists(x => x.Type == ModType.Speed))
-                        sb.Append($"ManiaModSpeed {GameBase.AudioEngine.PlaybackRate}x");
+                        sb.Append($"Speed {GameBase.AudioEngine.PlaybackRate}x");
                 }
 
                 ChangeDiscordPresence(mapString, sb.ToString(), mapLength);
