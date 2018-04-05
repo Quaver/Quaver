6996b7ae40daee2d4396e306efa6467ae3bbb55a
Swan <contact@swan.moe>
2018-04-05 16:30:03 -0400

    Add sample navbar overlay + NavbarButton
    Former-commit-id: 89f19613cb2075372cfcbcd2c67741b52c856bc6 [formerly 71ea40960260d4c82ebd3fd5e09d53b538384b8d]
    Former-commit-id: 0be3bf18562afd5a754cf2bdc5443cd06618c159

---
+++ Quaver/Graphics/Overlays/Navbar/Navbar.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Quaver.GameState;
+using Quaver.Graphics.Base;
+using Quaver.Graphics.Buttons;
+using Quaver.Graphics.Enums;
+using Quaver.Graphics.Sprites;
+using Quaver.Graphics.UniversalDim;
+using Quaver.Main;
+using Quaver.States;
+
+namespace Quaver.Graphics.Overlays.Navbar
+{
+    /// <summary>
+    ///     A navbar overlay
+    /// </summary>
+    internal class Navbar : IGameStateComponent
+    {
+        /// <summary>
+        ///     The actual navbar sprite
+        /// </summary>
+        internal QuaverSprite Nav { get; set; }
+
+        /// <summary>
+        ///     The container for the navbar
+        /// </summary>
+        private QuaverContainer Container { get; set; }
+
+         /// <summary>
+        ///     Init
+        /// </summary>
+        /// <param name="state"></param>
+        public void Initialize(IGameState state)
+        {
+            Container = new QuaverContainer();
+            
+            Nav = new QuaverSprite()
+            {
+                Size = new UDim2D(0, 50, 1, 0),
+                Alignment = Alignment.TopLeft,
+                Tint = new Color(0f, 0f, 0f, 0.5f),
+                Parent = Container
+            };
+            
+            // Replace with actual sprites
+            var home = new NavbarButton(this, GameBase.QuaverUserInterface.BlankBox, "Home", "Go to the main menu")
+            {
+                Parent = Nav
+            };
+
+            var play = new NavbarButton(this, GameBase.QuaverUserInterface.BlankBox, "Play", "Play Quaver")
+            {
+                Parent = Nav
+            };
+
+            var test = new NavbarButton(this, GameBase.QuaverUserInterface.BlankBox, "Meme", "dd")
+            {
+                Parent = Nav
+            };
+        }
+
+         /// <summary>
+        ///     Unload
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void UnloadContent()
+        {
+            Container.Destroy();
+        }
+
+        public void Update(double dt)
+        {
+            Container.Update(dt);
+        }
+
+        public void Draw()
+        {
+            Container.Draw();
+        }
+    }
+}
