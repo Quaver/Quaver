0317be1bfcd06f4a7a61506b177e3eda4a9ba2e2
Swan <contact@swan.moe>
2018-04-05 16:30:16 -0400

    Add test state for Navbar
    Former-commit-id: 0b80b25f7db4e9d7f80fca2d9b367d72bfde8e34 [formerly 0dd484cdb6395c4e3f90c832f9822f2f1b1cdbe7]
    Former-commit-id: f23944e499dfd4e666e72e5e258afff8f65fb341

---
+++ Quaver/States/Tests/NavbarTestState.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.GameState;
+using Quaver.Graphics.Overlays.Navbar;
+using Quaver.Graphics.Sprites;
+using Quaver.Main;
+using Quaver.States.Enums;
+
+namespace Quaver.States.Tests
+{
+    internal class NavbarTestState : IGameState
+    {
+        /// <summary>
+        ///     Test Screen
+        /// </summary>
+        public State CurrentState { get; set; } = State.TestScreen;
+        
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public bool UpdateReady { get; set; }
+
+        private QuaverContainer Container { get; set; }
+
+        /// <summary>
+        ///     Navbar sprite
+        /// </summary>
+        private Navbar Nav { get; set; }
+        
+        public void Initialize()
+        {
+            Container = new QuaverContainer();
+            Nav = new Navbar();
+            Nav.Initialize(this);
+            
+            UpdateReady = true;
+        }
+
+        public void UnloadContent()
+        {
+        }
+
+        public void Update(double dt)
+        {
+            Nav.Update(dt);
+            Container.Update(dt);
+        }
+
+        public void Draw()
+        {
+            GameBase.GraphicsDevice.Clear(Color.Teal);
+            GameBase.SpriteBatch.Begin();
+            
+            Nav.Draw();
+            
+            GameBase.SpriteBatch.End();
+        }
+    }
+}
