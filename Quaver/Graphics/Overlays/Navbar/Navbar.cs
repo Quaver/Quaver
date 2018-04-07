307ccb4a3b7d5ce8c3fb93ef146f1d19d993cb34
Swan <contact@swan.moe>
2018-04-07 17:43:33 -0400

    Add animations for Navbar show/hide
    Former-commit-id: 706ccfc9849b6ec4e60ff0c087ef6e65672cd07a [formerly 9ef99d29401b9eb50d13b2419fe72e3a419e426d]
    Former-commit-id: 95afbe0820cd2c424eaea788b8148b2cfb2fa007

--- Quaver/Graphics/Overlays/Navbar/Navbar.cs
+++ Quaver/Graphics/Overlays/Navbar/Navbar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Quaver.GameState;
 using Quaver.Graphics.Base;
 using Quaver.Graphics.Buttons;
@@ -9,6 +10,7 @@ using Quaver.Graphics.Enums;
 using Quaver.Graphics.Sprites;
 using Quaver.Graphics.Text;
 using Quaver.Graphics.UniversalDim;
+using Quaver.Helpers;
 using Quaver.Main;
 using Quaver.States;
 
@@ -53,6 +55,16 @@ namespace Quaver.Graphics.Overlays.Navbar
         internal QuaverContainer Container { get; set; }
 
         /// <summary>
+        ///     If the navbar is shown
+        /// </summary>
+        private bool IsShown { get; set; }
+
+        /// <summary>
+        ///     If the navbar is currently in an animation
+        /// </summary>
+        private bool InAnimation { get; set; }
+
+        /// <summary>
         ///     Initialize
         /// </summary>
         /// <param name="state"></param>
@@ -72,7 +84,7 @@ namespace Quaver.Graphics.Overlays.Navbar
             {
                 Size = new UDim2D(0, 50, 1, 0),
                 Alignment = Alignment.TopLeft,
-                Tint = new Color(0f, 0f, 0f, 0.5f),
+                Tint = new Color(0f, 0f, 0f, 0.35f),
                 Parent = Container
             };
 
@@ -107,6 +119,10 @@ namespace Quaver.Graphics.Overlays.Navbar
             // Replace with actual sprites
             var home = CreateNavbarButton(NavbarAlignment.Left, GameBase.QuaverUserInterface.BlankBox, "Home", "Go to main menu", OnHomeButtonClicked);         
             var play = CreateNavbarButton(NavbarAlignment.Left, GameBase.QuaverUserInterface.BlankBox, "Play", "Play Quaver", OnPlayButtonClicked);
+            var keys4 = CreateNavbarButton(NavbarAlignment.Left, GameBase.QuaverUserInterface.BlankBox, "Game Mode: 4 Keys", "Change your selected game mode to 4K", OnPlayButtonClicked);
+            var keys7 = CreateNavbarButton(NavbarAlignment.Left, GameBase.QuaverUserInterface.BlankBox, "Game Mode: 7 Keys", "Change your selected game mode to 7K", OnPlayButtonClicked);
+            var quit = CreateNavbarButton(NavbarAlignment.Right, GameBase.QuaverUserInterface.BlankBox, "Quit", "Bye Bye.", OnPlayButtonClicked);
+            var settings = CreateNavbarButton(NavbarAlignment.Right, GameBase.QuaverUserInterface.BlankBox, "Settings", "Configure the game.", OnPlayButtonClicked);
         }
 
          /// <summary>
@@ -124,6 +140,12 @@ namespace Quaver.Graphics.Overlays.Navbar
         /// <param name="dt"></param>
         public void Update(double dt)
         {
+            if (GameBase.KeyboardState.IsKeyDown(Keys.Z))
+                PerformHideAnimation(dt);
+            
+            if (GameBase.KeyboardState.IsKeyDown(Keys.X))
+                PerformShowAnimation(dt);
+            
             Container.Update(dt);
         }
 
@@ -151,6 +173,50 @@ namespace Quaver.Graphics.Overlays.Navbar
             return button;
         }
 
+        /// <summary>
+        ///     Peforms an animation which hides the navbar.
+        /// </summary>
+        /// <param name="dt"></param>
+        private void PerformHideAnimation(double dt)
+        {
+            // The position in which the navbar is considered hidden.
+            const float hiddenPos = -50f;
+
+            // Don't perform the animation anymore after reaching a certain height.
+            if (Math.Abs(Container.PosY - hiddenPos) < 0.1)
+            {
+                IsShown = false;
+                InAnimation = false;
+                Container.Visible = false;
+                return;
+            }
+            
+            Container.PosY = GraphicsHelper.Tween(hiddenPos, Container.PosY, Math.Min(dt / 30, 1));
+        }
+
+         /// <summary>
+        ///     Performs an animation which shows the navbar.
+        /// </summary>
+        /// <param name="dt"></param>
+        private void PerformShowAnimation(double dt)
+         {
+             // Make the container visible again when performing this animation.
+             Container.Visible = true;
+             
+            // The original position of the navbar
+            const int origPos = 0;
+          
+            // Don't perform the animation anymore after reaching a certain height.
+            if (Math.Abs(Container.PosY - origPos) < 0.1)
+            {
+                IsShown = true;
+                InAnimation = false;
+                return;
+            }
+            
+            Container.PosY = GraphicsHelper.Tween(origPos, Container.PosY, Math.Min(dt / 30, 1));
+        }
+        
         /// <summary>
         ///     Called when the home button is clicked.
         /// </summary>
