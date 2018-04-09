583527c717573cdd3c4b6f63a198e4487ba88122
Swan <contact@swan.moe>
2018-04-09 17:33:18 -0400

    Add method to load in icons we actually need

---
+++ Quaver/Main/FontAwesome.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Quaver.Helpers;
+using Quaver.Resources;
+
+namespace Quaver.Main
+{
+    internal static class FontAwesome
+    {
+        internal static Texture2D Home { get; set; }
+
+        /// <summary>
+        ///     Loads all FontAwesome icon textures.
+        /// </summary>
+        internal static void Load()
+        {
+            Home = ResourceHelper.LoadTexture2DFromPng(QuaverResources.fa_home);
+        }
+    }
+}
