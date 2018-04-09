f3249a20b10b804205f67cec94503ce0d7ae8f26
Swan <contact@swan.moe>
2018-04-09 19:57:53 -0400

    Load in more icons
    Former-commit-id: 19530c5fd684d1e7faf337eea669d700efc8b39a [formerly 7923ad33b8b6f60a7651772ec8a311e41b3978fb]
    Former-commit-id: f1352b8ded61dd45dbef0014b254075f3e09f1dd

--- Quaver/Main/FontAwesome.cs
+++ Quaver/Main/FontAwesome.cs
@@ -7,6 +7,13 @@ namespace Quaver.Main
     internal static class FontAwesome
     {
         internal static Texture2D Home { get; set; }
+        internal static Texture2D Cog { get; set; }
+        internal static Texture2D PowerOff { get; set; }
+        internal static Texture2D Discord { get; set; }
+        internal static Texture2D GamePad { get; set; }
+        internal static Texture2D Coffee { get; set; }
+        internal static Texture2D Cloud { get; set; }
+        internal static Texture2D Github { get; set; }
 
         /// <summary>
         ///     Loads all FontAwesome icon textures.
@@ -14,6 +21,13 @@ namespace Quaver.Main
         internal static void Load()
         {
             Home = ResourceHelper.LoadTexture2DFromPng(QuaverResources.fa_home);
+            Cog = ResourceHelper.LoadTexture2DFromPng(QuaverResources.fa_cog_wheel_silhouette);
+            PowerOff = ResourceHelper.LoadTexture2DFromPng(QuaverResources.fa_power_button_off);
+            Discord = ResourceHelper.LoadTexture2DFromPng(QuaverResources.fa_discord);
+            GamePad = ResourceHelper.LoadTexture2DFromPng(QuaverResources.fa_gamepad_console);
+            Coffee = ResourceHelper.LoadTexture2DFromPng(QuaverResources.fa_coffee_cup_on_a_plate_black_silhouettes);
+            Cloud = ResourceHelper.LoadTexture2DFromPng(QuaverResources.fa_cloud_storage_download);
+            Github = ResourceHelper.LoadTexture2DFromPng(QuaverResources.fa_github_logo);
         }
     }
 }
