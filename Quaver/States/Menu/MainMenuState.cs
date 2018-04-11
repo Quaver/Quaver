752eb01eb223760d1dd0e412a836c54398703757
Swan <contact@swan.moe>
2018-04-10 22:24:54 -0400

    Remove import buttons from main menu state

--- Quaver/States/Menu/MainMenuState.cs
+++ Quaver/States/Menu/MainMenuState.cs
@@ -15,13 +15,10 @@ using Quaver.Helpers;
 using Quaver.Logging;
 using Quaver.Main;
 using Quaver.Modifiers;
-using Quaver.Peppy;
 using Quaver.States.Enums;
-using Quaver.States.Import;
 using Quaver.States.Options;
 using Quaver.States.Select;
 using Quaver.States.Tests;
-using Quaver.StepMania;
 
 namespace Quaver.States.Menu
 {
@@ -53,24 +50,9 @@ namespace Quaver.States.Menu
         public QuaverButton OptionsMenuQuaverButton { get; set; }
 
         /// <summary>
-        ///     QuaverButton to import .qp
-        /// </summary>
-        public QuaverButton ImportQpQuaverButton { get; set; }
-
-        /// <summary>
         ///     QuaverButton to export .qp
         /// </summary>
         public QuaverButton ExportQpQuaverButton { get; set; }
-
-        /// <summary>
-        ///     QuaverButton to import .osz
-        /// </summary>
-        public QuaverButton ImportPeppyQuaverButton { get; set; }
-
-        /// <summary>
-        ///     QuaverButton to convert .sm files
-        /// </summary>
-        public QuaverButton ConvertStepManiaQuaverButton { get; set; }
 
         /// <summary>
         ///     Initialize
@@ -104,11 +86,8 @@ namespace Quaver.States.Menu
             test.Clicked += (sender, args) => GameBase.GameStateManager.ChangeState(new NavbarTestState());
             
             // Initialize the QuaverUserInterface buttons
-            CreateOszImportButton();
-            CreateQpImportButton();
             CreateMenuButtons();
             CreateQpExportButton();
-            CreateConvertSmButton();
 
             UpdateReady = true;
         }
@@ -119,8 +98,6 @@ namespace Quaver.States.Menu
         public void UnloadContent()
         {
             UpdateReady = false;
-            SwitchSongSelectQuaverButton.Clicked -= OnSongSelectButtonClick;
-            OptionsMenuQuaverButton.Clicked -= OnOptionsSelectButtonClick;
             QuaverContainer.Destroy();
         }
 
@@ -190,52 +167,7 @@ namespace Quaver.States.Menu
             GameBase.LoadedSkin.SoundClick.Play(GameBase.SoundEffectVolume, 0, 0);
             GameBase.GameStateManager.ChangeState(new OptionsState());
         }
-
-        /// <summary>
-        ///     Responsible for creating the import .osz button
-        /// </summary>
-        private void CreateOszImportButton()
-        {
-            // Import .osz QuaverButton
-            ImportPeppyQuaverButton = new QuaverTextButton(new Vector2(200, 40), "Import .osz")
-            {
-                Alignment = Alignment.TopCenter,
-                Parent = QuaverContainer
-            };
-
-            ImportPeppyQuaverButton.Clicked += OnImportOsuButtonClick;
-        }
-
-        /// <summary>
-        ///     Responsible for creating the button to convert .sm files
-        /// </summary>
-        private void CreateConvertSmButton()
-        {
-            ConvertStepManiaQuaverButton = new QuaverTextButton(new Vector2(200, 400), "Convert StepMania file")
-            {
-                Alignment = Alignment.MidLeft,
-                Parent = QuaverContainer
-            };
-
-            ConvertStepManiaQuaverButton.Clicked += OnConvertSmButtonClick;
-        }
-
-        /// <summary>
-        ///     Responsible for creating the import .qp button
-        /// </summary>
-        private void CreateQpImportButton()
-        {
-            // Import .osz QuaverButton
-            ImportQpQuaverButton = new QuaverTextButton(new Vector2(200, 40), "Import Quaver Mapset")
-            {
-                Alignment = Alignment.BotCenter,
-                Parent = QuaverContainer
-            };
-
-            ImportQpQuaverButton.Clicked += OnImportQpButtonClick;
-        }
-
-
+        
         /// <summary>
         ///     Responsible for creating the import .qp button
         /// </summary>
@@ -249,66 +181,6 @@ namespace Quaver.States.Menu
             };
 
             ExportQpQuaverButton.Clicked += OnExportButtonClick;
-        }
-
-        /// <summary>
-        ///     Upon clicking the button to import a .qp archive, this will handle
-        ///     the dialog portion of it.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        internal static void OnImportQpButtonClick(object sender, EventArgs e)
-        {
-            // Create the openFileDialog object.
-            var openFileDialog = new OpenFileDialog()
-            {
-                InitialDirectory = "c:\\",
-                Filter = "Quaver Mapset (*.qp)|*.qp",
-                FilterIndex = 0,
-                RestoreDirectory = true,
-                Multiselect = true
-            };
-
-            // If the dialog couldn't be shown, that's an issue, so we'll return for now.
-            if (openFileDialog.ShowDialog() != DialogResult.OK)
-                return;
-
-            // Proceed to extract and convert the map, show loading screen.
-            GameBase.GameStateManager.AddState(new MapImportLoadingState());
-
-            // Run the converter for all file names
-            Task.Run(() =>
-            {
-                foreach (var fileName in openFileDialog.FileNames)
-                    ImportQp(fileName);
-
-                // When all the maps have been converted, select the last imported map and make that the selected one.
-            }).ContinueWith(async t =>
-            {
-                await MapImportLoadingState.AfterImport();
-            });
-        }
-
-        /// <summary>
-        ///     Responsible for extracting the files from the .qp 
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <param name="num"></param>
-        private static void ImportQp(string fileName)
-        {
-            var extractPath = $@"{Config.ConfigManager.SongDirectory}/{Path.GetFileNameWithoutExtension(fileName)}/";
-
-            try
-            {
-                using (var archive = new ZipFile(fileName))
-                {
-                    archive.ExtractAll(extractPath, ExtractExistingFileAction.OverwriteSilently);
-                }
-            }
-            catch (Exception e)
-            {
-                Logger.LogError(e, LogType.Runtime);
-            }
         }
 
         /// <summary>
@@ -348,82 +220,6 @@ namespace Quaver.States.Menu
             { 
                 //Logger.Error(ex);
             }
-        }
-
-        /// <summary>
-        ///     Upon clicking the import button, we'll want to display an open file dialog,
-        ///     then attempt to convert all 4k & 7k maps and create a new song directory
-        ///     for the import queue.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public static void OnImportOsuButtonClick(object sender, EventArgs e)
-        {
-            // Create the openFileDialog object.
-            var openFileDialog = new OpenFileDialog()
-            {
-                InitialDirectory = "c:\\",
-                Filter = "Peppy Map Set (*.osz)|*.osz",
-                FilterIndex = 0,
-                RestoreDirectory = true,
-                Multiselect = true
-            };
-
-            // If the dialog couldn't be shown, that's an issue, so we'll return for now.
-            if (openFileDialog.ShowDialog() != DialogResult.OK)
-                return;
-
-            // Proceed to extract and convert the map, show loading screen.
-            GameBase.GameStateManager.AddState(new MapImportLoadingState());
-
-            // Run the converter for all file names
-            Task.Run(() =>
-            {
-                for (var i = 0; i < openFileDialog.FileNames.Length; i++)
-                    Osu.ConvertOsz(openFileDialog.FileNames[i], i);
-
-                // When all the maps have been converted, select the last imported map and make that the selected one.
-            }).ContinueWith(async t =>
-            {
-                await MapImportLoadingState.AfterImport();
-            });
-        }
-
-        /// <summary>
-        ///     Called when the user clicks to convert a StepMania (.sm) file
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnConvertSmButtonClick(object sender, EventArgs e)
-        {
-            // Create the openFileDialog object.
-            var openFileDialog = new OpenFileDialog()
-            {
-                InitialDirectory = "c:\\",
-                Filter = "StepMania File (*.sm)|*.sm",
-                FilterIndex = 0,
-                RestoreDirectory = true,
-                Multiselect = true
-            };
-
-            // If the dialog couldn't be shown, that's an issue, so we'll return for now.
-            if (openFileDialog.ShowDialog() != DialogResult.OK)
-                return;
-
-            // Proceed to extract and convert the map, show loading screen.
-            GameBase.GameStateManager.AddState(new MapImportLoadingState());
-
-            // Run the converter for all file names
-            Task.Run(() =>
-            {
-                foreach (var t in openFileDialog.FileNames)
-                    StepManiaConverter.ConvertSm(t);
-
-                // When all the maps have been converted, select the last imported map and make that the selected one.
-            }).ContinueWith(async t =>
-            {
-                await MapImportLoadingState.AfterImport();
-            });
         }
     }
 }
