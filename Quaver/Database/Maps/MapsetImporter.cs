437b551529a34258f6614ac9e4c339b15716c596
Swan <contact@swan.moe>
2018-04-10 22:25:54 -0400

    Add `AfterImport()` to MapsetImporter class
    Former-commit-id: b943ebff618a4fec117661a5fedd8a8e8d2b95f8 [formerly a1c501445e4243ab137436d9577439f0b07ad23e]
    Former-commit-id: 5b07cd2e27a580fdb291dc9ec031db633f49bdd5

--- Quaver/Database/Maps/MapsetImporter.cs
+++ Quaver/Database/Maps/MapsetImporter.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Ionic.Zip;
 using Quaver.Config;
 using Quaver.Logging;
 using Quaver.Main;
@@ -32,10 +36,65 @@ namespace Quaver.Database.Maps
         /// </summary>
         /// <param name="source"></param>
         /// <param name="e"></param>
-        internal static void OnDirectoryChange(object source, FileSystemEventArgs e)
+        private static void OnDirectoryChange(object source, FileSystemEventArgs e)
         {
-            Logger.LogInfo($"Detected directory change at: {e.FullPath}", LogType.Runtime);
+            if (!GameBase.ImportQueueReady)
+                Logger.LogInfo($"Detected directory change at: {e.FullPath}", LogType.Runtime);
+            
             GameBase.ImportQueueReady = true;
+        }
+        
+        /// <summary>
+        ///     Responsible for extracting the files from the .qp 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="num"></param>
+        internal static void Import(string fileName)
+        {
+            var extractPath = $@"{ConfigManager.SongDirectory}/{Path.GetFileNameWithoutExtension(fileName)}/";
+
+            try
+            {
+                using (var archive = new ZipFile(fileName))
+                {
+                    archive.ExtractAll(extractPath, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, LogType.Runtime);
+            }
+        }
+        
+        /// <summary>
+        ///     Reloads the maps properly 
+        /// </summary>
+        /// <returns></returns>
+        internal static async Task AfterImport()
+        {
+            var oldMaps = GameBase.Mapsets;
+
+            // Import all the maps to the db
+            await MapCache.LoadAndSetMapsets();
+
+            // Update the selected map with the new one.
+            // This button should only be on the song select state, so no need to check for states here.
+            var newMapsets = GameBase.Mapsets.Where(x => !oldMaps.Any(y => y.Directory == x.Directory)).ToList();
+
+            // In the event that the user imports maps when there weren't any maps previously.
+            if (oldMaps.Count == 0)
+            {
+            }
+            else if (newMapsets.Count > 0)
+            {
+                var map = newMapsets.Last().Maps.Last();
+                Console.WriteLine(map.Artist + " " + map.Title);
+
+                // Switch map and load audio for song and play it.
+                Map.ChangeSelected(map);
+            }
+
+            Logger.LogSuccess("Successfully completed the conversion task.", LogType.Runtime);
         }
     }
 }
