04c399d18199cad377b2dc94695ea0266c7d261d
Swan <contact@swan.moe>
2018-04-12 19:28:01 -0400

    Implement a `BindedValue<T>` for Configuration

---
+++ Quaver/Config/Bindings/BindedValue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Quaver.Config.Bindings
+{
+    /// <summary>
+    ///     Generic class for values that you want to keep track of when they change.
+    ///     This is usually used for configuration variables.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class BindedValue<T>
+    {
+        /// <summary>
+        ///     Emitted when this value changes.
+        /// </summary>
+        internal EventHandler<BindedValueEventArgs<T>> OnValueChanged;
+
+        /// <summary>
+        ///     The binded value string of this property.
+        /// </summary>
+        internal BindedValueString Name { get; }
+
+        /// <summary>
+        ///     The containing binded value.
+        /// </summary>
+        private T _value;
+        internal T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnValueChanged?.Invoke(this, new BindedValueEventArgs<T>(value));
+            }
+        }
+
+        /// <summary>
+        ///     Constructor that takes in an action to call upon changing the value Bindable.
+        /// </summary>
+        /// <param name="action"></param>
+        public BindedValue(BindedValueString s, EventHandler<BindedValueEventArgs<T>> action = null)
+        {
+            Name = s;
+            
+            if (action != null)
+                OnValueChanged += action;
+        }
+    }
+    
+    /// <summary>
+    ///     EventArgs containing the value that was changed.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class BindedValueEventArgs<T> : EventArgs
+    {        
+        /// <summary>
+        ///     Ctor - 
+        /// </summary>
+        /// <param name="value"></param>
+        internal BindedValueEventArgs(T value)
+        {
+            Value = value;
+        }
+        
+        /// <summary>
+        ///     The value passed when 
+        /// </summary>
+        internal T Value { get; set; }
+    }
+}
