3ebed7a8840c654676c4e097595f7ce47957db5f
Swan <contact@swan.moe>
2018-04-20 15:57:20 -0400

    Add basic checkbox functionality + test state
    Former-commit-id: a2053e8d26b18e5578840b358cb82dd993d1f0e4 [formerly e084b0fd86fe1c3a0ebdc63e932074e46e0f0800]
    Former-commit-id: 68013c56b3cb1db2027220fb4cfb3e9787d86e25

---
+++ Quaver/Graphics/Buttons/QuaverCheckbox.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Quaver.Config;
+using Quaver.Graphics.Base;
+using Quaver.Graphics.Colors;
+using Quaver.Main;
+
+namespace Quaver.Graphics.Buttons
+{
+    internal class QuaverCheckbox : QuaverButton
+    {
+        /// <summary>
+        ///     The binded value for this checkbox.
+        /// </summary>
+        internal BindedValue<bool> BindedValue { get; }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Ctor - 
+        /// </summary>
+        /// <param name="bindedValue"></param>
+        /// <param name="size"></param>
+        internal QuaverCheckbox(BindedValue<bool> bindedValue, Vector2 size, DrawRectangle clickArea = null)
+        {
+            BindedValue = bindedValue;
+
+            Size.X.Offset = size.X;
+            Size.Y.Offset = size.Y;
+            
+            // Hook onto this value.
+            BindedValue.OnValueChanged += OnValueChanged;
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Update
+        /// </summary>
+        /// <param name="dt"></param>
+        internal override void Update(double dt)
+        {
+            // Always set the checkbox image.
+            SetCheckboxImage();
+            
+            base.Update(dt);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     When the checkbox is clicked, set the value to its opposite. 
+        /// </summary>
+        protected override void OnClicked()
+        {
+            BindedValue.Value = !BindedValue.Value;
+            GameBase.AudioEngine.PlaySoundEffect(GameBase.LoadedSkin.SoundClick);
+        }
+
+        /// <summary>
+        ///     Sets the checkbox's image based on the binded value.
+        /// </summary>
+        private void SetCheckboxImage()
+        {
+            Image = BindedValue.Value ? FontAwesome.CircleClosed : FontAwesome.CircleOpen;
+            Tint = BindedValue.Value ? QuaverColors.MainAccent : QuaverColors.MainAccentInactive;
+        }
+        
+        /// <summary>
+        ///     EventHandler for the binded value. Updates the UI accordingly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnValueChanged(object sender, BindedValueEventArgs<bool> e)
+        {
+             SetCheckboxImage();
+        }
+        
+        /// <inheritdoc />
+        /// <summary>
+        ///     Mouse Out
+        /// </summary>
+        protected override void MouseOut()
+        {
+            // TODO: Display a tooltip, perhaps?
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Mouse Over
+        /// </summary>
+        protected override void MouseOver()
+        {
+            // TODO: Display a tooltip, perhaps?
+        }
+    }
+}
---
+++ Quaver/States/Tests/CheckboxTestState.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.Config;
+using Quaver.GameState;
+using Quaver.Graphics.Buttons;
+using Quaver.Graphics.Enums;
+using Quaver.Graphics.Overlays.Navbar;
+using Quaver.Graphics.Sprites;
+using Quaver.Main;
+using Quaver.States.Enums;
+
+namespace Quaver.States.Tests
+{
+    internal class CheckboxTestState : IGameState
+    {
+        /// <summary>
+        ///     Test Screen
+        /// </summary>
+        public State CurrentState { get; set; } = State.TestScreen;
+        
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public bool UpdateReady { get; set; }
+
+        private QuaverContainer Container { get; set; }
+
+        /// <summary>
+        ///     Navbar sprite
+        /// </summary>
+        private Navbar Nav { get; set; }
+
+        public void Initialize()
+        {
+            Container = new QuaverContainer();
+            Nav = new Navbar();
+            Nav.Initialize(this);
+            
+            var mem = new QuaverCheckbox(ConfigManager.DownScroll4K, new Vector2(40, 40))
+            {
+                Parent = Container,
+                Alignment = Alignment.MidCenter
+            };
+            
+            UpdateReady = true;
+        }
+
+        public void UnloadContent()
+        {
+            Nav.UnloadContent();
+            Container.Destroy();
+        }
+
+        public void Update(double dt)
+        {
+            Nav.Update(dt);
+            Container.Update(dt);
+        }
+
+        public void Draw()
+        {
+            GameBase.GraphicsDevice.Clear(Color.DarkOliveGreen);
+            GameBase.SpriteBatch.Begin();
+            
+            Nav.Draw();
+            Container.Draw();
+            
+            GameBase.SpriteBatch.End();
+        }
+    }
+}
