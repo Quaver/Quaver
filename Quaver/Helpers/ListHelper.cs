9031fe3026b2ce8d4bb6a6df4d71f7db16f258a0
Swan <contact@swan.moe>
2018-04-21 19:19:08 -0400

    Add helper class to swap list items
    Former-commit-id: 3f58f602263b2aca053b08d2e95ad5a5f64848a2 [formerly 23b1436068d541dc987e639f79660af8fdac65ac]
    Former-commit-id: 26e02f74444ce97add268bbcd6e277739a4d7d51

---
+++ Quaver/Helpers/ListHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Quaver.Helpers
+{
+    internal static class ListHelper
+    {
+        internal static void Swap<T>(IList<T> list, int indexA, int indexB)
+        {
+            T tmp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = tmp;
+        }
+    }
+}
