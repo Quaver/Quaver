d6e42d8485dcbec58a96fca402fcaa32867353b8
Swan <contact@swan.moe>
2018-04-21 17:13:24 -0400

    Add basic dropdown functionality
    Former-commit-id: c85e2e691c340371319be85a71d0764da221c38a [formerly c1393a838ee6d6ceeebd98432c75d3142b60f9e8]
    Former-commit-id: e9d191f7d71d3193a29cf4e8a3267a812f3f5dbb

---
+++ Quaver/Graphics/Buttons/Dropdowns/QuaverDropdown.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Quaver.Graphics.Enums;
+using Quaver.Graphics.Sprites;
+
+namespace Quaver.Graphics.Buttons.Dropdowns
+{
+    internal class QuaverDropdown : QuaverSprite
+    {
+        /// <summary>
+        ///     The options for the dropdown buttons.
+        /// </summary>
+        internal List<QuaverDropdownButton> Options { get; set; }
+
+        /// <summary>
+        ///     The list of options strings.
+        /// </summary>
+        internal List<string> OptionsStringList { get; set; }
+
+        /// <summary>
+        ///     If the dropdown is currently open or closed.
+        /// </summary>
+        internal bool IsOpen { get; set; }
+        
+        /// <summary>
+        ///     The size of each button.
+        /// </summary>
+        private Vector2 ButtonSize { get; }
+
+        /// <summary>
+        ///     Ctor - 
+        /// </summary>
+        internal QuaverDropdown(List<string> options, EventHandler<DropdownButtonClickedEventArgs> onClick, int width = 300, uint selectedIndex = 0)
+        {
+            // Only allow correct selected button.
+            if (selectedIndex > options.Count - 1)
+                throw new ArgumentException("Selected dropdown index must not be greater than the size of the options.");
+                   
+            // Set options lists
+            Options = new List<QuaverDropdownButton>();
+            OptionsStringList = options;
+            
+            // Set button size.
+            SizeX = width;
+            ButtonSize = new Vector2(width, 35);
+
+            // Add selected buttons for each.
+            foreach (var option in options)
+            {
+                // Create the new button
+                var btn = new QuaverDropdownButton(this, ButtonSize, option)
+                {
+                    Parent = this,
+                    IsSelected = options.IndexOf(option) == selectedIndex
+                };
+
+                btn.Visible = btn.IsSelected;
+                
+                // Set the position of the button accordingly.
+                if (Options.Count == 0)
+                    PosY = 0;
+                else
+                {
+                    var last = Options.Last();
+                    btn.PosY = last.PosY + last.SizeY;
+                }
+                
+                // Add click handler.
+                btn.Clicked += onClick;
+                
+                // Add button option to list.
+                Options.Add(btn);
+            }
+        }
+    }
+}
