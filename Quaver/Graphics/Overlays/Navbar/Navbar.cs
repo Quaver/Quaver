ab02061693c84dd27a0fbd8b990bbfe77390861b
Swan <contact@swan.moe>
2018-04-21 17:13:34 -0400

    Add test state for dropdowns
    Former-commit-id: e726b64779aa586a43ace7f617a5853817cee84e [formerly 9fbaab9edf8082119f76bfd57c6e251a65ccc25e]
    Former-commit-id: 9271b270ac6d928401774bfc156c3c032e8767af

--- Quaver/Graphics/Overlays/Navbar/Navbar.cs
+++ Quaver/Graphics/Overlays/Navbar/Navbar.cs
@@ -126,7 +126,7 @@ namespace Quaver.Graphics.Overlays.Navbar
             Github = CreateNavbarButton(NavbarAlignment.Right, FontAwesome.Github, "GitHub", "Contribute to the project!", OnGithubButtonClicked);
             
             // Test States.
-            // CreateNavbarButton(NavbarAlignment.Right, FontAwesome.Coffee, "Test State", "Go to testing", (sender, args) => GameBase.GameStateManager.ChangeState(new SliderTestState()));
+            CreateNavbarButton(NavbarAlignment.Right, FontAwesome.Coffee, "Test State", "Go to testing", (sender, args) => GameBase.GameStateManager.ChangeState(new DropdownTestState()));
 #endregion
         }
 
