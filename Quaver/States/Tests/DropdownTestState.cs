513ae147945ca37bf39f2723b3b7b6c45b6503dc
Swan <contact@swan.moe>
2018-04-21 17:13:34 -0400

    Add test state for dropdowns
    Former-commit-id: e726b64779aa586a43ace7f617a5853817cee84e [formerly 6ab67b788ef5092a8aca003a795fe513d0d2d6f0]
    Former-commit-id: 9271b270ac6d928401774bfc156c3c032e8767af

---
+++ Quaver/States/Tests/DropdownTestState.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.GameState;
+using Quaver.Graphics.Buttons.Dropdowns;
+using Quaver.Graphics.Enums;
+using Quaver.Graphics.Overlays.Navbar;
+using Quaver.Graphics.Sprites;
+using Quaver.Main;
+using Quaver.States.Enums;
+
+namespace Quaver.States.Tests
+{
+    internal class DropdownTestState : IGameState
+    {
+        /// <summary>
+        ///     Test Screen
+        /// </summary>
+        public State CurrentState { get; set; } = State.TestScreen;
+        
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public bool UpdateReady { get; set; }
+
+        private QuaverContainer Container { get; set; }
+
+        /// <summary>
+        ///     Navbar sprite
+        /// </summary>
+        private Navbar Nav { get; set; }
+
+        private QuaverDropdown Dropdown { get; set; }
+
+        public void Initialize()
+        {
+            Container = new QuaverContainer();
+            Nav = new Navbar();
+            Nav.Initialize(this);
+
+            Dropdown = new QuaverDropdown(new List<string>() {"hi", "bye"}, (o ,e) => Console.WriteLine("MEMES! " + e.ButtonText))
+            {
+                Parent = Container,
+                Alignment = Alignment.MidCenter
+            };
+            
+            UpdateReady = true;
+        }
+
+        public void UnloadContent()
+        {
+        }
+
+        public void Update(double dt)
+        {
+            Nav.Update(dt);
+            Container.Update(dt);
+        }
+
+        public void Draw()
+        {
+            GameBase.GraphicsDevice.Clear(Color.DarkOliveGreen);
+            GameBase.SpriteBatch.Begin();
+            
+            Nav.Draw();
+            Container.Draw();
+            
+            GameBase.SpriteBatch.End();
+        }
+    }
+}
