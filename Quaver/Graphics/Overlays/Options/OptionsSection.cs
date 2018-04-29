d85cd41aebd7fdf2a8e78483159c6df7c787d918
Swan <me@swan.moe>
2018-04-28 18:00:39 -0700

    Add class container for options sections

---
+++ Quaver/Graphics/Overlays/Options/OptionsSection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Quaver.Graphics.Overlays.Options
+{
+    internal class OptionsSection
+    {        
+        /// <summary>
+        ///     The stringified name of the section.
+        /// </summary>
+        internal string Name { get; }
+
+        /// <summary>
+        ///     The icon displayed for this section.
+        /// </summary>
+        internal Texture2D Icon { get; }
+
+        /// <summary>
+        ///     Ctor - 
+        /// </summary>
+        internal OptionsSection(string name, Texture2D icon)
+        {
+            Name = name;
+            Icon = icon;
+        }
+    }
+}
