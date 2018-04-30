373c054ab53e30f191652f06430483d9e6a3242b
Swan <me@swan.moe>
2018-04-30 13:50:01 -0400

    Auto-create options section creator upon instantiation

--- Quaver/Graphics/Overlays/Options/OptionsSection.cs
+++ Quaver/Graphics/Overlays/Options/OptionsSection.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.Graphics.Base;
 using Quaver.Graphics.Buttons;
+using Quaver.Graphics.Enums;
+using Quaver.Graphics.Sprites;
+using Quaver.Graphics.UniversalDim;
 
 namespace Quaver.Graphics.Overlays.Options
 {
@@ -16,17 +21,27 @@ namespace Quaver.Graphics.Overlays.Options
         internal Texture2D Icon { get; }
 
         /// <summary>
-        ///     The button displayed in the menu bar for this section.
+        ///     The container of the options section.
         /// </summary>
-        internal QuaverTextButton MenuBarButton { get; set; }
+        internal QuaverSprite Container { get; set; }
 
         /// <summary>
         ///     Ctor - 
         /// </summary>
-        internal OptionsSection(string name, Texture2D icon)
+        internal OptionsSection(Drawable overlay, string name, Texture2D icon)
         {
             Name = name;
             Icon = icon;
+            
+            Container = new QuaverSprite()
+            {
+                Parent = overlay,               
+                Size = new UDim2D(800, 450),
+                Alignment = Alignment.MidCenter,
+                PosY = 90,
+                Tint = new Color(0f, 0f, 0f, 1f),
+                Visible = false
+            };        
         }
     }
 }
