4c511391aa10ba70896ddf994f4110f404e01c29
Swan <me@swan.moe>
2018-05-06 13:52:26 -0400

    Add method to get the index of the nearest lane object

--- Quaver/States/Gameplay/GameModes/Keys/KeysHitObjectPool.cs
+++ Quaver/States/Gameplay/GameModes/Keys/KeysHitObjectPool.cs
@@ -1,7 +1,30 @@
-﻿namespace Quaver.States.Gameplay.GameModes.Keys
+﻿using Quaver.API.Gameplay;
+using Quaver.States.Gameplay.HitObjects;
+
+namespace Quaver.States.Gameplay.GameModes.Keys
 {
-    public class KeysHitObjectPool
+    internal class KeysHitObjectPool : HitObjectPool
     {
-        
+        internal KeysHitObjectPool(int size) : base(size)
+        {
+        }
+
+        /// <summary>
+        ///     Gets the index of the nearest object in a given lane.
+        /// </summary>
+        /// <param name="lane"></param>
+        /// <param name="songTime"></param>
+        /// <returns></returns>
+        public int GetIndexOfNearestLaneObject(int lane, double songTime)
+        {
+            // Search for closest ManiaHitObject that is inside the HitTiming Window
+            for (var i = 0; i < Size && i < Objects.Count; i++)
+            {
+                if (Objects[i].Info.Lane == lane && Objects[i].Info.StartTime - songTime > -JudgeWindow.Okay)
+                    return i;
+            }
+            
+            return -1;
+        }
     }
 }
