46ec4060adc16a5c7438d37d7d171af8eb9400de
Swan <me@swan.moe>
2018-05-16 15:22:52 -0400

    Add ProgressBar class + general GameplayInterface class

--- Quaver/Graphics/UserInterface/ProgressBar.cs
+++ Quaver/Graphics/UserInterface/ProgressBar.cs
@@ -1,7 +1,90 @@
-﻿namespace Quaver.Graphics.UserInterface
+﻿using System;
+using Microsoft.Xna.Framework;
+using Quaver.Graphics.Base;
+using Quaver.Graphics.Colors;
+using Quaver.Graphics.Enums;
+using Quaver.Graphics.Sprites;
+using Quaver.Graphics.UniversalDim;
+using Quaver.Helpers;
+
+namespace Quaver.Graphics.UserInterface
 {
-    public class ProgressBar
+    /// <inheritdoc />
+    /// <summary>
+    ///     General purpose progress bar.
+    ///     TODO: Make this with BindedFloat class instead for auto-updates.
+    /// </summary>
+    internal class ProgressBar : QuaverSprite
     {
+        /// <summary>
+        ///     The  maximum value of the progress bar.
+        /// </summary>
+        internal float MaxValue { get; }
+
+        /// <summary>
+        ///     The current value of the progress bar.
+        /// </summary>
+        internal float CurrentValue { get; set; }
+
+        /// <summary>
+        ///     The active color of the progress bar
+        ///     
+        /// </summary>
+        internal Color ActiveColor { get; set; } = QuaverColors.MainAccent;
+
+        /// <summary>
+        ///     The inactive color of the progress bar.
+        /// </summary>
+        internal Color InactiveColor { get; set; } = QuaverColors.MainAccentInactive;
+
+        /// <summary>
+        ///     The current active progress bar.
+        /// </summary>
+        private QuaverSprite ActiveProgressBar { get; }
+
+        /// <summary>
+        ///     The current percentage of the progress bar.
+        /// </summary>
+        private float Percentage => (CurrentValue - 0) * 100 / MaxValue - 0 * 100;
         
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="maxValue"></param>
+        /// <param name="initialValue"></param>
+        /// <param name="size"></param>
+        /// <param name="parent"></param>
+        /// <param name="alignment"></param>
+        internal ProgressBar(float maxValue, float initialValue, UDim2D size, Drawable parent, Alignment alignment)
+        {
+            MaxValue = maxValue;
+            CurrentValue = initialValue;
+            
+            // Set the initial color of it.
+            Tint = InactiveColor;
+            Size = size;
+            Alignment = alignment;
+            Parent = parent;
+
+            // Create the active progress bar.
+            ActiveProgressBar = new QuaverSprite
+            {
+                Tint = ActiveColor,
+                Alignment = alignment,
+                Parent = parent,
+                Size = size
+            };
+        }
+        
+        /// <inheritdoc />
+        /// <summary>
+        ///     Update the active progress.
+        /// </summary>
+        /// <param name="dt"></param>
+        internal override void Update(double dt)
+        { 
+            ActiveProgressBar.Size = new UDim2D(GraphicsHelper.Tween(SizeX * (Percentage / 100f), ActiveProgressBar.SizeX, Math.Min(dt / 60f, 1)), SizeY);
+            base.Update(dt);
+        }
     }
 }
--- Quaver/States/Gameplay/GameplayInterface.cs
+++ Quaver/States/Gameplay/GameplayInterface.cs
@@ -1,7 +1,79 @@
-﻿namespace Quaver.States.Gameplay
+﻿using Quaver.API.Maps;
+using Quaver.GameState;
+using Quaver.Graphics.Enums;
+using Quaver.Graphics.Sprites;
+using Quaver.Graphics.UniversalDim;
+using Quaver.Graphics.UserInterface;
+using Quaver.Main;
+
+namespace Quaver.States.Gameplay
 {
-    public class GameplayInterface
+    internal class GameplayInterface : IGameStateComponent
     {
+        /// <summary>
+        ///     Reference to the gameplay screen itself.
+        /// </summary>
+        private GameplayScreen Screen { get; }
         
+        /// <summary>
+        ///     Contains general purpose stuff for this screen such as the following:
+        ///         - Score/Accuracy Display
+        ///         - Leaderboard display
+        ///         - Song progress time display
+        /// </summary>
+        private QuaverContainer Container { get; }
+
+        /// <summary>
+        ///     The progress bar for the song time.
+        /// </summary>
+        internal ProgressBar SongTimeProgressBar { get; set;  }
+
+        /// <summary>
+        ///     Ctor -
+        /// </summary>
+        internal GameplayInterface(GameplayScreen screen)
+        {
+            Screen = screen;
+            Container = new QuaverContainer();
+        }
+       
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="state"></param>
+        public void Initialize(IGameState state)
+        {
+            // Initialize the progress bar.
+            SongTimeProgressBar = new ProgressBar(Qua.FindSongLength(GameBase.SelectedMap.Qua), 0, new UDim2D(GameBase.WindowRectangle.Width, 6),
+                                                        Container, Alignment.BotLeft);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void UnloadContent()
+        {
+            Container.Destroy();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dt"></param>
+        public void Update(double dt)
+        {
+            // Update the current vaalue of the song time progress bar.
+            SongTimeProgressBar.CurrentValue = (float) Screen.AudioTiming.CurrentTime;
+            
+            Container.Update(dt);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Draw()
+        {
+            Container.Draw();
+        }
     }
 }
