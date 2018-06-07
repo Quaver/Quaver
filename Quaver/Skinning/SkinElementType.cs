1feddc29b4fa38260444bc3c48966e3e97f62400
Swan <me@swan.moe>
2018-06-07 17:18:19 -0400

    Update submodule

--- Quaver/Skinning/SkinElementType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Quaver.Skinning
-{
-    internal enum SkinElementType
-    {
-        Static, // Static, one off image.
-        Spritesheet // Spritesheet element. Loaded as a list of textures.
-    }
-}
