bae4137fecc917a4f54b79f877eed9d6f2158ff7
Swan <me@swan.moe>
2018-07-07 13:04:47 -0400

    Add parallax effect for backgrounds

--- Quaver/Graphics/UserInterface/Background.cs
+++ Quaver/Graphics/UserInterface/Background.cs
@@ -15,21 +15,47 @@ namespace Quaver.Graphics.UserInterface
         {
             Image = image;            
             Dim = dim;
-            Size = new UDim2D(GameBase.WindowRectangle.Width, GameBase.WindowRectangle.Height);
-
+            Size = new UDim2D(GameBase.WindowRectangle.Width + 100, GameBase.WindowRectangle.Height + 100);
+            
             BrightnessEffect = new Sprite
             {
                 Tint = Color.Black,
                 Parent = this,
-                Size = new UDim2D(GameBase.WindowRectangle.Width, GameBase.WindowRectangle.Height),
+                Size = new UDim2D(SizeX, SizeY),
                 Alpha = Dim / 100f
             };
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="dt"></param>
+        internal override void Update(double dt)
+        {
+            PerformParallaxEffect();        
+            base.Update(dt);
+        }
+
+        /// <summary>
+        ///     Changes the dim of the the background.
+        /// </summary>
+        /// <param name="dim"></param>
         internal void ChangeDim(int dim)
         {
             Dim = dim;
             BrightnessEffect.Alpha = Dim / 100f;
+        }
+
+        /// <summary>
+        ///     Adds a parallax effect for the background.
+        /// </summary>
+        private void PerformParallaxEffect()
+        {
+            // Parallax
+            var mousePos = GameBase.MouseState.Position;
+
+            PosY = (mousePos.Y - GameBase.WindowRectangle.Height / 2f) / 60f - 50;
+            PosX = (mousePos.X - GameBase.WindowRectangle.Width / 2f) / 60f - 50;
         }
     }
 }
