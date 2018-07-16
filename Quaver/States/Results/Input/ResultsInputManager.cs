25a8b3a3a433984bf3074584647b9905f3408dad
Swan <me@swan.moe>
2018-07-16 12:10:26 -0400

    Handle all existing input in ResultsInputManager

---
+++ Quaver/States/Results/Input/ResultsInputManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Quaver.Helpers;
+using Quaver.Input;
+using Quaver.Main;
+using Quaver.States.Select;
+
+namespace Quaver.States.Results.Input
+{
+    internal class ResultsInputManager
+    {
+        /// <summary>
+        ///     Reference to the parent results screen.
+        /// </summary>
+        private ResultsScreen Screen { get; }
+
+        /// <summary>
+        ///     Ctor
+        /// </summary>
+        /// <param name="screen"></param>
+        internal ResultsInputManager(ResultsScreen screen) => Screen = screen;
+
+        /// <summary>
+        ///     Handles all the input for the entire results screen.
+        /// </summary>
+        /// <param name="dt"></param>
+        internal void HandleInput(double dt)
+        {
+            if (InputHelper.IsUniqueKeyPress(Keys.F2))
+                Screen.ExportReplay();
+
+            if (InputHelper.IsUniqueKeyPress(Keys.Escape))
+                GameBase.GameStateManager.ChangeState(new SongSelectState());
+        }
+    }
+}
