d05545aa2a7b56288f31a039b93054785fe5c0d2
Swan <me@swan.moe>
2018-08-02 17:10:12 -0400

    Migrate results screen

---
+++ Quaver/Screens/Results/ResultsScreenView.cs
@@ -0,0 +1,238 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Quaver.Assets;
+using Quaver.Helpers;
+using Quaver.Screens.Results.UI;
+using Quaver.Screens.Results.UI.ScoreResults;
+using Quaver.Screens.Results.UI.Statistics;
+using Wobble;
+using Wobble.Graphics;
+using Wobble.Graphics.Transformations;
+using Wobble.Graphics.UI;
+using Wobble.Screens;
+using Wobble.Window;
+
+namespace Quaver.Screens.Results
+{
+    public class ResultsScreenView : ScreenView
+    {
+        /// <summary>
+        ///     The background image sprite.
+        /// </summary>
+        private BackgroundImage Background { get; set; }
+
+        /// <summary>
+        ///     The sprite that displays the map's information.
+        /// </summary>
+        private MapInformation MapInformation { get; set; }
+
+        /// <summary>
+        ///     The sprite that displays the score results information.
+        /// </summary>
+        private ScoreResultsTable ScoreResults { get; set; }
+
+        /// <summary>
+        ///     Information regarding online results.
+        /// </summary>
+        private ScoreResultsTable OnlineResults { get; set; }
+
+        /// <summary>
+        ///     Sprite that gives the breakdown of judgements.
+        /// </summary>
+        public JudgementBreakdown JudgementBreakdown { get; private set; }
+
+        /// <summary>
+        ///     The container for the buttons.
+        /// </summary>
+        public ResultsButtonContainer ButtonContainer { get; private set; }
+
+        /// <summary>
+        ///     The sprite that contains all of the score data.
+        /// </summary>
+        private ResultsScoreStatistics ScoreStatistics { get; set; }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="screen"></param>
+        public ResultsScreenView(Screen screen) : base(screen)
+        {
+            CreateBackground();
+            CreateMapInformation();
+            CreateScoreResults();
+            CreateOnlineResultsInfo();
+            CreateJudgementBreakdown();
+            CreateButtonContainer();
+            CreateScoreStatistics();
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime) => Container?.Update(gameTime);
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Draw(GameTime gameTime)
+        {
+            GameBase.Game.GraphicsDevice.Clear(Color.Black);
+            Container?.Draw(gameTime);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public override void Destroy() => Container?.Destroy();
+
+        /// <summary>
+        ///     Ceates the background image sprite.
+        /// </summary>
+        private void CreateBackground() => Background = new BackgroundImage(UserInterface.MenuBackground)
+            {Parent = Container};
+
+        /// <summary>
+        ///     Creates the map information sprite.
+        /// </summary>
+        private void CreateMapInformation() => MapInformation = new MapInformation((ResultsScreen) Screen)
+        {
+            Parent = Container,
+            Transformations =
+            {
+                new Transformation(TransformationProperty.Y, Easing.EaseOutBounce, -125, 60, 500)
+            }
+        };
+
+        /// <summary>
+        ///     Creates the score results sprite.
+        /// </summary>
+        private void CreateScoreResults()
+        {
+            var resultsScreen = (ResultsScreen) Screen;
+
+            var columns = new List<ScoreResultsInfoItem>()
+            {
+                new ScoreResultsInfoItem("Mods", ModHelper.GetModsString(resultsScreen.ScoreProcessor.Mods)),
+                new ScoreResultsInfoItem("Score", resultsScreen.ScoreProcessor.Score.ToString("N0")),
+                new ScoreResultsInfoItem("Accuracy", StringHelper.AccuracyToString(resultsScreen.ScoreProcessor.Accuracy)),
+                new ScoreResultsInfoItem("Max Combo", resultsScreen.ScoreProcessor.MaxCombo.ToString("N0") + "x")
+            };
+
+            ScoreResults = new ScoreResultsTable(resultsScreen, "Score Results", columns)
+            {
+                Parent = Container,
+                Y = 60 + MapInformation.Height + 20,
+                Alignment = Alignment.TopCenter,
+                X = -WindowManager.Width,
+            };
+
+            var transformation = new Transformation(TransformationProperty.X, Easing.EaseInOutElastic, ScoreResults.X, -ScoreResults.Width / 2f - 10, 800);
+            ScoreResults.Transformations.Add(transformation);
+        }
+
+        /// <summary>
+        ///     Creates the table for online results.
+        /// </summary>
+        private void CreateOnlineResultsInfo()
+        {
+            var resultsScreen = (ResultsScreen)Screen;
+
+            var columns = new List<ScoreResultsInfoItem>()
+            {
+                new ScoreResultsInfoItem("Score Rating"),
+                new ScoreResultsInfoItem("Map Rank"),
+                new ScoreResultsInfoItem("Ranks Gained")
+            };
+
+            OnlineResults = new ScoreResultsTable(resultsScreen, "Online Results", columns)
+            {
+                Parent = Container,
+                Y = 60 + MapInformation.Height + 20,
+                Alignment = Alignment.TopCenter,
+                X = WindowManager.Width
+            };
+            
+            var transformation = new Transformation(TransformationProperty.X, Easing.EaseInOutElastic, OnlineResults.X, OnlineResults.Width / 2f + 10, 800);
+            OnlineResults.Transformations.Add(transformation);
+        }
+
+        /// <summary>
+        ///     Creates the judgement breakdown sprite.
+        /// </summary>
+        private void CreateJudgementBreakdown()
+        {
+            var resultsScreen = (ResultsScreen)Screen;
+
+            JudgementBreakdown = new JudgementBreakdown(resultsScreen.ScoreProcessor)
+            {
+                Parent = Container,
+                Alignment = Alignment.TopCenter,
+                Y = ScoreResults.Y + ScoreResults.Height + 20,
+                X = -WindowManager.Width
+            };
+
+            var transformation = new Transformation(TransformationProperty.X, Easing.EaseOutElastic, JudgementBreakdown.X,
+                                                                                    -JudgementBreakdown.Width / 2f - 10, 1200);
+
+            JudgementBreakdown.Transformations.Add(transformation);
+        }
+
+        /// <summary>
+        ///     Creates the button container sprite.
+        /// </summary>
+        private void CreateButtonContainer()
+        {
+            var resultsScreen = (ResultsScreen)Screen;
+
+            ButtonContainer = new ResultsButtonContainer(resultsScreen)
+            {
+                Parent = Container,
+                Alignment = Alignment.TopCenter,
+                Y = WindowManager.Height,
+                X = 0
+            };
+
+            var transformation = new Transformation(TransformationProperty.Y, Easing.EaseOutBounce, ButtonContainer.Y,
+                                                        JudgementBreakdown.Y + JudgementBreakdown.Height + 20, 1200);
+
+            ButtonContainer.Transformations.Add(transformation);
+        }
+
+        /// <summary>
+        ///     Creates the score data container sprite.
+        /// </summary>
+        private void CreateScoreStatistics()
+        {
+            var stats = new List<StatisticContainer>();
+
+            var resultsScreen = (ResultsScreen)Screen;
+
+            // Only add data if we're coming originally from gameplay.
+            if (resultsScreen.Type == ResultsScreenType.FromGameplay)
+            {
+                stats.Add(new StatisticContainer("Hit Offset", new HitOffsetGraph(resultsScreen.ScoreProcessor)));
+                stats.Add(new StatisticContainer("Health", new HealthGraph(resultsScreen.ScoreProcessor)));
+            }
+
+            // Create the score statistics sprite along with all of the stats screens we have.
+            ScoreStatistics = new ResultsScoreStatistics(resultsScreen, JudgementBreakdown, stats)
+            {
+                Parent = Container,
+                Alignment = Alignment.TopCenter,
+                Y = JudgementBreakdown.Y,
+                X = WindowManager.Width
+            };
+
+            var transformation = new Transformation(TransformationProperty.X, Easing.EaseOutElastic, ScoreStatistics.X,
+                                                    ScoreStatistics.Width / 2f + 10, 1200);
+
+            ScoreStatistics.Transformations.Add(transformation);
+        }
+    }
+}
---
+++ Quaver/Screens/Results/UI/MapInformation.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.API.Helpers;
+using Quaver.Assets;
+using Quaver.Skinning;
+using Wobble;
+using Wobble.Graphics;
+using Wobble.Graphics.Sprites;
+using Wobble.Window;
+
+namespace Quaver.Screens.Results.UI
+{
+    public class MapInformation : Sprite
+    {
+        /// <summary>
+        ///     Reference to the results screen itself.
+        /// </summary>
+        private ResultsScreen Screen { get; }
+
+        /// <summary>
+        ///     The background for the map.
+        /// </summary>
+        private Sprite Background { get; }
+
+        /// <summary>
+        ///     The border for the background.
+        /// </summary>
+        private Sprite BackgroundBorder { get; }
+
+        /// <summary>
+        ///     The title of the song.
+        /// </summary>
+        private SpriteText Title { get; }
+
+        /// <summary>
+        ///     The creator of the map
+        /// </summary>
+        private SpriteText Creator { get; }
+
+        /// <summary>
+        ///     The player of this score
+        /// </summary>
+        private SpriteText Player { get; }
+
+        /// <summary>
+        ///     The date played.
+        /// </summary>
+        private SpriteText Date { get; }
+
+        /// <summary>
+        ///     The grade achieved on the play.
+        /// </summary>
+        private Sprite GradeImage { get; }
+
+        /// <summary>
+        ///     The spacing between the text and the background, and the text's Y spacing.
+        /// </summary>
+        private Vector2 TextSpacing { get; } = new Vector2(20, 25);
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="screen"></param>
+        public MapInformation(ResultsScreen screen)
+        {
+            Screen = screen;
+
+            Alignment = Alignment.TopCenter;
+            Size = new ScalableVector2(WindowManager.Width - 100, 125);
+            Y = -Height;
+
+            Tint = Color.Black;
+            Alpha = 0.45f;
+
+            BackgroundBorder = new Sprite
+            {
+                Parent = this,
+                Alignment = Alignment.MidLeft,
+                Size = new ScalableVector2(195, 110),
+                X = 10,
+                Tint = Color.White
+            };
+
+            Background = new Sprite
+            {
+                Parent = BackgroundBorder,
+                Size = new ScalableVector2(BackgroundBorder.Width - 2, BackgroundBorder.Height - 2),
+                Alignment = Alignment.TopLeft,
+                Position = new ScalableVector2(1, 1),
+                Image = UserInterface.MenuBackground
+            };
+
+            Title = new SpriteText(Fonts.AllerRegular16, Screen.SongTitle)
+            {
+                Parent = this,
+                X = BackgroundBorder.X + BackgroundBorder.Width,
+                TextScale = 0.95f,
+                Y = 25
+            };
+
+            Title.X += Title.MeasureString().X / 2 + TextSpacing.X;
+
+            Creator = new SpriteText(Fonts.AllerRegular16, $"By: {Screen.Qua.Creator}")
+            {
+                Parent = this,
+                X = BackgroundBorder.X + BackgroundBorder.Width,
+                TextScale = 0.75f,
+                Y = Title.Y + TextSpacing.Y
+            };
+
+            Creator.X += Creator.MeasureString().X / 2 + TextSpacing.X;
+
+            Player = new SpriteText(Fonts.AllerRegular16, $"Played by: {Screen.Replay.PlayerName}")
+            {
+                Parent = this,
+                X = BackgroundBorder.X + BackgroundBorder.Width,
+                TextScale = 0.75f,
+                Y = Creator.Y + TextSpacing.Y
+            };
+
+            Player.X += Player.MeasureString().X / 2 + TextSpacing.X;
+
+            var date = Screen.GameplayScreen != null ? DateTime.Now : Screen.Replay.Date;
+            Date = new SpriteText(Fonts.AllerRegular16, $"Date: {date.ToShortDateString()} @ {date:hh:mm:sstt}")
+            {
+                Parent = this,
+                X = BackgroundBorder.X + BackgroundBorder.Width,
+                TextScale = 0.75f,
+                Y = Player.Y + TextSpacing.Y
+            };
+
+            Date.X += Date.MeasureString().X / 2 + TextSpacing.X;
+
+            Texture2D gradeTexture;
+
+            if (Screen.GameplayScreen != null && Screen.GameplayScreen.Failed)
+                gradeTexture = SkinManager.Skin.Grades[API.Enums.Grade.F];
+            else
+                gradeTexture = SkinManager.Skin.Grades[GradeHelper.GetGradeFromAccuracy(Screen.ScoreProcessor.Accuracy)];
+
+            GradeImage = new Sprite()
+            {
+                Parent = this,
+                Image = gradeTexture,
+                Size = new ScalableVector2(100, 100f * gradeTexture.Width / gradeTexture.Height),
+                Alignment = Alignment.MidRight,
+                X = -40
+            };
+        }
+    }
+}
