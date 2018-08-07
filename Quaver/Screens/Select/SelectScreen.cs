7b87f46d46e4220e111ab6e5eee4462e8d4e7eda
Swan <me@swan.moe>
2018-08-07 16:31:31 -0400

    Add AvailableMapsets + Discord Rich Presence to select screen

--- Quaver/Screens/Select/SelectScreen.cs
+++ Quaver/Screens/Select/SelectScreen.cs
@@ -1,4 +1,14 @@
-﻿using Wobble.Screens;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using osu_database_reader;
+using Quaver.Database.Maps;
+using Quaver.Database.Scores;
+using Quaver.Screens.Gameplay;
+using Quaver.Screens.Loading;
+using Wobble.Discord;
+using Wobble.Input;
+using Wobble.Screens;
+using Keys = Microsoft.Xna.Framework.Input.Keys;
 
 namespace Quaver.Screens.Select
 {
@@ -10,7 +20,36 @@ namespace Quaver.Screens.Select
         public sealed override ScreenView View { get; protected set; }
 
         /// <summary>
+        ///     The list of available mapsets in the screen.
+        ///     This value can change as the user searches for maps.
         /// </summary>
-        public SelectScreen() => View = new SelectScreenView(this);
+        public List<Mapset> AvailableMapsets { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public SelectScreen()
+        {
+            // Default the available mapsets to all of them since the user
+            // hasn't searched or filtered by anything.
+            AvailableMapsets = MapManager.Mapsets;
+
+            DiscordManager.Client.CurrentPresence.Details = "Selecting a song";
+            DiscordManager.Client.CurrentPresence.State = "In the menus";
+            DiscordManager.Client.SetPresence(DiscordManager.Client.CurrentPresence);
+
+            View = new SelectScreenView(this);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            if (KeyboardManager.IsUniqueKeyPress(Keys.Enter))
+                ScreenManager.ChangeScreen(new MapLoadingScreen(new List<LocalScore>()));
+
+            base.Update(gameTime);
+        }
     }
 }
