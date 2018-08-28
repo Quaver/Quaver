a7470bee1d5adad36090256297aa567cb8ed81e8
Swan <me@swan.moe>
2018-08-28 16:38:45 -0400

    Start login process after retrieving auth ticket

--- Quaver/Online/OnlineManager.cs
+++ Quaver/Online/OnlineManager.cs
@@ -8,17 +8,10 @@ namespace Quaver.Online
     public static class OnlineManager
     {
         /// <summary>
-        ///     The online client for us (self)
-        /// </summary>
-        public static OnlineClient Self { get; private set; }
-
-        /// <summary>
         ///     Logs into the Quaver server.
         /// </summary>
         public static void Login()
         {
-            Self = new OnlineClient();
-
             Logger.LogImportant($"Attempting to log into the Quaver server...", LogType.Network);
 
             if (!SteamManager.AuthSessionTicketValidated)
@@ -27,7 +20,7 @@ namespace Quaver.Online
                 throw new Exception("Failed to login");
             }
 
-            Self.Login(SteamUser.GetSteamID().m_SteamID, SteamFriends.GetPersonaName(), SteamManager.PTicket, SteamManager.PcbTicket);
+            OnlineClient.Login(SteamUser.GetSteamID().m_SteamID, SteamFriends.GetPersonaName(), SteamManager.PTicket, SteamManager.PcbTicket);
         }
     }
 }
