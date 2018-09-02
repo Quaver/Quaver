789a70c3bf2407d370b8eb9e33f15df222943c44
Swan <me@swan.moe>
2018-09-02 12:49:16 -0400

    Handle events raised during the auth process

--- Quaver/Online/OnlineManager.cs
+++ Quaver/Online/OnlineManager.cs
@@ -1,8 +1,10 @@
 using System;
 using Amib.Threading;
+using Quaver.Graphics.Notifications;
 using Quaver.Logging;
 using Quaver.Scheduling;
 using Quaver.Server.Client;
+using Quaver.Server.Client.Events.Login;
 using Steamworks;
 
 namespace Quaver.Online
@@ -12,16 +14,14 @@ namespace Quaver.Online
         /// <summary>
         ///    The online client that connects to the Quaver servers.
         /// </summary>
-        private static OnlineClient _onlineClient;
+        private static OnlineClient _client;
         public static OnlineClient Client
         {
-            get => _onlineClient;
-            set
+            get => _client;
+            private set
             {
-                if (_onlineClient == null)
-                    _onlineClient = value;
-                else
-                    throw new InvalidOperationException("OnlineClient can only be created once.");
+                _client?.Disconnect();
+                _client = value;
             }
         }
 
@@ -43,16 +43,103 @@ namespace Quaver.Online
                 throw new Exception("Failed to login");
             }
 
-            if (Client == null)
-                Client = new OnlineClient();
-            else
-            {
-                /*if (Client.IsConnected)
-                    return;*/
-            }
+            // Create the new online client and subscribe to all of its online events.
+            Client = new OnlineClient();
+            SubscribeToEvents();
 
             // Initiate the connection to the game server.
             Thread.QueueWorkItem(() => Client.Connect(SteamUser.GetSteamID().m_SteamID, SteamFriends.GetPersonaName(), SteamManager.PTicket, SteamManager.PcbTicket));
+        }
+
+        /// <summary>
+        ///     Subscribes to all events
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
+        private static void SubscribeToEvents()
+        {
+            if (Client == null)
+                throw new InvalidOperationException("Cannot subscribe to events if there is no OnlineClient.");
+
+            Client.OnLoginFailed += OnLoginFailed;
+            Client.OnChooseUsername += OnChooseAUsername;
+            Client.OnChooseUsernameResponse += OnChooseAUsernameResponse;
+        }
+
+        /// <summary>
+        ///     When login to the server fails, this event handler will be called.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OnLoginFailed(object sender, FailureToLoginEventArgs e)
+        {
+            switch (e.ResponseCode)
+            {
+                case -100:
+                    NotificationManager.Show(NotificationLevel.Error, "No response from the server.");
+                    break;
+                case 401:
+                    NotificationManager.Show(NotificationLevel.Error, "You are banned.");
+                    break;
+                case 400:
+                    NotificationManager.Show(NotificationLevel.Error, "An issue has occurred during the login process.");
+                    break;
+            }
+
+            Logger.LogError(e.Error, LogType.Network);
+        }
+
+        /// <summary>
+        ///     The client is new to the server, so they need to choose a username to use.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OnChooseAUsername(object sender, ChooseAUsernameEventArgs e)
+        {
+            if (Client == null)
+                throw new InvalidOperationException("Cannot choose a username if there is no OnlineClient.");
+
+            // TODO: Replace with an actual screen to do this.
+            Logger.LogSuccess($"Choose a username to use", LogType.Runtime);
+            var username = Console.ReadLine();
+
+            Thread.QueueWorkItem(() =>
+            {
+                Client.ChooseUsername(username, SteamUser.GetSteamID().m_SteamID, SteamFriends.GetPersonaName(), SteamManager.PTicket, SteamManager.PcbTicket);
+            });
+        }
+
+        /// <summary>
+        ///     Called when the client receives a response after selecting a username.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OnChooseAUsernameResponse(object sender, ChooseAUsernameResponseEventArgs e)
+        {
+            Logger.LogImportant(e.Message, LogType.Network);
+
+            switch (e.Status)
+            {
+                // Success
+                case 200:
+                    NotificationManager.Show(NotificationLevel.Success, "Account has successfully been created!");
+                    break;
+                // Unauthorized
+                case 401:
+                    NotificationManager.Show(NotificationLevel.Error, e.Message);
+                    break;
+                // Username already taken.
+                case 409:
+                    NotificationManager.Show(NotificationLevel.Error, "The username you have selected is already taken.");
+                    break;
+                // Invalid username choice.
+                case 422:
+                    NotificationManager.Show(NotificationLevel.Error, "The username you have chosen is invalid.");
+                    break;
+                // No server response.
+                default:
+                    NotificationManager.Show(NotificationLevel.Error, "No response from the server.");
+                    break;
+            }
         }
     }
 }
