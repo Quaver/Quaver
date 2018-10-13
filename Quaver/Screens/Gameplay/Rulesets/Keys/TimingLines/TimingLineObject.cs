82fd2eb9f60ef073bbb78732e2b92cca98c11f54
staravia <denysenesouma@gmail.com>
2018-10-13 07:43:39 -0700

    Update TimingLineManager

--- Quaver/Screens/Gameplay/Rulesets/Keys/TimingLines/TimingLineObject.cs
+++ Quaver/Screens/Gameplay/Rulesets/Keys/TimingLines/TimingLineObject.cs
@@ -1,44 +1,51 @@
 using Quaver.Screens.Gameplay.Rulesets.HitObjects;
+using Quaver.Screens.Gameplay.Rulesets.Keys.HitObjects;
 using Quaver.Screens.Gameplay.Rulesets.Keys.Playfield;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Wobble.Graphics;
 using Wobble.Graphics.Sprites;
 
 namespace Quaver.Screens.Gameplay.Rulesets.Keys.TimingLines
 {
     public class TimingLineObject
     {
-        /// <summary>
-        ///     Reference to the actual playfield.
-        /// </summary>
-        private GameplayPlayfieldKeys Playfield { get; set; }
-
         private GameplayRulesetKeys Ruleset { get; set; }
 
-        private TimingLineInfo Info { get; set; }
+        public TimingLineInfo Info { get; set; }
 
         private Sprite TimingLineSprite { get; set; }
 
-        public TimingLineObject(GameplayRulesetKeys ruleset) => Ruleset = ruleset;
+        public float PositionY { get; private set; }
 
-        public void InitializeSprite(IGameplayPlayfield playfield, TimingLineInfo info)
+        public TimingLineObject(GameplayRulesetKeys ruleset, TimingLineInfo info)
         {
-            Playfield = (GameplayPlayfieldKeys)playfield;
+            Ruleset = ruleset;
             Info = info;
-            /*
+
+            // todo: implement sprite position, size, and parent
             TimingLineSprite = new Sprite()
             {
                 Alignment = Alignment.TopLeft,
-                Position = new ScalableVector2(PositionX, PositionY),
-                SpriteEffect = Effects,
-                Image = GetHitObjectTexture(),
-            };*/
+                Position = new ScalableVector2(0, PositionY)
+            };
+        }
+
+        public void UpdateSpritePosition(long trackPosition)
+        {
+            // Calculate Sprite Position
+            var manager = (HitObjectManagerKeys)Ruleset.HitObjectManager;
+            var speed = GameplayRulesetKeys.IsDownscroll ? -HitObjectManagerKeys.ScrollSpeed : HitObjectManagerKeys.ScrollSpeed;
+            PositionY = (float)(manager.HitPositionOffset + (Info.OffsetYFromReceptor - trackPosition) * speed);
+
+            // Update Sprite Position
+            TimingLineSprite.Y = PositionY;
         }
 
         public void Destroy()
         {
-            
+            TimingLineSprite.Destroy();
         }
     }
 }
