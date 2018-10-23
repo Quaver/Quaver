ed18a21affbb115002a6f021c918ec8a6805afdb
Swan <me@swan.moe>
2018-10-22 20:47:15 -0400

    Rename to TimingLine & inherit from Sprite

--- Quaver/Screens/Gameplay/Rulesets/Keys/Playfield/Lines/TimingLine.cs
+++ Quaver/Screens/Gameplay/Rulesets/Keys/Playfield/Lines/TimingLine.cs
@@ -1,30 +1,20 @@
-using Quaver.Assets;
 using Quaver.Screens.Gameplay.Rulesets.Keys.HitObjects;
-using Quaver.Screens.Gameplay.Rulesets.Keys.Playfield;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Wobble.Graphics;
 using Wobble.Graphics.Sprites;
 
-namespace Quaver.Screens.Gameplay.Rulesets.Keys.TimingLines
+namespace Quaver.Screens.Gameplay.Rulesets.Keys.Playfield.Lines
 {
-    public class TimingLineObject
+    public class TimingLine : Sprite
     {
         /// <summary>
         ///     Reference to the Keys ruleset.
         /// </summary>
-        private GameplayRulesetKeys Ruleset { get; set; }
+        private GameplayRulesetKeys Ruleset { get; }
 
         /// <summary>
         ///     Timing Line position and time information
         /// </summary>
         public TimingLineInfo Info { get; set; }
-
-        /// <summary>
-        ///     The Timing Line Sprite
-        /// </summary>
-        private Sprite TimingLineSprite { get; set; }
 
         /// <summary>
         ///     Track offset of the current Timing Line
@@ -37,30 +27,27 @@ namespace Quaver.Screens.Gameplay.Rulesets.Keys.TimingLines
         public float PositionY { get; private set; }
 
         /// <summary>
-        ///     Offset 
+        ///     Offset
         /// </summary>
         public static float GlobalYOffset { get; set; } = 0;
 
+        /// <inheritdoc />
         /// <summary>
         ///     Creates and initializes a new Timing Line Object
         /// </summary>
         /// <param name="ruleset"></param>
         /// <param name="info"></param>
-        public TimingLineObject(GameplayRulesetKeys ruleset, TimingLineInfo info)
+        public TimingLine(GameplayRulesetKeys ruleset, TimingLineInfo info)
         {
             var playfield = (GameplayPlayfieldKeys)ruleset.Playfield;
             Ruleset = ruleset;
             Info = info;
 
             // Initialize Sprite
-            TimingLineSprite = new Sprite()
-            {
-                Alignment = Alignment.TopLeft,
-                Width = playfield.Width,
-                Height = 2,
-                Parent = playfield.Stage.TimingLineContainer,
-                Image = UserInterface.BlankBox
-            };
+            Alignment = Alignment.TopLeft;
+            Width = playfield.Width;
+            Height = 2;
+            Parent = playfield.Stage.TimingLineContainer;
         }
 
         /// <summary>
@@ -70,18 +57,14 @@ namespace Quaver.Screens.Gameplay.Rulesets.Keys.TimingLines
         public void UpdateSpritePosition(long trackPosition)
         {
             // Calculate Sprite Position
-            var manager = (HitObjectManagerKeys)Ruleset.HitObjectManager;
+            var manager = (HitObjectManagerKeys) Ruleset.HitObjectManager;
+
             var speed = GameplayRulesetKeys.IsDownscroll ? -HitObjectManagerKeys.ScrollSpeed : HitObjectManagerKeys.ScrollSpeed;
             TrackOffset = Info.TrackOffset - trackPosition;
-            PositionY = (float)(manager.HitPositionOffset + (TrackOffset * speed)) + GlobalYOffset;
+            PositionY = manager.HitPositionOffset + TrackOffset * speed + GlobalYOffset;
 
             // Update Sprite Position
-            TimingLineSprite.Y = PositionY;
+            Y = PositionY;
         }
-
-        /// <summary>
-        ///     Destroy this object
-        /// </summary>
-        public void Destroy() => TimingLineSprite.Destroy();
     }
 }
