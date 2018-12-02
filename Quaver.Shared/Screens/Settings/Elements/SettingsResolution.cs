6b1dadec9f5a275b7735adcdf6dcfd1add3dfa11
Daniel Dimitrov <bgdppeu@gmail.com>
2018-12-02 16:38:03 +0200

    Support 21:9 resolution

--- Quaver.Shared/Screens/Settings/Elements/SettingsResolution.cs
+++ Quaver.Shared/Screens/Settings/Elements/SettingsResolution.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
@@ -35,6 +35,7 @@ namespace Quaver.Shared.Screens.Settings.Elements
                 new Point(1366, 768),
                 new Point(1600, 900),
                 new Point(1920, 1080),
+                new Point(2560, 1080),
                 new Point(2560, 1440)
             };
 
