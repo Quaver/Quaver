8481dba56decf674f1844655965916591c43cd41
staravia <denysenesouma@gmail.com>
2018-12-14 21:48:12 -0800

    Fix Playfield Initialization

--- Quaver.Shared/Screens/Gameplay/Rulesets/Keys/Playfield/GameplayPlayfieldKeys.cs
+++ Quaver.Shared/Screens/Gameplay/Rulesets/Keys/Playfield/GameplayPlayfieldKeys.cs
@@ -36,17 +36,17 @@ namespace Quaver.Shared.Screens.Gameplay.Rulesets.Keys.Playfield
         /// <summary>
         ///     The background of the playfield.
         /// </summary>
-        public Container BackgroundContainer { get; }
+        public Container BackgroundContainer { get; private set; }
 
         /// <summary>
         ///     The foreground of the playfield.
         /// </summary>
-        public Container ForegroundContainer { get; }
+        public Container ForegroundContainer { get; private set; }
 
         /// <summary>
         ///     The stage of the playfield.
         /// </summary>
-        public GameplayPlayfieldKeysStage Stage { get; }
+        public GameplayPlayfieldKeysStage Stage { get; private set; }
 
         /// <summary>
         ///     The X size of the playfield.
@@ -99,32 +99,16 @@ namespace Quaver.Shared.Screens.Gameplay.Rulesets.Keys.Playfield
             Ruleset = ruleset;
             Container = new Container();
             DetermineScrollDirections();
-
-            var skin = SkinManager.Skin.Keys[Screen.Map.Mode];
-            ReceptorPositionY = new float[ScrollDirections.Length];
-            ColumnLightingPositionY = new float[ScrollDirections.Length];
+            SetReceptorPositions();
             ApplyHitPositionsOffset();
+            CreateElementContainers();
+        }
 
-            for (var i = 0; i < ScrollDirections.Length; i++)
-            {
-                switch (ScrollDirections[i])
-                {
-                    case ScrollDirection.Down:
-                        ReceptorPositionY[i] = WindowManager.Height - (skin.ReceptorPosOffsetY + LaneSize * skin.NoteReceptorsUp[i].Height / skin.NoteReceptorsUp[i].Width);
-                        ColumnLightingPositionY[i] = ReceptorPositionY[i];
-                        break;
-                    case ScrollDirection.Up:
-                        // todo: reference current lane?
-                        var receptor = skin.NoteReceptorsUp[i];
-                        var hitObject = skin.NoteHitObjects[i][0];
-                        ReceptorPositionY[i] = skin.ReceptorPosOffsetY;
-                        ColumnLightingPositionY[i] = ReceptorPositionY[i] + skin.ColumnSize * (float)((double)receptor.Height / receptor.Width - (double)hitObject.Height / hitObject.Width);
-                        break;
-                    default:
-                        throw new Exception($"Scroll Direction in current lane index {i} does not exist.");
-                }
-            }
-
+        /// <summary>
+        ///     Create Foreground and Background Containers, as well as the Stage.
+        /// </summary>
+        private void CreateElementContainers()
+        {
             // Create background container
             BackgroundContainer = new Container
             {
@@ -142,57 +126,6 @@ namespace Quaver.Shared.Screens.Gameplay.Rulesets.Keys.Playfield
             };
 
             Stage = new GameplayPlayfieldKeysStage(Screen, this);
-        }
-
-        /// <summary>
-        ///     Set Hit Position Variables to every lane that each Hit Object will reference. 
-        /// </summary>
-        private void ApplyHitPositionsOffset()
-        {
-            var skin = SkinManager.Skin.Keys[Ruleset.Mode];
-            HitPositionOffsets = new float[ScrollDirections.Length];
-
-            for (var i = 0; i < ScrollDirections.Length; i++)
-            {
-                switch (ScrollDirections[i])
-                {
-                    case ScrollDirection.Down:
-                        HitPositionOffsets[i] = ReceptorPositionY[i] + skin.HitPosOffsetY;
-                        break;
-                    case ScrollDirection.Up:
-                        HitPositionOffsets[i] = ReceptorPositionY[i] - skin.HitPosOffsetY;
-                        break;
-                }
-            }
-        }
-
-        /// <inheritdoc />
-        /// <summary>
-        /// </summary>
-        /// <param name="gameTime"></param>
-        public void Update(GameTime gameTime)
-        {
-            Stage.Update(gameTime);
-            Container?.Update(gameTime);
-        }
-
-        /// <inheritdoc />
-        /// <summary>
-        /// </summary>
-        /// <param name="gameTime"></param>
-        public void Draw(GameTime gameTime) => Container.Draw(gameTime);
-
-        /// <inheritdoc />
-        /// <summary>
-        /// </summary>
-        public void Destroy() => Container?.Destroy();
-
-        /// <inheritdoc />
-        /// <summary>
-        /// </summary>
-        /// <param name="gameTime"></param>
-        public void HandleFailure(GameTime gameTime)
-        {
         }
 
         /// <summary>
@@ -276,6 +209,87 @@ namespace Quaver.Shared.Screens.Gameplay.Rulesets.Keys.Playfield
                 default:
                     throw new Exception("Map Mode does not exist.");
             }
+        }
+
+        /// <summary>
+        ///     Set Positions for Receptor and Hit Lighting
+        /// </summary>
+        private void SetReceptorPositions()
+        {
+            var skin = SkinManager.Skin.Keys[Screen.Map.Mode];
+            ReceptorPositionY = new float[ScrollDirections.Length];
+            ColumnLightingPositionY = new float[ScrollDirections.Length];
+
+            for (var i = 0; i < ScrollDirections.Length; i++)
+            {
+                switch (ScrollDirections[i])
+                {
+                    case ScrollDirection.Down:
+                        ReceptorPositionY[i] = WindowManager.Height - (skin.ReceptorPosOffsetY + LaneSize * skin.NoteReceptorsUp[i].Height / skin.NoteReceptorsUp[i].Width);
+                        ColumnLightingPositionY[i] = ReceptorPositionY[i];
+                        break;
+                    case ScrollDirection.Up:
+                        // todo: reference current lane?
+                        var receptor = skin.NoteReceptorsUp[i];
+                        var hitObject = skin.NoteHitObjects[i][0];
+                        ReceptorPositionY[i] = skin.ReceptorPosOffsetY;
+                        ColumnLightingPositionY[i] = ReceptorPositionY[i] + skin.ColumnSize * (float)((double)receptor.Height / receptor.Width - (double)hitObject.Height / hitObject.Width);
+                        break;
+                    default:
+                        throw new Exception($"Scroll Direction in current lane index {i} does not exist.");
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Set Hit Position Variables to every lane that each Hit Object will reference. 
+        /// </summary>
+        private void ApplyHitPositionsOffset()
+        {
+            var skin = SkinManager.Skin.Keys[Ruleset.Mode];
+            HitPositionOffsets = new float[ScrollDirections.Length];
+
+            for (var i = 0; i < ScrollDirections.Length; i++)
+            {
+                switch (ScrollDirections[i])
+                {
+                    case ScrollDirection.Down:
+                        HitPositionOffsets[i] = ReceptorPositionY[i] + skin.HitPosOffsetY;
+                        break;
+                    case ScrollDirection.Up:
+                        HitPositionOffsets[i] = ReceptorPositionY[i] - skin.HitPosOffsetY;
+                        break;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime)
+        {
+            Stage.Update(gameTime);
+            Container?.Update(gameTime);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Draw(GameTime gameTime) => Container.Draw(gameTime);
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public void Destroy() => Container?.Destroy();
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void HandleFailure(GameTime gameTime)
+        {
         }
     }
 }
