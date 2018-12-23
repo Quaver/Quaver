62cb747aa89cd6f078c33a028206fe74e0e6980a
Swan <me@swan.moe>
2018-12-23 12:09:39 -0500

    Fix null reference exception

--- Quaver.Shared/Screens/Gameplay/UI/SongInformation.cs
+++ Quaver.Shared/Screens/Gameplay/UI/SongInformation.cs
@@ -125,7 +125,7 @@ namespace Quaver.Shared.Screens.Gameplay.UI
             };
 
             // Get a formatted string of the activated mods.
-            var modsString = "Mods: " + (ModManager.CurrentModifiersList.Count > 0 ? $"{ModHelper.GetModsString(Screen.Ruleset.ScoreProcessor.Mods)}" : "None");
+            var modsString = "Mods: " + (ModManager.CurrentModifiersList.Count > 0 ? $"{ModHelper.GetModsString(ModManager.Mods)}" : "None");
             Mods = new SpriteText(Fonts.SourceSansProSemiBold, modsString, 13)
             {
                 Parent = this,
