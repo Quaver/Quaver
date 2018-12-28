f773c21981d3ac2a87a19a25ec5e29a9eea6a302
Adri- <adrien.boitelle@gmail.com>
2018-12-28 02:22:45 +0100

    add ColumnLightingY as a skin.ini option and a PlayField modifier

--- Quaver.Shared/Screens/Gameplay/Rulesets/Keys/Playfield/GameplayPlayfieldKeys.cs
+++ Quaver.Shared/Screens/Gameplay/Rulesets/Keys/Playfield/GameplayPlayfieldKeys.cs
@@ -87,14 +87,15 @@ namespace Quaver.Shared.Screens.Gameplay.Rulesets.Keys.Playfield
         {
             get
             {
-                if (GameplayRulesetKeys.IsDownscroll)
-                    return ReceptorPositionY;
-
                 var skin = SkinManager.Skin.Keys[Screen.Map.Mode];
+
+                if (GameplayRulesetKeys.IsDownscroll)
+                    return ReceptorPositionY + skin.ColumnLightingOffsetY;
+
 
                 var receptor = skin.NoteReceptorsUp[0];
                 var hitObject = skin.NoteHitObjects[0][0];
-                return ReceptorPositionY + skin.ColumnSize * (float)((double)receptor.Height / receptor.Width - (double)hitObject.Height / hitObject.Width);
+                return ReceptorPositionY - skin.ColumnLightingOffsetY + skin.ColumnSize * (float)((double)receptor.Height / receptor.Width - (double)hitObject.Height / hitObject.Width);
             }
         }
 
