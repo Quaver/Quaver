3f829f4f041f9b9c704be5865d357225a8b09fc7
Lleyton van Sambeeck <lleytonvs1@gmail.com>
2018-12-30 14:58:48 +0800

    Added Modifier

--- Quaver.Shared/Modifiers/ModManager.cs
+++ Quaver.Shared/Modifiers/ModManager.cs
@@ -93,6 +93,9 @@ namespace Quaver.Shared.Modifiers
                 case ModIdentifier.NoFail:
                     gameplayModifier = new ModNoFail();
                     break;
+                case ModIdentifier.Randomize:
+                    gameplayModifier = new ModRandomize();
+                    break;
                 default:
                     return;
             }
