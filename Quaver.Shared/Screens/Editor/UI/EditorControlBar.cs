6ed79090ef8c0f2f270a57fcd7b57f4ddd6adbb9
Swan <me@swan.moe>
2019-01-11 11:51:27 -0500

    Remove playback rate button

--- Quaver.Shared/Screens/Editor/UI/EditorControlBar.cs
+++ Quaver.Shared/Screens/Editor/UI/EditorControlBar.cs
@@ -19,10 +19,6 @@ namespace Quaver.Shared.Screens.Editor.UI
 
         /// <summary>
         /// </summary>
-        private EditorControlButton ButtonPlaybackRate { get; set; }
-
-        /// <summary>
-        /// </summary>
         private EditorControlButton ButtonBeatSnap { get; set; }
 
         /// <summary>
@@ -74,22 +70,12 @@ namespace Quaver.Shared.Screens.Editor.UI
 
             ButtonPlayTest.Clicked += (o, e) => NotificationManager.Show(NotificationLevel.Warning, "Play Testing is not implemented yet!");
 
-            ButtonPlaybackRate = new EditorControlButton(FontAwesome.Get(FontAwesomeIcon.fa_time), "Change Playback Rate", 60)
-            {
-                Parent = this,
-                Alignment = Alignment.BotCenter,
-                Size = new ScalableVector2(30, 30),
-                Y = ButtonPlayTest.Y - ButtonPlayTest.Height - 20
-            };
-
-            ButtonPlaybackRate.Clicked += (o, e) => NotificationManager.Show(NotificationLevel.Warning, "Not implemented yet");
-
             ButtonBeatSnap = new EditorControlButton(FontAwesome.Get(FontAwesomeIcon.fa_align_justify), "Change Beat Snap", 60)
             {
                 Parent = this,
                 Alignment = Alignment.BotCenter,
                 Size = new ScalableVector2(30, 30),
-                Y = ButtonPlaybackRate.Y - ButtonPlaybackRate.Height - 20
+                Y = ButtonPlayTest.Y - ButtonPlayTest.Height - 20
             };
 
             ButtonBeatSnap.Clicked += (o, e) => NotificationManager.Show(NotificationLevel.Warning, "Not implemented yet");
