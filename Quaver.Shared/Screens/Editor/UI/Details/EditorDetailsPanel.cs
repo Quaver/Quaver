db177e366e048b5079dec4b2c26d0ee7054b495d
Swan <me@swan.moe>
2019-02-13 09:42:37 -0500

    Add license headers

--- Quaver.Shared/Screens/Editor/UI/Details/EditorDetailsPanel.cs
+++ Quaver.Shared/Screens/Editor/UI/Details/EditorDetailsPanel.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Copyright (c) Swan & The Quaver Team <support@quavergame.com>.
+*/
+
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
