Quaver/Quaver

commit 920a4cda65b0bbce9a0bd8af6c564de70d885f39

Use ModHelper.GetRateFromMods rather than AudioTrack.Rate

Quaver.Shared/Screens/Gameplay/GameplayAudioTiming.cs
@@ -49,7 +49,6 @@ namespace Quaver.Shared.Screens.Gameplay
                 else
                 {
                     AudioEngine.LoadCurrentTrack();
-                    AudioEngine.Track.Rate = ModHelper.GetRateFromMods(ModManager.Mods);
                 }
 
                 if (Screen.IsPlayTesting)

Quaver.Shared/Screens/Select/SelectScreen.cs
@@ -11,6 +11,7 @@ using System.IO;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using Quaver.API.Helpers;
 using Quaver.Server.Common.Enums;
 using Quaver.Server.Common.Objects;
 using Quaver.Shared.Audio;
@@ -304,7 +305,7 @@ namespace Quaver.Shared.Screens.Select
         /// <returns></returns>
         private static float GetNextRate(bool faster)
         {
-            var current = AudioEngine.Track.Rate;
+            var current = ModHelper.GetRateFromMods(ModManager.Mods);
             var adjustment = 0.1f;
 
             // ReSharper disable once CompareOfFloatsByEqualityOperator

commit 82315c9f584862801d4e409ac6c5e3c6ac683a1f

GameplayAudioTiming: set audio rate
Fixes a regression introduced in 920a4cda65b0bbce9a0bd8af6c564de70d885f39.

Quaver.Shared/Screens/Gameplay/GameplayAudioTiming.cs
@@ -49,6 +49,7 @@ namespace Quaver.Shared.Screens.Gameplay
                 else
                 {
                     AudioEngine.LoadCurrentTrack();
+                    AudioEngine.Track.Rate = ModHelper.GetRateFromMods(ModManager.Mods);
                 }
 
                 if (Screen.IsPlayTesting)