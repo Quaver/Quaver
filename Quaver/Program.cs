76ae3945375c074244b8bd42bd01b1d3a00f330c
Swan <me@swan.moe>
2019-07-24 12:10:24 -0400

    Lock Quaver to one instance + handle IPC communication

--- Quaver/Program.cs
+++ Quaver/Program.cs
@@ -10,13 +10,19 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.IO.Pipes;
+using System.Net.Mime;
 using System.Threading;
 using Quaver.Shared;
 using Quaver.Shared.Config;
+using Quaver.Shared.Helpers;
+using Quaver.Shared.IPC;
 using Quaver.Shared.Online;
 using Wobble;
 using Wobble.Logging;
 using Wobble.Platform;
+using ZetaIpc.Runtime.Client;
+using ZetaIpc.Runtime.Server;
 
 namespace Quaver
 {
@@ -27,8 +33,44 @@ namespace Quaver
         /// </summary>
         public static string WorkingDirectory => WobbleGame.WorkingDirectory;
 
+        /// <summary>
+        /// </summary>
+        private static string Guid = "9151537b-304c-4619-bf54-d367ba7d87ac";
+
+        /// <summary>
+        ///     The name of the pipe used for IPC
+        /// </summary>
+        private static int IpcPort { get; } = 43596;
+
         [STAThread]
-        public static void Main()
+        public static void Main(string[] args)
+        {
+            // Prevents more than one instance of Quaver to run at a time
+            using(var mutex = new Mutex(false, "Global\\" + Guid))
+            {
+                if(!mutex.WaitOne(0, false))
+                {
+                    Console.WriteLine("Quaver is already running");
+
+                    // Send to running instance only if we have actual data to send
+                    if (args.Length > 0)
+                        SendToRunningInstanceIpc(args[0]);
+
+                    return;
+                }
+
+                Run();
+            }
+
+            // Uncomment this and comment the above mutex to allow multiple instances of Quaver
+            // to be run
+            // Run();
+        }
+
+        /// <summary>
+        ///     Starts the game
+        /// </summary>
+        private static void Run()
         {
             Logger.Initialize();
 
@@ -38,6 +80,8 @@ namespace Quaver
                 var exception = args.ExceptionObject as Exception;
                 Logger.Error(exception, LogType.Runtime);
             };
+
+            StartIpcServer();
 
             // Change the working directory to where the executable is.
             Directory.SetCurrentDirectory(WorkingDirectory);
@@ -57,9 +101,39 @@ namespace Quaver
             NativeAssemblies.Copy();
             ConfigManager.Initialize();
             SteamManager.Initialize();
+            RegistryHelper.RegisterUriScheme();
 
             using (var game = new QuaverGame())
                 game.Run();
+        }
+
+        /// <summary>
+        ///     Starts an IPC server to receive messages from new instances of Quaver
+        ///     that want to communicate with the main process
+        /// </summary>
+        private static void StartIpcServer()
+        {
+            var s = new IpcServer();
+            s.Start(IpcPort);
+
+            Logger.Important($"Started IPC server on port: {IpcPort}", LogType.Runtime);
+
+            s.ReceivedRequest += (o, e) =>
+            {
+                QuaverIpcHandler.HandleMessage(e.Request);
+                e.Handled = true;
+            };
+        }
+
+        /// <summary>
+        ///     Creates an IPC client and sends a message to the already
+        ///     running instance of Quaver
+        /// </summary>
+        private static void SendToRunningInstanceIpc(string message)
+        {
+            var c = new IpcClient();
+            c.Initialize(IpcPort);
+            c.Send(message);
         }
     }
 }
