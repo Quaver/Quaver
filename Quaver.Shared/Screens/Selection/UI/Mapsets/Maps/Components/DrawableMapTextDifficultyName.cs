807aaee0d09cf0f221e78f0fdfcfbefc29ba4c05
Swan <me@swan.moe>
2019-08-17 12:16:25 -0400

    Make DrawableMapTextDifficultyName implement IDrawableMapMetadata

--- Quaver.Shared/Screens/Selection/UI/Mapsets/Maps/Components/DrawableMapTextDifficultyName.cs
+++ Quaver.Shared/Screens/Selection/UI/Mapsets/Maps/Components/DrawableMapTextDifficultyName.cs
@@ -1,12 +1,14 @@
 using Quaver.Shared.Assets;
 using Quaver.Shared.Helpers;
 using Quaver.Shared.Modifiers;
+using Quaver.Shared.Screens.Selection.UI.Mapsets.Maps.Metadata;
+using Wobble.Graphics.Animations;
 using Wobble.Graphics.Sprites.Text;
 using Wobble.Managers;
 
 namespace Quaver.Shared.Screens.Selection.UI.Mapsets.Maps.Components
 {
-    public class DrawableMapTextDifficultyName : SpriteTextPlus
+    public class DrawableMapTextDifficultyName : SpriteTextPlus, IDrawableMapMetadata
     {
         /// <summary>
         /// </summary>
@@ -36,5 +38,25 @@ namespace Quaver.Shared.Screens.Selection.UI.Mapsets.Maps.Components
         /// <param name="e"></param>
         private void OnModsChanged(object sender, ModsChangedEventArgs e)
             => Tint = ColorHelper.DifficultyToColor((float) Map.Map.DifficultyFromMods(ModManager.Mods));
+
+        /// <inheritdoc />
+        ///  <summary>
+        ///  </summary>
+        public void Open()
+        {
+            ClearAnimations();
+            Wait(200);
+            FadeTo(1, Easing.Linear, 250);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public void Close()
+        {
+            ClearAnimations();
+            Alpha = 0;
+            Visible = false;
+        }
     }
 }
