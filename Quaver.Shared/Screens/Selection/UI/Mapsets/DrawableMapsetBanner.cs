ed7a44b503be04ca98ece0eb3c7910150c925b3c
Swan <me@swan.moe>
2019-08-17 10:43:34 -0400

    Load & display banners for mapsets

---
+++ Quaver.Shared/Screens/Selection/UI/Mapsets/DrawableMapsetBanner.cs
@@ -0,0 +1,132 @@
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.Shared.Assets;
+using Quaver.Shared.Database.Maps;
+using Quaver.Shared.Graphics.Backgrounds;
+using Quaver.Shared.Scheduling;
+using Wobble;
+using Wobble.Assets;
+using Wobble.Graphics;
+using Wobble.Graphics.Animations;
+using Wobble.Graphics.Sprites;
+using Wobble.Graphics.UI;
+using Wobble.Window;
+
+namespace Quaver.Shared.Screens.Selection.UI.Mapsets
+{
+    public class DrawableMapsetBanner : Sprite
+    {
+        /// <summary>
+        /// </summary>
+        private DrawableMapset Mapset { get; }
+
+        /// <summary>
+        /// </summary>
+        private RenderTarget2D RenderTarget { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private Texture2D MapTexture { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private Texture2D DefaultBanner => UserInterface.DefaultBanner;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="mapset"></param>
+        public DrawableMapsetBanner(DrawableMapset mapset)
+        {
+            Mapset = mapset;
+            Alpha = 0;
+            LoadBackground();
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public override void Destroy()
+        {
+            DisposeTextures();
+            base.Destroy();
+        }
+
+        /// <summary>
+        ///     Loads the map's background under a separate thread
+        /// </summary>
+        public void LoadBackground() => ThreadScheduler.Run(() =>
+        {
+            DisposeTextures();
+
+            var path = MapManager.GetBackgroundPath(Mapset.Item.Maps.First());
+            MapTexture = File.Exists(path) ? AssetLoader.LoadTexture2DFromFile(path) : DefaultBanner;
+
+            if (MapTexture == DefaultBanner)
+            {
+                Image = MapTexture;
+                FadeIn();
+                return;
+            }
+
+            GameBase.Game.ScheduledRenderTargetDraws.Add(() =>
+            {
+                var scrollContainer = new ScrollContainer(new ScalableVector2(Width, Height),
+                    new ScalableVector2(Width, Height));
+
+                var maskedSprite = new Sprite()
+                {
+                    Alignment = Alignment.MidCenter,
+                    Size = new ScalableVector2(1024, 576), // Small 16:9 resolution to make backgrounds look a bit better
+                    Y = 100, // Small y offset. Usually captures the best part of the image (such as faces or text)
+                    Image = MapTexture
+                };
+
+                scrollContainer.AddContainedDrawable(maskedSprite);
+
+                var (pixelWidth, pixelHeight) = scrollContainer.AbsoluteSize * WindowManager.ScreenScale;
+
+                RenderTarget = new RenderTarget2D(GameBase.Game.GraphicsDevice, (int) pixelWidth, (int) pixelHeight, false,
+                    GameBase.Game.GraphicsDevice.PresentationParameters.BackBufferFormat, DepthFormat.None);
+
+                GameBase.Game.GraphicsDevice.SetRenderTarget(RenderTarget);
+                GameBase.Game.GraphicsDevice.Clear(Color.Transparent);
+
+                scrollContainer.Draw(new GameTime());
+                GameBase.Game.SpriteBatch.End();
+
+                GameBase.Game.GraphicsDevice.SetRenderTarget(null);
+                scrollContainer?.Destroy();
+                maskedSprite?.Destroy();
+
+                Image = RenderTarget;
+
+                FadeIn();
+            });
+        });
+
+        /// <summary>
+        ///     Disposes of the map textures
+        /// </summary>
+        public void DisposeTextures()
+        {
+            if (MapTexture != null && MapTexture != DefaultBanner)
+                MapTexture?.Dispose();
+
+
+            RenderTarget?.Dispose();
+        }
+
+        /// <summary>
+        ///     Fades in the image from 0 alpha
+        /// </summary>
+        private void FadeIn()
+        {
+            Alpha = 0;
+            ClearAnimations();
+            FadeTo(1, Easing.OutQuint, 1000);
+        }
+    }
+}
