0688a7831b223df94cf57b97f94f5d05a4ac4cbc
Swan <me@swan.moe>
2019-08-22 12:14:28 -0400

    Add animation to MapsetScrollContainer when new available maps are given

--- Quaver.Shared/Screens/Tests/MapsetScrollContainers/TestScreenMapsetScrollContainerView.cs
+++ Quaver.Shared/Screens/Tests/MapsetScrollContainers/TestScreenMapsetScrollContainerView.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
 using FontStashSharp;
 using Microsoft.Xna.Framework;
+using Quaver.Shared.Database.Maps;
 using Quaver.Shared.Helpers;
 using Quaver.Shared.Screens.Selection.Components;
 using Quaver.Shared.Screens.Selection.UI.FilterPanel;
 using Quaver.Shared.Screens.Selection.UI.Mapsets;
 using Quaver.Shared.Screens.Tests.FilterPanel;
+using Wobble.Bindables;
+using Wobble.Graphics.Animations;
+using Wobble.Graphics.Sprites;
 using Wobble.Graphics.UI.Buttons;
 using Alignment = Wobble.Graphics.Alignment;
 
@@ -14,10 +19,13 @@ namespace Quaver.Shared.Screens.Tests.MapsetScrollContainers
     {
         private bool Reordered { get; set; }
 
+        private MapsetScrollContainer ScrollContainer { get; }
+
         public TestScreenMapsetScrollContainerView(TestScreenMapsetScrollContainer screen) : base(screen)
         {
             new SelectJukebox() {Parent = Container};
-            var container = new MapsetScrollContainer(screen.AvailableMapsets)
+
+            ScrollContainer  = new MapsetScrollContainer(screen.AvailableMapsets)
             {
                 Parent = Container,
                 Alignment = Alignment.TopRight,
@@ -25,12 +33,30 @@ namespace Quaver.Shared.Screens.Tests.MapsetScrollContainers
                 X = -50
             };
 
-            var containerIndex = Container.Children.IndexOf(container);
+            var containerIndex = Container.Children.IndexOf(ScrollContainer);
 
             ListHelper.Swap(Container.Children, containerIndex, Container.Children.IndexOf(FilterPanel));
 
             Header.Parent = Container;
             Footer.Parent = Container;
+
+            screen.AvailableMapsets.ValueChanged += OnAvailableMapsetsChanged;
+        }
+
+        public override void Destroy()
+        {
+            var screen = (TestScreenMapsetScrollContainer) Screen;
+            // ReSharper disable once DelegateSubtraction
+            screen.AvailableMapsets.ValueChanged -= OnAvailableMapsetsChanged;
+            base.Destroy();
+        }
+
+        private void OnAvailableMapsetsChanged(object sender, BindableValueChangedEventArgs<List<Mapset>> e)
+        {
+            ScrollContainer.ClearAnimations();
+            ScrollContainer.MoveToX(ScrollContainer.Width + 50, Easing.OutQuint, 450);
+            ScrollContainer.Wait(50);
+            ScrollContainer.MoveToX(-50, Easing.OutQuint, 450);
         }
 
         public override void Draw(GameTime gameTime)
