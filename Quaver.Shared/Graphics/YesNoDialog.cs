71d08acc0212b1c58eb7e0cdafe5e1e301e288b9
Swan <me@swan.moe>
2019-09-13 13:23:22 -0400

    Add YesNoDialog test

---
+++ Quaver.Shared/Graphics/YesNoDialog.cs
@@ -0,0 +1,233 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Quaver.Shared.Assets;
+using Quaver.Shared.Scheduling;
+using Quaver.Shared.Screens.Menu.UI.Jukebox;
+using Wobble.Assets;
+using Wobble.Graphics;
+using Wobble.Graphics.Animations;
+using Wobble.Graphics.Sprites;
+using Wobble.Graphics.Sprites.Text;
+using Wobble.Graphics.UI.Dialogs;
+using Wobble.Input;
+using Wobble.Managers;
+
+namespace Quaver.Shared.Graphics
+{
+    public class YesNoDialog : DialogScreen
+    {
+        /// <summary>
+        ///     The time it takes to fade in/out the dialog
+        /// </summary>
+        private const int FadeTime = 150;
+
+        /// <summary>
+        /// </summary>
+        private string HeaderText { get; }
+
+        /// <summary>
+        /// </summary>
+        private string ConfirmationText { get; }
+
+        /// <summary>
+        /// </summary>
+        private Sprite Panel { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private Sprite Banner { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private Sprite Blackness { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private SpriteTextPlus Header { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private SpriteTextPlus Confirmation { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private IconButton YesButton { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private IconButton NoButton { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private Action YesAction { get; }
+
+        /// <summary>
+        /// </summary>
+        private Action NoAction { get; }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public YesNoDialog(string header, string confirmationText, Action yesAction = null, Action noAction = null) : base(0)
+        {
+            HeaderText = header;
+            ConfirmationText = confirmationText;
+            YesAction = yesAction;
+            NoAction = noAction;
+
+            FadeTo(0.75f, Easing.Linear, FadeTime);
+
+            // ReSharper disable once VirtualMemberCallInConstructor
+            CreateContent();
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public override void CreateContent()
+        {
+            CreatePanel();
+            CreateBanner();
+            CreateHeader();
+            CreateConfirmation();
+            CreateButtons();
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void HandleInput(GameTime gameTime)
+        {
+            if (KeyboardManager.IsUniqueKeyPress(Keys.Escape))
+            {
+                NoAction?.Invoke();
+                Close();
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        public void Close()
+        {
+            FadeTo(0, Easing.Linear, FadeTime);
+
+            const int fadeTime = FadeTime - 50;
+
+            Panel.ClearAnimations();
+            Panel.FadeTo(0, Easing.Linear, fadeTime);
+
+            Blackness.ClearAnimations();
+            Blackness.FadeTo(0, Easing.Linear, fadeTime);
+
+            Confirmation.ClearAnimations();
+            Confirmation.FadeTo(0, Easing.Linear, fadeTime);
+
+            NoButton.IsPerformingFadeAnimations = false;
+            NoButton.IsClickable = false;
+
+            YesButton.IsPerformingFadeAnimations = false;
+            YesButton.IsClickable = false;
+
+            ThreadScheduler.RunAfter(() => DialogManager.Dismiss(this), FadeTime);
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreatePanel()
+        {
+            Panel = new Sprite
+            {
+                Parent = this,
+                Alignment = Alignment.MidCenter,
+                Size = new ScalableVector2(770, 286),
+                Image = UserInterface.YesNoPanel,
+                Alpha = 0,
+                SetChildrenAlpha = true
+            };
+
+            Panel.FadeTo(1, Easing.Linear, FadeTime + 50);
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateBanner()
+        {
+            Banner = new Sprite
+            {
+                Parent = Panel,
+                Alignment = Alignment.TopCenter,
+                Size = new ScalableVector2(766, 151),
+                Image = UserInterface.DefaultBanner,
+                Alpha = 0,
+                Y = 2,
+            };
+
+            Blackness = new Sprite()
+            {
+                Parent = Banner,
+                Size = Banner.Size,
+                Tint = Color.Black,
+                Alpha = 0
+            };
+
+            Blackness.FadeTo(0.6f, Easing.Linear, FadeTime + 50);
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateHeader()
+        {
+            Header = new SpriteTextPlus(FontManager.GetWobbleFont(Fonts.LatoBlack), HeaderText.ToUpper(), 26)
+            {
+                Parent = Panel,
+                Alignment = Alignment.TopLeft,
+            };
+
+            Header.Y = -Header.Height - 6;
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateConfirmation()
+        {
+            Confirmation = new SpriteTextPlus(FontManager.GetWobbleFont(Fonts.LatoBlack), ConfirmationText, 24)
+            {
+                Parent = Banner,
+                Alignment = Alignment.MidCenter
+            };
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateButtons()
+        {
+            YesButton = new IconButton(UserInterface.CreateButton, (o, e) =>
+            {
+                YesAction?.Invoke();
+                Close();
+            })
+            {
+                Parent = Panel,
+                Alignment = Alignment.BotLeft,
+                Size = new ScalableVector2(221, 40),
+                Y = -50,
+                X = 140
+            };
+
+            NoButton = new IconButton(UserInterface.CancelButton, (o, e) =>
+            {
+                NoAction?.Invoke();
+                Close();
+            })
+            {
+                Parent = Panel,
+                Alignment = Alignment.BotRight,
+                Size = YesButton.Size,
+                Y = YesButton.Y,
+                X = -YesButton.X
+            };
+        }
+    }
+}
