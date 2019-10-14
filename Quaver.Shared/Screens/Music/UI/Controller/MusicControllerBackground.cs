b0aa220a4684e0ace4b81469e8b5ecd7c4f7f7d1
Swan <me@swan.moe>
2019-10-14 11:54:37 -0400

    Add background functionality

--- Quaver.Shared/Screens/Music/UI/Controller/MusicControllerBackground.cs
+++ Quaver.Shared/Screens/Music/UI/Controller/MusicControllerBackground.cs
@@ -1,6 +1,10 @@
 using Microsoft.Xna.Framework;
 using Quaver.Shared.Assets;
+using Quaver.Shared.Database.Maps;
+using Quaver.Shared.Graphics.Backgrounds;
+using Wobble.Bindables;
 using Wobble.Graphics;
+using Wobble.Graphics.Animations;
 using Wobble.Graphics.Sprites;
 using Wobble.Window;
 
@@ -10,11 +14,15 @@ namespace Quaver.Shared.Screens.Music.UI.Controller
     {
         /// <summary>
         /// </summary>
-        private Sprite Background { get; }
+        private Sprite Background { get; set; }
 
         /// <summary>
         /// </summary>
-        private Sprite Darkness { get; }
+        private Sprite Darkness { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private float DarknessVisibleAlpha { get; } = 0.7f;
 
         /// <inheritdoc />
         /// <summary>
@@ -25,23 +33,88 @@ namespace Quaver.Shared.Screens.Music.UI.Controller
             Alpha = 0;
             InputEnabled = false;
 
+            CreateBackground();
+            CreateDarkness();
+            InitializeBackgroundAndDarkness();
+
+            MapManager.Selected.ValueChanged += OnMapChanged;
+            BackgroundHelper.Loaded += OnBackgroundLoaded;
+
+            BackgroundHelper.Load(MapManager.Selected.Value);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        public override void Destroy()
+        {
+            // ReSharper disable once DelegateSubtraction
+            MapManager.Selected.ValueChanged -= OnMapChanged;
+            BackgroundHelper.Loaded -= OnBackgroundLoaded;
+
+            base.Destroy();
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateBackground()
+        {
             Background = new Sprite
             {
                 Alignment = Alignment.MidCenter,
-                Y = -200,
+                Y = -100,
                 Size = new ScalableVector2(1920, 1080),
                 Image = UserInterface.MenuBackgroundClear
             };
 
             AddContainedDrawable(Background);
+        }
 
+        /// <summary>
+        /// </summary>
+        private void CreateDarkness()
+        {
             Darkness = new Sprite
             {
                 Parent = this,
                 Size = Size,
                 Tint = Color.Black,
-                Alpha = 0.70f
             };
+        }
+
+        /// <summary>
+        /// </summary>
+        private void InitializeBackgroundAndDarkness()
+        {
+            if (BackgroundHelper.RawTexture == null || BackgroundHelper.Map != MapManager.Selected.Value)
+                return;
+
+            Background.Image = BackgroundHelper.RawTexture;
+            Darkness.Alpha = DarknessVisibleAlpha;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnMapChanged(object sender, BindableValueChangedEventArgs<Map> e)
+        {
+            Darkness.ClearAnimations();
+            Darkness.FadeTo(1, Easing.Linear, 100);
+
+            BackgroundHelper.Load(e.Value);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnBackgroundLoaded(object sender, BackgroundLoadedEventArgs e)
+        {
+            Background.Image = e.Texture;
+
+            Darkness.ClearAnimations();
+            Darkness.FadeTo(DarknessVisibleAlpha, Easing.Linear, 100);
         }
     }
 }
