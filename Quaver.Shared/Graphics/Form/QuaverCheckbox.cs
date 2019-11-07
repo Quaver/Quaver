54f8c8ba1c7294f942a53730541428d4f6aaede9
Swan <me@swan.moe>
2019-11-07 12:29:46 -0500

    Add QuaverCheckbox & LabelledCheckbox components

---
+++ Quaver.Shared/Graphics/Form/QuaverCheckbox.cs
@@ -0,0 +1,28 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.Shared.Assets;
+using Wobble.Assets;
+using Wobble.Bindables;
+using Wobble.Graphics.UI.Form;
+
+namespace Quaver.Shared.Graphics.Form
+{
+    public class QuaverCheckbox : Checkbox
+    {
+        /// <summary>
+        /// </summary>
+        private static Texture2D ActiveTexture => UserInterface.On;
+
+        /// <summary>
+        /// </summary>
+        private static Texture2D InactiveTexutre => UserInterface.Off;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="bindedValue"></param>
+        public QuaverCheckbox(Bindable<bool> bindedValue) : base(bindedValue, new Vector2(78, 23), ActiveTexture,
+            InactiveTexutre, false)
+        {
+        }
+    }
+}
