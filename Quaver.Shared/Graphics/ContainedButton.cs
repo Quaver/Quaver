2444f5a0d4ec0f59c664b86573370c575e77c54a
Swan <me@swan.moe>
2019-12-12 11:47:53 -0500

    Add ContainedButton component

---
+++ Quaver.Shared/Graphics/ContainedButton.cs
@@ -0,0 +1,34 @@
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using Wobble.Graphics;
+using Wobble.Graphics.UI.Buttons;
+using Wobble.Input;
+
+namespace Quaver.Shared.Graphics
+{
+    public class ContainedButton : ImageButton
+    {
+        /// <summary>
+        /// </summary>
+        private Drawable Container { get; }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="image"></param>
+        public ContainedButton(Drawable container, Texture2D image) : base(image) => Container = container;
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        protected override bool IsMouseInClickArea()
+        {
+            if (Container == null)
+                return base.IsMouseInClickArea();
+
+            var newRect = RectangleF.Intersect(ScreenRectangle, Container.ScreenRectangle);
+            return GraphicsHelper.RectangleContains(newRect, MouseManager.CurrentState.Position);
+        }
+    }
+}
