7da6771ae814a3745ed6de59396b0dadfd07401c
Swan <me@swan.moe>
2019-12-12 12:16:19 -0500

    Change color of empty multiplayer player slot

--- Quaver.Shared/Screens/Multi/UI/Players/EmptyMultiplayerSlot.cs
+++ Quaver.Shared/Screens/Multi/UI/Players/EmptyMultiplayerSlot.cs
@@ -7,8 +7,8 @@ namespace Quaver.Shared.Screens.Multi.UI.Players
     {
         public EmptyMultiplayerSlot()
         {
-            Tint = ColorHelper.HexToColor("#242424");
-            Alpha = 0.25f;
+            Tint = ColorHelper.HexToColor("#2A2A2A");
+            // Alpha = 0.25f;
         }
     }
 }
