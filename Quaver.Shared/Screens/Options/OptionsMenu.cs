097f9d63e680488783f7c16624a71d6973ebdf61
Swan <me@swan.moe>
2020-01-13 08:49:06 -0500

    Add more options

--- Quaver.Shared/Screens/Options/OptionsMenu.cs
+++ Quaver.Shared/Screens/Options/OptionsMenu.cs
@@ -84,8 +84,8 @@ namespace Quaver.Shared.Screens.Options
                     new OptionsSubcategory("Window", new List<OptionsItem>()
                     {
                         new OptionsItemScreenResolution(contentWidth, "Screen Resolution"),
-                        new OptionsItemCheckbox(contentWidth, "Fullscreen", ConfigManager.WindowFullScreen),
-                        new OptionsItemCheckbox(contentWidth, "Borderless", ConfigManager.WindowBorderless)
+                        new OptionsItemCheckbox(contentWidth, "Enable Fullscreen", ConfigManager.WindowFullScreen),
+                        new OptionsItemCheckbox(contentWidth, "Enable Borderless Window", ConfigManager.WindowBorderless)
                     }),
                     new OptionsSubcategory("Frame Time", new List<OptionsItem>()
                     {
@@ -106,6 +106,10 @@ namespace Quaver.Shared.Screens.Options
                         new OptionsItemSliderGlobalOffset(contentWidth, "Global Offset", ConfigManager.GlobalAudioOffset),
                         new OptionsItem(contentWidth, "Calibrate Offset")
                     }),
+                    new OptionsSubcategory("Effects", new List<OptionsItem>()
+                    {
+                       new OptionsItemCheckbox(contentWidth, "Pitch Audio With Playback Rate", ConfigManager.Pitched)
+                    }),
                     new OptionsSubcategory("Linux", new List<OptionsItem>()
                     {
                         new OptionsSlider(contentWidth, "Audio Device Period", ConfigManager.DevicePeriod),
@@ -114,23 +118,56 @@ namespace Quaver.Shared.Screens.Options
                 }),
                 new OptionsSection("Gameplay", FontAwesome.Get(FontAwesomeIcon.fa_gamepad_console), new List<OptionsSubcategory>
                 {
+                    new OptionsSubcategory("Scrolling", new List<OptionsItem>()
+                    {
+                       new OptionsSlider(contentWidth, "Scroll Speed (4 Keys)", ConfigManager.ScrollSpeed4K),
+                       new OptionsSlider(contentWidth, "Scroll Speed (7 Keys)", ConfigManager.ScrollSpeed7K)
+                    }),
+                    new OptionsSubcategory("Background", new List<OptionsItem>()
+                    {
+                       new OptionsSlider(contentWidth, "Background Brightness", ConfigManager.BackgroundBrightness),
+                       new OptionsItemCheckbox(contentWidth, "Enable Background Blur", ConfigManager.BlurBackgroundInGameplay)
+                    }),
                     new OptionsSubcategory("Sound", new List<OptionsItem>()
                     {
                         new OptionsItemCheckbox(contentWidth, "Enable Hitsounds", ConfigManager.EnableHitsounds),
                         new OptionsItemCheckbox(contentWidth, "Enable Keysounds", ConfigManager.EnableKeysounds)
                     }),
-                }),
-                new OptionsSection("Editor", FontAwesome.Get(FontAwesomeIcon.fa_pencil), new List<OptionsSubcategory>
-                {
-                    new OptionsSubcategory("Sound", new List<OptionsItem>()
+                    new OptionsSubcategory("Input", new List<OptionsItem>()
                     {
-                        new OptionsItemCheckbox(contentWidth, "Enable Hitsounds", ConfigManager.EditorEnableHitsounds),
-                        new OptionsItemCheckbox(contentWidth, "Enable Keysounds", ConfigManager.EditorEnableKeysounds),
+                        new OptionsItemCheckbox(contentWidth, "Enable Tap To Pause", ConfigManager.TapToPause),
+                        new OptionsItemCheckbox(contentWidth, "Skip Results Screen After Quitting", ConfigManager.SkipResultsScreenAfterQuit)
                     }),
-                    new OptionsSubcategory("Metronome", new List<OptionsItem>()
+                    new OptionsSubcategory("User Interface", new List<OptionsItem>()
                     {
-                        new OptionsItemCheckbox(contentWidth, "Enable Metronome", ConfigManager.EditorPlayMetronome),
-                        new OptionsItemCheckbox(contentWidth, "Play Metronome Half Beats", ConfigManager.EditorMetronomePlayHalfBeats)
+                        new OptionsItemCheckbox(contentWidth, "Show Spectators", ConfigManager.ShowSpectators),
+                        new OptionsItemCheckbox(contentWidth, "Display Timing Lines", ConfigManager.DisplayTimingLines),
+                        new OptionsItemCheckbox(contentWidth, "Display Judgement Counter", ConfigManager.DisplayJudgementCounter),
+                        new OptionsItemCheckbox(contentWidth, "Enable Combo Alerts", ConfigManager.DisplayComboAlerts),
+                        new OptionsItemCheckbox(contentWidth, "Enable Accuracy Display Animations", ConfigManager.SmoothAccuracyChanges),
+                    }),
+                    new OptionsSubcategory("Scoreboard", new List<OptionsItem>()
+                    {
+                        new OptionsItemCheckbox(contentWidth, "Display Scoreboard", ConfigManager.ScoreboardVisible),
+                        new OptionsItemCheckbox(contentWidth, "Display Unbeatable Scores", ConfigManager.DisplayUnbeatableScoresDuringGameplay)
+                    }),
+                    new OptionsSubcategory("Progress Bar", new List<OptionsItem>()
+                    {
+                        new OptionsItemCheckbox(contentWidth, "Display Song Time Progress Bar", ConfigManager.DisplaySongTimeProgress),
+                        new OptionsItemCheckbox(contentWidth, "Display Song Time Progress Bar Time Numbers", ConfigManager.DisplaySongTimeProgressNumbers)
+                    }),
+                    new OptionsSubcategory("Lane Cover", new List<OptionsItem>()
+                    {
+                       new OptionsItemCheckbox(contentWidth, "Enable Top Lane Cover", ConfigManager.LaneCoverTop),
+                       new OptionsSlider(contentWidth, "Top Lane Cover Height", ConfigManager.LaneCoverTopHeight),
+                       new OptionsItemCheckbox(contentWidth, "Enable Bottom Lane Cover", ConfigManager.LaneCoverBottom),
+                       new OptionsSlider(contentWidth, "Bottom Lane Cover Height", ConfigManager.LaneCoverBottomHeight),
+                       new OptionsItemCheckbox(contentWidth, "Display UI Elements Over Lane Covers", ConfigManager.UIElementsOverLaneCover)
+                    }),
+                    new OptionsSubcategory("Multiplayer", new List<OptionsItem>()
+                    {
+                        new OptionsItemCheckbox(contentWidth, "Enable Battle Royale Alerts", ConfigManager.EnableBattleRoyaleAlerts),
+                        new OptionsItemCheckbox(contentWidth, "Enable Battle Royale Background Flashing", ConfigManager.EnableBattleRoyaleBackgroundFlashing)
                     })
                 }),
                 new OptionsSection("Skin", FontAwesome.Get(FontAwesomeIcon.fa_check), new List<OptionsSubcategory>
@@ -141,12 +178,33 @@ namespace Quaver.Shared.Screens.Options
                 }),
                 new OptionsSection("Network", FontAwesome.Get(FontAwesomeIcon.fa_earth_globe), new List<OptionsSubcategory>
                 {
+                    new OptionsSubcategory("Login", new List<OptionsItem>()
+                    {
+                        new OptionsItemCheckbox(contentWidth, "Automatically Log Into The Server", ConfigManager.AutoLoginToServer),
+                    }),
+                    new OptionsSubcategory("Notifications", new List<OptionsItem>()
+                    {
+                        new OptionsItemCheckbox(contentWidth, "Display Online Friend Notifications", ConfigManager.DisplayFriendOnlineNotifications),
+                        new OptionsItemCheckbox(contentWidth, "Display Song Request Notifications", ConfigManager.DisplaySongRequestNotifications)
+                    })
                 }),
                 new OptionsSection("Integration", FontAwesome.Get(FontAwesomeIcon.fa_plus_black_symbol), new List<OptionsSubcategory>
                 {
+                    new OptionsSubcategory("Games", new List<OptionsItem>()
+                    {
+                        new OptionsItemCheckbox(contentWidth, "Load Songs From Other Installed Games", ConfigManager.AutoLoadOsuBeatmaps)
+                    }),
                 }),
                 new OptionsSection("Miscellaneous", FontAwesome.Get(FontAwesomeIcon.fa_question_sign), new List<OptionsSubcategory>
                 {
+                    new OptionsSubcategory("Effects", new List<OptionsItem>()
+                    {
+                        new OptionsItemCheckbox(contentWidth, "Display Menu Audio Visualizer", ConfigManager.DisplayMenuAudioVisualizer),
+                    }),
+                    new OptionsSubcategory("Song Select", new List<OptionsItem>()
+                    {
+                        new OptionsItemCheckbox(contentWidth, "Display Failed Local Scores", ConfigManager.DisplayFailedLocalScores)
+                    })
                 }),
             };
 
