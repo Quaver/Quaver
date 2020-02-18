b43b046c03ba13cdd1fd44a3f09392cbcbc6fc89
Swan <me@swan.moe>
2020-02-18 14:32:19 -0500

    Add custom font and styling to editor menu bar

--- Quaver.Shared/Screens/Edit/UI/Menu/EditorFileMenuBar.cs
+++ Quaver.Shared/Screens/Edit/UI/Menu/EditorFileMenuBar.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using ImGuiNET;
+using Quaver.Shared.Graphics.Menu.Border;
+using Wobble;
 using Wobble.Graphics.ImGUI;
 using Wobble.Graphics.UI.Buttons;
+using Wobble.Window;
 
 namespace Quaver.Shared.Screens.Edit.UI.Menu
 {
@@ -17,10 +24,10 @@ namespace Quaver.Shared.Screens.Edit.UI.Menu
 #if VISUAL_TESTS
         private static bool DestroyContext { get; } = false;
 #else
-        private static bool DestroyContext { get; } = true;
+        private static bool DestroyContext { get; } = false;
 #endif
 
-        public EditorFileMenuBar() : base(DestroyContext)
+        public EditorFileMenuBar() : base(DestroyContext, GetOptions())
         {
         }
 
@@ -29,14 +36,122 @@ namespace Quaver.Shared.Screens.Edit.UI.Menu
         /// </summary>
         protected override void RenderImguiLayout()
         {
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowBorderSize, 2);
+            ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, new Vector2(0, 10));
+            ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, new Vector2(12, 4));
+            ImGui.PushStyleColor(ImGuiCol.FrameBg, new Vector4(0, 0, 24, 0));
+            ImGui.PushStyleColor(ImGuiCol.WindowBg, new Vector4(0, 0, 24, 0));
+
             if (!ImGui.BeginMainMenuBar())
                 return;
 
             Height = ImGui.GetWindowSize().Y;
 
+            CreateFileSection();
+            CreateEditSection();
+            CreateViewSection();
+            CreateAudioSection();
+            CreateWebSection();
+            CreatePluginsSection();
+            CreateHelpSection();
+
             ImGui.EndMenuBar();
             Button.IsGloballyClickable = !ImGui.IsAnyItemHovered();
             IsActive = ImGui.IsAnyItemActive() || ImGui.IsAnyItemHovered() || ImGui.IsAnyItemFocused();
+
+            ImGui.PopStyleVar();
         }
+
+        /// <summary>
+        /// </summary>
+        private void CreateFileSection()
+        {
+            ImGui.PushFont(Options.Fonts.First().Context);
+
+            if (!ImGui.BeginMenu("File"))
+                return;
+
+            ImGui.EndMenu();
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateEditSection()
+        {
+            ImGui.PushFont(Options.Fonts.First().Context);
+
+            if (!ImGui.BeginMenu("Edit"))
+                return;
+
+            ImGui.EndMenu();
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateViewSection()
+        {
+            ImGui.PushFont(Options.Fonts.First().Context);
+
+            if (!ImGui.BeginMenu("View"))
+                return;
+
+            ImGui.EndMenu();
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreatePluginsSection()
+        {
+            ImGui.PushFont(Options.Fonts.First().Context);
+
+            if (!ImGui.BeginMenu("Plugins"))
+                return;
+
+            ImGui.EndMenu();
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateHelpSection()
+        {
+            ImGui.PushFont(Options.Fonts.First().Context);
+
+            if (!ImGui.BeginMenu("Help"))
+                return;
+
+            ImGui.EndMenu();
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateWebSection()
+        {
+            ImGui.PushFont(Options.Fonts.First().Context);
+
+            if (!ImGui.BeginMenu("Web"))
+                return;
+
+            ImGui.EndMenu();
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateAudioSection()
+        {
+            ImGui.PushFont(Options.Fonts.First().Context);
+
+            if (!ImGui.BeginMenu("Audio"))
+                return;
+
+            ImGui.EndMenu();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        private static ImGuiOptions GetOptions() => new ImGuiOptions(new List<ImGuiFont>
+        {
+            new ImGuiFont($@"{WobbleGame.WorkingDirectory}/Fonts/lato-black.ttf"),
+        }, false);
     }
 }
