a87a517d085cd70f392c80d87b3a5beaf98bfed4
Swan <me@swan.moe>
2020-03-14 15:11:06 -0400

    Remove mods when going from editor to select

--- Quaver.Shared/Screens/Edit/EditScreen.cs
+++ Quaver.Shared/Screens/Edit/EditScreen.cs
@@ -1035,6 +1035,7 @@ namespace Quaver.Shared.Screens.Edit
             GameBase.Game.IsMouseVisible = false;
             GameBase.Game.GlobalUserInterface.Cursor.Alpha = 1;
 
+            ModManager.RemoveAllMods();
             Exit(() => new SelectionScreen());
         }
 
