ed40bf79e3200a5ac95c02542917a083a1cc13e9
Swan <me@swan.moe>
2020-03-14 13:34:36 -0400

    Fix note placing on incorrect snap when clicking line

--- Quaver.Shared/Screens/Edit/UI/Playfield/EditorPlayfield.cs
+++ Quaver.Shared/Screens/Edit/UI/Playfield/EditorPlayfield.cs
@@ -644,6 +644,9 @@ namespace Quaver.Shared.Screens.Edit.UI.Playfield
             var fwdDiff = Math.Abs(time - timeFwd);
             var bwdDiff = Math.Abs(time - timeBwd);
 
+            if (Math.Abs(fwdDiff - bwdDiff) <= 2)
+                return time;
+
             // ReSharper disable once CompareOfFloatsByEqualityOperator
             if (bwdDiff < fwdDiff)
                 time = timeBwd;
