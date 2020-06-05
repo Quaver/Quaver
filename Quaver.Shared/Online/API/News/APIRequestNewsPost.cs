20523d8ae7063661d9c6c4ea1b22f1e4bc5ec811
Swan <me@swan.moe>
2020-06-05 16:28:30 -0400

    Implement main menu news panel

---
+++ Quaver.Shared/Online/API/News/APIRequestNewsPost.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Quaver.Server.Client;
+using Quaver.Shared.Online.API.Leaderboard;
+using Quaver.Shared.Screens.Download;
+using RestSharp;
+using Wobble.Assets;
+using Wobble.Logging;
+
+namespace Quaver.Shared.Online.API.News
+{
+    public class APIRequestNewsFeed : APIRequest<APIResponseNewsFeed>
+    {
+        private const string URL = "https://blog.quavergame.com/feed.json";
+        
+        public override APIResponseNewsFeed ExecuteRequest()
+        {
+            var request = new RestRequest(URL, Method.GET);
+            var client = new RestClient("https://blog.quavergame.com") { UserAgent = "Quaver" };
+
+            var response = client.Execute(request);
+
+            var json = JObject.Parse(response.Content);
+
+            var responseParsed = JsonConvert.DeserializeObject<APIResponseNewsFeed>(json.ToString());
+
+            LoadBanner(responseParsed);
+            
+            return responseParsed;
+        }
+
+        /// <summary>
+        ///     Loads the banner 
+        /// </summary>
+        private void LoadBanner(APIResponseNewsFeed feed)
+        {
+            if (feed.Items.Count == 0)
+            {
+                Logger.Warning($"Could not load most recent news feed banner due to no blog items", LogType.Runtime);
+                return;
+            }
+
+            var latestPost = feed.Items.First();
+            
+            try
+            {
+                using (var webClient = new WebClient())
+                {
+                    using (var mem = new MemoryStream(webClient.DownloadData(latestPost.Image)))
+                        feed.RecentPostBanner = AssetLoader.LoadTexture2D(mem);
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e, LogType.Runtime);
+            }
+        }
+    }
+}
---
+++ Quaver.Shared/Screens/Main/UI/News/NewsPost.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Quaver.Shared.Assets;
+using Quaver.Shared.Graphics;
+using Quaver.Shared.Helpers;
+using Quaver.Shared.Online.API.News;
+using Quaver.Shared.Scheduling;
+using Quaver.Shared.Screens.Menu.UI.Jukebox;
+using TimeAgo;
+using Wobble.Assets;
+using Wobble.Graphics;
+using Wobble.Graphics.Animations;
+using Wobble.Graphics.Sprites;
+using Wobble.Graphics.Sprites.Text;
+using Wobble.Graphics.UI.Buttons;
+using Wobble.Managers;
+
+namespace Quaver.Shared.Screens.Main.UI.News
+{
+    public class NewsPost : IconButton
+    {
+        /// <summary>
+        /// </summary>
+        private static APIResponseNewsFeed NewsPosts { get; set; }
+        
+        private LoadingWheel Wheel { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private Container Container { get; set; }
+
+        /// <summary>
+        /// </summary>
+        private Sprite Banner { get; set; }
+
+        private SpriteTextPlus Title { get; set; }
+
+        private SpriteTextPlus TimeAgo { get; set; }
+
+        private SpriteTextPlus ShortText { get; set; }
+
+        public NewsPost() : base(UserInterface.NewsPanel)
+        {
+            Size = new ScalableVector2(Image.Width, Image.Height);
+            SetChildrenAlpha = true;
+            
+            CreateContainer();
+            CreateLoadingWheel();
+            
+            Load();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Banner != null && Banner.Animations.Count == 0)
+            {
+                Banner.Alpha = Alpha;
+                Title.Alpha = Alpha;
+                TimeAgo.Alpha = Alpha;
+                ShortText.Alpha = Alpha;
+            }
+            
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// </summary>
+        private void CreateLoadingWheel()
+        {
+            Wheel = new LoadingWheel()
+            {
+                Parent = Container,
+                Alignment = Alignment.MidCenter,
+                Size = new ScalableVector2(40, 40),
+                Alpha = 0
+            };
+
+            Wheel.FadeTo(1, Easing.Linear, 450);
+        }
+
+        private void CreateContainer() => Container = new Container()
+        {
+            Parent = this,
+            Alignment = Alignment.MidCenter,
+            Size = new ScalableVector2(Width - 4, Height - 4),
+        };
+
+        /// <summary>
+        /// </summary>
+        private void Load()
+        {
+            if (NewsPosts != null)
+            {
+                Initialize(NewsPosts);
+                return;
+            }
+            
+            ThreadScheduler.Run(() =>
+            {
+                NewsPosts = new APIRequestNewsFeed().ExecuteRequest();
+                ScheduleUpdate(() => Initialize(NewsPosts));
+            });
+        }
+
+        private void Initialize(APIResponseNewsFeed newsFeed)
+        {
+            if (newsFeed.Items.Count == 0)
+                return;
+
+            var latestPost = newsFeed.Items.First();
+            
+            Wheel.ClearAnimations();
+            Wheel.FadeTo(0, Easing.Linear, 450);
+            
+            CreateBanner(newsFeed.RecentPostBanner);
+            CreateTitle(latestPost);
+            CreateTimeAgo(latestPost);
+            CreateShortText(latestPost);
+
+            Clicked += (sender, args) => BrowserHelper.OpenURL(latestPost.Url);
+        }
+
+        private void CreateBanner(Texture2D banner)
+        {
+            Banner = new Sprite
+            {
+                Parent = Container,
+                Size = new ScalableVector2(Container.Width, 121),
+                Alpha = 0,
+                Image = banner ?? UserInterface.UnknownAvatar
+            };
+
+            Banner.FadeTo(1, Easing.Linear, 450);
+        }
+
+        private void CreateTitle(NewsFeedItem item)
+        {
+            Title = new SpriteTextPlus(FontManager.GetWobbleFont(Fonts.LatoBlack), item.Title, 22)
+            {
+                Parent = Container,
+                X = 14,
+                Y = Banner.Y + Banner.Height + 16,
+                Tint = ColorHelper.HexToColor("#45D6F5"),
+                Alpha = 0
+            };
+
+            Title.TruncateWithEllipsis((int) Width - 60);
+            Title.FadeTo(1, Easing.Linear, 450);
+        }
+
+        private void CreateTimeAgo(NewsFeedItem item)
+        {
+            TimeAgo = new SpriteTextPlus(FontManager.GetWobbleFont(Fonts.LatoBlack), $"Published {item.DatePublished.TimeAgo()}",
+                20)
+            {
+                Parent = Container,
+                X = Title.X,
+                Y = Title.Y + Title.Height + 12,
+                Tint = ColorHelper.HexToColor("#808080"),
+                Alpha = 0
+            };
+
+            TimeAgo.FadeTo(1, Easing.Linear, 450);
+        }
+
+        private void CreateShortText(NewsFeedItem item)
+        {
+            ShortText = new SpriteTextPlus(FontManager.GetWobbleFont(Fonts.LatoBlack), item.ShortText, 20)
+            {
+                Parent = Container,
+                X = Title.X,
+                Y = TimeAgo.Y + TimeAgo.Height + 12,
+                MaxWidth = Container.Width - 20,
+                Alpha = 0
+            };
+            
+            ShortText.TruncateWithEllipsis((int) ShortText.MaxWidth * 2 - 100);
+            ShortText.FadeTo(1, Easing.Linear, 450);
+        }
+    }
+}
