e9979cf6a7f79e2e3165da8422603ab800ab3051
Illuminati-CRAZ <42056854+Illuminati-CRAZ@users.noreply.github.com>
2020-09-02 01:13:34 -0700

    Make cursor disappear after checks instead of before

--- Quaver.Shared/Screens/Edit/EditScreen.cs
+++ Quaver.Shared/Screens/Edit/EditScreen.cs
@@ -1221,8 +1221,6 @@ namespace Quaver.Shared.Screens.Edit
             if (Exiting)
                 return;
 
-            GameBase.Game.IsMouseVisible = false;
-
             if (WorkingMap.HitObjects.Count(x => x.StartTime >= Track.Time) == 0)
             {
                 NotificationManager.Show(NotificationLevel.Warning, "There aren't any hitobjects to play past this point!");
@@ -1234,6 +1232,8 @@ namespace Quaver.Shared.Screens.Edit
                 NotificationManager.Show(NotificationLevel.Warning, "Finish what you're doing before test playing!");
                 return;
             }
+
+            GameBase.Game.IsMouseVisible = false;
 
             Exit(() =>
             {
