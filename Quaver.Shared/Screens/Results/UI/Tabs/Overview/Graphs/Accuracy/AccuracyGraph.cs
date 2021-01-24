315528b8a36ac7da0f1da0de05b2ae018b456d32
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2021-01-24 02:10:05 +0100

    Change style from line to area graph

--- Quaver.Shared/Screens/Results/UI/Tabs/Overview/Graphs/Accuracy/AccuracyGraph.cs
+++ Quaver.Shared/Screens/Results/UI/Tabs/Overview/Graphs/Accuracy/AccuracyGraph.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Quaver.API.Enums;
 using Quaver.API.Helpers;
@@ -35,6 +36,8 @@ namespace Quaver.Shared.Screens.Results.UI.Tabs.Overview.Graphs.Accuracy
 
         private float AccuracyStart { get; set; }
 
+        private float AccuracyStep { get; set; }
+
         /// <summary>
         ///     Accuracy data points throughout the score
         /// </summary>
@@ -59,35 +62,89 @@ namespace Quaver.Shared.Screens.Results.UI.Tabs.Overview.Graphs.Accuracy
                 Parent = this,
                 Alpha = 0f,
                 Alignment = Alignment.MidRight,
-                Size = new ScalableVector2(Width - 125, Height * 0.9f)
+                Size = new ScalableVector2(Width - 60, Height * 0.9f)
             };
 
             CreateAccuracyHistory();
-            CreateGridlinesAndLabels();
             CreateDataPoints();
+            CreateGridlinesAndLabels();
         }
 
         private void CreateAccuracyHistory()
         {
-            AccuracyHistory = new List<(int, float)>();
-            MinAccuracy = 1.0f;
             if (!(Processor.Value is ScoreProcessorKeys keysProcessor)) return;
             // keysProcessor.Map is not set for replays
             var simulatedProcessor =
                 new ScoreProcessorKeys(Map.Qua, keysProcessor.Mods, keysProcessor.Windows);
 
+            AccuracyHistory = new List<(int, float)>();
+            MinAccuracy = 100f;
+
             var i = 0;
+            var previousTime = int.MinValue;
             foreach (var stat in keysProcessor.Stats)
             {
                 simulatedProcessor.CalculateScore(stat.Judgement, stat.KeyPressType == KeyPressType.Release);
-                var acc = simulatedProcessor.Accuracy / 100;
-                var dataPoint = (stat.SongPosition, acc);
-                AccuracyHistory.Add(dataPoint);
-                // skip the first 10 judgements because of heavy fluctuations
-                if (i < 10)
-                    i++;
-                else
+                var acc = simulatedProcessor.Accuracy;
+                if (stat.SongPosition == previousTime)
+                    AccuracyHistory.Remove(AccuracyHistory.Last()); // prevent multiple accuracies on a single time
+                AccuracyHistory.Add((stat.SongPosition, acc));
+                previousTime = stat.SongPosition;
+                // skip the first judgements because of heavy fluctuations
+                if (i > 20)
                     MinAccuracy = Math.Min(acc, MinAccuracy);
+                i++;
+            }
+
+            switch (GradeHelper.GetGradeFromAccuracy(MinAccuracy))
+            {
+                case Grade.SS:
+                    AccuracyStep = 0.25f;
+                    break;
+                case Grade.S:
+                    AccuracyStep = 1.0f;
+                    break;
+                case Grade.A:
+                    AccuracyStep = 2.5f;
+                    break;
+                case Grade.B:
+                    AccuracyStep = 5.0f;
+                    break;
+                default:
+                    AccuracyStep = 10.0f;
+                    break;
+            }
+
+            AccuracyStart = MinAccuracy - (MinAccuracy % AccuracyStep);
+        }
+
+        private void CreateDataPoints()
+        {
+            var start = AccuracyHistory.First().Item1;
+            var end = AccuracyHistory.Last().Item1;
+
+            for (var i = 0; i < AccuracyHistory.Count; i++)
+            {
+                var (time, acc) = AccuracyHistory[i];
+
+                var nextTime = i == AccuracyHistory.Count - 1 ? 1 : AccuracyHistory[i + 1].Item1;
+                var songProgress = (float) (time - start) / (end - start);
+                var nextSongProgress = (float) (nextTime - start) / (end - start);
+
+                var y = (acc - AccuracyStart) / (100f - AccuracyStart);
+                var width = nextSongProgress - songProgress;
+
+                var point = new Sprite
+                {
+                    Parent = ContentContainer,
+                    Alignment = Alignment.TopLeft,
+                    X = songProgress * ContentContainer.Width,
+                    Y = (1f - y) * ContentContainer.Height,
+                    Size = new ScalableVector2(width * ContentContainer.Width, y * ContentContainer.Height),
+                    Visible = true,
+                    Alpha = 1f,
+                    Tint = GetColor(acc)
+                };
             }
         }
 
@@ -95,35 +152,14 @@ namespace Quaver.Shared.Screens.Results.UI.Tabs.Overview.Graphs.Accuracy
         /// </summary>
         private void CreateGridlinesAndLabels()
         {
-            float accStepSize;
-            switch (GradeHelper.GetGradeFromAccuracy(MinAccuracy * 100))
-            {
-                case Grade.SS:
-                    accStepSize = 0.0025f;
-                    break;
-                case Grade.S:
-                    accStepSize = 0.010f;
-                    break;
-                case Grade.A:
-                    accStepSize = 0.025f;
-                    break;
-                case Grade.B:
-                    accStepSize = 0.050f;
-                    break;
-                default:
-                    accStepSize = 0.100f;
-                    break;
-            }
-
-            AccuracyStart = MinAccuracy - (MinAccuracy % accStepSize);
             var lineCount =
-                (int) Math.Round((1.0f - AccuracyStart) / accStepSize); // Round because of possible float inaccuracy
+                (int) Math.Round((100f - AccuracyStart) / AccuracyStep); // Round because of possible float inaccuracy
 
             // <= because we also want to draw the final line
             for (var i = 0; i <= lineCount; i++)
             {
                 var relativeY = (float) i / lineCount;
-                var acc = Math.Round(100f - i * accStepSize * 100, 2);
+                var acc = Math.Round(100f - i * AccuracyStep, 2);
 
                 var line = new Sprite
                 {
@@ -132,7 +168,7 @@ namespace Quaver.Shared.Screens.Results.UI.Tabs.Overview.Graphs.Accuracy
                     Tint = ColorHelper.HexToColor("#808080"),
                     Alignment = Alignment.TopCenter,
                     Y = relativeY * ContentContainer.Height,
-                    Size = new ScalableVector2(Width, 2),
+                    Size = new ScalableVector2(ContentContainer.Width, 2),
                 };
 
                 var text = new SpriteTextPlus(FontManager.GetWobbleFont(Fonts.LatoBlack), $"{acc}%", 20,
@@ -147,33 +183,28 @@ namespace Quaver.Shared.Screens.Results.UI.Tabs.Overview.Graphs.Accuracy
             }
         }
 
-        private void CreateDataPoints()
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        private static Color GetColor(float accuracy)
         {
-            var start = AccuracyHistory.First().Item1;
-            var end = AccuracyHistory.Last().Item1;
-
-            for (var i = 0; i < AccuracyHistory.Count; i++)
+            switch (GradeHelper.GetGradeFromAccuracy(accuracy))
             {
-                var (time, acc) = AccuracyHistory[i];
-                var nextTime = i == AccuracyHistory.Count - 1 ? 1 : AccuracyHistory[i + 1].Item1;
-
-                var songProgress = (float) (time - start) / (end - start);
-                var nextSongProgress = (float) (nextTime - start) / (end - start);
-
-                var y = (acc - AccuracyStart) / (1f - AccuracyStart);
-                var width = nextSongProgress - songProgress;
-
-                var point = new Sprite
-                {
-                    Parent = ContentContainer,
-                    Alignment = Alignment.TopLeft,
-                    X = songProgress * ContentContainer.Width,
-                    Y = (1f - y) * ContentContainer.Height,
-                    Size = new ScalableVector2(width * ContentContainer.Width, 5),
-                    Visible = true,
-                    Alpha = 1f,
-                    Tint = ColorHelper.HexToColor("#ffffff"),
-                };
+                case Grade.X:
+                    return new Color(251,255,182);
+                case Grade.SS:
+                    return new Color(255,241,137);
+                case Grade.S:
+                    return new Color(255,231,107);
+                case Grade.A:
+                    return new Color(86,254,110);
+                case Grade.B:
+                    return new Color(0,209,255);
+                case Grade.C:
+                    return new Color(217,107,206);
+                default:
+                    return new Color(249,100,93);
             }
         }
     }
