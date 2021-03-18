86707f7078f3533334560d5caec3c7cad7a2d870
Swan <me@swan.moe>
2021-03-18 12:25:56 -0400

    Update submodules

---
+++ Quaver.Shared/Screens/Edit/UI/AutoMods/EditorAutoModScrollPanel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Quaver.Shared.Graphics.Containers;
+using Wobble.Assets;
+using Wobble.Graphics;
+using Wobble.Graphics.Sprites;
+
+namespace Quaver.Shared.Screens.Edit.UI.AutoMods
+{
+    public class EditorAutoModScrollPanel : PoolableScrollContainer<Drawable>
+    {
+        public EditorAutoModScrollPanel() : base(new List<Drawable>(), int.MaxValue,
+            0, new ScalableVector2(0, 0), new ScalableVector2(0, 0))
+        {
+            Image = AssetLoader.LoadTexture2DFromFile(@"C:\users\swan\desktop\automod-scroll-panel.png");
+            Size = new ScalableVector2(720, 600);
+
+            Scrollbar.Visible = false;
+        }
+
+        protected override PoolableSprite<Drawable> CreateObject(Drawable item, int index) => null;
+    }
+}
