644de39eb25139c63ca48bb76f8273ce218348e5
Swan <me@swan.moe>
2021-03-18 12:26:46 -0400

    Add AutoMod panel design

--- Quaver.Shared/Screens/Edit/UI/AutoMods/EditorAutoModScrollPanel.cs
+++ Quaver.Shared/Screens/Edit/UI/AutoMods/EditorAutoModScrollPanel.cs
@@ -1,23 +1,118 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Quaver.API.Maps;
+using Quaver.API.Maps.AutoMod;
+using Quaver.API.Maps.AutoMod.Issues;
 using Quaver.Shared.Graphics.Containers;
-using Wobble.Assets;
+using Quaver.Shared.Helpers;
+using Wobble.Bindables;
 using Wobble.Graphics;
-using Wobble.Graphics.Sprites;
+using Wobble.Graphics.Animations;
+using Wobble.Graphics.UI.Dialogs;
+using Wobble.Input;
 
 namespace Quaver.Shared.Screens.Edit.UI.AutoMods
 {
-    public class EditorAutoModScrollPanel : PoolableScrollContainer<Drawable>
+    public sealed class EditorAutoModScrollPanel : PoolableScrollContainer<AutoModIssue>
     {
-        public EditorAutoModScrollPanel() : base(new List<Drawable>(), int.MaxValue,
-            0, new ScalableVector2(0, 0), new ScalableVector2(0, 0))
-        {
-            Image = AssetLoader.LoadTexture2DFromFile(@"C:\users\swan\desktop\automod-scroll-panel.png");
-            Size = new ScalableVector2(720, 600);
+        private Qua Map { get; }
 
-            Scrollbar.Visible = false;
+        private Bindable<AutoModMapset> AutoMod { get; }
+
+        private Bindable<AutoModIssueCategory> FilterCategory { get; }
+
+        private static ScalableVector2 DefaultSize { get; } = new ScalableVector2(720, 600);
+
+        public EditorAutoModScrollPanel(Qua map, Bindable<AutoModMapset> autoMod,
+            Bindable<AutoModIssueCategory> category) : base(new List<AutoModIssue>(), 16, 0, DefaultSize,
+            DefaultSize)
+        {
+            Map = map;
+            AutoMod = autoMod;
+            FilterCategory = category;
+
+            Tint = ColorHelper.HexToColor("#242424");
+            AddBorder(ColorHelper.HexToColor("#BEBEBE"), 2);
+            Border.Alpha = 0.50f;
+
+            EasingType = Easing.OutQuint;
+            TimeToCompleteScroll = 1200;
+            ScrollSpeed = 320;
+            InputEnabled = true;
+
+            Scrollbar.Tint = ColorHelper.HexToColor("#656565");
+            Scrollbar.Width = 4;
+            Scrollbar.X = 8;
+
+            FilterIssues();
+
+            CreatePool();
+            RecalculateContainerHeight();
+
+            AutoMod.ValueChanged += OnAutoModUpdated;
+            FilterCategory.ValueChanged += OnFilterCategoryChanged;
         }
 
-        protected override PoolableSprite<Drawable> CreateObject(Drawable item, int index) => null;
+        public override void Update(GameTime gameTime)
+        {
+            InputEnabled = GraphicsHelper.RectangleContains(ScreenRectangle, MouseManager.CurrentState.Position)
+                           && DialogManager.Dialogs.Count == 0
+                           && !KeyboardManager.CurrentState.IsKeyDown(Keys.LeftAlt)
+                           && !KeyboardManager.CurrentState.IsKeyDown(Keys.RightAlt);
+
+            base.Update(gameTime);
+        }
+
+        public override void Destroy()
+        {
+            // ReSharper disable once DelegateSubtraction
+            AutoMod.ValueChanged -= OnAutoModUpdated;
+
+            base.Destroy();
+        }
+
+        protected override PoolableSprite<AutoModIssue> CreateObject(AutoModIssue item, int index)
+            => new EditorAutoModIssue(this, item, index);
+
+        private void FilterIssues()
+        {
+            AvailableItems = new List<AutoModIssue>();
+
+            foreach (var issue in AutoMod.Value.Issues)
+            {
+                if (FilterCategory.Value == AutoModIssueCategory.None || issue.Category == FilterCategory.Value)
+                    AvailableItems.Add(issue);
+            }
+
+            foreach (var issue in AutoMod.Value.Mods[Map].Issues)
+            {
+                if (FilterCategory.Value == AutoModIssueCategory.None || issue.Category == FilterCategory.Value)
+                    AvailableItems.Add(issue);
+            }
+        }
+
+        private void Refresh() => ScheduleUpdate(() =>
+        {
+            Pool.ForEach(x => x.Destroy());
+            Pool = new List<PoolableSprite<AutoModIssue>>();
+
+            PoolStartingIndex = 0;
+            ContentContainer.Y = 0;
+            PreviousContentContainerY = ContentContainer.Y;
+            TargetY = PreviousContentContainerY;
+            PreviousTargetY = PreviousContentContainerY;
+
+            FilterIssues();
+
+            CreatePool();
+            RecalculateContainerHeight();
+        });
+
+        private void OnAutoModUpdated(object sender, BindableValueChangedEventArgs<AutoModMapset> e)
+            => Refresh();
+
+        private void OnFilterCategoryChanged(object sender, BindableValueChangedEventArgs<AutoModIssueCategory> e)
+            => Refresh();
     }
 }
