7be9c1ed613c4ec5d62b72d66c347fd72bc4e29d
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2022-06-15 00:16:43 +0200

    Place property attributes above property

--- Quaver.Shared/Online/API/Playlists/PlaylistInformationResponse.cs
+++ Quaver.Shared/Online/API/Playlists/PlaylistInformationResponse.cs
@@ -6,22 +6,47 @@ namespace Quaver.Shared.Online.API.Playlists
 {
     public class PlaylistInformationResponse
     {
-        [JsonProperty("status")] public int Status;
-        [JsonProperty("playlist")] public PlaylistInformationResponsePlaylist PlaylistInformation;
+        [JsonProperty("status")]
+        public int Status;
+
+        [JsonProperty("playlist")]
+        public PlaylistInformationResponsePlaylist PlaylistInformation;
     }
 
     public class PlaylistInformationResponsePlaylist
     {
-        [JsonProperty("id")] public int Id;
-        [JsonProperty("user_id")] public int UserId;
-        [JsonProperty("name")] public string Name;
-        [JsonProperty("description")] public string Description;
-        [JsonProperty("like_count")] public int LikeCount;
-        [JsonProperty("map_count")] public int MapCount;
-        [JsonProperty("time_created")] public string TimeCreated;
-        [JsonProperty("time_last_updated")] public string TimeLastUpdated;
-        [JsonProperty("owner_id")] public int OwnerId;
-        [JsonProperty("owner_username")] public string OwnerUsername;
-        [JsonProperty("maps")] public List<MapInformationResponseMap> Maps;
+        [JsonProperty("id")]
+        public int Id;
+
+        [JsonProperty("user_id")]
+        public int UserId;
+
+        [JsonProperty("name")]
+        public string Name;
+
+        [JsonProperty("description")]
+        public string Description;
+
+        [JsonProperty("like_count")]
+        public int LikeCount;
+
+        [JsonProperty("map_count")]
+        public int MapCount;
+
+        [JsonProperty("time_created")]
+        public string TimeCreated;
+
+        [JsonProperty("time_last_updated")]
+        public string TimeLastUpdated;
+
+        [JsonProperty("owner_id")]
+        public int OwnerId;
+
+        [JsonProperty("owner_username")]
+        public string OwnerUsername;
+
+        [JsonProperty("maps")]
+        public List<MapInformationResponseMap> Maps;
+
     }
 }
