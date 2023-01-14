9b83e82dc6d1f131086a6493c7bc92fcfcf82598
Illuminati-CRAZ <42056854+Illuminati-CRAZ@users.noreply.github.com>
2023-01-14 14:08:16 -0800

    Revert back to C# 8

--- Quaver.Shared/Screens/Options/Items/Custom/OptionsItemPrioritizeGameMode.cs
+++ Quaver.Shared/Screens/Options/Items/Custom/OptionsItemPrioritizeGameMode.cs
@@ -8,35 +8,36 @@ using Quaver.Shared.Graphics;
 using Quaver.Shared.Graphics.Form.Dropdowns;
 using Wobble.Graphics;
 
-namespace Quaver.Shared.Screens.Options.Items.Custom;
-
-public class OptionsItemPrioritizedGameMode : OptionsItemDropdown
+namespace Quaver.Shared.Screens.Options.Items.Custom
 {
-    public OptionsItemPrioritizedGameMode(RectangleF containerRect, string name) : base(containerRect, name,
-        new Dropdown(GetOptions(), new ScalableVector2(180, 35), 22, Colors.MainAccent, GetSelectedIndex()))
+    public class OptionsItemPrioritizedGameMode : OptionsItemDropdown
     {
-        Dropdown.ItemSelected += (sender, args) =>
+        public OptionsItemPrioritizedGameMode(RectangleF containerRect, string name) : base(containerRect, name,
+            new Dropdown(GetOptions(), new ScalableVector2(180, 35), 22, Colors.MainAccent, GetSelectedIndex()))
         {
-            if (ConfigManager.PrioritizedGameMode== null)
-                return;
-
-            ConfigManager.PrioritizedGameMode.Value = args.Text switch
+            Dropdown.ItemSelected += (sender, args) =>
             {
-                "None" => (GameMode)0,
-				"4 Keys" => GameMode.Keys4,
-				"7 Keys" => GameMode.Keys7,
-                _ => throw new InvalidEnumArgumentException()
+                if (ConfigManager.PrioritizedGameMode == null)
+                    return;
+
+                ConfigManager.PrioritizedGameMode.Value = args.Text switch
+                {
+                    "None" => (GameMode)0,
+                    "4 Keys" => GameMode.Keys4,
+                    "7 Keys" => GameMode.Keys7,
+                    _ => throw new InvalidEnumArgumentException()
+                };
             };
-        };
-    }
+        }
 
-    private static List<string> GetOptions() => new() { "None", "4 Keys", "7 Keys" };
+        private static List<string> GetOptions() => new List<string>{ "None", "4 Keys", "7 Keys" };
 
-    private static int GetSelectedIndex()
-    {
-        if (ConfigManager.PrioritizedGameMode == null)
-            return 0;
+        private static int GetSelectedIndex()
+        {
+            if (ConfigManager.PrioritizedGameMode == null)
+                return 0;
 
-        return (int)ConfigManager.PrioritizedGameMode.Value;
+            return (int)ConfigManager.PrioritizedGameMode.Value;
+        }
     }
 }
