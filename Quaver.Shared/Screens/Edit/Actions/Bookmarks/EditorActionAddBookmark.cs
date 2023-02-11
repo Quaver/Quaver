54fd220ece97129bcb0e35cd45d46274ed363994
Swan <me@swan.moe>
2023-02-11 18:52:19 -0500

    Implement bookmark add action

---
+++ Quaver.Shared/Screens/Edit/Actions/Bookmarks/EditorActionAddBookmark.cs
@@ -0,0 +1,40 @@
+using MoonSharp.Interpreter.Interop;
+using Quaver.API.Maps;
+using Quaver.API.Maps.Structures;
+
+namespace Quaver.Shared.Screens.Edit.Actions.Bookmarks
+{
+    public class EditorActionAddBookmark : IEditorAction
+    {
+        public EditorActionType Type { get; } = EditorActionType.AddBookmark;
+        
+        private EditorActionManager ActionManager { get; }
+
+        private Qua WorkingMap { get; }
+
+        private BookmarkInfo Bookmark { get; }
+
+        [MoonSharpVisible(false)]
+        public EditorActionAddBookmark(EditorActionManager manager, Qua map, BookmarkInfo bookmark)
+        {
+            ActionManager = manager;
+            WorkingMap = map;
+            Bookmark = bookmark;
+        }
+        
+        [MoonSharpVisible(false)]
+        public void Perform()
+        {
+            WorkingMap.Bookmarks.Add(Bookmark);
+            WorkingMap.Sort();
+            
+            ActionManager.TriggerEvent(Type, new EditorActionBookmarkAddedEventArgs(Bookmark));
+        }
+
+        [MoonSharpVisible(false)]
+        public void Undo()
+        {
+            
+        }
+    }
+}
