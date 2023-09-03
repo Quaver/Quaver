185124373c3d61191d2e423dbfe1bf90f8ece641
Warp <warpaboi@gmail.com>
2023-09-03 13:54:28 +0200

    dont limit characters to 11

--- Quaver.Shared/Screens/Edit/UI/Panels/Layers/Dialogs/DialogChangeLayerColor.cs
+++ Quaver.Shared/Screens/Edit/UI/Panels/Layers/Dialogs/DialogChangeLayerColor.cs
@@ -75,7 +75,6 @@ namespace Quaver.Shared.Screens.Edit.UI.Panels.Layers.Dialogs
                 X = 24,
                 Tint = ColorHelper.HexToColor("#2F2F2F"),
                 AlwaysFocused = true,
-                MaxCharacters = 11,
                 StoppedTypingActionCalltime = 100
             };
 
