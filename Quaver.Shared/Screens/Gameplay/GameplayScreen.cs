91fd36bc4878781980624b68992df1069c90ab21
Warp <warpaboi@gmail.com>
2023-09-19 14:45:31 +0200

    dont toggle autoplay when a dialog is open

--- Quaver.Shared/Screens/Gameplay/GameplayScreen.cs
+++ Quaver.Shared/Screens/Gameplay/GameplayScreen.cs
@@ -1500,7 +1500,7 @@ namespace Quaver.Shared.Screens.Gameplay
         public void HandleAutoplayTabInput(GameTime gameTime)
         {
             // Handle play test autoplay input.
-            if (IsPlayTesting && KeyboardManager.IsUniqueKeyPress(Keys.Tab) && !KeyboardManager.IsShiftDown() && !OnlineChat.Instance.IsOpen)
+            if (IsPlayTesting && KeyboardManager.IsUniqueKeyPress(Keys.Tab) && !KeyboardManager.IsShiftDown() && !OnlineChat.Instance.IsOpen && DialogManager.Dialogs.Count == 0)
             {
                 var inputManager = (KeysInputManager) Ruleset.InputManager;
 
