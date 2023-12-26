dff719f5a0fde55699a0f75fba6a8298d5ab4be8
Warp <warpaboi@gmail.com>
2023-12-26 20:03:49 +0100

    Update API request URLs for Privacy Policy and Terms of Service

--- Quaver.Shared/Online/API/Legal/APIRequestPrivacyPolicy.cs
+++ Quaver.Shared/Online/API/Legal/APIRequestPrivacyPolicy.cs
@@ -4,11 +4,11 @@ namespace Quaver.Shared.Online.API.Legal
 {
     public class APIRequestPrivacyPolicy : APIRequest<string>
     {
-        private const string BaseUrl = "https://raw.githubusercontent.com";
+        private const string BaseUrl = "https://wiki.quavergame.com";
         
         public override string ExecuteRequest()
         {
-            var request = new RestRequest($"{BaseUrl}/Quaver/Quaver.Wiki/master/Legal/Privacy/en.md", Method.GET);
+            var request = new RestRequest($"{BaseUrl}/md/Legal/Privacy/en.md", Method.GET);
             var client = new RestClient(BaseUrl);
 
             var response = client.Execute(request);
--- Quaver.Shared/Online/API/Legal/APIRequestTOS.cs
+++ Quaver.Shared/Online/API/Legal/APIRequestTOS.cs
@@ -4,11 +4,11 @@ namespace Quaver.Shared.Online.API.Legal
 {
     public class APIRequestTOS : APIRequest<string>
     {
-        private const string BaseUrl = "https://raw.githubusercontent.com";
+        private const string BaseUrl = "https://wiki.quavergame.com";
         
         public override string ExecuteRequest()
         {
-            var request = new RestRequest($"{BaseUrl}/Quaver/Quaver.Wiki/master/Legal/Terms/en.md", Method.GET);
+            var request = new RestRequest($"{BaseUrl}/md/Legal/Terms/en.md", Method.GET);
             var client = new RestClient(BaseUrl);
 
             var response = client.Execute(request);
