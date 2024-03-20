728ce3cec4b6e8b699340c4f621158550067d7a6
WilliamQiufeng <williamqiufeng@outlook.com>
2024-03-20 21:27:40 +0800

    Stub SpriteProxy

---
+++ Quaver.Shared/Screens/Gameplay/Rulesets/Keys/Storyboard/Proxy/SpriteProxy.cs
@@ -0,0 +1,15 @@
+using Wobble.Graphics.Sprites;
+
+namespace Quaver.Shared.Screens.Gameplay.Rulesets.Keys.Storyboard.Proxy;
+
+// TODO decide if this should be included
+public class SpriteProxy
+{
+    private readonly Sprite _sprite;
+
+    public SpriteProxy(Sprite sprite)
+    {
+        _sprite = sprite;
+    }
+    
+}
