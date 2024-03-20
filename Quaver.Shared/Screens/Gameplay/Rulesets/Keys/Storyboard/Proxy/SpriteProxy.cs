13c2c60f7c8c0f96f7c1b62082e1580ec168d10e
WilliamQiufeng <williamqiufeng@outlook.com>
2024-03-20 22:28:01 +0800

    Add properties to SpriteProxy

--- Quaver.Shared/Screens/Gameplay/Rulesets/Keys/Storyboard/Proxy/SpriteProxy.cs
+++ Quaver.Shared/Screens/Gameplay/Rulesets/Keys/Storyboard/Proxy/SpriteProxy.cs
@@ -1,3 +1,4 @@
+using Wobble.Graphics;
 using Wobble.Graphics.Sprites;
 
 namespace Quaver.Shared.Screens.Gameplay.Rulesets.Keys.Storyboard.Proxy;
@@ -11,5 +12,28 @@ public class SpriteProxy
     {
         _sprite = sprite;
     }
-    
+
+    public ScalableVector2 Position
+    {
+        get => _sprite.Position;
+        set => _sprite.Position = value;
+    }
+
+    public ScalableVector2 Size
+    {
+        get => _sprite.Size;
+        set => _sprite.Size = value;
+    }
+
+    public float Rotation
+    {
+        get => _sprite.Rotation;
+        set => _sprite.Rotation = value;
+    }
+
+    public float Alpha
+    {
+        get => _sprite.Alpha;
+        set => _sprite.Alpha = value;
+    }
 }
