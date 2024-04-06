107eb61c6e3a8aa60e921301d7b0ba0ec94b8c2b
WilliamQiufeng <williamqiufeng@outlook.com>
2024-04-06 16:20:23 +0800

    Add keybind config for toggling playtest autoplay

--- Quaver.Shared/Screens/Options/OptionsMenu.cs
+++ Quaver.Shared/Screens/Options/OptionsMenu.cs
@@ -362,6 +362,7 @@ namespace Quaver.Shared.Screens.Options
                         new OptionsItemKeybind(containerRect, "Increase Scroll Speed", ConfigManager.KeyIncreaseScrollSpeed),
                         new OptionsItemKeybind(containerRect, "Decrease Map Offset", ConfigManager.KeyDecreaseMapOffset),
                         new OptionsItemKeybind(containerRect, "Increase Map Offset", ConfigManager.KeyIncreaseMapOffset),
+                        new OptionsItemKeybind(containerRect, "Toggle Playtest Autoplay", ConfigManager.KeyTogglePlaytestAutoplay),
                     }),
                     new OptionsSubcategory("Gameplay User Interface", new List<OptionsItem>()
                     {
