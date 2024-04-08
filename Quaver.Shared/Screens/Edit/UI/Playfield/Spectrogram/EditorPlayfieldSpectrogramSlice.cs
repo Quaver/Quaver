f195bff1bf84d6e19602cb21dfff4cec4e8bedbb
WilliamQiufeng <williamqiufeng@outlook.com>
2024-04-08 18:06:59 +0800

    Add spectrogram

---
+++ Quaver.Shared/Screens/Edit/UI/Playfield/Spectrogram/EditorPlayfieldSpectrogramSlice.cs
@@ -0,0 +1,150 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Wobble.Graphics.Sprites;
+using Wobble;
+using Wobble.Graphics.Animations;
+using Quaver.Shared.Config;
+using Quaver.Shared.Screens.Edit.UI.Playfield.Waveform;
+using Wobble.Logging;
+
+namespace Quaver.Shared.Screens.Edit.UI.Playfield.Spectrogram
+{
+    public class EditorPlayfieldSpectrogramSlice : Sprite
+    {
+        private EditorPlayfield Playfield { get; set; }
+
+        private Sprite SliceSprite { get; set; }
+
+        private int SliceSize { get; }
+
+        private double SliceTimeMilliSeconds { get; }
+
+        private Texture2D SliceTexture { get; set; }
+        
+        private float LengthMs { get; set; }
+        
+        private int SampleRate { get; set; }
+
+        private int ReferenceWidth { get; } = 1024;
+
+        public EditorPlayfieldSpectrogramSlice(EditorPlayfield playfield, float lengthMs, int sliceSize,
+            float[,] sliceData,
+            double sliceTime, int sampleRate)
+        {
+            Playfield = playfield;
+            SliceSize = sliceSize;
+            SliceTimeMilliSeconds = sliceTime;
+            LengthMs = lengthMs;
+            SampleRate = sampleRate;
+
+            CreateSlice(sliceData);
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            SliceSprite.PerformTransformations(gameTime);
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SliceSprite.X = Playfield.ScreenRectangle.X;
+            SliceSprite.Y = Playfield.HitPositionY - (float)(SliceTimeMilliSeconds + LengthMs) * Playfield.TrackSpeed -
+                            Height;
+            SliceSprite.Height = LengthMs * Playfield.TrackSpeed;
+            SliceSprite.Tint = new Color(
+                255,
+                255,
+                255,
+                255);
+            SliceSprite.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// </summary>
+        public override void Destroy()
+        {
+            SliceSprite?.Destroy();
+            SliceSprite = null;
+            SliceTexture?.Dispose();
+
+            base.Destroy();
+        }
+
+        public void UpdatePlayfield(EditorPlayfield playfield) => Playfield = playfield;
+
+        private void CreateSlice(float[,] sliceData)
+        {
+            SliceSprite = new Sprite { Alpha = 0 };
+
+            var textureHeight = SliceSize;
+
+            SliceTexture = new Texture2D(GameBase.Game.GraphicsDevice, ReferenceWidth, textureHeight);
+
+            var dataColors = new Color[ReferenceWidth * textureHeight];
+            Logger.Debug($"Slice {ReferenceWidth} x {textureHeight}", LogType.Runtime);
+
+            for (var y = 0; y < textureHeight; y++)
+            {
+                for (var x = 0; x < EditorPlayfieldSpectrogram.FftCount; x++)
+                {
+                    var textureX = CalculateTextureXLinear(x);
+                    if (textureX == -1) continue;
+                    var intensity =
+                        MathF.Sqrt(GetAverageData(sliceData, y, x)) * 3f; // scale it (sqrt to make low values more visible)
+                    intensity = Math.Clamp(intensity, 0, 1);
+                    var index = DataColorIndex(textureHeight, y, textureX);
+                    var nextTextureX = CalculateTextureXLinear(x + 1);
+                    if (nextTextureX == -1) nextTextureX = ReferenceWidth - 1;
+                    for (var i = index; i < DataColorIndex(textureHeight, y, nextTextureX) && i < dataColors.Length; i++)
+                    {
+                        dataColors[i] = SpectrogramColormap.GetColor(intensity);
+                    }
+                    
+                    // if (index + (int)Playfield.Width < dataColors.Length)
+                    //     dataColors[index + (int)Playfield.Width] = new Color(intensity, 0, 0, 1);
+                }
+            }
+
+            SliceTexture.SetData(dataColors);
+            SliceSprite.Image = SliceTexture;
+            SliceSprite.Width = (int)Playfield.Width;
+            SliceSprite.Height = LengthMs;
+            SliceSprite.FadeTo(1, Easing.Linear, 250);
+        }
+
+        private int CalculateTextureX(int x)
+        {
+            var minFrequency = (float)SampleRate / EditorPlayfieldSpectrogram.FftCount;
+            const float maxFrequency = 20000;
+            var a = 1 / MathF.Log(maxFrequency / minFrequency);
+            var b = -a * MathF.Log(minFrequency);
+            var frequency = (float)x * SampleRate / EditorPlayfieldSpectrogram.FftCount;
+            if (frequency < minFrequency || frequency > maxFrequency) return -1;
+            var processedProgress = a * MathF.Log(frequency) + b;
+            return (int)(processedProgress * ReferenceWidth);
+        }
+        private int CalculateTextureXLinear(int x)
+        {
+            var minFrequency = (float)SampleRate / EditorPlayfieldSpectrogram.FftCount;
+            const float maxFrequency = 20000;
+            var frequency = (float)x * SampleRate / EditorPlayfieldSpectrogram.FftCount;
+            if (frequency < minFrequency || frequency > maxFrequency) return -1;
+            
+            return (int)((frequency - minFrequency) / (maxFrequency - minFrequency) * ReferenceWidth);
+        }
+
+
+        private float GetAverageData(float[,] data, int y, int x)
+        {
+            return data[y, x];
+        }
+
+        private int DataColorIndex(int textureHeight, int y, int x)
+        {
+            return (textureHeight - y - 1) * ReferenceWidth + x;
+        }
+    }
+}
