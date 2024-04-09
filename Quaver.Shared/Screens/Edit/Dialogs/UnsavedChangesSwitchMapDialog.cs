6239a1fcfb97c4e0561c5d9c63744a5e8657ed1c
WilliamQiufeng <williamqiufeng@outlook.com>
2024-04-09 19:26:39 +0800

    Add a NO button to difficulty switching confirmation dialog

--- Quaver.Shared/Screens/Edit/Dialogs/UnsavedChangesSwitchMapDialog.cs
+++ Quaver.Shared/Screens/Edit/Dialogs/UnsavedChangesSwitchMapDialog.cs
@@ -1,11 +1,15 @@
+using Quaver.Shared.Assets;
 using Quaver.Shared.Database.Maps;
 using Quaver.Shared.Graphics;
 using Quaver.Shared.Graphics.Notifications;
+using Quaver.Shared.Screens.Menu.UI.Jukebox;
+using Wobble.Graphics;
 
 namespace Quaver.Shared.Screens.Edit.Dialogs
 {
     public class UnsavedChangesSwitchMapDialog : YesNoDialog
     {
+        private IconButton YellowNoButton { get; }
         public UnsavedChangesSwitchMapDialog(EditScreen screen, Map map) : base("SAVE CHANGES",
             "You have unsaved changes. Would you like to save\n" +
             "before switching to another difficulty?")
@@ -18,7 +22,32 @@ namespace Quaver.Shared.Screens.Edit.Dialogs
                 screen.SwitchToMap(map);
             };
 
-            NoAction += () => screen.SwitchToMap(map, true);
+            const float scale = 0.90f;
+
+            YesButton.Size = new ScalableVector2(YesButton.Width * scale, YesButton.Height * scale);
+            NoButton.Size = new ScalableVector2(NoButton.Width * scale, NoButton.Height * scale);
+
+            YellowNoButton = new IconButton(UserInterface.NoYellowButton, (sender, args) =>
+            {
+                screen.SwitchToMap(map, true);
+                Close();
+            })
+            {
+                Parent = Panel,
+                Size = YesButton.Size,
+                Alignment = Alignment.BotCenter,
+                Y = YesButton.Y
+            };
+
+            YesButton.X -= 80;
+            NoButton.X = -YesButton.X;
+        }
+        
+        public override void Close()
+        {
+            YellowNoButton.IsClickable = false;
+            YellowNoButton.IsPerformingFadeAnimations = false;
+            base.Close();
         }
     }
 }
