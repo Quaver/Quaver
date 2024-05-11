ae70d17156afa81112fa4661f7c65dd2537ea757
WilliamQiufeng <williamqiufeng@outlook.com>
2024-05-11 15:09:16 +0800

    Remove MultiplayerSharedMapsetDownload.Download overload because it's identical to its parent implementation

--- Quaver.Shared/Screens/Download/MultiplayerSharedMapsetDownload.cs
+++ Quaver.Shared/Screens/Download/MultiplayerSharedMapsetDownload.cs
@@ -18,40 +18,5 @@ namespace Quaver.Shared.Screens.Download
         public MultiplayerSharedMapsetDownload(int id, string artist, string title, bool download = true) : base(id, artist, title, download)
         {
         }
-
-        /// <summary>
-        ///     Allows the user to download the mapset if shared in multiplayer
-        /// </summary>
-        public override void Download()
-        {
-            if (IsDownloading)
-                return;
-
-            IsDownloading = true;
-
-            Logger.Important($"Downloading mapset {MapsetId}...", LogType.Network);
-
-            var dir = $"{ConfigManager.DataDirectory.Value}/Downloads";
-            var path = $"{dir}/{MapsetId}.qp";
-            Directory.CreateDirectory(dir);
-
-            try
-            {
-                OnlineManager.Client?.DownloadSharedMultiplayerMap(path, (o, e) => Progress.Value = e, (o, e) =>
-                {
-                    Logger.Important($"Finished downloading mapset: {MapsetId}. Cancelled: {e.Cancelled} | Error: {e.Error}", LogType.Network);
-                    MapsetImporter.Queue.Add(path);
-
-                    Completed.Value = e;
-                    MapsetDownloadManager.CurrentDownloads.Remove(this);
-                    Dispose();
-                });
-            }
-            catch (Exception e)
-            {
-                Logger.Error(e, LogType.Runtime);
-                NotificationManager.Show(NotificationLevel.Error, $"There was an error downloading mapset: {Artist} - {Title} ({MapsetId})");
-            }
-        }
     }
 }
