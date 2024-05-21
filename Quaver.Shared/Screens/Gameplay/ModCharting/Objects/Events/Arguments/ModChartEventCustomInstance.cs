98fed4b3237ee0931abd2ca00a0cad72a2b2a8c4
WilliamQiufeng <williamqiufeng@outlook.com>
2024-05-21 23:43:37 +0800

    Fix custom event arguments not being able to be accessed

--- Quaver.Shared/Screens/Gameplay/ModCharting/Objects/Events/Arguments/ModChartEventCustomInstance.cs
+++ Quaver.Shared/Screens/Gameplay/ModCharting/Objects/Events/Arguments/ModChartEventCustomInstance.cs
@@ -1,4 +1,7 @@
+using MoonSharp.Interpreter;
+
 namespace Quaver.Shared.Screens.Gameplay.ModCharting.Objects.Events.Arguments;
 
+[MoonSharpUserData]
 public record ModChartEventCustomInstance(ModChartEventType EventType, params object[] Arguments)
     : ModChartEventInstance(EventType);
