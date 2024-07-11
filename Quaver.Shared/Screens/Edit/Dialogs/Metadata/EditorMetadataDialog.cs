bef1f9863969cf484053e3f1d0f36a8535ed2722
Warp <warpaboi@gmail.com>
2024-07-11 18:12:19 +0200

    change editor metadata dialog gamemode dropdown -> keycount textbox + scratch toggle reposition everything to make dialog smaller

--- Quaver.Shared/Screens/Edit/Dialogs/Metadata/EditorMetadataDialog.cs
+++ Quaver.Shared/Screens/Edit/Dialogs/Metadata/EditorMetadataDialog.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.Xna.Framework;
 using Quaver.API.Helpers;
 using Quaver.API.Maps;
@@ -33,7 +35,9 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
 
         private LabelledTextbox Tags { get; set; }
 
-        private EditorMetadataModeDropdown GameMode { get; set; }
+        private LabelledTextbox KeyCount { get; set; }
+
+        private LabelledCheckbox ScratchKey { get; set; }
 
         private Tooltip CurrentToolTip { get; set; }
 
@@ -64,27 +68,28 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
         public EditorMetadataDialog(EditScreen screen) : base("EDIT METADATA", "Edit the values to change the metadata...")
         {
             Screen = screen;
-            Panel.Height = 930;
             Panel.Image = UserInterface.MetadataPanel;
 
             CreateArtistTextbox();
             CreateSongTitleTextbox();
-            CreateCreatorTextbox();
             CreateDifficultyNameTextbox();
             CreateSourceTextbox();
             CreateTagsTextbox();
-            CreateGameModeDropdown();
+            CreateCreatorTextbox();
+            CreateKeyCountTextbox();
             CreateBpmAffectsSvCheckbox();
+            CreateScratchKeyCheckbox();
             CreateLegacyLNRenderingCheckbox();
 
             TabControl = new TextboxTabControl(new List<Textbox>()
             {
                 Artist.Textbox,
                 Title.Textbox,
-                Creator.Textbox,
                 DifficultyName.Textbox,
                 Source.Textbox,
-                Tags.Textbox
+                Tags.Textbox,
+                Creator.Textbox,
+                KeyCount.Textbox
             })
             {
                 Parent = this,
@@ -96,6 +101,8 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
 
             YesButton.Y += 32;
             NoButton.Y = YesButton.Y;
+
+            Panel.Height = Panel.Children.Max(x => x.Y + x.Height) + YesButton.Height + 40;
 
             YesAction = OnClickedYes;
         }
@@ -111,12 +118,13 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
 
         private void CreateArtistTextbox()
         {
-            Artist = new LabelledTextbox(Panel.Width * 0.90f, "Artist Name", LabelSize, TextboxHeight,
-                LabelSize, TextboxLabelSpacing, "Enter the name of the artist", WorkingMap.Artist)
+            Artist = new LabelledTextbox(Panel.Width * 0.9f * 0.95f * 0.5f, "Artist Name", LabelSize, TextboxHeight,
+                LabelSize, TextboxLabelSpacing, "Name of the artist", WorkingMap.Artist)
             {
                 Parent = Panel,
                 Y = Banner.Height + 20,
-                Alignment = Alignment.TopCenter,
+                X = Panel.Width * 0.05f,
+                Alignment = Alignment.TopLeft,
                 Textbox = { AllowSubmission = false },
                 Tint = Color.Transparent
             };
@@ -125,24 +133,12 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
         private void CreateSongTitleTextbox()
         {
             Title = new LabelledTextbox(Artist.Width, "Song Title", LabelSize, TextboxHeight, LabelSize,
-                TextboxLabelSpacing, "Enter the title of the song", WorkingMap.Title)
+                TextboxLabelSpacing, "Title of the song", WorkingMap.Title)
             {
                 Parent = Panel,
-                Y = Artist.Y + Artist.Height + Spacing,
-                Alignment = Alignment.TopCenter,
-                Textbox = { AllowSubmission = false },
-                Tint = Color.Transparent
-            };
-        }
-
-        private void CreateCreatorTextbox()
-        {
-            Creator = new LabelledTextbox(Artist.Width, "Creator Username", LabelSize, TextboxHeight, LabelSize,
-                TextboxLabelSpacing, "Enter your username", WorkingMap.Creator)
-            {
-                Parent = Panel,
-                Y = Title.Y + Title.Height + Spacing,
-                Alignment = Alignment.TopCenter,
+                Y = Artist.Y,
+                X = -Artist.X,
+                Alignment = Alignment.TopRight,
                 Textbox = { AllowSubmission = false },
                 Tint = Color.Transparent
             };
@@ -151,11 +147,12 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
         private void CreateDifficultyNameTextbox()
         {
             DifficultyName = new LabelledTextbox(Artist.Width, "Difficulty Name", LabelSize, TextboxHeight, LabelSize,
-                TextboxLabelSpacing, "Enter the name of the difficulty", WorkingMap.DifficultyName)
+                TextboxLabelSpacing, "Name of the difficulty", WorkingMap.DifficultyName)
             {
                 Parent = Panel,
-                Y = Creator.Y + Creator.Height + Spacing,
-                Alignment = Alignment.TopCenter,
+                Y = Artist.Y + Artist.Height + Spacing,
+                X = Artist.X,
+                Alignment = Alignment.TopLeft,
                 Textbox = { AllowSubmission = false },
                 Tint = Color.Transparent
             };
@@ -164,8 +161,23 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
         private void CreateSourceTextbox()
         {
             Source = new LabelledTextbox(Artist.Width, "Source", LabelSize, TextboxHeight, LabelSize,
-                TextboxLabelSpacing, "Enter the source of the song (album, mixtape, etc)",
+                TextboxLabelSpacing, "Source of the song (album, mixtape, etc)",
                 WorkingMap.Source)
+            {
+                Parent = Panel,
+                Y = DifficultyName.Y,
+                X = -DifficultyName.X,
+                Alignment = Alignment.TopRight,
+                Textbox = { AllowSubmission = false },
+                Tint = Color.Transparent
+            };
+        }
+
+        private void CreateTagsTextbox()
+        {
+            Tags = new LabelledTextbox(Panel.Width * 0.9f, "Tags", LabelSize, TextboxHeight, LabelSize,
+                TextboxLabelSpacing, "Comma-separated tags to help players find your map",
+                WorkingMap.Tags)
             {
                 Parent = Panel,
                 Y = DifficultyName.Y + DifficultyName.Height + Spacing,
@@ -175,41 +187,67 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
             };
         }
 
-        private void CreateTagsTextbox()
+        private void CreateCreatorTextbox()
         {
-            Tags = new LabelledTextbox(Artist.Width, "Tags", LabelSize, TextboxHeight, LabelSize,
-                TextboxLabelSpacing, "Enter comma-separated tags to help players find your map",
-                WorkingMap.Tags)
+            Creator = new LabelledTextbox(Artist.Width, "Creator Username", LabelSize, TextboxHeight, LabelSize,
+                TextboxLabelSpacing, "Your username", WorkingMap.Creator)
             {
                 Parent = Panel,
-                Y = Source.Y + Source.Height + Spacing,
-                Alignment = Alignment.TopCenter,
+                Y = Tags.Y + Tags.Height + Spacing,
+                X = Artist.X,
+                Alignment = Alignment.TopLeft,
                 Textbox = { AllowSubmission = false },
                 Tint = Color.Transparent
             };
         }
 
-        private void CreateGameModeDropdown()
+        private void CreateKeyCountTextbox()
         {
-            GameMode = new EditorMetadataModeDropdown(Screen.WorkingMap)
+            var regex = "";
+            for (var i = 1; i <= ModeHelper.MaxKeyCount; i++)
+            {
+                regex += $"^{i}$";
+                if (i != ModeHelper.MaxKeyCount)
+                    regex += "|";
+            }
+
+            KeyCount = new LabelledTextbox(Artist.Width, "Key Count", LabelSize, TextboxHeight, LabelSize, 
+            TextboxLabelSpacing, $"1-{ModeHelper.MaxKeyCount}", ModeHelper.ToKeyCount(WorkingMap.Mode).ToString())
             {
                 Parent = Panel,
-                Y = Tags.Y + Tags.Height + Spacing,
-                X = 70,
-                Alignment = Alignment.TopLeft,
+                Y = Creator.Y,
+                X = -Creator.X,
+                Alignment = Alignment.TopRight,
+                Tint = Color.Transparent,
+                Textbox =
+                {
+                    AllowSubmission = false,
+                    AllowedCharacters = new Regex(regex),
+                }
             };
         }
 
         private void CreateBpmAffectsSvCheckbox()
         {
-            // Trailing whitespace needed to keep it left-aligned.
-            BpmAffectsScrollVelocity = new LabelledCheckbox("BPM AFFECTS SV:               ", 20,
+            BpmAffectsScrollVelocity = new LabelledCheckbox("BPM AFFECTS SV:", 20,
                 new QuaverCheckbox(new Bindable<bool>(!WorkingMap.BPMDoesNotAffectScrollVelocity)) { DisposeBindableOnDestroy = true })
             {
                 Parent = Panel,
-                Y = GameMode.Y + 7,
-                X = -GameMode.X + 25,
+                Y = KeyCount.Y + KeyCount.Height + Spacing,
+                X = -(Artist.X + KeyCount.Width + (Panel.Width * 0.05f)),
                 Alignment = Alignment.TopRight,
+            };
+        }
+
+        private void CreateScratchKeyCheckbox()
+        {
+            ScratchKey = new LabelledCheckbox("SCRATCH KEY:", 20,
+                new QuaverCheckbox(new Bindable<bool>(WorkingMap.HasScratchKey)) { DisposeBindableOnDestroy = true })
+            {
+                Parent = Panel,
+                Y = BpmAffectsScrollVelocity.Y,
+                X = -BpmAffectsScrollVelocity.X,
+                Alignment = Alignment.TopLeft,
             };
         }
 
@@ -233,9 +271,10 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
             DifficultyName.Visible = false;
             Source.Visible = false;
             Tags.Visible = false;
-            GameMode.Visible = false;
+            KeyCount.Visible = false;
             BpmAffectsScrollVelocity.Visible = false;
             LegacyLNRendering.Visible = false;
+            ScratchKey.Visible = false;
 
             base.Close();
         }
@@ -264,10 +303,12 @@ namespace Quaver.Shared.Screens.Edit.Dialogs.Metadata
                     WorkingMap.NormalizeSVs();
             }
 
-            WorkingMap.Mode = GameMode.SelectedMode;
+            if (string.IsNullOrEmpty(KeyCount.Textbox.RawText.Trim()))
+                return;
+            WorkingMap.Mode = ModeHelper.FromKeyCount(int.Parse(KeyCount.Textbox.RawText.Trim()));
 
             // 7K+1
-            WorkingMap.HasScratchKey = GameMode.Dropdown.SelectedIndex == EditorMetadataModeDropdown.Keys7Plus1Index;
+            WorkingMap.HasScratchKey = ScratchKey.Checkbox.BindedValue.Value;
 
             var keyCount = ModeHelper.ToKeyCount(WorkingMap.Mode, WorkingMap.HasScratchKey);
             WorkingMap.HitObjects.RemoveAll(x => x.Lane > keyCount);
