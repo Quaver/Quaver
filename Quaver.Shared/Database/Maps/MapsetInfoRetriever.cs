36b73172844db2fcca54e1411fa981a0dc6a8fdb
WilliamQiufeng <williamqiufeng@outlook.com>
2024-08-19 18:17:19 +0800

    Use channels to process requests and responses, removing the need to wait milliseconds Add a bunch of documentations

--- Quaver.Shared/Database/Maps/MapsetInfoRetriever.cs
+++ Quaver.Shared/Database/Maps/MapsetInfoRetriever.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections.Concurrent;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 using Quaver.Server.Client.Structures;
-using Quaver.Shared.Graphics.Notifications;
 using Quaver.Shared.Online;
 
 using Wobble.Logging;
@@ -16,9 +15,12 @@ public static class MapsetInfoRetriever
     /// <summary>
     ///     Info retrieval requests yet to be processed
     /// </summary>
-    private static readonly ConcurrentQueue<MapsetInfoRequest> RequestQueue = new();
+    private static readonly Channel<MapsetInfoRequest> RequestChannel = Channel.CreateUnbounded<MapsetInfoRequest>();
 
-    private static readonly ConcurrentQueue<MapsetInfoResponse> ResponseQueue = new();
+    /// <summary>
+    ///     The info retrieved that is yet to be synced to the database
+    /// </summary>
+    private static readonly Channel<MapsetInfoResponse> ResponseChannel = Channel.CreateUnbounded<MapsetInfoResponse>();
 
     /// <summary>
     ///     <see cref="CancellationTokenSource"/> that can be used to cancel all running tasks
@@ -40,7 +42,7 @@ public static class MapsetInfoRetriever
     /// </summary>
     public static event EventHandler MapsetInfoRetrieved;
 
-    private static bool s_infoUpdateEnabled = false;
+    private static bool s_infoUpdateEnabled;
 
     /// <summary>
     ///     Whether the responses are processed and updated at the moment.
@@ -64,18 +66,19 @@ public static class MapsetInfoRetriever
     /// <summary>
     ///     Enqueues a map to be updated
     /// </summary>
-    /// <param name="mapId"></param>
-    /// <param name="mapsetId"></param>
-    public static void Enqueue(Map map)
+    public static async Task Enqueue(Map map)
     {
-        RequestQueue.Enqueue(new MapsetInfoRequest(map));
+        await RequestChannel.Writer.WriteAsync(new MapsetInfoRequest(map));
         Logger.Debug($"Retrieving status for mapset {map.MapId}", LogType.Runtime);
 
         if (TokenSource == null)
             StartWorkers();
     }
 
-    public static void StartWorkers()
+    /// <summary>
+    ///     Enables workers to fetch map online info
+    /// </summary>
+    private static void StartWorkers()
     {
         StopWorkers();
         TokenSource = new CancellationTokenSource();
@@ -85,68 +88,84 @@ public static class MapsetInfoRetriever
         }
     }
 
-    public static void StopWorkers()
+    /// <summary>
+    ///     Stop all workers. This does not include the sync worker
+    /// </summary>
+    private static void StopWorkers()
     {
         TokenSource?.Cancel();
         TokenSource = null;
     }
 
+    /// <summary>
+    ///     Takes a map in the <see cref="RequestChannel"/> and retrieves its online info.
+    ///     If failed for under <see cref="MapsetInfoRequest.MaxRetrieveAttempts"/>,
+    ///     the request will be put back on queue for a retry
+    /// </summary>
+    /// <param name="cancellationToken"></param>
     private static async Task Worker(CancellationToken cancellationToken)
     {
         while (true)
         {
-            await Task.Delay(100, cancellationToken);
+            var request = await RequestChannel.Reader.ReadAsync(cancellationToken);
 
-            if (!RequestQueue.TryDequeue(out var request))
-                continue;
-
-            request.MapsResponse ??= OnlineManager.Client?.RetrieveMapInfo(request.Map.Id);
+            request.MapsResponse ??= OnlineManager.Client?.RetrieveMapInfo(request.Map.MapId);
             if (request.MapsResponse == null)
             {
-                Requeue(request);
+                await Requeue(request);
                 continue;
             }
 
-            ResponseQueue.Enqueue(new MapsetInfoResponse(request.Map, request.MapsResponse));
+            await ResponseChannel.Writer.WriteAsync(new MapsetInfoResponse(request.Map, request.MapsResponse),
+                cancellationToken);
         }
         // ReSharper disable once FunctionNeverReturns
     }
 
+    /// <summary>
+    ///     The worker that syncs the map online info fetched to the database and mapset
+    /// </summary>
+    /// <param name="cancellationToken"></param>
     private static async Task ApplyMapInfoUpdates(CancellationToken cancellationToken)
     {
         while (true)
         {
-            await Task.Delay(1000, cancellationToken);
-            while (ResponseQueue.TryDequeue(out var response))
-            {
-                if (MapManager.FindMapFromOnlineId(response.Map.MapId) is { } map)
-                    response.Map = map;
+            var response = await ResponseChannel.Reader.ReadAsync(cancellationToken);
 
-                response.Map.RankedStatus = response.MapsResponse.Map.RankedStatus;
-                response.Map.DateLastUpdated = response.MapsResponse.Map.DateLastUpdated;
-                response.Map.OnlineOffset = response.MapsResponse.Map.OnlineOffset;
-                MapDatabaseCache.UpdateMap(response.Map);
-                Logger.Debug(
-                    $"Info for map {response.Map.MapId} has been updated", LogType.Runtime);
-            }
+            // We better find 'local' instance, rather than database instance
+            // since they aren't the same in terms of reference
+            if (MapManager.FindMapFromOnlineId(response.Map.MapId) is { } map)
+                response.Map = map;
+
+            response.Map.RankedStatus = response.MapsResponse.Map.RankedStatus;
+            response.Map.DateLastUpdated = response.MapsResponse.Map.DateLastUpdated;
+            response.Map.OnlineOffset = response.MapsResponse.Map.OnlineOffset;
+            MapDatabaseCache.UpdateMap(response.Map);
+            Logger.Debug(
+                $"Info for map {response.Map.MapId} has been updated", LogType.Runtime);
 
             MapsetInfoRetrieved?.Invoke(null, EventArgs.Empty);
         }
         // ReSharper disable once FunctionNeverReturns
     }
 
-    private static void Requeue(MapsetInfoRequest request)
+    /// <summary>
+    ///     Checks if the request has been retried for under <see cref="MapsetInfoRequest.MaxRetrieveAttempts"/>.
+    ///     If yes, puts the request back to the <see cref="RequestChannel"/> to check
+    /// </summary>
+    /// <param name="request"></param>
+    private static async Task Requeue(MapsetInfoRequest request)
     {
         request.RetrieveAttempts++;
         if (request.RetrieveAttempts >= MapsetInfoRequest.MaxRetrieveAttempts)
             return;
-        RequestQueue.Enqueue(request);
+        await RequestChannel.Writer.WriteAsync(request);
     }
 
     private struct MapsetInfoRequest
     {
         public const int MaxRetrieveAttempts = 5;
-        public Map Map;
+        public readonly Map Map;
         public int RetrieveAttempts;
         public MapsResponse MapsResponse;
 
@@ -159,7 +178,7 @@ public static class MapsetInfoRetriever
     private struct MapsetInfoResponse
     {
         public Map Map;
-        public MapsResponse MapsResponse;
+        public readonly MapsResponse MapsResponse;
 
         public MapsetInfoResponse(Map map, MapsResponse mapsResponse)
         {
