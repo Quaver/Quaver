7f5a7482f13d930f0a7766f6d32e22959a0185b2
WilliamQiufeng <williamqiufeng@outlook.com>
2024-09-24 21:36:47 +0800

    Add $ to Default group to show they are special

--- Quaver.Shared/Screens/Edit/Plugins/Timing/EditorScrollVelocityPanel.cs
+++ Quaver.Shared/Screens/Edit/Plugins/Timing/EditorScrollVelocityPanel.cs
@@ -132,7 +132,7 @@ namespace Quaver.Shared.Screens.Edit.Plugins.Timing
                     if (timingGroup is not ScrollGroup)
                         continue;
 
-                    var prettyId = id == Qua.GlobalScrollGroupId ? "Default" : id;
+                    var prettyId = id == Qua.GlobalScrollGroupId ? "$Default" : id;
 
                     if (ImGui.BeginTabItem($"{prettyId}##TabItem"))
                     {
