2268a2eecbaed802f59e33df8786640cb217a7bb
WilliamQiufeng <williamqiufeng@outlook.com>
2024-11-16 02:29:44 +0000

    Use reverse prefix max/min to query max/min position better

--- Quaver.Shared/Screens/Gameplay/Rulesets/Keys/SVDirectionChange.cs
+++ Quaver.Shared/Screens/Gameplay/Rulesets/Keys/SVDirectionChange.cs
@@ -1,18 +1,23 @@
+using Quaver.API.Maps.Structures;
+
 namespace Quaver.Shared.Screens.Gameplay.Rulesets.Keys
 {
     /// <summary>
     ///     Represents a change in the SV direction (positive to negative or negative to positive).
     /// </summary>
-    public struct SVDirectionChange
+    public class SVDirectionChange : IStartTime
     {
         /// <summary>
         ///     Start time of the SV that changed the direction.
         /// </summary>
-        public float StartTime;
+        public float StartTime { get; set; }
 
         /// <summary>
         ///     Position at the time of the direction change.
         /// </summary>
-        public long Position;
+        public long Position { get; set; }
+
+        public long BackPrefixMaxPosition { get; set; }
+        public long BackPrefixMinPosition { get; set; }
     }
 }
