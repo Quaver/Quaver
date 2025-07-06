10e8a3a24d8c4cfb6c572d18dee00a69c251b669
WilliamQiufeng <williamqiufeng@outlook.com>
2025-07-06 15:37:47 +0100

    Add a bunch of destroying statements for edit screen

--- Quaver.Shared/Screens/Edit/Actions/EditorPluginActionManager.cs
+++ Quaver.Shared/Screens/Edit/Actions/EditorPluginActionManager.cs
@@ -15,7 +15,7 @@ namespace Quaver.Shared.Screens.Edit.Actions
         /// <summary>
         /// </summary>
         [MoonSharpVisible(false)]
-        public EditorActionManager ActionManager { get; }
+        public EditorActionManager ActionManager { get; private set; }
 
         /// <summary>
         /// </summary>
@@ -252,5 +252,10 @@ namespace Quaver.Shared.Screens.Edit.Actions
 
         public void MoveObjectsToTimingGroup(List<HitObjectInfo> hitObjects, string timingGroupId) =>
             ActionManager.MoveObjectsToTimingGroup(hitObjects, timingGroupId, true);
+
+        public void Destroy()
+        {
+            ActionManager = null;
+        }
     }
 }
