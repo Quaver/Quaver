bf2646af686207a1a146d3708eae3ae7ed933aa4
Warp <warp@warp.tf>
2026-06-05 18:05:06 +0200

    allow skinning of all modes in song select

--- Quaver.Shared/Helpers/GameModeHelper.cs
+++ Quaver.Shared/Helpers/GameModeHelper.cs
@@ -41,6 +41,11 @@ namespace Quaver.Shared.Helpers
         public static void SetGameModeTexture(IEnumerable<GameMode> mapModes, Sprite sprite, SpriteTextPlus text)
         {
             var modes = mapModes.Distinct().OrderBy(m => ModeHelper.ToKeyCount(m)).ToList();
+            bool k4 = modes.Count == 1 && modes[0] == GameMode.Keys4;
+            bool k7 = modes.Count == 1 && modes[0] == GameMode.Keys7;
+            bool k47 = modes.Count == 2 && modes.Contains(GameMode.Keys4) && modes.Contains(GameMode.Keys7);
+
+            sprite.Tint = Color.White;
 
             if (modes.Count == 0)
             {
@@ -49,20 +54,20 @@ namespace Quaver.Shared.Helpers
                 return;
             }
 
-            if (modes.Count == 1 && modes[0] == GameMode.Keys4 && SkinManager.Skin?.SongSelect?.GameMode4K != null)
+            if (k4 && SkinManager.Skin?.SongSelect?.GameMode4K != null)
             {
                 sprite.Image = SkinManager.Skin.SongSelect.GameMode4K;
                 text.Text = "";
                 return;
             }
-            if (modes.Count == 1 && modes[0] == GameMode.Keys7 && SkinManager.Skin?.SongSelect?.GameMode7K != null)
+            if (k7 && SkinManager.Skin?.SongSelect?.GameMode7K != null)
             {
                 sprite.Image = SkinManager.Skin.SongSelect.GameMode7K;
                 text.Text = "";
                 return;
             }
 
-            if (modes.Count == 2 && modes.Contains(GameMode.Keys4) && modes.Contains(GameMode.Keys7))
+            if (k47)
             {
                 if (SkinManager.Skin?.SongSelect?.GameMode4K7K != null)
                 {
@@ -76,10 +81,17 @@ namespace Quaver.Shared.Helpers
                 return;
             }
 
-            var color = modes.Count == 1 ? modeColors[modes[0]] : new Color(128, 128, 128);
+            if ((!k4 && !k7 && !k47) && SkinManager.Skin?.SongSelect?.GameModeOther != null)
+            {
+                sprite.Image = SkinManager.Skin.SongSelect.GameModeOther;
+            }
+            else
+            {
+                var color = modes.Count == 1 ? modeColors[modes[0]] : new Color(128, 128, 128);
 
-            sprite.Image = UserInterface.ModePanel;
-            sprite.Tint = color;
+                sprite.Image = UserInterface.ModePanel;
+                sprite.Tint = color;
+            }
 
             string modesText = "";
 
