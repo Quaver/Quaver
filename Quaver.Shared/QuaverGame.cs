0050eed01be2a2e54508d38840277b1e179afbe8
AiAe <ayyayyedev@gmail.com>
2026-07-05 21:10:53 +0300

    Fix visual tests & remove exo2 font

--- Quaver.Shared/QuaverGame.cs
+++ Quaver.Shared/QuaverGame.cs
@@ -313,6 +313,10 @@ namespace Quaver.Shared
             QuaverLocalization.Configure(ConfigManager.Language.Value);
             Resources.AddStore(new DllResourceStore("Quaver.Resources.dll"));
 
+#if VISUAL_TESTS
+            Fonts.LoadWobbleFonts();
+#endif
+
             Graphics.IsFullScreen = ConfigManager.WindowFullScreen.Value;
             Window.IsBorderless = ConfigManager.WindowBorderless.Value;
             ChangeResolution();
@@ -371,7 +375,9 @@ namespace Quaver.Shared
             Transitioner.Dispose();
             DiscordHelper.Shutdown();
             base.UnloadContent();
-            SteamAPI.Shutdown();
+
+            if (SteamManager.IsInitialized)
+                SteamAPI.Shutdown();
         }
 
         /// <inheritdoc />
