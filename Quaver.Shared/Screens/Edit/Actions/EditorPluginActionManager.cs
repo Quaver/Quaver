fc98c0a6080faf3a649f9d5d975a479ab9d32a22
plummyyummy <sweetplumquaver@gmail.com>
2026-09-05 12:16:42 -0700

    add getter/setter to song time, start/stop playback

--- Quaver.Shared/Screens/Edit/Actions/EditorPluginActionManager.cs
+++ Quaver.Shared/Screens/Edit/Actions/EditorPluginActionManager.cs
@@ -209,6 +209,14 @@ namespace Quaver.Shared.Screens.Edit.Actions
 
         /// <summary>
         /// </summary>
+        public void StartPlayback() => ActionManager.StartPlayback();
+
+        /// <summary>
+        /// </summary>
+        public void StopPlayback() => ActionManager.StopPlayback();
+
+        /// <summary>
+        /// </summary>
         /// <param name="input"></param>
         public void GoToObjects(string input) => ActionManager.GoToObjects(input, true);
 
